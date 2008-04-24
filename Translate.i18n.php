fc468e85bd5265e0f89010cba94a782e19ccd20c
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-04-24 10:37:44 +0000

    Define some user rights in extensions for localisation on Special:ListGroupRights

--- Translate.i18n.php
+++ Translate.i18n.php
@@ -104,7 +104,8 @@ Changes are not saved until you click save button below.',
 	'translate-checks-xhtml'      => 'Please replace the following tags with correct ones: <strong>$1</strong>',
 	'translate-checks-plural'     => 'Definition uses <nowiki>{{PLURAL:}}</nowiki> but translation does not.',
 
-	'tog-translate-nonewsletter' => 'Do not send me e-mail newsletters (relevant only for users with a confirmed e-mailaddress)'
+	'tog-translate-nonewsletter' => 'Do not send me e-mail newsletters (relevant only for users with a confirmed e-mailaddress)',
+	'right-translate'            => 'Edit using the translate interface',
 );
 
 /** Afrikaans (Afrikaans)
