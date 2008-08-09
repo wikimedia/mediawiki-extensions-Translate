28fb775eaf8196519d25a077cff88496f952a215
Alexandre Emsenhuber <ialex@users.mediawiki.org>
2008-08-09 13:58:38 +0000

    Fixed E_STRICT and escaping issue

--- utils/ToolBox.php
+++ utils/ToolBox.php
@@ -7,7 +7,7 @@ class TranslateToolbox {
 	 * available translations for a message. Only shown when it
 	 * actually is a translatable/translated message.
 	 */
-	function toolboxAllTranslations(&$skin) {
+	static function toolboxAllTranslations(&$skin) {
 		global $wgTitle;
 
 		$inMessageGroup = TranslateUtils::messageKeyToGroup( $wgTitle->getNamespace(), $wgTitle->getBaseText() );
@@ -18,7 +18,7 @@ class TranslateToolbox {
 			// Add a slash at the end, to not have basename in the result of Special:Prefixindex
 			$prefix = $wgTitle->getNsText().":".$wgTitle->getBaseText()."/";
 			$desc = wfMsg( 'translate-sidebar-alltrans' );
-			$url = SpecialPage::getTitleFor( 'Prefixindex' )->getLocalURL('from='.$prefix);
+			$url = htmlspecialchars( SpecialPage::getTitleFor( 'Prefixindex' )->getLocalURL('from='.$prefix) );
 
 			// Add the actual toolbox entry.
 			// Add newlines and tabs for nicer HTML output.
