b775a3a94f8cb8a98bf5a5037457ed3c82c5843d
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-05-09 22:07:39 +0000

    Update messages

--- PageTranslation.i18n.php
+++ PageTranslation.i18n.php
@@ -22,16 +22,20 @@ $messages['en'] = array(
 	
 	# Specific page on the special page
 	'tpt-badtitle' => 'Page name given ($1) is not a valid title',
-	'tpt-oldrevision' => '$2 is not the latest version of the page $1. Only latest versions can be marked up for translating.',
-	'tpt-notsuitable' => 'Page $1 is not suitable for translation. Please make sure it has <nowiki><translate></nowiki>-tags and has a valid syntax.',
-	'tpt-saveok' => '$1 has been marked up for translation with $2 translatable sections. The page can now be <span class="plainlinks">[$3 translated]</span>.',
+	'tpt-oldrevision' => '$2 is not the latest version of the page $1.
+Only latest versions can be marked for translation.',
+	'tpt-notsuitable' => 'Page $1 is not suitable for translation.
+Make sure it has <nowiki><translate></nowiki> tags and has a valid syntax.',
+	'tpt-saveok' => 'The page "$1" has been marked up for translation with $2 translatable sections.
+The page can now be <span class="plainlinks">[$3 translated]</span>.',
 	'tpt-badsect' => '"$1" is not a valid name for section $2.',
 	'tpt-deletedsections' => 'The following sections wil no longer be used',
-	'tpt-showpage-intro' => 'Below are listed new, existing and deleted sections. Before marking this version for translation, please check that the changes to sections are minimised to avoid unnecessary work for translations.',
+	'tpt-showpage-intro' => 'Below are listed new, existing and deleted sections.
+Before marking this version for translation, check that the changes to sections are minimised to avoid unnecessary work for translators.',
 	'tpt-mark-summary' => 'Marked this version for translation',
 	'tpt-edit-failed' => 'Could not update the page: $1',
 	'tpt-insert-failed' => 'Could not add sections to the database.',
-	'tpt-already-marked' => 'The latest version of this page is already marked for translation.',
+	'tpt-already-marked' => 'The latest version of this page has already been marked for translation.',
 
 	# Page list on the special page
 	'tpt-list-nopages' => 'No pages are marked for translation nor ready to be marked for translation.',
@@ -49,6 +53,8 @@ $messages['en'] = array(
 	'translate-tag-legend-fallback' => 'Translation in other language',
 	'translate-tag-legend-fuzzy' => 'Outdated translation',
 
-	'tpt-target-page' => 'This page cannot be updated manually. This page is a translation of page [[$1]] and the translation can be updated using [$2 the translation tool].',
-	'tpt-unknown-page' => 'This namespace is reserved for content page translations. The page you are trying to edit does not seem to correspond any page marked for translation.'
+	'tpt-target-page' => 'This page cannot be updated manually.
+This page is a translation of page [[$1]] and the translation can be updated using [$2 the translation tool].',
+	'tpt-unknown-page' => 'This namespace is reserved for content page translations.
+The page you are trying to edit does not seem to correspond any page marked for translation.'
 );
