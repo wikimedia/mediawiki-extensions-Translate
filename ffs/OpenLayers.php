0ba92c00c8db2d93348628e5f4bd33fc254d2244
Robert Leverington <roberthl@users.mediawiki.org>
2009-07-31 21:47:48 +0000

    Translate: * Add function to OpenLayers FFS for unescaping escaped JS strings, needs implementing in new FFS system.

--- ffs/OpenLayers.php
+++ ffs/OpenLayers.php
@@ -10,6 +10,33 @@
 
 class OpenLayersFormatReader extends SimpleFormatReader {
 
+	private static function unescapeJsString( $string ) {
+		// See ECMA 262 section 7.8.4 for string literal format
+		$pairs = array(
+			"\\" => "\\\\",
+			"\"" => "\\\"",
+			'\'' => '\\\'',
+			"\n" => "\\n",
+			"\r" => "\\r",
+
+			# To avoid closing the element or CDATA section
+			"<" => "\\x3c",
+			">" => "\\x3e",
+
+			# To avoid any complaints about bad entity refs
+			"&" => "\\x26",
+
+			# Work around https://bugzilla.mozilla.org/show_bug.cgi?id=274152
+			# Encode certain Unicode formatting chars so affected
+			# versions of Gecko don't misinterpret our strings;
+			# this is a common problem with Farsi text.
+			"\xe2\x80\x8c" => "\\u200c", // ZERO WIDTH NON-JOINER
+			"\xe2\x80\x8d" => "\\u200d", // ZERO WIDTH JOINER
+		);
+		$pairs = array_flip( $pairs );
+		return strtr( $string, $pairs );
+	}
+
 	private function leftTrim( $string ) {
 		$string = ltrim( $string );
 		$string = ltrim( $string, '"' );
@@ -61,7 +88,7 @@ class OpenLayersFormatReader extends SimpleFormatReader {
 			// Remove quotation marks and syntax.
 			$key = substr( $key, 1 );
 			$value = substr( $value, 1, -1 );
-			$messages[ $key ] = $value;
+			$messages[ $key ] = self::unescapeJsString( $value );
 		}
 
 		// Remove extraneous key that is sometimes present.
