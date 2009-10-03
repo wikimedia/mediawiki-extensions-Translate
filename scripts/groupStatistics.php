89318e815663699c387f4afab190f92289ac2eae
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-10-03 18:13:41 +0000

    Added option for a content column

--- scripts/groupStatistics.php
+++ scripts/groupStatistics.php
@@ -10,63 +10,62 @@
  */
 
 $mostSpokenLanguages = array(
-	// FIXME: Continent toevoegen (africa, america, asia, australia, europe (timezoneregion-), all ('searchall'))
-	// 'language code' => array( position, ethnologue, encarta, average ), // Remark
+	// 'language code' => array( position, ethnologue, encarta, average, continent ), // Remark
 	// Source: http://en.wikipedia.org/w/index.php?title=List_of_languages_by_number_of_native_speakers&oldid=317526109
-	'zh-hans'  => array( 1, 845000, 844700, 844850 ),
-	'zh-hant'  => array( 1, 845000, 844700, 844850 ),
-	'es'       => array( 2, 329000, 322000, 325500 ),
-	'en'       => array( 3, 328000, 341000, 334500 ),
-	'hi'       => array( 4, 182000, 366000, 274000 ), // Classified together with Urdu
-	'ur'       => array( 4,  60600,  60290,  60445 ), // Classified together with Hindi
-	'ar'       => array( 5, 221000, 422039, 321519 ),
-	'bn'       => array( 6, 181000, 207000, 194000 ),
-	'pt'       => array( 7, 178000, 176000, 177000 ),
-	'pt-br'    => array( 7, 178000, 176000, 177000 ),
-	'ru'       => array( 8, 144000, 167000, 155500 ),
-	'ja'       => array( 9, 122000, 125000, 123500 ),
-	'de'       => array( 10, 90300, 100130,  95215 ),
-	'jv'       => array( 11, 84600,  75567,  80083 ),
-	'wuu'      => array( 12, 77200,  77200,  77200 ), // No encarta data
-	'ko'       => array( 13, 75000,  78000,  76500 ),
-	'pnb'      => array( 14, 78300,  72188,  75244 ), // Most spoken variant
-	'fr'       => array( 15, 67800,  78000,  72900 ),
-	'te'       => array( 16, 69800,  69666,  69733 ),
-	'vi'       => array( 17, 68600,  68000,  68300 ),
-	'mr'       => array( 18, 68100,  68022,  68061 ),
-	'ta'       => array( 19, 65700,  66000,  65850 ),
-	'it'       => array( 20, 61700,  62000,  61850 ),
-	'tr'       => array( 21, 59000,  61000,  60000 ),
-	'fa'       => array( 22, 72000,  31300,  51650 ),
-	'yue'      => array( 23, 55500,  55000,  55250 ), // No encarta data
-	'tl'       => array( 24, 48900,  17000,  32950 ),
-	'gu'       => array( 25, 46500,  46100,  46300 ),
-	'nan'      => array( 26, 46200,  46200,  46200 ), // No encarta data, most spoken variant
-	'pl'       => array( 27, 40000,  44000,  42000 ),
-	'uk'       => array( 28, 39400,  47000,  43200 ),
-	'hsn'      => array( 29, 36000,  36000,  36000 ), // No encarta data
-	'ml'       => array( 30, 35706,  35706,  35706 ),
-	'kn'       => array( 31, 35400,  35400,  35400 ),
-	'mai'      => array( 32, 45000,  24191,  34595 ),
-	'bh'       => array( 33, 38500,  26254,  32377 ),
-	'my'       => array( 34, 32300,  32300,  32300 ),
-	'or'       => array( 35, 31700,  32300,  32000 ),
-	'ms'       => array( 36, 39100,  23600,  31350 ),
-	'su'       => array( 37, 34000,  27000,  30500 ),
-	'hak'      => array( 38, 30000,  30000,  30000 ), // No encarta data
-	'ro'       => array( 39, 23400,  26265,  24832 ),
-	'az'       => array( 40, 19100,  31400,  25250 ),
-	'ha'       => array( 41, 24200,  24200,  24200 ),
-	'ps'       => array( 42, 19000,  26811,  22905 ),
-	'gan-hans' => array( 43, 21000,  21000,  21000 ),
-	'gan-hant' => array( 43, 21000,  21000,  21000 ),
-	'id'       => array( 44, 23200,  17100,  20150 ),
-	'th'       => array( 45, 20050,  46100,  33075 ),
-	'nl'       => array( 46, 21700,  20000,  20850 ),
-	'yo'       => array( 47, 20000,  20000,  20000 ),
-	'sd'       => array( 48, 19720,  19720,  19720 ),
-	'uz'       => array( 49, 18466,  20100,  19283 ),
-	'sh'       => array( 50, 16400,  21100,  18750 ),
+	'zh-hans'  => array( 1, 845000, 844700, 844850, 'asia' ),
+	'zh-hant'  => array( 1, 845000, 844700, 844850, 'asia' ),
+	'es'       => array( 2, 329000, 322000, 325500, 'multiple' ),
+	'en'       => array( 3, 328000, 341000, 334500, 'multiple' ),
+	'hi'       => array( 4, 182000, 366000, 274000, 'asia' ), // Classified together with Urdu
+	'ur'       => array( 4,  60600,  60290,  60445, 'asia' ), // Classified together with Hindi
+	'ar'       => array( 5, 221000, 422039, 321519, 'multiple' ),
+	'bn'       => array( 6, 181000, 207000, 194000, 'asia' ),
+	'pt'       => array( 7, 178000, 176000, 177000, 'multiple' ),
+	'pt-br'    => array( 7, 178000, 176000, 177000, 'america' ),
+	'ru'       => array( 8, 144000, 167000, 155500, 'multiple' ),
+	'ja'       => array( 9, 122000, 125000, 123500, 'asia' ),
+	'de'       => array( 10, 90300, 100130,  95215, 'europe' ),
+	'jv'       => array( 11, 84600,  75567,  80083, 'asia' ),
+	'wuu'      => array( 12, 77200,  77200,  77200, 'asia' ), // No encarta data
+	'ko'       => array( 13, 75000,  78000,  76500, 'asia' ),
+	'pnb'      => array( 14, 78300,  72188,  75244, 'asia' ), // Most spoken variant
+	'fr'       => array( 15, 67800,  78000,  72900, 'multiple' ),
+	'te'       => array( 16, 69800,  69666,  69733, 'asia' ),
+	'vi'       => array( 17, 68600,  68000,  68300, 'asia' ),
+	'mr'       => array( 18, 68100,  68022,  68061, 'asia' ),
+	'ta'       => array( 19, 65700,  66000,  65850, 'asia' ),
+	'it'       => array( 20, 61700,  62000,  61850, 'europe' ),
+	'tr'       => array( 21, 59000,  61000,  60000, 'multiple' ),
+	'fa'       => array( 22, 72000,  31300,  51650, 'asia' ),
+	'yue'      => array( 23, 55500,  55000,  55250, 'asia' ), // No encarta data
+	'tl'       => array( 24, 48900,  17000,  32950, 'asia' ),
+	'gu'       => array( 25, 46500,  46100,  46300, 'asia' ),
+	'nan'      => array( 26, 46200,  46200,  46200, 'asia' ), // No encarta data, most spoken variant
+	'pl'       => array( 27, 40000,  44000,  42000, 'europe' ),
+	'uk'       => array( 28, 39400,  47000,  43200, 'europe' ),
+	'hsn'      => array( 29, 36000,  36000,  36000, 'asia' ), // No encarta data
+	'ml'       => array( 30, 35706,  35706,  35706, 'asia' ),
+	'kn'       => array( 31, 35400,  35400,  35400, 'asia' ),
+	'mai'      => array( 32, 45000,  24191,  34595, 'asia' ),
+	'bh'       => array( 33, 38500,  26254,  32377, 'asia' ),
+	'my'       => array( 34, 32300,  32300,  32300, 'asia' ),
+	'or'       => array( 35, 31700,  32300,  32000, 'asia' ),
+	'ms'       => array( 36, 39100,  23600,  31350, 'asia' ),
+	'su'       => array( 37, 34000,  27000,  30500, 'asia' ),
+	'hak'      => array( 38, 30000,  30000,  30000, 'asia' ), // No encarta data
+	'ro'       => array( 39, 23400,  26265,  24832, 'europe' ),
+	'az'       => array( 40, 19100,  31400,  25250, 'asia' ),
+	'ha'       => array( 41, 24200,  24200,  24200, 'africa' ),
+	'ps'       => array( 42, 19000,  26811,  22905, 'asia' ),
+	'gan-hans' => array( 43, 21000,  21000,  21000, 'asia' ),
+	'gan-hant' => array( 43, 21000,  21000,  21000, 'asia' ),
+	'id'       => array( 44, 23200,  17100,  20150, 'asia' ),
+	'th'       => array( 45, 20050,  46100,  33075, 'asia' ),
+	'nl'       => array( 46, 21700,  20000,  20850, 'europe' ),
+	'yo'       => array( 47, 20000,  20000,  20000, 'africa' ),
+	'sd'       => array( 48, 19720,  19720,  19720, 'asia' ),
+	'uz'       => array( 49, 18466,  20100,  19283, 'asia' ),
+	'sh'       => array( 50, 16400,  21100,  18750, 'europe' ),
 );
 
 $localisedWeights = array(
@@ -125,6 +124,8 @@ function showUsage() {
 	--speakers : add column for number of speakers (est.). Only valid when
 		     combined with --most.
 	--nol10n : do not add localised language name if I18ntags is installed.
+	--continent : add a continent column. Only available when output is not
+		      set or is 'wiki'.
 
 END;
 	STDERR( $msg );
@@ -190,6 +191,7 @@ if( isset( $options['most'] ) ) {
 }
 $out->element( 'Code', true );
 $out->element( 'Language', true );
+$out->element( 'Continent', true );
 
 if( isset( $options['most'] ) && isset( $options['speakers'] ) ) {
 	$out->element( 'Speakers', true );
@@ -285,22 +287,39 @@ foreach ( $languages as $code => $name ) {
 
 	// Output the the row
 	$out->blockstart();
+
 	// Fill language position field
 	if( isset( $options['most'] ) ) {
 		$out->element( $mostSpokenLanguages[$code][0] );
 	}
+
 	// Fill language code field
 	$out->element( $code );
+
 	// Fill language name field
-	if( !isset( $options['nol10n'] ) && function_exists( 'efI18nTagsInit' ) ) {
+	if( ( !isset( $options['output'] ) || $options['output'] == 'wiki' ) &&
+	  !isset( $options['nol10n'] ) && function_exists( 'efI18nTagsInit' ) ) {
 		$out->element( "{{#languagename:" . $code . "}}" );
 	} else {
 		$out->element( $name );
 	}
+
+	// Fill continent field
+	if( ( !isset( $options['output'] ) || $options['output'] == 'wiki' ) &&
+	  isset( $options['output'] ) ) {
+		if( $mostSpokenLanguages[$code][4] == 'multiple' ) {
+			$continent = '';
+		} else {
+			$content = "{{int:timezoneregion-" . $mostSpokenLanguages[$code][4] . "}}";
+		}
+		$out->element( $continent );
+	}
+
 	// Fill speakers field
 	if( isset( $options['most'] ) && isset( $options['speakers'] ) ) {
 		$out->element( number_format( $mostSpokenLanguages[$code][3] ) );
 	}
+
 	// Fill fields for groups
 	foreach ( $columns as $fields ) {
 		list( $invert, $upper, $total ) = $fields;
