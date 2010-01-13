f5ee1d16f1922ace9185d3617462bedca09f42de
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-01-13 19:50:37 +0000

    Use $wgTranslateGroupRoot

--- groups/Wikia/WikiaExtensions.php
+++ groups/Wikia/WikiaExtensions.php
@@ -1,16 +1,17 @@
 <?php
 
-
 class PremadeWikiaExtensionGroups extends PremadeMediawikiExtensionGroups {
 	protected $useConfigure = false;
 	protected $idPrefix = 'wikia-';
 	protected $path = null;
 
 	public function __construct() {
+		global $wgTranslateGroupRoot;
+
 		parent::__construct();
 		$dir = dirname( __FILE__ );
 		$this->definitionFile = $dir . '/extensions.txt';
-		$this->path = '/var/www/externals/wikia/';
+		$this->path = '$wgTranslateGroupRoot/wikia/';
 	}
 
 	protected function addAllMeta() {
