06a0329331e0e2082ae6940b16e75fe2b0fca66c
Raimond Spekking <raymond@users.mediawiki.org>
2010-05-23 21:29:06 +0000

    Follow-up r66789: Update for translatewiki

--- groups/MediaWikiExtensions.php
+++ groups/MediaWikiExtensions.php
@@ -435,7 +435,7 @@ class AllWikimediaExtensionsGroup extends AllMediawikiExtensionsGroup {
 		'ext-sitematrix',
 		'ext-skinperpage', // Wikimediafoundation.org
 		'ext-spamblacklist',
-		'ext-strategywiki-activetaskforces', // 2010-05-08: strategywiki.wikimedia.org
+		'ext-strategywiki-activestrategy', // 2010-05-08: strategywiki.wikimedia.org
 		'ext-syntaxhighlightgeshi',
 		'ext-timeline',
 		'ext-titleblacklist',
