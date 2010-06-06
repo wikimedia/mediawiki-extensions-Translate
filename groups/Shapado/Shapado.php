6d184839082ad1a13ff1bc4563cd7d07dd4b6e18
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-06-06 13:25:47 +0000

    Add support for Shapado.
    This is untested.

---
+++ groups/Shapado/Shapado.php
@@ -0,0 +1,34 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) die();
+
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/ads.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/announcements.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/answers.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/badges.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/close_requests.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/comments.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/default.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/devise.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/doc.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/errors.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/favorites.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/flags.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/global.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/groups.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/imports.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/layouts.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/mailers.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/manage.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/members.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/moderate.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/pages.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/questions.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/searches.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/sessions.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/shared.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/unfavorites.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/users.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/votes.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/welcome.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/widgets.yml";
+$wgTranslateGroupFiles[] = "$IP/extensions/Translate/groups/Shapado/wiki.yml";
