140100bf5ee84559eefb21e42792ccdd949b1e9f
Niklas Laxström <nikerabbit@users.mediawiki.org>
2010-06-15 14:17:29 +0000

    No longer used

--- Translate.php
+++ Translate.php
@@ -215,11 +215,6 @@ $wgTranslatePHPlotFont = '/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf';
  */
 $wgTranslateYamlLibrary = 'spyc';
 
-/**
- * Google API key if any. Currently only used for Google translation API.
- */
-$wgGoogleApiKey = false;
-
 function efTranslateInit() {
 	global $wgTranslatePHPlot, $wgAutoloadClasses, $wgHooks;
 	if ( $wgTranslatePHPlot ) {
