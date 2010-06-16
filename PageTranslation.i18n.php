a239ff4de5c4a95d7f17720b7ac50ae879389623
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-06-16 18:38:52 +0000

    Quote links and remove periods from log messages.

--- PageTranslation.i18n.php
+++ PageTranslation.i18n.php
@@ -97,8 +97,8 @@ Section text: <pre>$1</pre>',
 
 	'pt-log-header' => 'Log for actions related to the page translation system',
 	'pt-log-name' => 'Page translation log',
-	'pt-log-mark' => '{{GENDER:$2|marked}} revision $3 of page [[:$1]] for translation.',
-	'pt-log-unmark' => '{{GENDER:$2|removed}} page [[:$1]] from translation.',
+	'pt-log-mark' => '{{GENDER:$2|marked}} revision $3 of page "[[:$1]]" for translation',
+	'pt-log-unmark' => '{{GENDER:$2|removed}} page "[[:$1]]" from translation',
 );
 
 /** Message documentation (Message documentation)
