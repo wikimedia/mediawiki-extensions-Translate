7c64a8c522e6f85da97975c3c6f32bbf24601b2f
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-07-04 21:01:41 +0000

    Reduce verbosity. Thanks ialex.

--- TranslatePage.php
+++ TranslatePage.php
@@ -252,7 +252,7 @@ class SpecialTranslate extends SpecialPage {
 		$selector = new HTMLSelector( 'task', 'task', $this->options['task'] );
 
 		// Check if this is a page translation group to return only appropriate tasks.
-		$isPageTranslation = strpos( $this->group->getId(), 'page|' ) === 0 ? true : false;
+		$isPageTranslation = strpos( $this->group->getId(), 'page|' ) === 0;
 
 		foreach ( TranslateTasks::getTasks( $isPageTranslation ) as $id ) {
 			$label = call_user_func( array( 'TranslateTask', 'labelForTask' ), $id );
