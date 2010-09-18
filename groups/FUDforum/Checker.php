b9930a4ee1f91cc8c71fdc088bbfe5964abdc1b0
Niklas Laxström <nikerabbit@users.mediawiki.org>
2010-09-18 13:46:15 +0000

    Reducde code duplication and add one more check for FUDforum

--- groups/FUDforum/Checker.php
+++ groups/FUDforum/Checker.php
@@ -4,7 +4,7 @@
  *
  * @file
  * @author Niklas Laxström
- * @copyright Copyright © 2009, Niklas Laxström
+ * @copyright Copyright © 2009-2010, Niklas Laxström
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
@@ -14,56 +14,15 @@
  * @ingroup MessageCheckers
  */
 class FUDforumMessageChecker extends MessageChecker {
-	/**
-	 * Checks for missing and unknown variables in translations.
-	 *
-	 * @param $messages \array Iterable list of TMessage objects.
-	 * @param $code \string Language code of the translations.
-	 * @param $warnings \array Array where warnings are appended to.
-	 */
+
 	protected function FUDforumVariablesCheck( $messages, $code, &$warnings ) {
-		foreach ( $messages as $message ) {
-			$key = $message->key();
-			$definition = $message->definition();
-			$translation = $message->translation();
-
-			$varPattern = '\$[1-9]';
-			preg_match_all( "/$varPattern/U", $definition, $defVars );
-			preg_match_all( "/$varPattern/U", $translation, $transVars );
-
-			# Check for missing variables in the translation
-			$subcheck = 'missing';
-			$params = self::compareArrays( $defVars[0], $transVars[0] );
-			if ( count( $params ) ) {
-				$warnings[$key][] = array(
-					array( 'variable', $subcheck, $key, $code ),
-					'translate-checks-parameters',
-					array( 'PARAMS', $params ),
-					array( 'COUNT', count( $params ) ),
-				);
-			}
-
-			# Check for unknown variables in the translation
-			$subcheck = 'unknown';
-			$params = self::compareArrays( $transVars[0], $defVars[0] );
-			if ( count( $params ) ) {
-				$warnings[$key][] = array(
-					array( 'variable', $subcheck, $key, $code ),
-					'translate-checks-parameters-unknown',
-					array( 'PARAMS', $params ),
-					array( 'COUNT', count( $params ) ),
-				);
-			}
-		}
+		return parent::parameterCheck( $messages, $code, $warnings, '/\$[1-9]/' );
 	}
 
-	/**
-	 * Checks for incorrect syntax in translations.
-	 *
-	 * @param $messages \array Iterable list of TMessage objects.
-	 * @param $code \string Language code of the translations.
-	 * @param $warnings \array Array where warnings are appended to.
-	 */
+	protected function FUDforumLongVariablesCheck( $messages, $code, &$warnings ) {
+		return parent::parameterCheck( $messages, $code, $warnings, '/{VAR: [^}]+}/' );
+	}
+
 	protected function FUDforumSyntaxCheck( $messages, $code, &$warnings ) {
 		foreach ( $messages as $message ) {
 			$key = $message->key();
