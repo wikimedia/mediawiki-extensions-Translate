06cd879e10e48882bd781d6d3bc0addb91522e60
Purodha B Blissenbach <purodha@users.mediawiki.org>
2010-10-20 22:55:26 +0000

    Message confirmemail_body_changed has same parameters as confirmemail_body.

--- check-blacklist.php
+++ check-blacklist.php
@@ -35,6 +35,7 @@ array(
 		'blocklog-showlog', // Optional GENDER parameter
 		'blocklog-showsuppresslog', // Optional GENDER parameter
 		'confirmemail_body', // Optional time parameters
+		'confirmemail_body_changed', // Optional time parameters
 		'currentrev-asof', // Optional time parameters
 		'filehist-thumbtext', // Optional time parameters
 		'history-feed-item-nocomment', // Optional time parameters
