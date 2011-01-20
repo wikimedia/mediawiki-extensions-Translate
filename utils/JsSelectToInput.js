909b9a1246d29200a20b2ee8bea74c32109add19
Sam Reed <reedy@users.mediawiki.org>
2011-01-20 22:37:34 +0000

    Missing ; in JS in extensions we care about

--- utils/JsSelectToInput.js
+++ utils/JsSelectToInput.js
@@ -1,9 +1,9 @@
 function appendFromSelect(selectid,targetid) {
 	var select = document.getElementById(selectid);
 	var target = document.getElementById(targetid);
-	if(!target || !select) return
+	if(!target || !select) return;
 	var atxt = select.options[select.selectedIndex].value;
-	if(!atxt) return
+	if(!atxt) return;
 	/* Ugly hack */
 	target.value = target.value.replace(/default/, '' );
 	if(target.value.replace(/[\s\t\n]/ig,'') != '') atxt = ', ' + atxt;
