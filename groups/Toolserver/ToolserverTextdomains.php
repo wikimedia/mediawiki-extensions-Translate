9e82f415a6bc418c29b264eeaa549e3f8e31d541
Krinkle <krinkle@users.mediawiki.org>
2011-04-01 14:53:50 +0000

    Using new methods introduced in r85123 + fixing typo (sanatized > sanitized)

--- groups/Toolserver/ToolserverTextdomains.php
+++ groups/Toolserver/ToolserverTextdomains.php
@@ -29,119 +29,31 @@ class PremadeToolserverTextdomains extends PremadeMediawikiExtensionGroups {
 		$this->path = "$wgTranslateGroupRoot/ToolserverI18N/language/messages/";
 	}
 
-	/// Initialisation function
-	public function init() {
-		if ( $this->groups !== null ) return;
-
-		global $wgAutoloadClasses, $IP, $wgTranslateExtensionDirectory;
-
-		$postfix = 'Configure/load_txt_def/TxtDef.php';
-		if ( file_exists( "$IP/extensions/$postfix" ) ) {
-			$prefix = "$IP/extensions";
-		} elseif( file_exists( "$wgTranslateExtensionDirectory/$postfix" ) ) {
-			$prefix = $wgTranslateExtensionDirectory;
-		} else {
-			$prefix = false;
-		}
-
-		if ( $this->useConfigure && $prefix ) {
-			$wgAutoloadClasses['TxtDef'] = "$prefix/$postfix";
-			$tmp = TxtDef::loadFromFile( "$prefix/Configure/settings/Settings-ext.txt" );
-			$configureData = array_combine( array_map( array( __CLASS__, 'foldId' ), array_keys( $tmp ) ), array_values( $tmp ) );
-		} else {
-			$configureData = array();
-		}
-
-		$defines = file_get_contents( $this->definitionFile );
-
-		$linefeed = '(\r\n|\n)';
-
-		$sections = array_map( 'trim', preg_split( "/$linefeed{2,}/", $defines, - 1, PREG_SPLIT_NO_EMPTY ) );
-
-		$groups = $fixedGroups = array();
-
-		foreach ( $sections as $section ) {
-			$lines = array_map( 'trim', preg_split( "/$linefeed/", $section ) );
-			$newgroup = array();
-
-			foreach ( $lines as $line ) {
-				if ( $line === '' || $line[0] === '#' ) continue;
-
-				if ( strpos( $line, '=' ) === false ) {
-					if ( empty( $newgroup['name'] ) ) {
-						$newgroup['name'] = $line;
-					} else {
-						throw new MWException( "Trying to define name twice: " . $line );
-					}
-				} else {
-					list( $key, $value ) = array_map( 'trim', explode( '=', $line, 2 ) );
-					switch ( $key ) {
-					case 'file':
-					case 'var':
-					case 'id':
-					case 'descmsg':
-					case 'desc':
-					case 'magicfile':
-					case 'aliasfile':
-						$newgroup[$key] = $value;
-						break;
-					case 'optional':
-					case 'ignored':
-						$values = array_map( 'trim', explode( ',', $value ) );
-						if ( !isset( $newgroup[$key] ) ) {
-							$newgroup[$key] = array();
-						}
-						$newgroup[$key] = array_merge( $newgroup[$key], $values );
-						break;
-					case 'prefix':
-						list( $prefix, $messages ) = array_map( 'trim', explode( '|', $value, 2 ) );
-						if ( isset( $newgroup['prefix'] ) && $newgroup['prefix'] !== $prefix ) {
-							throw new MWException( "Only one prefix supported: {$newgroup['prefix']} !== $prefix" );
-						}
-						$newgroup['prefix'] = $prefix;
-
-						if ( !isset( $newgroup['mangle'] ) ) $newgroup['mangle'] = array();
-
-						$messages = array_map( 'trim', explode( ',', $messages ) );
-						$newgroup['mangle'] = array_merge( $newgroup['mangle'], $messages );
-						break;
-					default:
-						throw new MWException( "Unknown key:" . $key );
-					}
-				}
-			}
-
-			if ( count( $newgroup ) ) {
-				if ( empty( $newgroup['name'] ) ) {
-					throw new MWException( "Name missing\n" . print_r( $newgroup, true ) );
-				}
-				$groups[] = $newgroup;
-			}
-		}
-
-
+	protected function processGroups( $groups ) {
+		$configureData = $this->loadConfigureExtensionData();
+		$fixedGroups = array();
 		foreach ( $groups as $g ) {
 			if ( !is_array( $g ) ) {
 				$g = array( $g );
 			}
 
 			$name = $g['name'];
-			$sanatizedName = preg_replace( '/\s+/', '', strtolower( $name ) );
+			$sanitizedName = preg_replace( '/\s+/', '', strtolower( $name ) );
 
 			if ( isset( $g['id'] ) ) {
 				$id = $g['id'];
 			} else {
-				$id = $this->idPrefix . $sanatizedName;
+				$id = $this->idPrefix . $sanitizedName;
 			}
 
 			if ( isset( $g['file'] ) ) {
 				$file = $g['file'];
 			} else {
-				// TsIntuition text-domains are case-insensitive and internally
+				// Toolserver Intuition text-domains are case-insensitive and internally
 				// converts to lowercase names starting with a capital letter.
-				// eg. "My Tool" -> "Mytool.i18n.php"
+				// eg. "MyTool" -> "Mytool.i18n.php"
 				// No subdirectories!
-				$file = ucfirst( $sanatizedName ) . '.i18n.php';
+				$file = ucfirst( $sanitizedName ) . '.i18n.php';
 			}
 
 			if ( isset( $g['descmsg'] ) ) {
@@ -163,10 +75,11 @@ class PremadeToolserverTextdomains extends PremadeMediawikiExtensionGroups {
 				'url' => $url,
 			);
 
-			// Allow a custom prefix if needed
+			// Prefix is required, if not customized use the sanitized name
 			if ( !isset( $g['prefix'] ) ) {
-				$g['prefix'] = "$sanatizedName-";
+				$g['prefix'] = "$sanitizedName-";
 			}
+
 			// All messages are prefixed with their groupname
 			$g['mangle'] = array( '*' );
 			
@@ -184,7 +97,6 @@ class PremadeToolserverTextdomains extends PremadeMediawikiExtensionGroups {
 
 			$fixedGroups[$id] = $newgroup;
 		}
-
-		$this->groups = $fixedGroups;
+		return $fixedGroups;
 	}
 }
