a830fb178987fff106d82b74b5305bd4735675df
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-04-13 13:54:20 +0000

    Bump version.

--- Translate.php
+++ Translate.php
@@ -15,7 +15,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
 /**
  * Version number used in extension credits and in other placed where needed.
  */
-define( 'TRANSLATE_VERSION', '2011-03-11' );
+define( 'TRANSLATE_VERSION', '2011-04-13' );
 
 /**
  * Extension credits properties.
