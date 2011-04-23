1d57eee1950307dd0a04b2bcde47779abbaafd0e
Krinkle <krinkle@users.mediawiki.org>
2011-04-23 18:08:43 +0000

    add class to groupstatistics tables. Enables (local) scripts and stylesheets to target them

--- scripts/groupStatistics.php
+++ scripts/groupStatistics.php
@@ -190,11 +190,11 @@ require( dirname( __FILE__ ) . '/cli.inc' );
  */
 class TranslateStatsOutput extends wikiStatsOutput {
 	function heading() {
-		echo '{| class="sortable wikitable" border="2" cellpadding="4" cellspacing="0" style="background-color: #F9F9F9; border: 1px #AAAAAA solid; border-collapse: collapse; clear:both;" width="100%"' . "\n";
+		echo '{| class="mw-ext-translate-groupstatistics sortable wikitable" border="2" cellpadding="4" cellspacing="0" style="background-color: #F9F9F9; border: 1px #AAAAAA solid; border-collapse: collapse; clear:both;" width="100%"' . "\n";
 	}
 
 	function summaryheading() {
-		echo "\n" . '{| class="sortable wikitable" border="2" cellpadding="4" cellspacing="0" style="background-color: #F9F9F9; border: 1px #AAAAAA solid; border-collapse: collapse; clear:both;"' . "\n";
+		echo "\n" . '{| class="mw-ext-translate-groupstatistics sortable wikitable" border="2" cellpadding="4" cellspacing="0" style="background-color: #F9F9F9; border: 1px #AAAAAA solid; border-collapse: collapse; clear:both;"' . "\n";
 	}
 
 	function addFreeText( $freeText ) {
