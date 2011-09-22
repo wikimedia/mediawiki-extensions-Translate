803f6fb78f29f8a02e93cab307017ce609b9471a
Niklas Laxström <nikerabbit@users.mediawiki.org>
2011-09-22 20:02:07 +0000

    Backing out r85131 for now. See CR for the reasons.

--- MessageGroups.php
+++ MessageGroups.php
@@ -361,8 +361,7 @@ class CoreMessageGroup extends MessageGroupOld {
 			array( $checker, 'XhtmlCheck' ),
 			array( $checker, 'braceBalanceCheck' ),
 			array( $checker, 'pagenameMessagesCheck' ),
-			array( $checker, 'miscMWChecks' ),
-			array( $checker, 'prefMessagesCheck' ),
+			array( $checker, 'miscMWChecks' )
 		) );
 
 		return $checker;
