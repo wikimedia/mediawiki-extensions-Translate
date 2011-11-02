cf28a72fe372e861c25f727f10d0757f32aea58b
Niklas Laxström <nikerabbit@users.mediawiki.org>
2011-11-02 10:16:36 +0000

    Followup r101477 - we obviously need also language code

--- sql/translate_groupreviews.sql
+++ sql/translate_groupreviews.sql
@@ -5,9 +5,9 @@ CREATE TABLE /*$wgDBprefix*/translate_groupreviews (
   -- translation section pages limit that to shorter, because it needs to
   -- accommodate /sectionname/languagecode suffix to the page name.
   tgr_group varchar(200) binary NOT NULL,
-
+  tgr_lang varchar(20) binary NOT NULL,
   -- Any of user configured values from $wgTranslateWorkflowStates
   tgr_state varbinary(32) not null,
 
-	PRIMARY KEY (tgr_group, tgr_state)
+	PRIMARY KEY (tgr_group, tgr_lang)
 ) /*$wgDBTableOptions*/;
