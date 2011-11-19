7c7159a23a4b0e40f5714c4287d5fc4b4a667967
Niklas Laxström <nikerabbit@users.mediawiki.org>
2011-11-19 08:22:03 +0000

    Description message for wikipediamobile

--- TranslateGroupDescriptions.i18n.php
+++ TranslateGroupDescriptions.i18n.php
@@ -68,6 +68,7 @@ $messages['en'] = array(
 	//'bw-desc-voctrain' => '[[Translating:Voctrain|Voctrain]] is an OmegaWiki-based vocabulary trainer',
 	'bw-desc-wikiblame' => '[[Translating:WikiBlame|WikiBlame]] is able to quickly find the authors of a part of a page in a Wikimedia wiki',
 	'bw-desc-wikireader' => '[[Translating:WikiReader|WikiReader]] delivers an offline, text-only version of Wikipedia on a mobile device.',
+	'translate-group-desc-wikipediamobile' => '[[Translating:WikipediaMobile|WikipediaMobile]] is a cross-platform mobile and tablet application for reading and contributing to Wikipedia.',
 );
 
 /** Message documentation (Message documentation)
