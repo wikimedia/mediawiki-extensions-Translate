9f296a74519df831d26e74a5af6a38cf10c2d348
Niklas Laxström <nikerabbit@users.mediawiki.org>
2012-01-02 11:51:41 +0000

    I forgot to svn add the file again. ahoy r107816

---
+++ utils/FuzzyBot.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Do it all maintenance account
+ *
+ * @file
+ * @author Niklas Laxström
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2 or later
+ * @
+ */
+
+/**
+ * FuzzyBot - the misunderstood workhorse.
+ * @since 2012-01-02
+ */
+
+class FuzzyBot {
+	public static function getUser() {
+		$bot = User::newFromName( self::getName() );
+		if ( $bot->isAnon() ) {
+			$bot->addToDatabase();
+		}
+		return $bot;
+	}
+
+	public static function getName() {
+		global $wgTranslateFuzzyBotName;
+		return $wgTranslateFuzzyBotName;
+	}
+}
