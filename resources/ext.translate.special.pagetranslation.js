691696bf7276c35b7c8f0ccdb978c8ebb0bdd021
Niklas Laxström <nikerabbit@users.mediawiki.org>
2012-02-29 14:59:42 +0000

    Consistently indent, even though it leads to huge indentation, because I had problems understanding the code. Not many non-whitespace changes.

--- resources/ext.translate.special.pagetranslation.js
+++ resources/ext.translate.special.pagetranslation.js
@@ -7,71 +7,74 @@
  * Credits: http://jqueryui.com/demos/autocomplete/#multiple
  */
 jQuery( function( $ ) {
+	"use strict"
+	
 	$.widget( "ui.combobox", {
-	_create: function() {
-		var self = this,
-			select = this.element.hide(),
-			selected = select.children( ":selected" );
-		function split( val ) {
-			return val.split( /,\s*/ );
-		}
-		var input = this.input = $( '#tpt-prioritylangs' )
-			.autocomplete( {
-			delay: 0,
-			minLength: 0,
-			source: function( request, response ) {
-				var term = split( request.term ).pop();
-				var matcher = new RegExp( $.ui.autocomplete.escapeRegex( term ), "i" );
-				response( select.children( "option" ).map(function() {
-				var text = $( this ).text();
-				var value = $( this ).val();
-				var term = split( request.term ).pop();
-				if ( this.value && ( !request.term || matcher.test(text) ) )
-					return {
-					label: text.replace(
-						new RegExp(
-						"(?![^&;]+;)(?!<[^<>]*)(" +
-						$.ui.autocomplete.escapeRegex(term) +
-						")(?![^<>]*>)(?![^&;]+;)", "gi"
-						), "<strong>$1</strong>" ),
-					value: value,
-					option: this
-					};
-				}) );
-			},
-			select: function( event, ui ) {
-				ui.item.option.selected = true;
-				self._trigger( "selected", event, {
-					item: ui.item.option
-				});
-				var terms = split( $(this).val() );
-				// remove the current input
-				terms.pop();
-				// add the selected item
-				terms.push( ui.item.value );
-				// add placeholder to get the comma-and-space at the end
-				terms.push( "" );
-				$( this ).val( terms.join( ", " ) );
-				return false;
+		_create: function() {
+			var self = this,
+				select = this.element.hide(),
+				selected = select.children( ":selected" );
+				
+			function split( val ) {
+				return val.split( /,\s*/ );
 			}
 			
-			});
+			var input = this.input = $( '#tpt-prioritylangs' )
+				.autocomplete( {
+					delay: 0,
+					minLength: 0,
+					source: function( request, response ) {
+						var term = split( request.term ).pop();
+						var matcher = new RegExp( $.ui.autocomplete.escapeRegex( term ), "i" );
+						response( select.children( "option" ).map( function() {
+							var text = $( this ).text();
+							var value = $( this ).val();
+							var term = split( request.term ).pop();
+							if ( this.value && ( !request.term || matcher.test(text) ) ) {
+								return {
+									label: text.replace(
+										new RegExp(
+										"(?![^&;]+;)(?!<[^<>]*)(" +
+										$.ui.autocomplete.escapeRegex( term ) +
+										")(?![^<>]*>)(?![^&;]+;)", "gi"
+										), "<strong>$1</strong>" ),
+									value: value,
+									option: this
+								};
+							}
+						} ) );
+					},
+					select: function( event, ui ) {
+						ui.item.option.selected = true;
+						self._trigger( "selected", event, {
+							item: ui.item.option
+						});
+						var terms = split( $(this).val() );
+						// remove the current input
+						terms.pop();
+						// add the selected item
+						terms.push( ui.item.value );
+						// add placeholder to get the comma-and-space at the end
+						terms.push( "" );
+						$( this ).val( terms.join( ", " ) );
+						return false;
+					}
+				} );
 
-		input.data( "autocomplete" )._renderItem = function( ul, item ) {
-			return $( "<li></li>" )
-			.data( "item.autocomplete", item )
-			.append( "<a>" + item.label + "</a>" )
-			.appendTo( ul );
-		};
+				input.data( "autocomplete" )._renderItem = function( ul, item ) {
+					return $( "<li>" )
+						.data( "item.autocomplete", item )
+						.append( "<a>" + item.label + "</a>" )
+						.appendTo( ul );
+				};
+			}, // End of _create
 
-		},
-
-		destroy: function() {
-			this.input.remove();
-			this.element.show();
-			$.Widget.prototype.destroy.call( this );
-		}
-	});
+			destroy: function() {
+				this.input.remove();
+				this.element.show();
+				$.Widget.prototype.destroy.call( this );
+			}
+		} );
 
 	$( "#wpUserLanguage" ).combobox();
 } );
