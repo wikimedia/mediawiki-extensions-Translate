acd59ce0920145bedcc0de8af9815dab1c5ecba8
Niklas Laxström <nikerabbit@users.mediawiki.org>
2012-03-15 14:19:58 +0000

    Unicode wheee

--- scripts/cldr-plural-to-yaml.php
+++ scripts/cldr-plural-to-yaml.php
@@ -2,7 +2,7 @@
 /**
  * Script to format CLDR plural definitions to more usable format.
  *
- * @author Niklas Laxstrom
+ * @author Niklas Laxström
  *
  * @copyright Copyright © 2010, Niklas Laxström
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
