c80c576b99be3a78646d6440129aa72790fb7ff4
Santhosh Thottingal <santhosh@users.mediawiki.org>
2012-03-20 11:32:50 +0000

    Remove comment related to the code that was moved to a new file in r114250

--- resources/ext.translate.special.pagetranslation.js
+++ resources/ext.translate.special.pagetranslation.js
@@ -1,10 +1,5 @@
 /*
  * @author Santhosh Thottingal
- * jQuery autocomplete based multiple selector for input box.
- * Autocompleted values will be available in input filed as comma separated values.
- * The values for autocompletion is from the language selector in this case.
- * The input field is created in PHP code.
- * Credits: http://jqueryui.com/demos/autocomplete/#multiple
  */
 jQuery( function( $ ) {
 	$( "#wpUserLanguage" ).multiselectautocomplete( { inputbox : '#tpt-prioritylangs' } );
