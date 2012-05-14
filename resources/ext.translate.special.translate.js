579d9c789ac57a7457857d08935cc6d42ac4de1e
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-05-14 09:49:01 +0000

    (bug 36006) Warning about unsaved translations
    A simplified version of the editWarning module from the Vector
    extension.
    
    Patch set 2: Simplified even more. The hack for Firefox appears to be
                 unnecessary here. Also added ":visible", so that only
                 <textarea>s on the screen will trigger this and saved or
                 closed will not.
    Patch set 3: Updated the comment and added message documentation.
    Patch set 4: Trailing whitespace.
    
    Change-Id: Ibda30490a4795526dce8337d6b64a11f115f2843

--- resources/ext.translate.special.translate.js
+++ resources/ext.translate.special.translate.js
@@ -54,3 +54,38 @@ jQuery( function( $ ) {
 		}
 	} );
 } );
+
+/**
+ * A warning to be shown if a user tries to close the page or navigate away
+ * from it without saving the written translation.
+ *
+ * Based on editWarning from the Vector extension, but greatly
+ * simplified.
+ */
+
+( function ( $, mw ) {
+	"use strict";
+	function ourWindowOnBeforeUnloadRegister() {
+		pageShowHandler();
+		if ( window.addEventListener ) {
+			window.addEventListener( 'pageshow', pageShowHandler, false );
+		} else if ( window.attachEvent ) {
+			window.attachEvent( 'pageshow', pageShowHandler );
+		}
+	}
+
+	function pageShowHandler() {
+		// Re-add onbeforeunload handler
+		window.onbeforeunload = ourWindowOnBeforeUnload;
+	}
+
+	function ourWindowOnBeforeUnload() {
+		if ( $( '.mw-ajax-dialog:visible' ).length ) {
+			// Return our message
+			return mw.msg( 'translate-js-support-unsaved-warning' );
+		}
+	}
+
+	$( document ).ready( ourWindowOnBeforeUnloadRegister );
+
+} )( jQuery, mediaWiki );
