910965c6b2a121ae136e457c06ad04c9b4fa4521
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-05-29 10:02:43 +0000

    Cleanup messagetable.js
    * Remove BC for 1.17 (1.18 now minimum)
    * Use standard wrapper
    * Use explicit $( 'document' ).ready
    * Added todo for further refactoring
    * PS3: More BC for 1.18 removed
    
    Change-Id: I39d71be15036ae7ca08511db45c9a06075f85066

--- resources/ext.translate.messagetable.js
+++ resources/ext.translate.messagetable.js
@@ -1,40 +1,34 @@
-jQuery( function( $ ) {
-	// BC for MW < 1.18
-	if ( !mw.util.wikiScript ) {
-		mw.util.wikiScript = function( str ) {
-			return mw.config.get( 'wgScriptPath' ) + '/' + ( str || 'index' ) + mw.config.get( 'wgScriptExtension' );
-		}
-	}
+(function ( $, mw ) {
+	"use strict";
 
-	$( ".mw-translate-messagereviewbutton" ).click( function() {
-		var $b = $(this);
+	$( 'document' ).ready( function () {
+		$( ".mw-translate-messagereviewbutton" ).click( function() {
+			var $b = $(this);
 
-		var successFunction = function( data, textStatus ) {
-			if ( data.error ) {
-				var reason = mw.msg( "api-error-" + data.error.code );
-				$b.val( mw.msg( "translate-messagereview-failure", reason ) );
-			} else {
-				$b.val( mw.msg( "translate-messagereview-done" ) );
-			}
-		};
+			// TODO: avoid creating functions inside a loop
+			var successFunction = function( data, textStatus ) {
+				if ( data.error ) {
+					var reason = mw.msg( "api-error-" + data.error.code );
+					$b.val( mw.msg( "translate-messagereview-failure", reason ) );
+				} else {
+					$b.val( mw.msg( "translate-messagereview-done" ) );
+				}
+			};
 
-		var failFunction = function( jqXHR, textStatus ) {
-			$b.val( mw.msg( "translate-messagereview-failure", jqXHR.statusText ) );
-		};
+			var failFunction = function( jqXHR, textStatus ) {
+				$b.val( mw.msg( "translate-messagereview-failure", jqXHR.statusText ) );
+			};
 
-		var params = {
-			action: "translationreview",
-			token: $b.data( "token" ),
-			revision: $b.data( "revision" ),
-			format: "json"
-		};
-		$b.val( mw.msg( "translate-messagereview-progress" ) );
-		// BC for MW < 1.18
-		if ( typeof $b.prop === "undefined" ) {
-			$b.attr( "disabled", "disabled" );
-		} else {
+			var params = {
+				action: "translationreview",
+				token: $b.data( "token" ),
+				revision: $b.data( "revision" ),
+				format: "json"
+			};
+			$b.val( mw.msg( "translate-messagereview-progress" ) );
 			$b.prop( "disabled", true );
-		}
-		$.post( mw.util.wikiScript( "api" ), params, successFunction ).fail( failFunction );
+
+			$.post( mw.util.wikiScript( "api" ), params, successFunction ).fail( failFunction );
+		} );
 	} );
-} );
+} )( jQuery, mediaWiki );
