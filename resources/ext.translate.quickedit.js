60c49b0e2c8c89817699b50ff2dbf9c1524035fc
Harry Burt <jarry1250@gmail.com>
2012-07-19 12:41:26 +0100

    Resolve "On succesful save, update the MessageTable display too"
    Specifically, add onSuccess-style callback for the editor
    that updates the main MessageTable, changing the text,
    removing the green background and adding a class for
    extensibility.
    
    Change-Id: I3a6350e115bf86eefc3dc1e09b0e68d47af5d822

--- resources/ext.translate.quickedit.js
+++ resources/ext.translate.quickedit.js
@@ -10,7 +10,6 @@
  * called from Special:Translate/editpage?page=Namespace:pagename.
  *
  * TODO list:
- * * On succesful save, update the MessageTable display too.
  * * Instead of hc'd onscript, give them a class and use necessary triggers
  *
  * @author Niklas Laxström
@@ -179,9 +178,18 @@
 			
 			var dialog = $( '<div>' ).attr( 'id', id ).appendTo( $( 'body' ) );
 			
-			var callbacks = {}
+			var callbacks = {};
 			callbacks.close = function () { dialog.dialog( 'close' ); };
 			callbacks.next = function () { mw.translate.openNext( page, group ); };
+			callbacks.success = function ( text ) {
+				var $tr = $( 'tr', 'table.mw-sp-translate-table' ).filter( function () {
+					return $( this ).data( 'title' ) === page;
+				} );
+				$tr.find( 'td' ).last()
+					.html( convertWhiteSpaceToHTML( text ) )
+					.removeClass( 'untranslated' )
+					.addClass( 'justtranslated' );
+			};
 			mw.translate.openEditor( dialog, page, group, callbacks );
 
 			dialog.dialog( {
@@ -295,12 +303,13 @@
 			var page = $this.data( 'title' );
 			var group = $this.data( 'group' );
 			var next = $( this.next );
-			var callbacks = {}
+			var callbacks = {};
 			callbacks.success = function ( text ) {
 				// Update the cell value with the new translation
 				$this.find( 'td' ).last()
+					.html( convertWhiteSpaceToHTML( text ) )
 					.removeClass( 'untranslated' )
-					.html( convertWhiteSpaceToHTML( text ) );
+					.addClass( 'justtranslated' );
 			};
 			callbacks.close = function () {
 				$this.html( current );
