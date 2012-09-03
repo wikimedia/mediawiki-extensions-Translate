3da01342e0c6e7c05c14480f13bb51e7a779a8e2
Siebrand Mazeland <s.mazeland@xs4all.nl>
2012-09-03 13:15:00 +0200

    Trivial change to see if tests are triggered.
    Change-Id: Id1095a09d6606e8aa39a00eb37025a6f51e43014

--- Translate.php
+++ Translate.php
@@ -15,7 +15,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
 /**
  * Version number used in extension credits and in other placed where needed.
  */
-define( 'TRANSLATE_VERSION', '2012-07-04' );
+define( 'TRANSLATE_VERSION',  '2012-07-04' );
 
 /**
  * Extension credits properties.
