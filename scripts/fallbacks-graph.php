ccf5ab9750ef9e9308f268598a8a190d5e64833c
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-09-17 19:07:48 +0000

    New script for visualizing language fallbacks
    Change-Id: I7aa41b0d8b546e6b19601cf7a3b0a4f12549811b

---
+++ scripts/fallbacks-graph.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Script for creating graphml xml file of language fallbacks.
+ *
+ * @author Niklas Laxstrom
+ *
+ * @copyright Copyright © 2012, Niklas Laxström
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @file
+ */
+
+// Standard boilerplate to define $IP
+if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
+} else {
+	$dir = dirname( __FILE__ ); $IP = "$dir/../../..";
+}
+require_once( "$IP/maintenance/Maintenance.php" );
+
+/// Creates graphml xml file of language fallbacks.
+class FallbacksCompare extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Creates graphml xml file of language fallbacks..';
+	}
+
+	public function execute() {
+		$template = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<graphml
+	xmlns="http://graphml.graphdrawing.org/xmlns"  
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns
+		http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd"
+	xmlns:y="http://www.yworks.com/xml/graphml">
+		
+	<key id="code" for="node" yfiles.type="nodegraphics"/>
+	<graph id="G" edgedefault="directed">
+$1
+	</graph>
+</graphml>
+
+XML;
+
+		$langs = Language::fetchLanguageNames( null, 'mw' );
+		$nodes = $edges = array();
+		foreach ( $langs as $code => $name ) {
+
+			$fallbacks = Language::getFallbacksFor( $code );
+			if ( $fallbacks === array( 'en' ) ) continue;
+
+			$nodes[$code] = $this->createNode( $code );
+
+			$prev = $code;
+			foreach ( $fallbacks as $fb ) {
+				$nodes[$fb] = $this->createNode( $fb );
+				$edges[$fb.$prev] = Xml::element( 'edge', array( 'source' => $prev, 'target' => $fb ) );
+				$prev = $fb;
+			}
+		}
+
+		$output = array_merge( $nodes, $edges );
+		$output = "\t\t" . implode( "\n\t\t", $output );
+		echo str_replace( '$1', $output, $template );
+	}
+
+	protected function createNode( $code ) {
+		return
+			  Xml::openElement( 'node', array( 'id' => $code ) )
+			. Xml::openElement( 'data', array( 'key' => "code" ) )
+			. Xml::openElement( 'y:Shpapenode' )
+			. Xml::element( 'y:NodeLabel', array(), $code )
+			. Xml::closeElement( 'y:Shpapenode' )
+			. Xml::closeElement( 'data' )
+			. Xml::closeElement( 'node' )
+		;
+	}
+
+
+}
+
+$maintClass = 'FallbacksCompare';
+require_once( DO_MAINTENANCE );
