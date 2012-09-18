597d5eed4ec9535cd5dcbed313c1dc37e4583600
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-09-18 01:31:33 +0000

    Core of automatic group workflow changes
    Not active, needs docs for syntax.
    Includes refactoring and tests for ApiGroupReview
    
    Change-Id: I456238440d5334c9c78f788793ace7d01e26de78

---
+++ tests/MessageGroupWorkflowStateUpdaterJobTest.php
@@ -0,0 +1,151 @@
+<?php
+
+/**
+ * @group Database
+ */
+class MessageGroupWorkflowStateUpdaterJobTest extends MediaWikiTestCase {
+
+	protected $storables = array(
+		'wgTranslateEC',
+		'wgTranslateAC',
+		'wgTranslateCC',
+		'wgHooks',
+	);
+
+	public function setUp() {
+		$this->store = array();
+
+		foreach ( $this->storables as $var ) {
+			global $$var;
+			$this->store[$var] = $$var;
+		}
+
+		global $wgHooks;
+		$wgHooks['TranslatePostInitGroups'] = array( array( $this, 'getTestGroups' ) );
+		MessageGroups::clearCache();
+	}
+
+	public function tearDown() {
+		foreach ( $this->storables as $var ) {
+			global $$var;
+			$$var = $this->store[$var];
+		}
+		MessageGroups::clearCache();
+	}
+
+	public function getTestGroups( &$list ) {
+		$list['group-trans'] = new MessageGroupWithTransitions();
+		$list['group-notrans'] = new MessageGroupWithoutTransitions();
+		return false;
+	}
+
+	public function testGetGroupsWithTransitions() {
+		$handle = new MockMessageHandle();
+		$groups = MessageGroupWorkflowStateUpdaterJob::getGroupsWithTransitions( $handle );
+		foreach ( $groups as $id => $transitions ) {
+			$this->assertEquals( 'group-trans', $id );
+		}
+	}
+
+	/**
+	 * @dataProvider StatValueProvider
+	 */
+	public function testGetStatValue( $type, $expected ) {
+		$stats = array(
+			MessageGroupStats::TOTAL => 666,
+			MessageGroupStats::FUZZY => 111,
+			MessageGroupStats::TRANSLATED => 222,
+			MessageGroupStats::PROOFREAD => 111,
+		);
+		$actual = MessageGroupWorkflowStateUpdaterJob::getStatValue( $stats, $type );
+		$this->assertEquals( $expected, $actual );
+	}
+
+	public function StatValueProvider() {
+		return array(
+			array( 'UNTRANSLATED', 333 ),
+			array( 'OUTDATED', 111 ),
+			array( 'TRANSLATED', 222 ),
+			array( 'PROOFREAD', 111 ),
+		);
+	}
+
+	/**
+	 * @dataProvider MatchConditionProvider
+	 */
+	public function testMatchCondition( $expected, $value, $condition, $max) {
+		$actual = MessageGroupWorkflowStateUpdaterJob::matchCondition( $value, $condition, $max );
+		$this->assertEquals( $expected, $actual );
+	}
+
+
+	public function MatchConditionProvider() {
+		return array(
+			array( true, 0, 'ZERO', 666 ),
+			array( false, 1, 'ZERO', 666 ),
+			array( true, 1, 'NONZERO', 666 ),
+			array( false, 0, 'NONZERO', 666 ),
+			array( true, 666, 'MAX', 666 ),
+			array( false, 0, 'MAX', 666 ),
+			array( false, 12, 'MAX', 666 ),
+		);
+	}
+
+	public function testGetNewState() {
+		$group = MessageGroups::getGroup( 'group-trans' );
+		$states = $group->getWorkflowStates();
+		$transitions = $states['transitions'];
+
+
+		$stats = array( 5, 0, 0, 0 );
+		$newstate = MessageGroupWorkflowStateUpdaterJob::getNewState( $stats, $transitions );
+		$this->assertEquals( 'unset', $newstate, 'all zero, should be unset' );
+
+		$stats = array( 5, 1, 0, 0 );
+		$newstate = MessageGroupWorkflowStateUpdaterJob::getNewState( $stats, $transitions );
+		$this->assertEquals( 'inprogress', $newstate, 'one translated message' );
+
+		$stats = array( 5, 0, 1, 0 );
+		$newstate = MessageGroupWorkflowStateUpdaterJob::getNewState( $stats, $transitions );
+		$this->assertEquals( 'inprogress', $newstate, 'one outdated message' );
+
+		$stats = array( 5, 1, 1, 0 );
+		$newstate = MessageGroupWorkflowStateUpdaterJob::getNewState( $stats, $transitions );
+		$this->assertEquals( 'inprogress', $newstate, 'one translated and one outdated message' );
+
+		$stats = array( 5, 5, 0, 0 );
+		$newstate = MessageGroupWorkflowStateUpdaterJob::getNewState( $stats, $transitions );
+		$this->assertEquals( 'proofreading', $newstate, 'all translated' );
+	}
+
+}
+
+class MockMessageHandle extends MessageHandle {
+	public function __construct() {}
+
+	public function getGroupIds() {
+		return array( 'group-trans', 'group-notrans' );
+	}
+}
+
+class MessageGroupWithoutTransitions extends WikiMessageGroup {
+	public function __construct() {}
+
+	public function getWorkflowStates() {
+		return array();
+	}
+}
+
+class MessageGroupWithTransitions extends MessageGroupWithoutTransitions {
+	public function getWorkflowStates() {
+		return array(
+			'transitions' => array(
+				array( 'unset', array( 'UNTRANSLATED' => 'MAX', 'OUTDATED' => 'ZERO', 'TRANSLATED' => 'ZERO' ) ),
+				array( 'inprogress', array( 'UNTRANSLATED' => 'NONZERO' ) ),
+				array( 'proofreading', array( 'TRANSLATED' => 'MAX' ) ),
+				array( 'ready', array( 'PROOFREAD' => 'MAX' ) ),
+			)
+		);
+	}
+}
+
