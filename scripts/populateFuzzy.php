77cba3d7c014e52184784abd35d72c84bb7813b3
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-09-24 04:52:34 +0000

    (bug 40373) Added namespace option to pupulateFuzzy
    Also made it a maintenance class
    
    Change-Id: Ic311375eff0e5f96828798ccdfb9770f463dc445

--- scripts/populateFuzzy.php
+++ scripts/populateFuzzy.php
@@ -3,57 +3,78 @@
  * A script to populate fuzzy tags to revtag table.
  *
  * @author Niklas Laxstrom
- * @copyright Copyright © 2009-2011, Niklas Laxström
+ * @copyright Copyright © 2009-2012, Niklas Laxström
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  * @file
  */
 
-/// @cond
-
-require( dirname( __FILE__ ) . '/cli.inc' );
-
-$db = wfGetDB( DB_MASTER );
-
-$count = $db->selectField( 'page', 'count(*)', array( 'page_namespace' => $wgTranslateMessageNamespaces ), __METHOD__ );
-if ( !$count ) {
-	echo "Nothing to update";
-	exit();
+// Standard boilerplate to define $IP
+if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
+} else {
+	$dir = __DIR__; $IP = "$dir/../../..";
 }
+require_once( "$IP/maintenance/Maintenance.php" );
 
-$tables = array( 'page', 'text', 'revision' );
-$fields = array( 'page_id', 'page_title', 'page_namespace', 'rev_id', 'old_text', 'old_flags' );
-$conds = array(
-	'page_latest = rev_id',
-	'old_id = rev_text_id',
-	'page_namespace' => $wgTranslateMessageNamespaces,
-);
-
-$limit = max( 1000, intval( $count / 100 ) );
-$offset = 0;
-while ( true ) {
-	$inserts = array();
-	echo "$offset/$count\n";
-	$options = array( 'LIMIT' => $limit, 'OFFSET' => $offset );
-	$res = $db->select( $tables, $fields, $conds, __METHOD__, $options );
-
-	if ( !$res->numRows() ) {
-		break;
+/// A script to populate fuzzy tags to revtag table.
+class PopulateFuzzy extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'A script to populate fuzzy tags to revtag table.';
+		$this->addOption( 'namespace', 'Namepace name or id', /*required*/false, /*has arg*/true );
 	}
 
-	foreach ( $res as $r ) {
-		$text = Revision::getRevisionText( $r );
-		if ( strpos( $text, TRANSLATE_FUZZY ) !== false ) {
-			$inserts[] = array(
-				'rt_page' => $r->page_id,
-				'rt_revision' => $r->rev_id,
-				'rt_type' => RevTag::getType( 'fuzzy' ),
-			);
+	public function execute() {
+		global $wgTranslateMessageNamespaces;
+
+		$namespace = $this->getOption( 'namespace', $wgTranslateMessageNamespaces );
+		if ( is_string( $namespace ) ) {
+			if ( !MWNamespace::exists( $namespace ) ) {
+				$namespace = MWNamespace::getCanonicalIndex( $namespace );
+				if ( $namespace === null ) {
+					$this->error( 'Bad namespace', true );
+				}
+			}
+		}
+
+		$db = wfGetDB( DB_MASTER );
+		$tables = array( 'page', 'text', 'revision' );
+		$fields = array( 'page_id', 'page_title', 'page_namespace', 'rev_id', 'old_text', 'old_flags' );
+		$conds = array(
+			'page_latest = rev_id',
+			'old_id = rev_text_id',
+			'page_namespace' => $namespace,
+		);
+
+		$limit = 100;
+		$offset = 0;
+		while ( true ) {
+			$inserts = array();
+			$this->output( '.', 0 );
+			$options = array( 'LIMIT' => $limit, 'OFFSET' => $offset );
+			$res = $db->select( $tables, $fields, $conds, __METHOD__, $options );
+
+			if ( !$res->numRows() ) {
+				break;
+			}
+
+			foreach ( $res as $r ) {
+				$text = Revision::getRevisionText( $r );
+				if ( strpos( $text, TRANSLATE_FUZZY ) !== false ) {
+					$inserts[] = array(
+						'rt_page' => $r->page_id,
+						'rt_revision' => $r->rev_id,
+						'rt_type' => RevTag::getType( 'fuzzy' ),
+					);
+				}
+			}
+
+			$offset += $limit;
+
+			$db->replace( 'revtag', 'rt_type_page_revision', $inserts, __METHOD__ );
 		}
 	}
-
-	$offset += $limit;
-
-	$db->replace( 'revtag', 'rt_type_page_revision', $inserts, __METHOD__ );
 }
 
-/// @endcond
+$maintClass = 'PopulateFuzzy';
+require_once( RUN_MAINTENANCE_IF_MAIN );
