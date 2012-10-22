3b31a551c3e57803739887ffc211ec9a8b6d2bd3
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-10-22 14:04:07 +0000

    Added tests to database fuzzy marker updates
    Also added $wgTranslateGroupFiles => array() to speed up tests.
    
    Change-Id: I41007348dbcc0b2fb4aaf7df9896780ef28471c3

--- tests/MessageGroupStatesUpdaterJobTest.php
+++ tests/MessageGroupStatesUpdaterJobTest.php
@@ -16,6 +16,7 @@ class MessageGroupStatesUpdaterJobTest extends MediaWikiTestCase {
 			'wgTranslateEC' => array(),
 			'wgTranslateMessageIndex' => array( 'DatabaseMessageIndex' ),
 			'wgTranslateWorkflowStates' => false,
+			'wgTranslateGroupFiles' => array(),
 		) );
 		$wgHooks['TranslatePostInitGroups'] = array( array( $this, 'getTestGroups' ) );
 		MessageGroups::clearCache();
