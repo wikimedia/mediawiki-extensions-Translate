9865c6004b935fdefc7b4f51793b85c039735bbb
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-11-29 20:09:20 +0000

    WIP message list
    Change-Id: I757c33affbbf5a017216604033edf97d8e16fe66

---
+++ utils/TuxMessageTable.php
@@ -0,0 +1,120 @@
+<?php
+
+class TuxMessageTable extends MessageTable {
+	public $context;
+
+	public function header() {
+		return '<div class="grid tux-messagelist">';
+	}
+
+	public function contents() {
+		$optional = wfMessage( 'translate-optional' )->escaped();
+
+		$this->doLinkBatch();
+
+		$sourceLang = Language::factory( $this->group->getSourceLanguage() );
+		$targetLang = Language::factory( $this->collection->getLanguage() );
+		$titleMap = $this->collection->keys();
+
+		$output =  '';
+
+		$this->collection->initMessages(); // Just to be sure
+		foreach ( $this->collection as $key => $m ) {
+			$tools = array();
+			$title = $titleMap[$key];
+
+			$original = $m->definition();
+			$translation = $m->translation();
+
+			$hasTranslation = $translation !== null;
+			if ( $hasTranslation ) {
+				$message = $translation;
+				$extraAttribs = self::getLanguageAttributes( $targetLang );
+			} else {
+				$message = $original;
+				$extraAttribs = self::getLanguageAttributes( $sourceLang );
+			}
+
+			wfRunHooks( 'TranslateFormatMessageBeforeTable', array( &$message, $m, $this->group, $targetLang, &$extraAttribs ) );
+
+			// Using Html::element( a ) because Linker::link is memory hog.
+			// It takes about 20 KiB per call, and that times 5000 is quite
+			// a lot of memory.
+			global $wgLang;
+			$niceTitle = htmlspecialchars( $wgLang->truncate( $title->getPrefixedText(), -35 ) );
+			$linkAttribs = array(
+				'href' => $title->getLocalUrl( array( 'action' => 'edit' ) + $this->editLinkParams ),
+			);
+			$linkAttribs += TranslationEditPage::jsEdit( $title, $this->group->getId() );
+
+			$tools['edit'] = Html::element( 'a', $linkAttribs, $niceTitle );
+
+			$anchor = 'msg_' . $key;
+			$anchor = Xml::element( 'a', array( 'id' => $anchor, 'href' => "#$anchor" ), "↓" );
+
+			$extra = '';
+
+			$linkAttribs = array(
+				'href' => $title->getLocalUrl( array( 'action' => 'edit' ) + $this->editLinkParams ),
+			);
+			$linkAttribs += TranslationEditPage::jsEdit( $title, $this->group->getId() );
+
+			$edit = Html::element( 'a', $linkAttribs, 'Edit' );
+
+			$tqeData = $extraAttribs + array(
+				'data-title' => $title->getPrefixedText(),
+				'data-group' => $this->group->getId(),
+				'id' => 'tqe-anchor-' . substr( sha1( $title->getPrefixedText() ), 0, 12 ),
+				'class' => 'row tux-message tqe-inlineeditable ' . ( $hasTranslation ? 'translated' : 'untranslated' )
+			);
+
+			$userId = $this->context->getUser()->getId();
+			$status = '';
+			$reviewers = $m->getProperty( 'reviewers' );
+
+			if ( $m->hasTag( 'optional' ) ) {
+				$status = '<span class="tux-info tux-optional">Optional</span>';
+			} elseif ( $m->hasTag( 'fuzzy' ) ) {
+				$status = '<span class="tux-warning tux-status-fuzzy">Review</span>';
+				$translation = str_replace( TRANSLATE_FUZZY, '', $translation );
+			} elseif ( is_array( $reviewers ) && in_array( $userId, $reviewers ) ) {
+				$status = '<span class="tux-status-proofread">Proofread</span>';
+			} elseif ( $translation !== null ) {
+				$status = '<span class="tux-status-translated">Translated</span>';
+			}
+
+			$output .= Xml::tags( 'div', $tqeData,
+				'<div class="nine columns tux-list-message"><span class="tux-list-source">' .
+				TranslateUtils::convertWhiteSpaceToHTML( $original ) . '</span>' .
+				'<span class="tux-list-translation">' .
+				TranslateUtils::convertWhiteSpaceToHTML( $translation )
+				. '</span></div>'
+				. "<div class='two columns tux-list-status text-center'>$status</div>"
+				. "<div class='one column tux-list-edit text-center'>$edit</div>"
+				
+			);
+
+			$output .= "\n";
+		}
+
+		return $output;
+	}
+
+	public function fullTable() {
+		$this->includeAssets();
+		$this->context->getOutput()->addModules( 'ext.translate.grid' );
+
+$bar = StatsBar::getNew( $this->group->getId(), $this->collection->getLanguage() );
+		$html = $bar->getHtml( $this->context );
+
+
+		$more = '<div class="tux-ajax-loader mw-ajax-loader"><div class="tux-ajax-loader-count">666 more message</div><div class="tux-ajax-loader-more">Loading 15...</div></div>';
+
+		$more .= '<div class="tux-action-bar row"><div class="three columns">' . $html . '</div>';
+		$more .= '<div class="three columns text-center"><button>Hello world</button></div>';
+		$more .= '<div class="four columns text-center"><button>Do something</button></div>';
+		
+		return $this->header() . $this->contents() . $more . '</div>';
+	}
+
+}
