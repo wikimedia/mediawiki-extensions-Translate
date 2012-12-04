79ae3c45d33c7c2e8c9165f0ca5e85f9def81717
Siebrand Mazeland <s.mazeland@xs4all.nl>
2012-12-04 21:39:36 +0100

    Add group description message for Wikisource Mobile
    Change-Id: I774805285b426a6660a6c78c3ae83f124412068e

--- TranslateGroupDescriptions.i18n.php
+++ TranslateGroupDescriptions.i18n.php
@@ -66,6 +66,7 @@ $messages['en'] = array(
 	'bw-desc-wikireader' => '[[Translating:WikiReader|WikiReader]] delivers an offline, text-only version of Wikipedia on a mobile device',
 	'translate-group-desc-wikimedia-mobile' => 'Aggregated message group containing mobile applications for Wikimedia projects',
 	'translate-group-desc-wikimedia-mobile-wikipedia' => '[[Translating:WikipediaMobile|WikipediaMobile]] is a cross-platform mobile and tablet application for reading and contributing to Wikipedia',
+	'translate-group-desc-wikimedia-mobile-wikisource' => '[[Translating:WikisourceMobile|WikisourceMobile]] is a cross-platform mobile and tablet application for reading and contributing to Wikisource',
 	'translate-group-desc-wikimedia-mobile-wiktionary' => '[[Translating:WiktionaryMobile|WiktionaryMobile]] is a cross-platform mobile and tablet application for reading and contributing to Wiktionary',
 	'translate-group-desc-wikimedia-mobile-wlm' => '[[Translating:WLMMobile|WikiLovesMonuments Mobile]] is a mobile application for contributing to the [[:meta:Wiki Loves Monuments|Wiki Loves Monuments project]]',
 	'translate-group-desc-waymarked-trails-help' => 'The help pages of the Waymarked Trails web sites: http://waymarkedtrails.org/help/about',
