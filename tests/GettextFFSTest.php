eede1e80931c2d745ac0a4a3410733db084bc3b7
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-12-05 14:07:26 +0000

    GettextFFS: Empty msgctxt must be different from no msgctxt
    Tests included
    
    Change-Id: I63e179ca7194ba4e4c8944bbd3040c0cf0db68df

--- tests/GettextFFSTest.php
+++ tests/GettextFFSTest.php
@@ -35,15 +35,51 @@ class GettextFFSTest extends MediaWikiTestCase {
 
 			array(
 				'1437e478b59e220640bf530f7e3bac93950eb8ae-"¤_=FJQ"_¤r_£_ab',
-				array( 'id' => '"¤#=FJQ"<>¤r £}[]}%ab', 'ctxt' => '' ),
+				array( 'id' => '"¤#=FJQ"<>¤r £}[]}%ab', 'ctxt' => false ),
 				'legacy'
 			),
 			array(
 				'1437e4-"¤#=FJQ"<>¤r_£}[]}%ab',
-				array( 'id' => '"¤#=FJQ"<>¤r £}[]}%ab', 'ctxt' => '' ),
+				array( 'id' => '"¤#=FJQ"<>¤r £}[]}%ab', 'ctxt' => false ),
 				'simple'
 			),
 
+		);
+	}
+
+	public function testHashing() {
+		$item1 = array(
+			'id' => 'a',
+			'str' => 'b',
+			'ctxt' => false,
+		);
+
+		$item2 = array(
+			'id' => 'a',
+			'str' => 'b',
+			'ctxt' => '',
+		);
+
+		$this->assertNotEquals(
+			GettextFFS::generateKeyFromItem( $item1, 'legacy' ),
+			GettextFFS::generateKeyFromItem( $item2, 'legacy' ),
+			'Empty msgctxt is different from no msgctxt'
+		);
+
+		$this->assertNotEquals(
+			GettextFFS::generateKeyFromItem( $item1, 'simple' ),
+			GettextFFS::generateKeyFromItem( $item2, 'simple' ),
+			'Empty msgctxt is different from no msgctxt'
+		);
+
+		$this->assertEquals(
+			sha1( $item1['id'] ) . '-' . $item1['id'],
+			GettextFFS::generateKeyFromItem( $item1, 'legacy' )
+		);
+
+		$this->assertEquals(
+			substr( sha1( $item1['id'] ), 0, 6) . '-' . $item1['id'],
+			GettextFFS::generateKeyFromItem( $item1, 'simple' )
 		);
 	}
 
