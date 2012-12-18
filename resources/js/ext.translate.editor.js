d924a9bce948af50abb1e48eecd1751d7fca7802
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-12-18 17:27:22 +0530

    Limit the listing of suggestions
    This is just to avoid ugly long list. Will be done in proper
    way when helper API is ready
    
    Change-Id: I911fc3211dbe7b110d8e18fd80bd9d1ed97dd407

--- resources/js/ext.translate.editor.js
+++ resources/js/ext.translate.editor.js
@@ -398,6 +398,12 @@
 							translateEditor.$editor.find( '.in-other-languages-title' )
 								.after( $otherLanguage );
 						}
+						if ( index > 2 ) {
+							// FIXME this is wrong way to filter. but to be addressed when
+							// there is a translation helper api to do this properly
+							return false;
+						}
+
 					} );
 				}
 			} ).fail( function () {
@@ -446,6 +452,12 @@
 
 						translateEditor.$editor.find( '.tm-suggestions-title' )
 							.after( $suggestion );
+
+						if ( index > 2 ) {
+							// FIXME this is wrong way to filter. but to be addressed when
+							// there is a translation helper api to do this properly
+							return false;
+						}
 					} );
 				}
 			} ).fail( function () {
