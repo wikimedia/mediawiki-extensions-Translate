97320ae0b68ba611ce4ca487eb756b980db01cf0
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-01-02 08:42:05 +0000

    Hook doc test code was swapped
    Change-Id: Ib5e4bc3098830dfaaa9233a68ede2b3ca3799e2c

--- tests/HookDocTest.php
+++ tests/HookDocTest.php
@@ -119,26 +119,26 @@ class HookDocTest extends MediaWikiTestCase {
 	}
 
 	public function testHookIsDocumentedPHP() {
-		foreach ( $this->documented['php'] as $hook => $params ) {
-			$this->assertArrayHasKey( $hook, $this->used['php'], "PHP hook $hook is documented" );
+		foreach ( $this->used['php'] as $hook => $params ) {
+			$this->assertArrayHasKey( $hook, $this->documented['php'], "PHP hook $hook is documented" );
 		}
 	}
 
 	public function testHookExistsPHP() {
-		foreach ( $this->used['php'] as $hook => $params ) {
-			$this->assertArrayHasKey( $hook, $this->documented['php'], "Documented php hook $hook exists" );
+		foreach ( $this->documented['php'] as $hook => $params ) {
+			$this->assertArrayHasKey( $hook, $this->used['php'], "Documented php hook $hook exists" );
 		}
 	}
 
 	public function testHookIsDocumentedJS() {
-		foreach ( $this->documented['js'] as $hook => $params ) {
-			$this->assertArrayHasKey( $hook, $this->used['js'], "Js hook $hook is documented" );
+		foreach ( $this->used['js'] as $hook => $params ) {
+			$this->assertArrayHasKey( $hook, $this->documented['js'], "Js hook $hook is documented" );
 		}
 	}
 
 	public function testHookExistsJS() {
-		foreach ( $this->used['js'] as $hook => $params ) {
-			$this->assertArrayHasKey( $hook, $this->documented['js'], "Documented js hook $hook exists" );
+		foreach ( $this->documented['js'] as $hook => $params ) {
+			$this->assertArrayHasKey( $hook, $this->used['js'], "Documented js hook $hook exists" );
 		}
 	}
 }
