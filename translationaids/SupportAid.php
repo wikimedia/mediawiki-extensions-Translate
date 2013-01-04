85ca2b501a9327b015efdd07a1ae57ff48cd67ba
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-01-04 09:38:28 +0000

    Added a file missing from I8849715f
    Change-Id: I8a2466508003a217656834794b48078034875571

---
+++ translationaids/SupportAid.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Translation aid provider.
+ *
+ * @file
+ * @author Niklas Laxström
+ * @copyright Copyright © 2013, Niklas Laxström
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+/**
+ * Translation aid which gives an url where users can ask for hlep
+ *
+ * @ingroup TranslationAids
+ * @since 2013-01-02
+ */
+class SupportAid extends TranslationAid {
+	public function getData() {
+		global $wgTranslateSupportUrl;
+		if ( !$wgTranslateSupportUrl ) {
+			throw new TranslationHelperException( "Support page not configured" );
+		}
+
+		$supportTitle = Title::newFromText( $wgTranslateSupportUrl['page'] );
+		if ( !$supportTitle ) {
+			throw new TranslationHelperException( "Support page not configured properly" );
+		}
+
+		$supportParams = $wgTranslateSupportUrl['params'];
+		$title = $this->handle->getTitle();
+		foreach ( $supportParams as &$value ) {
+			$value = str_replace( '%MESSAGE%', $title->getPrefixedText(), $value );
+		}
+
+		return array(
+			'url' => $supportTitle->getFullUrl( $supportParams ),
+		);
+	}
+}
