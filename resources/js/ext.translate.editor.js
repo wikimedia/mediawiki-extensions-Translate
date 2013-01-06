401f231917770abc22e0d975d3bb31d219198ef2
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-01-06 20:18:21 +0200

    Display an "Add description" link
    Change-Id: Iaafbad731cf587aed2eac22d2f9c207ba325cd80

--- resources/js/ext.translate.editor.js
+++ resources/js/ext.translate.editor.js
@@ -605,6 +605,8 @@
 					readMore,
 					$readMore = null;
 
+				// TODO This returns an error for 'Page display title' in translatable pages.
+				// Something smarter must be done with it.
 				if ( !result.helpers ) {
 					return false; // That is unlikely. but to be safe.
 				}
@@ -612,7 +614,10 @@
 				// Message documentation
 				documentation = result.helpers.documentation;
 
-				if ( documentation.value !== '' ) {
+				if ( documentation.value === '' ) {
+					translateEditor.$editor.find( '.message-desc-edit' )
+						.text( mw.msg( 'tux-editor-add-desc' ) );
+				} else {
 					$messageDoc = translateEditor.$editor.find( '.message-desc' );
 					$messageDoc.html( documentation.html );
 
