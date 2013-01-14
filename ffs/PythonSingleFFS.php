6fee6d838855774da31dc880718046295aba160e
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-01-14 21:08:07 +0000

    Add debugging to investigate PHP warnings
    Change-Id: I8de065376cb6a5cf484fc80b46fc8693b71541de

--- ffs/PythonSingleFFS.php
+++ ffs/PythonSingleFFS.php
@@ -97,6 +97,9 @@ class PythonSingleFFS extends SimpleFFS {
 		$json = wfShellExec( "python -c $command" );
 
 		$parsed = FormatJson::decode( $json, true );
+		if ( !is_array( $parsed ) ) {
+			throw new MWException( "Failed to decode python file $filename" );
+		}
 		$sections = array();
 		foreach ( $parsed as $code => $messages ) {
 			$sections[$code] = array( 'MESSAGES' => $messages );
