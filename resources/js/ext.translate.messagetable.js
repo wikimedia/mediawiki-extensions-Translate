8d6e562cbdab66e9d25f519cddbaa75ff254779e
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-01-17 19:51:37 +0200

    Cleanup whitespace and remove repeated jQuery calls
    Change-Id: I704eea57edd46378c9b596ea1a2dc8f03fa8dd06

--- resources/js/ext.translate.messagetable.js
+++ resources/js/ext.translate.messagetable.js
@@ -1,7 +1,6 @@
 ( function ( $, mw ) {
 	'use strict';
 
-
 	mw.translate = mw.translate || {};
 
 	mw.translate = $.extend( mw.translate, {
@@ -38,6 +37,7 @@
 		if ( actualWidth >= parseInt( $( '.row' ).width(), 10 ) ) {
 			$( '.tux-message-selector .more ul' ) // Overflow menu
 				.prepend( $( '.row.tux-message-selector > li.column:last' ).prev() );
+
 			// See if more items to be pushed to the overflow menu
 			messageFilterOverflowHandler() ;
 		}
@@ -75,105 +75,106 @@
 		} );
 
 		$( '.tux-messagetable-loader' ).appear( function () {
-			var messagegroup, targetLanguage, pageSize, offset, remaining;
-
-			messagegroup = $( this ).data( 'messagegroup' );
-			pageSize = $( this ).data( 'pagesize' );
-			offset = $( this ).data( 'offset' );
-			remaining = $( this ).data( 'remaining' );
-			targetLanguage = $( '.tux-messagelist' ).data( 'targetlangcode' );
+			var messagegroup, pageSize, targetLanguage, sourceLanguage,
+				remaining, $messageList,
+				$this = $( this ),
+				offset = $this.data( 'offset' );
 
 			if ( offset === '-1' ) {
 				return false;
 			}
 
+			messagegroup = $this.data( 'messagegroup' );
+			pageSize = $this.data( 'pagesize' );
+			remaining = $this.data( 'remaining' );
+			$messageList = $( '.tux-messagelist' );
+			sourceLanguage = $messageList.data( 'sourcelangcode' );
+			targetLanguage = $messageList.data( 'targetlangcode' );
+
 			$.when(
 				mw.translate.getMessages( messagegroup, targetLanguage, offset, pageSize )
 			).then( function ( result ) {
-					var messages = result.query.messagecollection;
+				var messages = result.query.messagecollection;
 
-					$.each( messages, function ( index ) {
-						var message,
-							$message,
-							status = '',
-							statusMsg = '',
-							$messageWrapper;
+				$.each( messages, function ( index, message ) {
+					var $message,
+						status = '',
+						statusMsg = '',
+						$messageWrapper;
 
-						message = messages[index];
-
-						if ( message.translation ) {
-							status = 'translated';
-						}
-
-						//if ( message.tags.length ) {
-							// FIXME: proofread is not coming in tags.
-							//status += message.tags.join( ' ' );
-						//}
-
-						if ( status ) {
-							statusMsg = 'tux-status-' + status;
-						}
-
-						$messageWrapper = $( '<div>' )
-							.addClass( 'row tux-message' )
-							.attr( {
-								'data-translation': message.translation,
-								'data-source': message.definition,
-								'data-title': message.title,
-								'data-group': messagegroup
-							} );
-
-						$message = $( '<div>' )
-							.addClass( 'row tux-message-item ' + status )
-							.append(
-								$( '<div>' )
-									.addClass( 'nine columns tux-list-message' )
-									.append(
-										$( '<span>' )
-											.addClass( 'tux-list-source' )
-											.attr( 'lang', $( '.tux-messagelist' ).data( 'sourcelangcode' ) )
-											.text( message.definition ),
-										$( '<span>' )
-											.addClass( 'tux-list-translation' )
-											.attr( 'lang', $( '.tux-messagelist' ).data( 'targetlangcode' ) )
-											.text( message.translation || '' )
-										),
-								$( '<div>' )
-									.addClass( 'two columns tux-list-status text-center' )
-									.append(
-										$( '<span>' )
-											.addClass( 'tux-status-' + status )
-											.text( statusMsg? mw.msg( statusMsg ): '' )
-									),
-								$( '<div>' )
-									.addClass( 'one column tux-list-edit text-center' )
-									.append(
-										$( '<a>' )
-											.attr( {
-												'title': mw.msg( 'translate-edit-title', message.key )
-											} )
-											.text( mw.msg( 'tux-edit' ) )
-									)
-							);
-
-						$messageWrapper.append( $message );
-						$( '.tux-messagetable-loader' ).before( $messageWrapper );
-
-						// Attach translate editor to the message
-						$messageWrapper.translateeditor();
-					} );
-
-					if ( result['query-continue'] ) {
-						offset = result['query-continue'].messagecollection.mcoffset;
-						$( '.tux-messagetable-loader' ).data( 'offset', offset )
-							.data( 'remaining', remaining - pageSize );
-						$( '.tux-messagetable-loader-count' )
-							.text( mw.msg( 'tux-messagetable-more-messages', remaining - pageSize  ) );
-					} else {
-						// End of messages
-						$( '.tux-messagetable-loader' ).data( 'offset', -1 )
-							.addClass( 'hide' );
+					if ( message.translation ) {
+						status = 'translated';
 					}
+
+					//if ( message.tags.length ) {
+						// FIXME: proofread is not coming in tags.
+						//status += message.tags.join( ' ' );
+					//}
+
+					if ( status ) {
+						statusMsg = 'tux-status-' + status;
+					}
+
+					$messageWrapper = $( '<div>' )
+						.addClass( 'row tux-message' )
+						.attr( {
+							'data-translation': message.translation,
+							'data-source': message.definition,
+							'data-title': message.title,
+							'data-group': messagegroup
+						} );
+
+					$message = $( '<div>' )
+						.addClass( 'row tux-message-item ' + status )
+						.append(
+							$( '<div>' )
+								.addClass( 'nine columns tux-list-message' )
+								.append(
+									$( '<span>' )
+										.addClass( 'tux-list-source' )
+										.attr( 'lang', sourceLanguage )
+										.text( message.definition ),
+									$( '<span>' )
+										.addClass( 'tux-list-translation' )
+										.attr( 'lang', targetLanguage )
+										.text( message.translation || '' )
+									),
+							$( '<div>' )
+								.addClass( 'two columns tux-list-status text-center' )
+								.append(
+									$( '<span>' )
+										.addClass( 'tux-status-' + status )
+										.text( statusMsg ? mw.msg( statusMsg ) : '' )
+								),
+							$( '<div>' )
+								.addClass( 'one column tux-list-edit text-center' )
+								.append(
+									$( '<a>' )
+										.attr( {
+											'title': mw.msg( 'translate-edit-title', message.key )
+										} )
+										.text( mw.msg( 'tux-edit' ) )
+								)
+						);
+
+					$messageWrapper.append( $message );
+					$( '.tux-messagetable-loader' ).before( $messageWrapper );
+
+					// Attach translate editor to the message
+					$messageWrapper.translateeditor();
+				} );
+
+				if ( result['query-continue'] ) {
+					offset = result['query-continue'].messagecollection.mcoffset;
+					$( '.tux-messagetable-loader' ).data( 'offset', offset )
+						.data( 'remaining', remaining - pageSize );
+					$( '.tux-messagetable-loader-count' )
+						.text( mw.msg( 'tux-messagetable-more-messages', remaining - pageSize  ) );
+				} else {
+					// End of messages
+					$( '.tux-messagetable-loader' ).data( 'offset', -1 )
+						.addClass( 'hide' );
+				}
 			} );
 		}, {
 			// Appear callback need to be called more than once.
