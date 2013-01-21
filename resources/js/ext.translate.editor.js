590d1889af77e82b142b9d923230b654383413ca
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-01-21 20:20:00 +0530

    Disable translation text area when user has no translation rights
    Change-Id: Ie8e2816cad117ed8bbc121f16bd90b8cb7b72135

--- resources/js/ext.translate.editor.js
+++ resources/js/ext.translate.editor.js
@@ -396,8 +396,10 @@
 						.addClass( 'tux-editor-ask-permission' )
 						.attr( {
 							'href': mw.util.wikiGetlink( mw.config.get( 'wgTranslatePermissionUrl' ) )
-					} )
-				);
+						} )
+					);
+				// Disable the text area if user has no translation rights
+				$textArea.prop( 'disabled', true );
 			}
 
 			$skipButton = $( '<button>' )
