43109da55aa1b09642891aae23547eb8908a8a8f
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-02-11 23:07:44 +0530

    MessageTable plugin
    Refactor all message table related code and make it as a jQuery plugin
    
    Change-Id: Icf0b31207df5c18e1e53bcf9b399641988a9f2e7

--- resources/js/ext.translate.messagetable.js
+++ resources/js/ext.translate.messagetable.js
@@ -50,226 +50,314 @@
 		}
 	} );
 
-	function messageFilterOverflowHandler() {
-		var actualWidth = 0;
-
-		// Calculate the total width required for the filters
-		$( '.row.tux-message-selector > li' ).each( function () {
-			actualWidth += $( this ).outerWidth( true );
-		} );
-
-		// Grid row has a min width. After that scrollbars will appear.
-		// We are checking whether the filters is wider than the current grid row width
-		if ( actualWidth >= parseInt( $( '.nine.columns' ).width(), 10 ) ) {
-			$( '.tux-message-selector .more ul' ) // Overflow menu
-				.prepend( $( '.row.tux-message-selector > li.column:last' ).prev() );
-
-			// See if more items to be pushed to the overflow menu
-			messageFilterOverflowHandler();
-		}
+	function MessageTable( container, options ) {
+		this.$container = $( container );
+		this.options = options;
+		this.$loader = $( '.tux-messagetable-loader' );
+		this.init();
+		this.listen();
 	}
 
-	/**
-	 * Add a message to the message table
-	 */
-	function addMessage( message ) {
-		var $message, targetLanguage, targetLanguageDir, sourceLanguage, sourceLanguageDir,
-			status = '',
-			statusMsg = '',
-			statusClass = '',
-			$messageWrapper,
-			$messageList;
+	MessageTable.prototype = {
+		init: function () {
+			this.load();
+		},
 
-		$messageList = $( '.tux-messagelist' );
+		listen: function () {
+			var messageTable = this;
 
-		sourceLanguage = $messageList.data( 'sourcelangcode' );
-		sourceLanguageDir = $.uls.data.getDir( sourceLanguage );
-		targetLanguage = $messageList.data( 'targetlangcode' );
-		targetLanguageDir = $.uls.data.getDir( targetLanguage );
-
-		status = message.properties.status;
-		statusClass = 'tux-status-' + status;
-
-		if ( message.tags.length
-			&& $.inArray( 'optional', message.tags ) >= 0
-			&& status === 'untranslated'
-		) {
-			status = 'optional';
-			statusClass = 'tux-status-optional';
-		}
-
-		// Fuzzy translations need warning class
-		if ( status === 'fuzzy' ) {
-			statusClass = statusClass + ' tux-warning';
-		}
-
-		// Label the status if it is not untranslated
-		if ( status !== 'untranslated' ) {
-			statusMsg = 'tux-status-' + status;
-		}
-
-		$messageWrapper = $( '<div>' )
-			.addClass( 'row tux-message' )
-			.data( 'message', message );
-
-		$message = $( '<div>' )
-			.addClass( 'row tux-message-item ' + status )
-			.append(
-				$( '<div>' )
-					.addClass( 'eight columns tux-list-message' )
-					.append(
-						$( '<span>' )
-							.addClass( 'tux-list-source' )
-							.attr( {
-								lang: sourceLanguage,
-								dir: sourceLanguageDir
-							} )
-							.text( message.definition ),
-						// Bidirectional isolation.
-						// This should be removed some day when proper
-						// unicode-bidi: isolate
-						// is supported everywhere
-						$( '<span>' )
-							.html( $( 'body' ).hasClass( 'rtl' ) ? '&rlm;' : '&lrm;' ),
-						$( '<span>' )
-							.addClass( 'tux-list-translation' )
-							.attr( {
-								lang: targetLanguage,
-								dir: targetLanguageDir
-							} )
-							.text( message.translation || '' )
-						),
-				$( '<div>' )
-					.addClass( 'two columns tux-list-status text-center' )
-					.append(
-						$( '<span>' )
-							.addClass( statusClass )
-							.text( statusMsg ? mw.msg( statusMsg ) : '' )
-					),
-				$( '<div>' )
-					.addClass( 'two column tux-list-edit text-right' )
-					.append(
-						$( '<a>' )
-							.attr( {
-								'title': mw.msg( 'translate-edit-title', message.key )
-							} )
-							.text( mw.msg( 'tux-edit' ) )
-					)
-			);
-
-		$messageWrapper.append( $message );
-		$( '.tux-messagetable-loader' ).before( $messageWrapper );
-
-		// Attach translate editor to the message
-		$messageWrapper.translateeditor( {
-			message: message
-		} );
-	}
-
-	function messageTableLoader() {
-		var messagegroup,
-			pageSize,
-			remaining,
-			targetLanguage,
-			query,
-			$loader = $( '.tux-messagetable-loader' ),
-			$messageList = $( '.tux-messagelist' ),
-			offset = $loader.data( 'offset' ),
-			filter = $loader.data( 'filter' );
-
-		messagegroup = $loader.data( 'messagegroup' );
-		pageSize = $loader.data( 'pagesize' );
-		targetLanguage = $messageList.data( 'targetlangcode' );
-
-		if ( offset === -1 ) {
-			return;
-		}
-
-		$.when(
-			mw.translate.getMessages( messagegroup, targetLanguage, offset, pageSize, filter )
-		).then( function ( result ) {
-			var messages = result.query.messagecollection;
-
-			$.each( messages, function ( index, message ) {
-				message.group = messagegroup;
-				addMessage( message );
+			$( window ).scroll( function () {
+				delay( function() {
+					messageTable.scroll();
+				}, 300 );
+			} ).resize( function () {
+				messageTable.resize();
+				$( '.tux-action-bar' ).width( $( '.tux-messagelist' ).width() );
 			} );
 
-			if ( result['query-continue'] === undefined ) {
-				// End of messages
-				$loader.data( 'offset', -1 ).addClass( 'hide' );
+			$( '.tux-message-filter-box' ).on( 'input propertychange', function () {
+				delay( messageTable.search( $( this ).val() ), 300 );
+			} );
+
+			this.$loader.appear( function () {
+				messageTable.load();
+			}, {
+				// Appear callback need to be called more than once.
+				one: false
+			} );
+		},
+
+		/**
+		 * Add a message to the message table
+		 */
+		add: function ( message ) {
+			var $message, targetLanguage, targetLanguageDir, sourceLanguage, sourceLanguageDir,
+				status = '',
+				statusMsg = '',
+				statusClass = '',
+				$messageWrapper;
+
+			sourceLanguage = this.$container.data( 'sourcelangcode' );
+			sourceLanguageDir = $.uls.data.getDir( sourceLanguage );
+			targetLanguage = this.$container.data( 'targetlangcode' );
+			targetLanguageDir = $.uls.data.getDir( targetLanguage );
+
+			status = message.properties.status;
+			statusClass = 'tux-status-' + status;
+
+			if ( message.tags.length
+				&& $.inArray( 'optional', message.tags ) >= 0
+				&& status === 'untranslated'
+			) {
+				status = 'optional';
+				statusClass = 'tux-status-optional';
+			}
+
+			// Fuzzy translations need warning class
+			if ( status === 'fuzzy' ) {
+				statusClass = statusClass + ' tux-warning';
+			}
+
+			// Label the status if it is not untranslated
+			if ( status !== 'untranslated' ) {
+				statusMsg = 'tux-status-' + status;
+			}
+
+			$messageWrapper = $( '<div>' )
+				.addClass( 'row tux-message' )
+				.data( 'message', message );
+
+			$message = $( '<div>' )
+				.addClass( 'row tux-message-item ' + status )
+				.append(
+					$( '<div>' )
+						.addClass( 'eight columns tux-list-message' )
+						.append(
+							$( '<span>' )
+								.addClass( 'tux-list-source' )
+								.attr( {
+									lang: sourceLanguage,
+									dir: sourceLanguageDir
+								} )
+								.text( message.definition ),
+							// Bidirectional isolation.
+							// This should be removed some day when proper
+							// unicode-bidi: isolate
+							// is supported everywhere
+							$( '<span>' )
+								.html( $( 'body' ).hasClass( 'rtl' ) ? '&rlm;' : '&lrm;' ),
+							$( '<span>' )
+								.addClass( 'tux-list-translation' )
+								.attr( {
+									lang: targetLanguage,
+									dir: targetLanguageDir
+								} )
+								.text( message.translation || '' )
+							),
+					$( '<div>' )
+						.addClass( 'two columns tux-list-status text-center' )
+						.append(
+							$( '<span>' )
+								.addClass( statusClass )
+								.text( statusMsg ? mw.msg( statusMsg ) : '' )
+						),
+					$( '<div>' )
+						.addClass( 'two column tux-list-edit text-right' )
+						.append(
+							$( '<a>' )
+								.attr( {
+									'title': mw.msg( 'translate-edit-title', message.key )
+								} )
+								.text( mw.msg( 'tux-edit' ) )
+						)
+				);
+
+			$messageWrapper.append( $message );
+			this.$loader.before( $messageWrapper );
+
+			// Attach translate editor to the message
+			$messageWrapper.translateeditor( {
+				message: message
+			} );
+		},
+
+		/**
+		 * Search the message filter
+		 *
+		 * @param {String} query
+		 */
+		search: function ( query ) {
+			var resultCount = 0,
+				$result,
+				matcher = new RegExp( '\\b' + escapeRegex( query ), 'i' );
+
+			this.$container.find( '.tux-message' ).each( function () {
+				var $message = $( this ),
+					message = $message.data( 'message' );
+
+				if ( matcher.test( message.definition ) || matcher.test( message.translation ) ) {
+					$message.removeClass( 'hide' );
+					resultCount++;
+				} else {
+					$message.addClass( 'hide' );
+				}
+			} );
+
+			$result = this.$container.find( '.tux-message-filter-result' );
+			if ( !$result.length ) {
+				$result = $( '<div>' ).addClass( 'row highlight tux-message-filter-result' )
+					.append(
+						$( '<div>' )
+							.addClass( 'ten columns advanced-search' ),
+						$( '<button>' )
+							.addClass( 'two columns button advanced-search' )
+							.text( mw.msg( 'tux-message-filter-advanced-button' ) )
+					);
+				this.$container.prepend( $result );
+			}
+
+			if ( !query ) {
+				$result.addClass( 'hide' );
+				$( '.tux-message-filter-box-clear' ).addClass( 'hide' );
+			} else {
+				$result.removeClass( 'hide' )
+					.find( 'div' )
+					.text( mw.msg( 'tux-message-filter-result', resultCount, query ) );
+				$result.find( 'button' ).on( 'click', function () {
+					window.location.href = new mw.Uri( mw.util.wikiGetlink( 'Special:SearchTranslations' ) )
+						.extend( { query: query } );
+				} );
+				$( '.tux-message-filter-box-clear' ).removeClass( 'hide' );
+			}
+		},
+
+		resize: function () {
+			var actualWidth = 0;
+
+			// Calculate the total width required for the filters
+			$( '.row.tux-message-selector > li' ).each( function () {
+				actualWidth += $( this ).outerWidth( true );
+			} );
+
+			// Grid row has a min width. After that scrollbars will appear.
+			// We are checking whether the message table is wider than the current grid row width.
+			if ( actualWidth >= parseInt( $( '.nine.columns' ).width(), 10 ) ) {
+				$( '.tux-message-selector .more ul' ) // Overflow menu
+					.prepend( $( '.row.tux-message-selector > li.column:last' ).prev() );
+
+				// See if more items to be pushed to the overflow menu
+				this.resize();
+			}
+		},
+
+		load: function () {
+			var messagegroup,
+				pageSize,
+				remaining,
+				targetLanguage,
+				query,
+				messageTable = this,
+				$messageList = $( '.tux-messagelist' ),
+				offset = this.$loader.data( 'offset' ),
+				filter = messageTable.$loader.data( 'filter' );
+
+			messagegroup = messageTable.$loader.data( 'messagegroup' );
+			pageSize = messageTable.$loader.data( 'pagesize' );
+			targetLanguage = $messageList.data( 'targetlangcode' );
+
+			if ( offset === -1 ) {
 				return;
 			}
 
-			// Dynamically loaded messages should pass the search filter if present.
-			query = $( '.tux-message-filter-box' ).val();
-			if ( query ) {
-				search( query );
-			}
+			$.when(
+				mw.translate.getMessages( messagegroup, targetLanguage, offset, pageSize, filter )
+			).then( function ( result ) {
+				var messages = result.query.messagecollection;
 
-			$loader.data( 'offset', result['query-continue'].messagecollection.mcoffset );
+				$.each( messages, function ( index, message ) {
+					message.group = messagegroup;
+					messageTable.add( message );
+				} );
 
-			remaining = result.query.metadata.remaining;
-			$( '.tux-messagetable-loader-count' ).text(
-				mw.msg( 'tux-messagetable-more-messages', remaining )
-			);
-			$( '.tux-messagetable-loader-more' ).text(
-				mw.msg( 'tux-messagetable-loading-messages', Math.min( remaining, pageSize ) )
-			);
-		} );
-	}
+				if ( result['query-continue'] === undefined ) {
+					// End of messages
+					messageTable.$loader.data( 'offset', -1 ).addClass( 'hide' );
+					return;
+				}
 
-	/**
-	 * Search the message filter
-	 *
-	 * @param {String} query
-	 */
-	function search( query ) {
-		var $messageTable,
-			resultCount = 0,
-			$result,
-			matcher = new RegExp( '\\b' + escapeRegex( query ), 'i' );
+				// Dynamically loaded messages should pass the search filter if present.
+				query = $( '.tux-message-filter-box' ).val();
+				if ( query ) {
+					messageTable.search( query );
+				}
 
-		$messageTable = $( '.tux-messagelist' );
-		$messageTable.find( '.tux-message' ).each( function () {
-			var $message = $( this ),
-				message = $message.data( 'message' );
+				messageTable.$loader.data( 'offset', result['query-continue'].messagecollection.mcoffset );
 
-			if ( matcher.test( message.definition ) || matcher.test( message.translation ) ) {
-				$message.removeClass( 'hide' );
-				resultCount++;
-			} else {
-				$message.addClass( 'hide' );
-			}
-		} );
-
-		$result = $messageTable.find( '.tux-message-filter-result' );
-		if ( !$result.length ) {
-			$result = $( '<div>' ).addClass( 'row highlight tux-message-filter-result' )
-				.append(
-					$( '<div>' )
-						.addClass( 'ten columns advanced-search' ),
-					$( '<button>' )
-						.addClass( 'two columns button advanced-search' )
-						.text( mw.msg( 'tux-message-filter-advanced-button' ) )
+				remaining = result.query.metadata.remaining;
+				$( '.tux-messagetable-loader-count' ).text(
+					mw.msg( 'tux-messagetable-more-messages', remaining )
 				);
-			$messageTable.prepend( $result );
-		}
+				$( '.tux-messagetable-loader-more' ).text(
+					mw.msg( 'tux-messagetable-loading-messages', Math.min( remaining, pageSize ) )
+				);
 
-		if ( !query ) {
-			$result.addClass( 'hide' );
-			$( '.tux-message-filter-box-clear' ).addClass( 'hide' );
-		} else {
-			$result.removeClass( 'hide' )
-				.find( 'div' )
-				.text( mw.msg( 'tux-message-filter-result', resultCount, query ) );
-			$result.find( 'button' ).on( 'click', function () {
-				window.location.href = new mw.Uri( mw.util.wikiGetlink( 'Special:SearchTranslations' ) )
-					.extend( { query: query } );
 			} );
-			$( '.tux-message-filter-box-clear' ).removeClass( 'hide' );
+		},
+
+		scroll: function () {
+			var $window,
+				$tuxActionBar,
+				isFloating,
+				needFloat, needStick,
+				windowScrollBottom,
+				messageListOffset,
+				messageListHeight,
+				messageListBottom;
+
+			$window = $( window );
+			$tuxActionBar = $( '.tux-action-bar' );
+			isFloating = $tuxActionBar.hasClass( 'floating' );
+
+			windowScrollBottom = $window.scrollTop() + $window.height();
+			messageListOffset = this.$container.offset();
+			messageListHeight = this.$container.height();
+			messageListBottom = messageListOffset.top + messageListHeight;
+			needFloat = windowScrollBottom < messageListBottom;
+			needStick = windowScrollBottom > ( messageListBottom + $tuxActionBar.height() );
+
+			if ( !isFloating && needFloat ) {
+				$tuxActionBar
+					.addClass( 'floating' )
+					.width( this.$container.width() );
+			} else if ( isFloating && needStick ) {
+				$tuxActionBar.removeClass( 'floating' );
+			} else if ( isFloating && needFloat ) {
+				$tuxActionBar.css( 'left', messageListOffset.left - $window.scrollLeft() );
+			}
 		}
-	}
+	};
+
+	/*
+	 * messagetable PLUGIN DEFINITION
+	 */
+
+	$.fn.messagetable = function ( options ) {
+		return this.each( function () {
+			var $this = $( this ),
+				data = $this.data( 'messagetable' );
+
+			if ( !data ) {
+				$this.data( 'messagetable', ( data = new MessageTable( this, options ) ) );
+			}
+
+			if ( typeof options === 'string' ) {
+				data[options].call( $this );
+			}
+		} );
+	};
+
+	$.fn.messagetable.Constructor = MessageTable;
+
 
 	$( 'document' ).ready( function () {
 		// Currently used only in the pre-TUX editor
@@ -302,62 +390,13 @@
 			$.post( mw.util.wikiScript( 'api' ), params, successFunction ).fail( failFunction );
 		} );
 
-		$( '.tux-messagetable-loader' ).appear( messageTableLoader, {
-			// Appear callback need to be called more than once.
-			one: false
-		} );
-
-		$( '.tux-message-filter-box' ).on( 'input propertychange', function () {
-			delay( search( $( this ).val() ), 300 );
-		} );
-
 		$( '.tux-message-filter-box-clear' ).on( 'click', function () {
 			$( '.tux-message-filter-box' ).focus().val( '' );
 			$( this ).addClass( 'hide' );
 		} );
 
-		messageFilterOverflowHandler();
+		$( '.tux-messagelist' ).messagetable();
 	} );
-
-	$( window ).resize( function () {
-		messageFilterOverflowHandler();
-		$( '.tux-action-bar' ).width( $( '.tux-messagelist' ).width() );
-	} );
-
-	$( window ).scroll( function () {
-		delay( messageListScrollHandler, 300 );
-	} );
-
-	function messageListScrollHandler() {
-		var $window,
-			$tuxActionBar,
-			isFloating,
-			needFloat, needStick,
-			windowScrollBottom,
-			messageListOffset, messageListHeight, messageListBottom,
-			$messageList = $( '.tux-messagelist' );
-
-		$window = $( window );
-		$tuxActionBar = $( '.tux-action-bar' );
-		isFloating = $tuxActionBar.hasClass( 'floating' );
-
-		windowScrollBottom = $window.scrollTop() + $window.height();
-		messageListOffset = $messageList.offset();
-		messageListHeight = $messageList.height();
-		messageListBottom = messageListOffset.top + messageListHeight;
-		needFloat = windowScrollBottom < messageListBottom;
-		needStick = windowScrollBottom > ( messageListBottom + $tuxActionBar.height() );
-
-		if ( !isFloating && needFloat ) {
-			$tuxActionBar
-				.addClass( 'floating' )
-				.width( $messageList.width() );
-		} else if ( isFloating && needStick ) {
-			$tuxActionBar.removeClass( 'floating' );
-		} else if ( isFloating && needFloat ) {
-			$tuxActionBar.css( 'left', messageListOffset.left - $window.scrollLeft() );
-		}
-	}
 
 	var delay = ( function () {
 		var timer = 0;
@@ -376,5 +415,4 @@
 	function escapeRegex( value ) {
 		return value.replace( /[\-\[\]{}()*+?.,\\\^$\|#\s]/g, '\\$&' );
 	}
-
 }( jQuery, mediaWiki ) );
