d832fe91396e54d2940059ce4be5c14bf6be727e
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-02-20 11:39:24 +0530

    Display a warning when translating to a non-priority language
    Change-Id: I4b35e934a64b1405f1a6f90bac9eb1a0284ad258

--- Translate.php
+++ Translate.php
@@ -456,6 +456,7 @@ $wgResourceModules['ext.translate.special.translate'] = array(
 	'scripts' => 'resources/js/ext.translate.special.translate.js',
 	'position' => 'top',
 	'dependencies' => array(
+		'mediawiki.jqueryMsg',
 		'mediawiki.Uri',
 		'mediawiki.api.parse',
 		'ext.translate.base',
@@ -471,6 +472,8 @@ $wgResourceModules['ext.translate.special.translate'] = array(
 		'translate-js-support-unsaved-warning',
 		'translate-documentation-language',
 		'translate-workflow-state-',
+		'tpt-discouraged-language-force',
+		'tpt-discouraged-language',
 	),
 ) + $resourcePaths;
 
