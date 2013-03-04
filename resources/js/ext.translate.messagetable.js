85fcdebe350477911df1463286df3d0d883f1ec1
Bharat Kumar Jonnalagadda <dev.malkum@gmail.com>
2013-03-04 16:04:12 +0530

    (bug 45432) "X" should clear search filter in message list
    Bug: 45432
    Change-Id: Ida8925ce5c5f45f607f6fed5fb9e7cba4c51c006

--- resources/js/ext.translate.messagetable.js
+++ resources/js/ext.translate.messagetable.js
@@ -84,6 +84,10 @@
 				delay( messageTable.search( $( this ).val() ), 300 );
 			} );
 
+			$( '.tux-message-filter-box-clear' ).on( 'click', function () {
+				$( '.tux-message-filter-box' ).focus().val( '' ).trigger( 'input' );
+			} );
+
 			this.$loader.appear( function () {
 				messageTable.load();
 			}, {
@@ -635,10 +639,7 @@
 			$.post( mw.util.wikiScript( 'api' ), params, successFunction ).fail( failFunction );
 		} );
 
-		$( '.tux-message-filter-box-clear' ).on( 'click', function () {
-			$( '.tux-message-filter-box' ).focus().val( '' );
-			$( this ).addClass( 'hide' );
-		} );
+
 
 	} );
 
