1be76bee54ecb08d93f4fe2a29c8f504de0b690f
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-03-10 12:58:26 +0000

    Apply light formatting in page mode
    Bug: 45548
    Change-Id: I1e131ad18008fa05784c4f67efd798a9c0598484

--- Translate.php
+++ Translate.php
@@ -203,9 +203,6 @@ $wgJobClasses['TTMServerMessageUpdateJob'] = 'TTMServerMessageUpdateJob';
 
 require( "$dir/Resources.php" );
 
-/** @endcond */
-
-
 # == Configuration variables ==
 
 # === Basic configuration ===
