ec2af79adb419535be5ab7eb870e9e524bde306d
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-03-11 14:34:05 +0530

    (Bug 45433) - Floating/sticking headers/footers should be adjusted when filtering
    Change-Id: I2a2f797d51e755ea302a9f586f80ebe41d5d8108

--- resources/js/ext.translate.messagetable.js
+++ resources/js/ext.translate.messagetable.js
@@ -319,6 +319,10 @@
 			}
 
 			this.$loader.trigger( 'appear' );
+
+			// Trigger a scroll event for the window to make sure all floating toolbars
+			// are in their position.
+			$( window ).trigger( 'scroll' );
 		},
 
 		resize: function () {
