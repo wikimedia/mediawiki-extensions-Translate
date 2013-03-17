8002eb673b63c42f9242b29695b1395ac215ede5
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-03-17 10:16:02 +0000

    Only record skipped fuzzy and untranslated as hard
    Change-Id: Ia558bfa2c02edf0df26b16b05dd5b0c38dac3043

--- resources/js/ext.translate.editor.js
+++ resources/js/ext.translate.editor.js
@@ -157,15 +157,18 @@
 		 * Record it to mark as hard.
 		 */
 		skip: function () {
-			var translateEditor = this,
-				api = new mw.Api();
+			var api = new mw.Api();
 
-			api.post( {
-				action: 'hardmessages',
-				title: translateEditor.message.title,
-				token: mw.user.tokens.get( 'editToken' )
-			} );
-			// We don't care about the result of the above ajax call
+			// Only record skips of fuzzy or untranslated as hards
+			// @TODO device better algorithm
+			if ( this.$messageItem.is( '.fuzzy, .untranslated' ) ) {
+				// We can just ignore the result even if it fails
+				api.post( {
+					action: 'hardmessages',
+					title: this.message.title,
+					token: mw.user.tokens.get( 'editToken' )
+				} );
+			}
 		},
 
 		/**
