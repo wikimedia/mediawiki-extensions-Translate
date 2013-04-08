6f7399f9af751fe859aa2214d3e29454cdca9ca4
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-04-08 12:47:26 +0300

    Remove the hide tab when there are no more warnings
    Bug: 45833
    Change-Id: I4d2c4540dce17557b1bb80d0f153f5f64264d435

--- resources/js/ext.translate.editor.js
+++ resources/js/ext.translate.editor.js
@@ -598,7 +598,12 @@
 		 * @param type
 		 */
 		removeWarning: function ( type ) {
-			this.$editor.find( '.tux-warning' ).find( '.' + type ).remove();
+			var $tuxWarning = this.$editor.find( '.tux-warning' );
+
+			$tuxWarning.find( '.' + type ).remove();
+			if ( !$tuxWarning.children().length ) {
+				this.$editor.find( '.tux-more-warnings' ).addClass( 'hide' );
+			}
 		},
 
 		/**
