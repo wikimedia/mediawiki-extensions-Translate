9ad914d87888b3f708016a9b9e99176706072af7
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-04-12 13:15:24 +0000

    First step of Special:TranslateSandbox
    * Lists open requests
    * Can promote or delete
    
    Change-Id: If1747b0f803586d1d300957644649396e5acf14b

---
+++ TranslateSandbox.i18n.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Translations for the sandbox feature.
+ *
+ * @file
+ * @license GPL2+
+ */
+
+$messages = array();
+
+/** English
+ * @author Nike
+ */
+$messages['en'] = array(
+	'translatesandbox' => 'Manage translator sandbox',
+	'tsb-filter-pending' => 'Pending requests',
+);
+
+/** Message documentation (Message documentation)
+ * @author Nike
+ */
+$messages['qqq'] = array(
+	'translatesandbox' => '{{doc-special|TranslateSandbox}}',
+	'tsb-filter-pending' => 'A filter option on Special:TranslateSandbox',
+);
+
---
+++ resources/js/ext.translate.special.translatesandbox.js
@@ -0,0 +1,68 @@
+/**
+ * JS for special page.
+ * @author Niklas Laxström
+ * @license GPL2+
+ */
+
+(function ( $, mw ) {
+	function doApiAction( options ) {
+		var api = new mw.Api();
+
+		options = $.extend( {}, {
+			action: 'translatesandbox',
+			token: $( '#token' ).val()
+		}, options );
+
+		api.post( options )
+			.done( function () { window.alert( 'Success' ); } )
+			.fail( function () { window.alert( 'Failure' ); } )
+		;
+	}
+
+	$( document ).ready( function () {
+		var $requests, $detailsPane;
+
+		$detailsPane = $( '.details.pane' );
+		$requests = $( '.requests .request' );
+		$requests.on( 'click', function () {
+			var $this = $( this );
+
+			$detailsPane.empty().append(
+				$( '<div>' )
+					.addClass( 'username row' )
+					.text( $this.find( '.username' ).text() ),
+				$( '<div>' )
+					.addClass( 'email row' )
+					.text( $this.find( '.email' ).text() ),
+				$( '<div>' )
+					.addClass( 'languages row' )
+					.append(
+						$( '<span>' ).text( 'Afrikaans' ),
+						$( '<span>' ).text( 'español' )
+					 ),
+				$( '<div>' )
+					.addClass( 'actions row' )
+					.append(
+						$( '<button>' )
+							.addClass( 'accept primary button' )
+							.text( 'Accept' )
+							.on( 'click', function () {
+								doApiAction( {
+									userid: $this.data( 'data' ).id,
+									'do': 'promote'
+								} );
+							} ),
+						$( '<button>' )
+							.addClass( 'delete destructive button' )
+							.text( 'Delete' )
+							.on( 'click', function () {
+								doApiAction( {
+									userid: $this.data( 'data' ).id,
+									'do': 'delete'
+								} );
+							} )
+					 )
+			);
+		} );
+	} );
+}( jQuery, mediaWiki ) );
