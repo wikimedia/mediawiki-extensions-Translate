01eb67b50ace6178d1475bcb4be4400986b9d5d8
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-04-14 11:15:06 +0300

    Add dropdown menu as a separate module
    Change-Id: If1bc5c3b6a2d9a8284e5aa68fd0801f41b417f37

---
+++ resources/js/ext.translate.dropdownmenu.js
@@ -0,0 +1,10 @@
+( function ( $ ) {
+	$( document ).ready( function () {
+		// Hide the workflow selector when clicking outside of it
+		$( 'html' ).on( 'click', function ( e ) {
+			if ( !e.isDefaultPrevented() ) {
+				$( '.dropdown-menu' ).addClass( 'hide' );
+			}
+		} );
+	} );
+} )( jQuery );
