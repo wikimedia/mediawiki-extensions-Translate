e5dcfc026635a5ef5447e37245924b9d30f4fa77
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-04-15 10:40:44 +0000

    Default remind email content
    Change-Id: I8f56ab1453d6993e3005d36bac9a3c7d04b3f608

--- TranslateSandbox.i18n.php
+++ TranslateSandbox.i18n.php
@@ -14,6 +14,15 @@ $messages = array();
 $messages['en'] = array(
 	'translatesandbox' => 'Manage translator sandbox',
 	'tsb-filter-pending' => 'Pending requests',
+
+	// Reminders
+	'tsb-reminder-title-generic' => 'Complete your introduction to become a verified translator',
+	'tsb-reminder-content-generic' => 'Hi $1,
+
+You recently signed up to {{SITENAME}}. You are only few steps away from unlocking
+free translation and extra translation helpers.
+
+Please log in to $2 and make some more translations.',
 );
 
 /** Message documentation (Message documentation)
@@ -22,5 +31,11 @@ $messages['en'] = array(
 $messages['qqq'] = array(
 	'translatesandbox' => '{{doc-special|TranslateSandbox}}',
 	'tsb-filter-pending' => 'A filter option on Special:TranslateSandbox',
+
+	// Reminders
+	'tsb-reminder-title-generic' => 'Subject of an email',
+	'tsb-reminder-content-generic' => 'Body of an email.
+* $1: user name of the recipient
+* $2: url to the website',
 );
 
