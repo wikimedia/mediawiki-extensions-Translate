7f839329bcfc92909efedb1695d61bb9e2e7aa5e
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-05-29 17:56:17 +0300

    MediaWiki language extension bundle 2013.05

--- Translate.php
+++ Translate.php
@@ -25,7 +25,7 @@ define( 'TRANSLATE_VERSION', '2013-04-29' );
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'Translate',
-	'version' => TRANSLATE_VERSION,
+	'version' => '[https://www.mediawiki.org/wiki/MLEB MLEB 2013.05]',
 	'author' => array( 'Niklas Laxström', 'Siebrand Mazeland' ),
 	'descriptionmsg' => 'translate-desc',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Translate',
