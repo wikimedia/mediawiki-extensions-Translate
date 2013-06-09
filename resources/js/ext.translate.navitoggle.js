4f0f3fc51c13565635ae9c0d5f33a4671a70496e
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-06-09 20:44:06 +0000

    Hide sidebar while using the translation tool
    Bug: 45836
    Change-Id: I5019569f395e1e8211ae6cc9f6a3a4019076be2b

---
+++ resources/js/ext.translate.navitoggle.js
@@ -0,0 +1,47 @@
+/**
+ * Introduces a toggle icon than can be used to hide navigation menu in vector
+ * @author Niklas Laxström
+ * @license GPL2+
+ */
+(function ( mw, $ ) {
+	'use strict';
+
+	var delay = ( function () {
+		var timer = 0;
+
+		return function ( callback, milliseconds ) {
+			clearTimeout( timer );
+			timer = setTimeout( callback, milliseconds );
+		};
+	} () );
+
+	$( document ).ready( function () {
+		var $miniLogo, $toggle, rtl, delim,
+			$body = $( 'body' );
+
+		rtl = $body.hasClass( 'rtl' );
+		delim = rtl ?
+			$( "#mw-head-base" ).css( 'margin-right' ) :
+			$( "#mw-head-base" ).css( 'margin-left' );
+
+		$miniLogo = $( '#p-logo' )
+			.clone()
+			.removeAttr( 'id' )
+			.addClass( 'tux-navi-minilogo' );
+
+		$toggle = $( '<div>' )
+			.addClass( 'tux-navitoggle' )
+			.css( rtl ? 'right' : 'left' , delim )
+			.click( function () {
+				$body.toggleClass( 'tux-navi-collapsed' );
+				// Allow for animations etc to go
+				delay( function () {
+					$( window ).trigger( 'resize' );
+					$( window ).trigger( 'scroll' );
+				}, 250 );
+			} );
+
+		$( 'body' ).append( $miniLogo, $toggle );
+
+	} );
+}( mediaWiki, jQuery ) );
