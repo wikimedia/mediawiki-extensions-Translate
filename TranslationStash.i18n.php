37b330cb2c00a2e6af7d7f0d1468a525f20a46a0
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-06-18 12:42:32 +0530

    [WIP]Special page TranslationStash
    Change-Id: Ia112b86a07f75c476139f92fde8941559bfb6799

---
+++ TranslationStash.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations for the TranslationStash feature.
+ *
+ * @file
+ * @license GPL2+
+ */
+
+$messages = array();
+
+
+/** English
+ * @author Nike
+ */
+$messages['en'] = array(
+	'translationstash' => 'Welcome',
+);
---
+++ resources/js/ext.translate.special.translationstash.js
@@ -0,0 +1,102 @@
+( function ( $, mw ) {
+	'use strict';
+	function getMessages( messageGroup, language, offset, limit ) {
+		var api = new mw.Api();
+
+		return api.get( {
+			action: 'query',
+			list: 'messagecollection',
+			mcgroup: messageGroup,
+			format: 'json',
+			mclanguage: language,
+			mcoffset: offset,
+			mclimit: limit,
+			//mcfilter: '!translated',
+			mcprop: 'definition|translation|tags|properties'
+		} );
+	}
+
+	function addMessage( message ) {
+		var $messageWrapper, $message,
+			$messageTable = $( '.tux-messagelist' ),
+			sourceLanguage = $messageTable .data( 'sourcelangcode' ),
+			sourceLanguageDir = $.uls.data.getDir( sourceLanguage ),
+			targetLanguage = $messageTable .data( 'targetlangcode' ),
+			targetLanguageDir = $.uls.data.getDir( targetLanguage ),
+			status = message.properties.status,
+			statusClass = 'tux-status-' + status;
+
+		$messageWrapper = $( '<div>' )
+			.addClass( 'row tux-message' );
+
+		$message = $( '<div>' )
+			.addClass( 'row message tux-message-item ' + status )
+			.append(
+				$( '<div>' )
+					.addClass( 'eight columns tux-list-message' )
+					.append(
+						$( '<span>' )
+							.addClass( 'tux-list-source' )
+							.attr( {
+								lang: sourceLanguage,
+								dir: sourceLanguageDir
+							} )
+							.text( message.definition ),
+						// Bidirectional isolation.
+						// This should be removed some day when proper
+						// unicode-bidi: isolate
+						// is supported everywhere
+						$( '<span>' )
+							.html( $( 'body' ).hasClass( 'rtl' ) ? '&rlm;' : '&lrm;' ),
+						$( '<span>' )
+							.addClass( 'tux-list-translation' )
+							.attr( {
+								lang: targetLanguage,
+								dir: targetLanguageDir
+							} )
+							.text( message.translation || '' )
+						),
+				$( '<div>' )
+					.addClass( 'two columns tux-list-status text-center' )
+					.append(
+						$( '<span>' )
+							.addClass( statusClass )
+					),
+				$( '<div>' )
+					.addClass( 'two column tux-list-edit text-right' )
+					.append(
+						$( '<a>' )
+							.attr( {
+								'title': mw.msg( 'translate-edit-title', message.key )
+							} )
+							.text( mw.msg( 'tux-edit' ) )
+					)
+			);
+
+		$messageWrapper.append( $message );
+		$messageTable.append( $messageWrapper );
+		// Attach translate editor to the message
+		$messageWrapper.translateeditor( {
+			message: message
+		} );
+	}
+
+	$( 'document' ).ready( function () {
+		var $messageTable = $( '.tux-messagelist' ),
+			messagegroup = '!recent'; //FIXME
+		getMessages( messagegroup,  $messageTable.data( 'targetLang' ), 0, 5  )
+			.done( function ( result ) {
+				var messages = result.query.messagecollection;
+				$.each( messages, function ( index, message ) {
+					message.group = messagegroup;
+					addMessage( message );
+					if ( index === 0 ) {
+						$( '.tux-message:first' ).data( 'translateeditor' ).show();
+					}
+					console.log(message);
+				} );
+			} ).fail( function ( errorCode, response ) {
+				// Handler error
+			} );
+	} );
+}( jQuery, mediaWiki ) );
