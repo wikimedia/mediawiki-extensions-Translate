fdd5a9415463d452ccb5b790ce10648d6c23e11b
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-10-02 11:51:25 +0530

    ts_key column length is too long and caused table creation error
    Change-Id: Ib183013c0c27001fc9efac12542d5ee612a496e0

--- sql/translate_stash.sql
+++ sql/translate_stash.sql
@@ -1,8 +1,7 @@
 -- Translate translation stash
 CREATE TABLE /*$wgDBprefix*/translate_stash (
   ts_user int NOT NULL,
-  -- 255 bytes for title and some slack for namespace prefix
-  ts_key varchar(300) binary NOT NULL,
+  ts_key varchar(255) binary NOT NULL,
   ts_value mediumblob NOT NULL,
   ts_metadata mediumblob NOT NULL,
 
