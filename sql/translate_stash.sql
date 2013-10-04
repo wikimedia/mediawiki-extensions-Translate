21fb5126ed34421eae92ea14ba7999219a590ead
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-10-04 10:39:37 +0530

    Translation stash storage: table changes
    * Use namespace in a separate column to avoid size limit error
    of ts_key column.
    * Update tests
    
    Change-Id: If950d1de558cb69d4ddba340ab0f506a98325d4f

--- sql/translate_stash.sql
+++ sql/translate_stash.sql
@@ -1,10 +1,10 @@
 -- Translate translation stash
 CREATE TABLE /*$wgDBprefix*/translate_stash (
   ts_user int NOT NULL,
-  -- 255 bytes for title and some slack for namespace prefix
-  ts_key varchar(300) binary NOT NULL,
+  ts_namespace int(11) NOT NULL,
+  ts_title varchar(255) binary NOT NULL,
   ts_value mediumblob NOT NULL,
   ts_metadata mediumblob NOT NULL,
 
-  PRIMARY KEY (ts_user, ts_key)
+  PRIMARY KEY (ts_user, ts_namespace, ts_title)
 ) /*$wgDBTableOptions*/;
