5173492a54047945fa5ca121db459fa4f3451218
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-10-10 15:31:39 +0200

    Update createCheckIndex.php to use Maintenance class
    Change-Id: Ia2c714ea5294a17aed5d95399e42b57494750424

--- scripts/createCheckIndex.php
+++ scripts/createCheckIndex.php
@@ -3,112 +3,139 @@
  * Creates serialised database of messages that need checking for problems.
  *
  * @author Niklas Laxstrom
- * @copyright Copyright © 2008-2011, Niklas Laxström
+ * @author Siebrand Mazeland
+ * @copyright Copyright © 2008-2013, Niklas Laxström, Siebrand Mazeland
  * @license GPL-2.0+
  * @file
  */
 
-$optionsWithArgs = array( 'groups' );
-require __DIR__ . '/cli.inc';
-
-$codes = Language::getLanguageNames( false );
-
-// Exclude this special language
-if ( $wgTranslateDocumentationLanguageCode ) {
-	unset( $codes[$wgTranslateDocumentationLanguageCode] );
-}
-
-// Skip source
-unset( $codes['en'] );
-
-$codes = array_keys( $codes );
-sort( $codes );
-
-if ( isset( $options['groups'] ) ) {
-	$reqGroups = array_map( 'trim', explode( ',', $options['groups'] ) );
+// Standard boilerplate to define $IP
+if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
 } else {
-	$reqGroups = false;
+	$dir = __DIR__;
+	$IP = "$dir/../../..";
 }
+require_once "$IP/maintenance/Maintenance.php";
 
-$verbose = isset( $options['verbose'] );
-
-$groups = MessageGroups::singleton()->getGroups();
-
-/**
- * @var $g MessageGroup
- */
-foreach ( $groups as $g ) {
-	$id = $g->getId();
-
-	// Skip groups that are not requested
-	if ( $reqGroups && !in_array( $id, $reqGroups ) ) {
-		unset( $g );
-		continue;
+class CreateCheckIndex extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Creates serialised database of messages that need ' .
+			'checking for problems.';
+		$this->addOption(
+			'group',
+			'Comma separated list of group IDs to process (can use * as wildcard). ' .
+			'Default: "*"',
+			false, /*required*/
+			true /*has arg*/
+		);
 	}
 
-	$checker = $g->getChecker();
-	if ( !$checker ) {
-		unset( $g );
-		continue;
-	}
+	public function execute() {
+		$codes = Language::fetchLanguageNames( false );
 
-	// Initialise messages, using unique definitions if appropriate
-	$collection = $g->initCollection( 'en', true );
-	if ( !count( $collection ) ) {
-		continue;
-	}
+		// Exclude the documentation language code
+		global $wgTranslateDocumentationLanguageCode;
+		if ( $wgTranslateDocumentationLanguageCode ) {
+			unset( $codes[$wgTranslateDocumentationLanguageCode] );
+		}
 
-	STDOUT( "Working with $id: ", $id );
+		// Skip source language code
+		unset( $codes['en'] );
 
-	foreach ( $codes as $code ) {
-		STDOUT( "$code ", $id );
+		$codes = array_keys( $codes );
+		sort( $codes );
 
-		$problematic = array();
+		$reqGroups = $this->getOption( 'group' );
+		if ( $reqGroups ) {
+			$reqGroups = explode( ',', $reqGroups );
+			$reqGroups = array_map( 'trim', $reqGroups );
+			$reqGroups = MessageGroups::expandWildcards( $reqGroups );
+		}
 
-		$collection->resetForNewLanguage( $code );
-		$collection->loadTranslations();
+		$verbose = isset( $options['verbose'] );
 
-		global $wgContLang;
+		$groups = MessageGroups::singleton()->getGroups();
 
-		foreach ( $collection as $key => $message ) {
-			$prob = $checker->checkMessageFast( $message, $code );
-			if ( $prob ) {
+		/** @var $g MessageGroup */
+		foreach ( $groups as $g ) {
+			$id = $g->getId();
 
-				if ( $verbose ) {
-					// Print it
-					$nsText = $wgContLang->getNsText( $g->namespaces[0] );
-					STDOUT( "# [[$nsText:$key/$code]]" );
+			// Skip groups that are not requested
+			if ( $reqGroups && !in_array( $id, $reqGroups ) ) {
+				unset( $g );
+				continue;
+			}
+
+			$checker = $g->getChecker();
+			if ( !$checker ) {
+				unset( $g );
+				continue;
+			}
+
+			// Initialise messages, using unique definitions if appropriate
+			$collection = $g->initCollection( 'en', true );
+			if ( !count( $collection ) ) {
+				continue;
+			}
+
+			$this->output( "Working with $id: ", $id );
+
+			foreach ( $codes as $code ) {
+				$this->output( "$code ", $id );
+
+				$problematic = array();
+
+				$collection->resetForNewLanguage( $code );
+				$collection->loadTranslations();
+
+				global $wgContLang;
+
+				foreach ( $collection as $key => $message ) {
+					$prob = $checker->checkMessageFast( $message, $code );
+					if ( $prob ) {
+
+						if ( $verbose ) {
+							// Print it
+							$nsText = $wgContLang->getNsText( $g->namespaces[0] );
+							$this->output( "# [[$nsText:$key/$code]]\n" );
+						}
+
+						// Add it to the array
+						$problematic[] = array( $g->namespaces[0], "$key/$code" );
+					}
 				}
 
-				// Add it to the array
-				$problematic[] = array( $g->namespaces[0], "$key/$code" );
+				self::tagFuzzy( $problematic );
 			}
 		}
-
-		tagFuzzy( $problematic );
-	}
-}
-
-function tagFuzzy( $problematic ) {
-	if ( !count( $problematic ) ) {
-		return;
 	}
 
-	$db = wfGetDB( DB_MASTER );
-	foreach ( $problematic as $p ) {
-		$title = Title::makeTitleSafe( $p[0], $p[1] );
-		$titleText = $title->getDBKey();
-		$res = $db->select( 'page', array( 'page_id', 'page_latest' ),
-			array( 'page_namespace' => $p[0], 'page_title' => $titleText ), __METHOD__ );
-
-		$inserts = array();
-		foreach ( $res as $r ) {
-			$inserts = array(
-				'rt_page' => $r->page_id,
-				'rt_revision' => $r->page_latest,
-				'rt_type' => RevTag::getType( 'fuzzy' )
-			);
+	static function tagFuzzy( $problematic ) {
+		if ( !count( $problematic ) ) {
+			return;
 		}
-		$db->replace( 'revtag', 'rt_type_page_revision', $inserts, __METHOD__ );
+
+		$db = wfGetDB( DB_MASTER );
+		foreach ( $problematic as $p ) {
+			$title = Title::makeTitleSafe( $p[0], $p[1] );
+			$titleText = $title->getDBKey();
+			$res = $db->select( 'page', array( 'page_id', 'page_latest' ),
+				array( 'page_namespace' => $p[0], 'page_title' => $titleText ), __METHOD__ );
+
+			$inserts = array();
+			foreach ( $res as $r ) {
+				$inserts = array(
+					'rt_page' => $r->page_id,
+					'rt_revision' => $r->page_latest,
+					'rt_type' => RevTag::getType( 'fuzzy' )
+				);
+			}
+			$db->replace( 'revtag', 'rt_type_page_revision', $inserts, __METHOD__ );
+		}
 	}
 }
+
+$maintClass = 'CreateCheckIndex';
+require_once RUN_MAINTENANCE_IF_MAIN;
