dde1ca7fb0502c3b8e589821588e27d7768809b5
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-10-11 23:52:36 +0200

    Update languageeditstats.php to use Maintenance class
    Change-Id: I33886d9987571235da2a77baca135bd0e1d57f12

--- scripts/languageeditstats.php
+++ scripts/languageeditstats.php
@@ -10,104 +10,110 @@
  * @ingroup Script Stats
  */
 
-$optionsWithArgs = array( 'top', 'days', 'bots', 'ns' );
-require __DIR__ . '/cli.inc';
-
-function showUsage() {
-	STDERR( <<<EOT
-Language statistics.
-Shows number of edits per language for all message groups.
-
-Usage: php languageeditstats.php [options...]
-
-Options:
-  --top       Show given number of language codes (default: 10)
-  --days      Calculate for given number of days (default: 7)
-  --bots      Include bot edits (default: false)
-  --ns        Comma separated list of Namespace IDs (default: all)
-
-EOT
-	);
-	exit( 1 );
-}
-
-/**
- * Process command line parameters
- */
-if ( isset( $options['help'] ) ) {
-	showUsage();
-}
-
-if ( isset( $options['days'] ) ) {
-	$hours = intval( $options['days'] ) * 24; // no day change cutoff
+// Standard boilerplate to define $IP
+if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
 } else {
-	$hours = 7 * 24;
+	$dir = __DIR__;
+	$IP = "$dir/../../..";
 }
+require_once "$IP/maintenance/Maintenance.php";
 
-if ( isset( $options['top'] ) ) {
-	$top = intval( $options['top'] );
-} else {
-	$top = 10;
-}
+class LanguageEditStats extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Script to show number of edits per language for all message groups';
+		$this->addOption(
+			'(optional) Show given number of language codes (default: 10)',
+			'top',
+			false, /*required*/
+			true /*has arg*/
+		);
+		$this->addOption(
+			'(optional) Calculate for given number of days (default: 7)',
+			'days',
+			false, /*required*/
+			true /*has arg*/
+		);
+		$this->addOption(
+			'(optional) Include bot edits',
+			'bots'
+		);
+		$this->addOption(
+			'(optional) Comma separated list of namespace IDs',
+			'ns',
+			false, /*required*/
+			true /*has arg*/
+		);
+	}
 
-if ( isset( $options['bots'] ) ) {
-	$bots = true;
-} else {
-	$bots = false;
-}
+	public function execute() {
+		$hours = $this->getOption( 'days' );
+		$hours = (int)$hours ? $hours * 7 : 7 * 24;
 
-$namespaces = array();
-if ( isset( $options['ns'] ) ) {
-	$input = explode( ',', $options['ns'] );
+		$top = $this->getOption( 'top' );
+		$top = (int)$top ? $top : 10;
 
-	foreach ( $input as $namespace ) {
-		if ( is_numeric( $namespace ) ) {
-			array_push( $namespaces, $namespace );
+		$bots = $this->hasOption( 'bots' );
+
+		$namespaces = array();
+		if ( $this->hasOption( 'ns' ) ) {
+			$input = explode( ',', $this->getOption( 'ns' ) );
+
+			foreach ( $input as $namespace ) {
+				if ( is_numeric( $namespace ) ) {
+					array_push( $namespaces, $namespace );
+				}
+			}
+		}
+
+		/**
+		 * Select set of edits to report on
+		 */
+		$rows = TranslateUtils::translationChanges( $hours, $bots, $namespaces );
+
+		/**
+		 * Get counts for edits per language code after filtering out edits by FuzzyBot
+		 */
+		$codes = array();
+		global $wgTranslateFuzzyBotName;
+		foreach ( $rows as $_ ) {
+			// Filter out edits by $wgTranslateFuzzyBotName
+			if ( $_->rc_user_text === $wgTranslateFuzzyBotName ) {
+				continue;
+			}
+
+			list( , $code ) = $this->figureMessage( $_->rc_title );
+
+			if ( !isset( $codes[$code] ) ) {
+				$codes[$code] = 0;
+			}
+
+			$codes[$code]++;
+		}
+
+		/**
+		 * Sort counts and report descending up to $top rows.
+		 */
+		arsort( $codes );
+		$i = 0;
+		foreach ( $codes as $code => $num ) {
+			if ( $i++ === $top ) {
+				break;
+			}
+
+			$this->output( "$code\t$num" );
 		}
 	}
-}
 
-function figureMessage( $text ) {
-	$pos = strrpos( $text, '/' );
-	$code = substr( $text, $pos + 1 );
-	$key = substr( $text, 0, $pos );
+	function figureMessage( $text ) {
+		$pos = strrpos( $text, '/' );
+		$code = substr( $text, $pos + 1 );
+		$key = substr( $text, 0, $pos );
 
-	return array( $key, $code );
-}
-
-/**
- * Select set of edits to report on
- */
-$rows = TranslateUtils::translationChanges( $hours, $bots, $namespaces );
-
-/**
- * Get counts for edits per language code after filtering out edits by FuzzyBot
- */
-$codes = array();
-foreach ( $rows as $_ ) {
-	// Filter out edits by $wgTranslateFuzzyBotName
-	if ( $_->rc_user_text === $wgTranslateFuzzyBotName ) {
-		continue;
+		return array( $key, $code );
 	}
-
-	list( , $code ) = figureMessage( $_->rc_title );
-
-	if ( !isset( $codes[$code] ) ) {
-		$codes[$code] = 0;
-	}
-
-	$codes[$code]++;
 }
 
-/**
- * Sort counts and report descending up to $top rows.
- */
-arsort( $codes );
-$i = 0;
-foreach ( $codes as $code => $num ) {
-	if ( $i++ === $top ) {
-		break;
-	}
-
-	STDOUT( "$code\t$num" );
-}
+$maintClass = 'LanguageEditStats';
+require_once RUN_MAINTENANCE_IF_MAIN;
