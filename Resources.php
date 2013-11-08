abf71ba41af205578cdbdfd63c8f9e46f3d210b3
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-11-08 16:58:23 +0530

    Update ULS RL module names
    Depends on Ic33ef034c794a523b5403867196c6098a58eb5b1
    
    Change-Id: I93c6a6e9c1043c1c377b440c6ede38d923b0b6bc

--- Resources.php
+++ Resources.php
@@ -229,7 +229,7 @@ $wgResourceModules['ext.translate.navitoggle'] = array(
 $wgResourceModules['ext.translate.pagetranslation.uls'] = array(
 	'scripts' => 'resources/js/ext.translate.pagetranslation.uls.js',
 	'dependencies' => array(
-		'ext.uls.init',
+		'ext.uls.mediawiki',
 		'mediawiki.Uri',
 		'mediawiki.util',
 	),
@@ -310,7 +310,7 @@ $wgResourceModules['ext.translate.special.searchtranslations'] = array(
 	'styles' => 'resources/css/ext.translate.special.searchtranslations.css',
 	'dependencies' => array(
 		'ext.translate.editor',
-		'ext.uls.init',
+		'ext.uls.mediawiki',
 		'ext.uls.geoclient',
 		'ext.translate.groupselector',
 	),
@@ -340,7 +340,7 @@ $wgResourceModules['ext.translate.special.translate'] = array(
 		'ext.translate.messagetable',
 		'ext.translate.navitoggle',
 		'ext.translate.workflowselector',
-		'ext.uls.init',
+		'ext.uls.mediawiki',
 	),
 	'messages' => array(
 		'translate-workflow-set-do',
@@ -363,7 +363,7 @@ $wgResourceModules['ext.translate.special.translatesandbox'] = array(
 	'dependencies' => array(
 		'ext.translate.translationstashstorage',
 		'mediawiki.api',
-		'jquery.uls.grid',
+		'ext.uls.mediawiki',
 		'jquery.ui.dialog',
 		'mediawiki.jqueryMsg',
 	),
