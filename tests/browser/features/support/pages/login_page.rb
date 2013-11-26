681d3ad41a7f025f4c2bc46418b6ef40d6cde3a2
mayankmadan <maddiemadan@gmail.com>
2013-11-27 00:42:00 +0530

    Prefer double-quoted strings in Ruby code
    Bug: 57597
    Change-Id: I72c4fbbfafa492ced625e1f5d87087b9ceba9d2e

--- tests/browser/features/support/pages/login_page.rb
+++ tests/browser/features/support/pages/login_page.rb
@@ -2,11 +2,11 @@ class LoginPage
 	include PageObject
 
 	include URL
-	page_url URL.url('Special:UserLogin')
+	page_url URL.url("Special:UserLogin")
 
-	text_field(:username, id: 'wpName1')
-	text_field(:password, id: 'wpPassword1')
-	button(:login, id: 'wpLoginAttempt')
+	text_field(:username, id: "wpName1")
+	text_field(:password, id: "wpPassword1")
+	button(:login, id: "wpLoginAttempt")
 
 	def login_with(username, password)
 		self.username = username
--- tests/browser/features/support/pages/stash_page.rb
+++ tests/browser/features/support/pages/stash_page.rb
@@ -2,26 +2,26 @@ class StashPage
 	include PageObject
 
 	include URL
-	page_url URL.url('Special:TranslationStash?<%=params[:extra]%>')
+	page_url URL.url("Special:TranslationStash?<%=params[:extra]%>")
 
-	a(:edit, text: 'Edit')
+	a(:edit, text: "Edit")
 
-	span(:language_selector, :class => 'uls')
-	text_field(:language_filter, :id => 'languagefilter')
+	span(:language_selector, :class => "uls")
+	text_field(:language_filter, :id => "languagefilter")
 
-	button(:skip_button, :class => 'tux-editor-skip-button')
+	button(:skip_button, :class => "tux-editor-skip-button")
 
-	span(:status_saved, :class => 'tux-status-translated')
+	span(:status_saved, :class => "tux-status-translated")
 
-	div(:translation_stats, :class => 'stash-stats')
-	div(:limit_message, :class => 'limit-reached')
+	div(:translation_stats, :class => "stash-stats")
+	div(:limit_message, :class => "limit-reached")
 
 	def make_a_translation(index = 0)
-		translation_element(index).when_present.set 'Pupu'
+		translation_element(index).when_present.set "Pupu"
 		save_button_element(index).click
 	end
 	def save_button_element(index = 0)
-		@browser.button(:class => 'tux-editor-save-button', :index => index)
+		@browser.button(:class => "tux-editor-save-button", :index => index)
 	end
 	def select_language(language)
 		self.language_selector_element.click
@@ -29,6 +29,6 @@ class StashPage
 		self.language_filter_element.send_keys :enter
 	end
 	def translation_element(index = 0)
-		@browser.text_field(:class => 'tux-textarea-translation', :index => index)
+		@browser.text_field(:class => "tux-textarea-translation", :index => index)
 	end
 end
