8a2dd13759b869b5f23957b6a65c6257a122b979
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-12-04 19:01:36 +0200

    Alphabetical order for the test elements
    Change-Id: I70970359fc988a02205ca31777dbd2cb1db58d02

--- tests/browser/features/support/pages/manage_translator_sandbox_page.rb
+++ tests/browser/features/support/pages/manage_translator_sandbox_page.rb
@@ -4,11 +4,13 @@ class ManageTranslatorSandboxPage
 	include URL
 	page_url URL.url("Special:ManageTranslatorSandbox?<%=params[:extra]%>")
 
-	text_field(:search, class: "request-filter-box")
+	div(:details, class: "details")
+
 	div(:requests_list, class: "requests-list")
 	div(:request_count, class: "request-count")
 
-	div(:details, class: "details")
+	text_field(:search, class: "request-filter-box")
+
 	div(:username) do |page|
 		page.requests_list_element.element.divs(class: "username")
 	end
