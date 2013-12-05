4dabf76cac092dd9bf76da3075f0259b58507c00
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-12-05 12:20:50 +0530

    Filter translators by language in Special:TranslatorSandbox
    Change-Id: Ib10bbe25a1cfd4d7f4fa4d71c6ece8798635d166

--- tests/browser/features/support/pages/manage_translator_sandbox_page.rb
+++ tests/browser/features/support/pages/manage_translator_sandbox_page.rb
@@ -1,3 +1,5 @@
+require "json"
+
 class ManageTranslatorSandboxPage
 	include PageObject
 
@@ -5,11 +7,18 @@ class ManageTranslatorSandboxPage
 	page_url URL.url("Special:ManageTranslatorSandbox?<%=params[:extra]%>")
 
 	div(:details, class: "details")
+	div(:details_header, class: "tsb-header")
+
+	text_field(:language_filter, id: "languagefilter")
+	button(:language_selector_button, class: "language-selector")
 
 	div(:requests_list, class: "requests-list")
 	div(:request_count, class: "request-count")
+	div(:request_footer, class: "request-footer")
 
 	text_field(:search, class: "request-filter-box")
+
+	checkbox(:select_all_checkbox, class: "request-selector-all")
 
 	div(:username) do |page|
 		page.requests_list_element.element.divs(class: "username")
@@ -17,6 +26,10 @@ class ManageTranslatorSandboxPage
 
 	def visible_users_element
 		@browser.elements(css: ".row.request:not(.hide) .username")
+	end
+
+	def visible_requests_element
+		@browser.elements(css: ".row.request:not(.hide)")
 	end
 
 	def hidden_users_element
@@ -28,5 +41,17 @@ class ManageTranslatorSandboxPage
 		visible_users_element.all? do |element|
 			element.text.match(/^#{prefix}/i)
 		end
+	end
+
+	def all_visible_requests_translate_to?(language)
+		Watir::Wait.until { hidden_users_element.size > 0 }
+		visible_requests_element.all? do |element|
+			user_data = JSON.parse(element.attribute_value("data-data"))
+			user_data["languagepreferences"]["languages"].include?(language)
+		end
+	end
+
+	def username_in_request(index)
+		visible_users_element[0].text
 	end
 end
