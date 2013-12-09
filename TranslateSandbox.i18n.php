69d5e317b867e51f0c2facf5dd89342329ba2cbf
Kartik Mistry <kartik.mistry@gmail.com>
2013-12-09 18:45:16 +0530

    Logging accepting and rejecting new users
    Change-Id: Id6533551ab5da19ae936a5536ee8879cc430c784

--- TranslateSandbox.i18n.php
+++ TranslateSandbox.i18n.php
@@ -54,6 +54,11 @@ Please come to $2 and make some more translations.',
 Our team will verify and upgrade your account soon.
 Then you will be able to translate without limits.',
 	'tsb-no-requests-from-new-users' => 'No requests from new users',
+
+	'log-name-translationsandbox' => 'Translation sandbox',
+	'log-description-translationsandbox' => 'A log of actions on translation sandbox users',
+	'logentry-translationsandbox-promoted' => '$1 {{GENDER:$2|promoted}} $3 to translator',
+	'logentry-translationsandbox-rejected' => '$1 {{GENDER:$2|rejected}} the request from $3 to become a translator',
 );
 
 /** Message documentation (Message documentation)
