190987eb495f0e1fe929a92729aa439aaab3cb72
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-12-19 16:03:39 +0100

    Sort special page names alphabetically
    Change-Id: I73fefc6a0862cd2b261c61cb3f27a85848e8345b

--- Translate.alias.php
+++ Translate.alias.php
@@ -11,23 +11,23 @@ $specialPageAliases = array();
 
 /** English (English) */
 $specialPageAliases['en'] = array(
-	'Translate' => array( 'Translate' ),
-	'Magic' => array( 'AdvancedTranslate', 'Magic' ),
-	'TranslationStats' => array( 'TranslationStats', 'TranslationStatistics' ),
-	'Translations' => array( 'Translations' ),
-	'LanguageStats' => array( 'LanguageStats' ),
-	'MessageGroupStats' => array( 'MessageGroupStats' ),
-	'PageTranslation' => array( 'PageTranslation' ),
-	'ImportTranslations' => array( 'ImportTranslations' ),
-	'ManageMessageGroups' => array( 'ManageMessageGroups' ),
-	'FirstSteps' => array( 'FirstSteps' ),
-	'SupportedLanguages' => array( 'SupportedLanguages' ),
-	'MyLanguage' => array( 'MyLanguage' ),
-	'PageTranslationDeletePage' => array( 'PageTranslationDeletePage' ),
 	'AggregateGroups' => array( 'AggregateGroups' ),
+	'FirstSteps' => array( 'FirstSteps' ),
+	'ImportTranslations' => array( 'ImportTranslations' ),
+	'LanguageStats' => array( 'LanguageStats' ),
+	'Magic' => array( 'AdvancedTranslate', 'Magic' ),
+	'ManageMessageGroups' => array( 'ManageMessageGroups' ),
+	'MessageGroupStats' => array( 'MessageGroupStats' ),
+	'MyLanguage' => array( 'MyLanguage' ),
+	'PageTranslation' => array( 'PageTranslation' ),
+	'PageTranslationDeletePage' => array( 'PageTranslationDeletePage' ),
 	'SearchTranslations' => array( 'SearchTranslations' ),
+	'SupportedLanguages' => array( 'SupportedLanguages' ),
+	'Translate' => array( 'Translate' ),
 	'TranslateSandbox' => array( 'ManageTranslatorSandbox', 'TranslateSandbox' ),
 	'TranslationStash' => array( 'TranslationStash' ),
+	'TranslationStats' => array( 'TranslationStats', 'TranslationStatistics' ),
+	'Translations' => array( 'Translations' ),
 );
 
 /** Afrikaans (Afrikaans) */
