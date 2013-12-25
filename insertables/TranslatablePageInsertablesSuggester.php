7f5690f47bdf374ac127e835f23d06320d22c969
m4tx <m4tx@m4tx.pl>
2013-12-26 00:55:36 +0100

    Fix insertables matching
    * Variable names containing hyphen aren't truncated now
    * Short number-only variable names (e.g. $1, $123) aren't matched now
    
    Bug: 58746
    Bug: 58445
    Change-Id: I292f0ba6195bd3a49408a90284e1c507910c83e5

--- insertables/TranslatablePageInsertablesSuggester.php
+++ insertables/TranslatablePageInsertablesSuggester.php
@@ -13,17 +13,21 @@ class TranslatablePageInsertablesSuggester extends MediaWikiInsertablesSuggester
 	public function getInsertables( $text ) {
 		$insertables = parent::getInsertables( $text );
 
-		// Translatable pages allow naming the variables. Basically anything is
-		// allowed in a variable name, but here we are stricter to avoid too many
-		// false positives.
+		// Translatable pages allow naming the variables. What the regex matches is:
+		// * Variable names starting with letter and then containing numbers or
+		//   hyphen (e.g. $a1, $mw-example)
+		// * Variable names starting with number and then containing letters or
+		//   hyphen (e.g. $1a, $123ex-ample)
+		// * Variable names containing only letters (e.g. $example)
+		// Variable names containing only numbers are already handled by parent.
 		$matches = array();
-		preg_match_all( '/\$([a-z0-9])+/', $text, $matches, PREG_SET_ORDER );
+		preg_match_all(
+			'/\$(([a-z]+[0-9-]+[a-z0-9-]*)|([0-9]+[a-z-]+[a-z0-9-]*)|([a-z]+))/',
+			$text,
+			$matches,
+			PREG_SET_ORDER
+		);
 		$new = array_map( function ( $match ) {
-			// Numerical ones are already handled by parent
-			if ( ctype_digit( $match[1] ) ) {
-				return null;
-			}
-
 			return new Insertable( $match[0], $match[0] );
 		}, $matches );
 
