53103248a48878ead555fcb061ca2b121ead82ba
Brion Vibber <brion@pobox.com>
2014-01-27 14:17:49 -0800

    Work in progress: AppleFFS module for iOS/Mac OS X Localizable.strings files
    Tests pass but may be incomplete; need to round-trip some real files and make sure this works.
    
    Bug: 60500
    Change-Id: I51d3b4d58b4a8e24e7e2fd19c9ca8cd9c434fd79

---
+++ ffs/AppleFFS.php
@@ -0,0 +1,228 @@
+<?php
+
+/**
+ * AppleFFS class implements support for Apple .strings files.
+ * This class reads and writes only UTF-16 files.
+ *
+ * WORK IN PROGRESS STUB NO WORKY YET
+ *
+ * @ingroup FFS
+ */
+class AppleFFS extends SimpleFFS {
+	public function supportsFuzzy() {
+		return 'write';
+	}
+
+	public function getFileExtensions() {
+		return array( '.strings' );
+	}
+
+	/**
+	 * @param $group FileBasedMessageGroup
+	 */
+	public function __construct( FileBasedMessageGroup $group ) {
+		parent::__construct( $group );
+	}
+
+	// READ
+
+	/**
+	 * @param $data array
+	 * @return array Parsed data.
+	 * @throws MWException
+	 */
+	public function readFromVariable( $data ) {
+		$data = self::fromUTF16( $data );
+		$lines = explode( "\n", $data );
+		$authors = $messages = array();
+		$linecontinuation = false;
+
+		$key = '';
+		$value = '';
+		foreach ( $lines as $line ) {
+			if ( $linecontinuation ) {
+				$linecontinuation = false;
+				$valuecont = $line;
+				$value .= $valuecont;
+			} else {
+				$i++;
+				if ( $line === '' ) {
+					continue;
+				}
+
+				if ( substr( $line, 0, 2 ) === '//' ) {
+					// Single-line comment
+					$match = array();
+					$ok = preg_match( '/\/\/\s*Author:\s*(.*)/', $line, $match );
+					if ( $ok ) {
+						$authors[] = $match[1];
+					}
+					continue;
+				}
+
+				if ( substr( $line, 0, 2 ) === '/*' ) {
+					if ( strpos( $line, '*/', 2 ) === false ) {
+						$linecontinuation = true;
+					}
+					continue;
+				}
+
+				list( $key, $value ) = self::readRow( $line );
+				$messages[$key] = $value;
+			}
+		}
+
+		$messages = $this->group->getMangler()->mangle( $messages );
+
+		return array(
+			'AUTHORS' => $authors,
+			'MESSAGES' => $messages,
+		);
+	}
+
+	/**
+	 * Parses non-empty strings file row to key and value.
+	 * @param string $line
+	 * @return array( string $key, string $val )
+	 */
+	public static function readRow( $line ) {
+		$match = array();
+		if ( preg_match( '/^"((?:\\\"|[^"])*)"\s*=\s*"((?:\\\"|[^"])*)"\s*;\s*$/', $line, $match ) ) {
+			$key = self::unescapeString( $match[1] );
+			$value = self::unescapeString( $match[2] );
+			if ( $key === '' ) {
+				throw new MWException( "Empty key in line $line" );
+			}
+			return array( $key, $value );
+		} else {
+			throw new MWException( "Unrecognized line format: $line" );
+		}
+	}
+
+	// Write
+
+	/**
+	 * @param $collection MessageCollection
+	 * @return string
+	 */
+	protected function writeReal( MessageCollection $collection ) {
+		$header = $this->doHeader( $collection );
+		$header .= $this->doAuthors( $collection );
+		$header .= "\n";
+
+		$output = '';
+		$mangler = $this->group->getMangler();
+
+		/**
+		 * @var TMessage $m
+		 */
+		foreach ( $collection as $key => $m ) {
+			$value = $m->translation();
+			$value = str_replace( TRANSLATE_FUZZY, '', $value );
+
+			if ( $value === '' ) {
+				continue;
+			}
+
+			// Just to give an overview of translation quality.
+			if ( $m->hasTag( 'fuzzy' ) ) {
+				$output .= "// Fuzzy\n";
+			}
+
+			$key = $mangler->unmangle( $key );
+			$output .= self::writeRow( $key, '=', $value );
+		}
+
+		if ( $output ) {
+			$data = $header . $output;
+		} else {
+			$data = $header;
+		}
+
+		return self::toUTF16( $data );
+	}
+
+	public static function fromUTF16( $data ) {
+		return mb_convert_encoding( $data, "UTF-8", "UTF-16LE" );
+	}
+
+	public static function toUTF16( $str ) {
+		return mb_convert_encoding( $str, "UTF-16LE", "UTF-8" );
+	}
+
+	/**
+	 * Writes well-formed properties file row with key and value.
+	 * @param string $key
+	 * @param string $value
+	 * @return string
+	 */
+	public static function writeRow( $key, $value ) {
+		return self::quoteString( $key ) . " = " . self::quoteString( $value ) . ";";
+	}
+
+	/**
+	 * Quote and escape Obj-C-style strings for .strings format
+	 */
+	protected static function quoteString( $str ) {
+		return "\"" . self::escapeString( $str ) . "\"";
+	}
+
+	/* Escape Obj-C-style strings; use backslash-escapes etc.
+	 *
+	 * @param string $str
+	 * @return string
+	 */
+	protected static function escapeString( $str ) {
+		$str = addcslashes( $str, "\\\"" );
+		$str = str_replace( "\n", '\\n', $str );
+		return $str;
+	}
+
+	/**
+	 * Unescape Obj-C-style strings; can include backslash-escapes
+	 *
+	 * @todo support \UXXXX
+	 *
+	 * @param string $str
+	 * @return string
+	 */
+	protected static function unescapeString( $str ) {
+		return stripcslashes( $str );
+	}
+
+	/**
+	 * @param $collection MessageCollection
+	 * @return string
+	 */
+	protected function doHeader( MessageCollection $collection ) {
+		if ( isset( $this->extra['header'] ) ) {
+			$output = $this->extra['header'];
+		} else {
+			global $wgSitename;
+
+			$code = $collection->code;
+			$name = TranslateUtils::getLanguageName( $code );
+			$native = TranslateUtils::getLanguageName( $code, $code );
+			$output = "// Messages for $name ($native)\n";
+			$output .= "// Exported from $wgSitename\n";
+		}
+
+		return $output;
+	}
+
+	/**
+	 * @param $collection MessageCollection
+	 * @return string
+	 */
+	protected function doAuthors( MessageCollection $collection ) {
+		$output = '';
+		$authors = $collection->getAuthors();
+		$authors = $this->filterAuthors( $authors, $collection->code );
+
+		foreach ( $authors as $author ) {
+			$output .= "// Author: $author\n";
+		}
+
+		return $output;
+	}
+}
