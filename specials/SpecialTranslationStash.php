5015d4a595dc94d3764cba22a51a077d8c96e0c7
Federico Leva <federicoleva@tiscali.it>
2014-01-28 20:53:06 +0100

    Don't try to sandbox unsandboxable users
    If a user is already past the sandbox stage, redirect them to the
    normal userrights process: they can't do anything on the main page.
    
    jgonera> Nikerabbit, I created a new account on translatewiki so that it
    has the same user name as on other wikis. I tried translating a message
    but I don't have the permissions. Unfortunately, "Get permission" link doesn't
    work. It leads to https://translatewiki.net/wiki/Special:TranslationStash
    which in turn redirects me to the main page...
    
    Change-Id: I475d99ee47c3307f1abe57f28f1d76067a15a7f8

--- specials/SpecialTranslationStash.php
+++ specials/SpecialTranslationStash.php
@@ -29,6 +29,12 @@ class SpecialTranslationStash extends TranslateSpecialPage {
 		$this->stash = new TranslationStashStorage( wfGetDB( DB_MASTER ) );
 
 		if ( !$this->hasPermissionToUse() ) {
+
+			if ( $this->getUser()->isLoggedIn() ) {
+				$out->redirect( Title::newFromText( 'Project:Translator' )->getLocalUrl() );
+				return;
+			}
+
 			$out->redirect( Title::newMainPage()->getLocalUrl() );
 
 			return;
