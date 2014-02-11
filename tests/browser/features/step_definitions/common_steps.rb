e9b36f70cc3ba5b3a5e6a1547e4c4a5d08c0b570
Zeljko Filipin <zeljko.filipin@gmail.com>
2014-02-11 15:15:26 +0100

    Moved Ruby code that multiple repositories use to mediawiki_selenium Ruby gem
    Bug: 56088
    Change-Id: Ie4bb4690c72708a1616767463ff51b26c3a9c50b

--- tests/browser/features/step_definitions/common_steps.rb
+++
@@ -1,7 +0,0 @@
-Given(/^I am logged in$/) do
-	visit(LoginPage).login_with(ENV["MEDIAWIKI_USER"], ENV["MEDIAWIKI_PASSWORD"])
-end
-
-Given(/^I have reset my preferences$/) do
-	visit(ResetPreferencesPage).submit_element.click
-end
--- tests/browser/features/support/pages/reset_preferences_page.rb
+++
@@ -1,7 +0,0 @@
-class ResetPreferencesPage
-	include PageObject
-	include URL
-	page_url URL.url("Special:Preferences/reset")
-
-	button(:submit, class: "mw-htmlform-submit")
-end
