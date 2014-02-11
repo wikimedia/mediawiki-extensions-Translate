f55755b7f60058c16be4039b57360d094f972a93
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2014-02-11 15:17:09 +0200

    Allow capital letters in insertable tvars
    Change-Id: I4d84161ef80b62ed7e23678db4e3bca3fe7f5829

--- insertables/TranslatablePageInsertablesSuggester.php
+++ insertables/TranslatablePageInsertablesSuggester.php
@@ -17,7 +17,7 @@ class TranslatablePageInsertablesSuggester extends MediaWikiInsertablesSuggester
 		// allowed in a variable name, but here we are stricter to avoid too many
 		// false positives.
 		$matches = array();
-		preg_match_all( '/\$([a-z0-9-])+/', $text, $matches, PREG_SET_ORDER );
+		preg_match_all( '/\$([a-zA-Z0-9-])+/', $text, $matches, PREG_SET_ORDER );
 		$new = array_map( function ( $match ) {
 			// Numerical ones are already handled by parent
 			if ( ctype_digit( $match[1] ) ) {
