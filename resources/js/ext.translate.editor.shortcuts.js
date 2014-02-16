b8d44c13f596c3575bfe5cbd04870528fe2172b1
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-02-16 15:48:42 +0000

    Improvements to TUX shortcuts
    Indicators for insertables were only partially visible,
    possibly due to CSS change in core that reduced margin.
    
    * Amend the message to mention the trigger
    * Position properly in RTL
    * Made the indicators more visible
    * Added indicators for up/down arrows
    * Removed special case for insertables
    * Update indicators after using a shortcut
    
    Change-Id: I16f12ea39ad16a5dfb3d4cf2fd1aeb72bee3343b

--- resources/js/ext.translate.editor.shortcuts.js
+++ resources/js/ext.translate.editor.shortcuts.js
@@ -6,34 +6,54 @@
 
 	var translateEditorShortcuts = {
 		showShortcuts: function () {
-			var editorOffset, minTop, maxTop;
+			var editorOffset, minTop, maxTop, maxLeft, middle, rtl;
+
+			// Any better way?
+			rtl = $( 'body' ).is( '.rtl' );
 
 			editorOffset = this.$editor.offset();
 			minTop = editorOffset.top;
-			maxTop = minTop + this.$editor.height();
+			maxTop = minTop + this.$editor.outerHeight();
+			middle = minTop + ( maxTop - minTop ) / 2;
+
+			maxLeft = editorOffset.left;
+			if ( !rtl ) {
+				 maxLeft += this.$editor.outerWidth();
+			}
 
 			this.hideShortcuts();
+
+			// For scrolling up and down
+			$( '<div>' )
+				.text( '↑' )
+				.offset( { top: middle - 10, left: maxLeft } )
+				.addClass( 'shortcut-popup' )
+				.appendTo( 'body' );
+
+			$( '<div>' )
+				.text( '↓' )
+				.offset( { top: middle + 10, left: maxLeft } )
+				.addClass( 'shortcut-popup' )
+				.appendTo( 'body' );
+
 			this.$editor.find( '.shortcut-activated:visible' ).each( function ( index ) {
-				var popup,
-					$this = $( this ),
+				var $this = $( this ),
 					offset = $this.offset();
 
+				if ( rtl ) {
+					offset.left += $this.outerWidth();
+				}
+
 				// Let's not have numbers appear outside the editor over other content
-				if ( offset.top + 15 > maxTop || offset.top < minTop ) {
+				if ( offset.top > maxTop || offset.top < minTop ) {
 					return;
 				}
 
-				popup = $( '<div>' )
+				$( '<div>' )
 					.text( index + 1 )
 					.offset( offset )
 					.addClass( 'shortcut-popup' )
-					.css( 'position', 'absolute' )
 					.appendTo( 'body' );
-
-				// Insertables need special positioning
-				if ( $this.is( '.insertable' ) ) {
-					popup.css( 'margin-top', '30px' );
-				}
 			} );
 		},
 
