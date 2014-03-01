97e48ba6848974cb6ff9d133398ffc4a0dd40efe
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-03-01 22:55:25 +0000

    WIP: registration process stats
    Change-Id: If176de23e2af4c109d5b15869b1383bab7b7d819

---
+++ scripts/translator-stats.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Script to gather translator stats.
+ *
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ * @file
+ */
+
+// Standard boilerplate to define $IP
+if ( getenv( 'MW_INSTALL_PATH' ) !== false ) {
+	$IP = getenv( 'MW_INSTALL_PATH' );
+} else {
+	$dir = __DIR__;
+	$IP = "$dir/../../..";
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class TS extends Maintenance {
+	public function execute() {
+		$dbr = wfGetDB( DB_SLAVE );
+		$users = $dbr->select(
+			array( 'user', 'user_groups' ),
+			array(
+				'user_name',
+				'user_registration',
+				'user_editcount',
+				'ug_group',
+			),
+			array(
+				'user_registration is not null'
+			),
+			__METHOD__,
+			array(
+				'ORDER BY' => 'user_id ASC',
+			),
+			array(
+				'user_groups' => array(
+					'LEFT JOIN',
+					array( 'user_id=ug_user', 'ug_group' => 'translator' )
+				)
+			)
+		);
+
+		echo "username\tregistration\tedit count\tis translator?\tpromoted\n";
+
+		$rejected = $dbr->select(
+			array( 'logging' ),
+			array(
+				'log_title',
+				'log_timestamp',
+			),
+			array(
+				'log_type' => 'translatorsandbox',
+				'log_action' => 'rejected',
+			),
+			__METHOD__
+		);
+
+		foreach ( $rejected as $r ) {
+			echo "{$r->log_title}\t{$r->log_timestamp}\t0\t\t\n";
+		}
+
+		foreach ( $users as $u ) {
+			$logs = $dbr->select(
+				'logging',
+				array(
+					'log_type',
+					'log_action',
+					'log_timestamp',
+					'log_params',
+				),
+				array(
+					'log_title' => $u->user_name,
+					'log_type' => array( 'rights', 'translatorsandbox' ),
+				),
+				__METHOD__,
+				array(
+					'ORDER BY' => 'log_id ASC',
+				)
+			);
+
+			$promoted = null;
+			foreach ( $logs as $log ) {
+				if ( $log->log_action === 'promoted' ) {
+					$promoted = $log->log_timestamp;
+					break;
+				} elseif ( $log->log_action === 'rights' ) {
+					@$data = unserialize( $log->log_params );
+					if ( $data === false ) {
+						$lines = explode( "\n", $log->log_params );
+						if ( strpos( $lines[1], 'translator' ) !== false ) {
+							$promoted = $log->log_timestamp;
+							break;
+						}
+					} elseif ( isset( $data['5::newgroups'] ) && in_array( 'translator', $data['5::newgroups'] ) ) {
+						$promoted = $log->log_timestamp;
+						break;
+					}
+				}
+			}
+
+			echo "{$u->user_name}\t{$u->user_registration}\t{$u->user_editcount}\t{$u->ug_group}\t{$promoted}\n";
+		}
+	}
+}
+
+$maintClass = 'TS';
+require_once RUN_MAINTENANCE_IF_MAIN;
