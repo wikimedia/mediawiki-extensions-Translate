a6db35c05c2217a52333c289294ad3db4a679397
Nemo bis <federicoleva@tiscali.it>
2014-03-16 09:56:02 +0000

    Display outdated page translations as such, without removing them
    It has always been the case in Translate and users ask for it loudly.
    This reverts commit daa576affc38dd0e9179d97b774a36df92f60edf.
    
    Bug: T60429
    Change-Id: I43aa0ec577d2fcd40418348ee430f7594935651d

--- tag/TranslateRenderJob.php
+++ tag/TranslateRenderJob.php
@@ -51,6 +51,7 @@ class TranslateRenderJob extends Job {
 
 		$group = $page->getMessageGroup();
 		$collection = $group->initCollection( $code );
+		$collection->loadTranslations( DB_MASTER );
 
 		$text = $page->getParse()->getTranslationPageText( $collection );
 
