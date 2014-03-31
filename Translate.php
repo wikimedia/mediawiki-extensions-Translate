fb0647c962617964630eec8a955c18dd39a2e975
Kartik Mistry <kartik.mistry@gmail.com>
2014-03-31 10:53:33 +0530

    Update version date
    Change-Id: I2add57e49c28ac200afe1000f00a4d9ee8a041ce

--- Translate.php
+++ Translate.php
@@ -17,7 +17,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Version number used in extension credits and in other places where needed.
  */
-define( 'TRANSLATE_VERSION', '2014-02-28' );
+define( 'TRANSLATE_VERSION', '2014-03-31' );
 
 /**
  * Extension credits properties.
