84d1159c8a7d5719460dd0b642ae8b8e1eb2cb49
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-06 19:04:30 +0200

    Remove classes used for supporting the old MediaWiki core format
    Change-Id: I2f8a7265a58c280f75071e19f513a97012fdaaf0

--- _autoload.php
+++ _autoload.php
@@ -60,8 +60,6 @@ $wgAutoloadClasses['SpecialTranslationStash'] = "$dir/specials/SpecialTranslatio
  */
 $wgAutoloadClasses['SimpleFormatReader'] = "$dir/ffs/Simple.php";
 $wgAutoloadClasses['SimpleFormatWriter'] = "$dir/ffs/Simple.php";
-$wgAutoloadClasses['WikiFormatReader'] = "$dir/ffs/Wiki.php";
-$wgAutoloadClasses['WikiFormatWriter'] = "$dir/ffs/Wiki.php";
 /**@}*/
 
 /**
@@ -240,8 +238,6 @@ $wgAutoloadClasses['ViewUntranslatedTask'] = "$dir/TranslateTasks.php";
  * @{
  */
 $wgAutoloadClasses['AggregateMessageGroup'] = "$dir/messagegroups/AggregateMessageGroup.php";
-$wgAutoloadClasses['CoreMessageGroup'] = "$dir/messagegroups/CoreMessageGroup.php";
-$wgAutoloadClasses['CoreMostUsedMessageGroup'] = "$dir/messagegroups/CoreMostUsedMessageGroup.php";
 $wgAutoloadClasses['FileBasedMessageGroup'] = "$dir/messagegroups/FileBasedMessageGroup.php";
 $wgAutoloadClasses['MediaWikiMessageGroup'] = "$dir/messagegroups/MediaWikiMessageGroup.php";
 $wgAutoloadClasses['MediaWikiExtensionMessageGroup'] =
