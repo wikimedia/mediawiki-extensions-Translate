5373026d0e028e248e6c00bd1c84e71f6bcd7345
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-16 18:29:26 +0200

    Don't die when seeing and agrregate group
    Change-Id: I6dbaf3e92155ee80dfe7ee13f7a3b48eefbab887

--- scripts/export.php
+++ scripts/export.php
@@ -128,7 +128,8 @@ class CommandlineExport extends Maintenance {
 		/** @var FileBasedMessageGroup $group */
 		foreach ( $groups as $groupId => $group ) {
 			if ( !$group instanceof FileBasedMessageGroup ) {
-				$this->error( "EE2: Unknown message group $groupId.", 1 );
+				$this->output( "EE2: Unknown message group $groupId.\n" );
+				continue;
 			}
 
 			if ( $group->isMeta() ) {
