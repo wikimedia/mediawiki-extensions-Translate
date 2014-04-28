3f8ccd27466d8addd214fce8b29a0fb3c79e4191
Kartik Mistry <kartik.mistry@gmail.com>
2014-04-28 11:19:58 +0530

    Bump version before MLEB release
    Change-Id: I438dbbec8748b79c8ef1dc06d9fdf301276b57c5

--- Translate.php
+++ Translate.php
@@ -17,7 +17,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Version number used in extension credits and in other places where needed.
  */
-define( 'TRANSLATE_VERSION', '2014-03-31' );
+define( 'TRANSLATE_VERSION', '2014-04-28' );
 
 /**
  * Extension credits properties.
