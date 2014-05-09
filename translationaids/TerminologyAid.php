fbb36eae70abd3d82b78212b7612b0a4b8b4580d
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-05-09 10:50:36 +0000

    POC for simple glossary thing
    * search for words in the Terminology: namespace
    * if found, give the user the content of Terminology:word/language
    
    Change-Id: I8db62fa2dd6893471458a32123db4c818b30f098

---
+++ translationaids/TerminologyAid.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Translation aid provider.
+ *
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ */
+
+/**
+ * XXX
+ *
+ * @ingroup TranslationAids
+ * @since 2013-XX
+ */
+class TerminologyAid extends TranslationAid {
+	public function getData() {
+		
+		$language = $this->handle->getCode();
+		
+		$db = wfGetDB( DB_MASTER );
+
+		$conds = array(
+			'page_namespace' => 1196,
+		);
+
+		$res = $db->select( 'page', 'page_title', $conds, __METHOD__);
+		$pagenames = array();
+		foreach ( $res as $row ) {
+			$pagenames[] = $row->page_title;
+		}
+
+		$contents = TranslateUtils::getContents( $pagenames, 1196 );
+		$definition = $this->getDefinition();
+		$terms = array();
+		
+		foreach ( $contents as $arr ) {
+			list( $text, $author ) = $arr;
+			if ( strpos( $definition, $text ) !== false ) {
+				$pos = strpos( $definition, $text );
+				
+				// Get translation
+				$translation = $text;
+				//$translation = $text.'/'.$language;
+				$translation = $contents[$text.'/'.$language][0];
+				if( is_null( $translation ) ) $translation = $text;
+				//if( !is_null( $contents[$text.'/'$language] ) ) $translation = $contents[$text.'/'$language][0];
+				
+				$terms[] = array(
+					'term' => $text,
+					'range' => array( $pos, $pos + strlen( $text ) ),
+					'translation' => $translation
+				);
+			}
+		}
+
+		if ( $terms === array() ) {
+			throw new TranslationHelperException( "No terms" );
+		}
+
+		$terms['**'] = 'term';
+		return $terms;
+	}
+}
