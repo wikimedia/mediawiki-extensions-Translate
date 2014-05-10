e166824e55ef4488b2df130900f0d196f3aae06f
Nemo bis <federicoleva@tiscali.it>
2014-05-10 12:13:22 +0200

    When marking for translations, if there are no differences just say so
    For now we can at least show a note before the submit button, reusing the
    information we have already collected in the process of making the page.
    
    Bug: 48672
    Change-Id: Ide1023e6868b3f455654d0e9a84de784b355778a

--- tag/SpecialPageTranslation.php
+++ tag/SpecialPageTranslation.php
@@ -574,6 +574,7 @@ class SpecialPageTranslation extends TranslateSpecialPage {
 
 		$diffOld = $this->msg( 'tpt-diff-old' )->escaped();
 		$diffNew = $this->msg( 'tpt-diff-new' )->escaped();
+		$hasChanges = false;
 
 		// Check whether page title was previously marked for translation.
 		// If the page is marked for translation the first time, default to checked.
@@ -597,12 +598,14 @@ class SpecialPageTranslation extends TranslateSpecialPage {
 			}
 
 			if ( $s->type === 'new' ) {
+				$hasChanges = true;
 				$name = $this->msg( 'tpt-section-new', $s->name )->escaped();
 			} else {
 				$name = $this->msg( 'tpt-section', $s->name )->escaped();
 			}
 
 			if ( $s->type === 'changed' ) {
+				$hasChanges = true;
 				$diff = new DifferenceEngine;
 				if ( method_exists( 'DifferenceEngine', 'setTextLanguage' ) ) {
 					$diff->setTextLanguage( $wgContLang );
@@ -641,6 +644,7 @@ class SpecialPageTranslation extends TranslateSpecialPage {
 
 		$deletedSections = $page->getParse()->getDeletedSections();
 		if ( count( $deletedSections ) ) {
+			$hasChanges = true;
 			$out->wrapWikiMsg( '==$1==', 'tpt-sections-deleted' );
 
 			/**
@@ -660,6 +664,7 @@ class SpecialPageTranslation extends TranslateSpecialPage {
 
 		// Display template changes if applicable
 		if ( $page->getMarkedTag() !== false ) {
+			$hasChanges = true;
 			$newTemplate = $page->getParse()->getTemplatePretty();
 			$oldPage = TranslatablePage::newFromRevision(
 				$page->getTitle(),
@@ -690,6 +695,10 @@ class SpecialPageTranslation extends TranslateSpecialPage {
 				$contentParams = array( 'class' => 'mw-tpt-sp-content' );
 				$out->addHTML( Xml::tags( 'div', $contentParams, $text ) );
 			}
+		}
+
+		if ( !$hasChanges ) {
+			$out->wrapWikiMsg( '<div class="successbox">$1</div>', 'tpt-mark-nochanges' );
 		}
 
 		$this->priorityLanguagesForm( $page );
