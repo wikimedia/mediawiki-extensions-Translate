5dca2088404f8ae85c23f28b34e68441bea4b715
Pratik Lahoti <pr4tiklahoti@gmail.com>
2014-05-16 19:11:50 +0530

    [WIP] First version of Special:PageMigration
    Change-Id: Ic62ff3fcf52c1862b5d9f8ec14c60e1a00a340bb

---
+++ specials/SpecialPageMigration.php
@@ -0,0 +1,24 @@
+<?php
+class SpecialPageMigration extends SpecialPage {
+	function __construct() {
+		parent::__construct( 'PageMigration' );
+	}
+
+	function execute( $par ) {
+		$request = $this->getRequest();
+		$output = $this->getOutput();
+		$this->setHeaders();
+		$output->addModules( 'ext.translate.special.pagemigration' );
+		# Get request data from, e.g.
+		$param = $request->getText( 'param' );
+		# Do stuff
+		# ...
+		$output->addHTML( "<div id='pageinput'>
+			<input type='text' id='pagename' placeholder='Enter page name'/>
+			<input type='text' id='langcode' placeholder='langcode'/>
+			<input type='submit' id='buttonImport' value='Import'/>
+			</div>");
+		$output->addHTML("<br/><div id='sourceunits'></div>
+			<div id='translationunits'></div>");
+	}
+}
