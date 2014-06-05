6bc4df9d16752b6d29b7b08584f5c01baaf0c73f
Pratik Lahoti <pr4tiklahoti@gmail.com>
2014-06-05 19:07:34 +0530

    Hover text for image icons used at Special:PageMigration
    Hover text has been added for image icons used at Special:PageMigration.
    
    Bug: 65961
    Change-Id: I2d0bfc06965dd1ae6ebd316bf406df1df585ad8e

--- resources/js/ext.translate.special.pagemigration.js
+++ resources/js/ext.translate.special.pagemigration.js
@@ -215,9 +215,12 @@
 		targetUnit = $( '<textarea>' ).addClass( 'mw-tpm-sp-unit__target five columns' )
 			.val( targetText );
 		actionUnit = $( '<div>' ).addClass( 'mw-tpm-sp-unit__actions two columns' );
-		actionUnit.append( $( '<span>' ).addClass( 'mw-tpm-sp-action mw-tpm-sp-action--delete' ),
-			$( '<span>' ).addClass( 'mw-tpm-sp-action mw-tpm-sp-action--swap' ),
-			$( '<span>' ).addClass( 'mw-tpm-sp-action mw-tpm-sp-action--add' ) );
+		actionUnit.append( $( '<span>' ).addClass( 'mw-tpm-sp-action mw-tpm-sp-action--delete' )
+				.attr( 'title', 'Delete unit' ),
+			$( '<span>' ).addClass( 'mw-tpm-sp-action mw-tpm-sp-action--swap' )
+				.attr( 'title', 'Swap with unit below' ),
+			$( '<span>' ).addClass( 'mw-tpm-sp-action mw-tpm-sp-action--add' )
+				.attr( 'title', 'Add a unit below' ) );
 		newUnit.append( sourceUnit, targetUnit, actionUnit );
 		return newUnit;
 	}
