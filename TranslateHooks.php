1b46b229ec69c5f9d093267458387a99361595cb
aude <aude.wiki@gmail.com>
2014-06-10 16:17:49 +0200

    Fix undefined index notice in Special:Search
    instead of insert after 'help' (which has been removed),
    insert before 'all'.
    
    also added test cases.
    
    Bug: 66410
    Change-Id: I17cc79cf8fd004b3d21110d685ebcba47e88b268

--- TranslateHooks.php
+++ TranslateHooks.php
@@ -364,7 +364,19 @@ class TranslateHooks {
 			'namespaces' => $wgTranslateMessageNamespaces,
 		);
 
-		$profiles = wfArrayInsertAfter( $profiles, $insert, 'help' );
+		// insert translations before 'all'
+		$index = array_search( 'all', array_keys( $profiles ) );
+
+		if ( !$index ) {
+			wfWarn( "'all' not found in $profiles" );
+			return true;
+		}
+
+		$profiles = array_merge(
+			array_slice( $profiles, 0, $index ),
+			$insert,
+			array_slice( $profiles, 0 )
+		);
 
 		return true;
 	}
