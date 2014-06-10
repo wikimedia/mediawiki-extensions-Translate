aa0c13318e568afbce747aa72226afc641cbe598
Pratik Lahoti <pr4tiklahoti@gmail.com>
2014-06-10 12:35:16 +0530

    Execute POST requests in serial manner at Special:PageMigration
    The POST requests for creating the translation pages were in a
    loop that lead to many parallel requests. They are now executed in
    a serial manner by using an ajax dispatcher which returns the list
    of promises.
    
    Change-Id: I010dcb3a8390aeb0caa513b88f796b9da5807a12

--- Resources.php
+++ Resources.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * JavaScript and CSS resource definitions.
  *
@@ -344,6 +345,7 @@ $wgResourceModules['ext.translate.special.pagemigration'] = array(
 	'dependencies' => array(
 		'mediawiki.api.edit',
 		'mediawiki.ui.button',
+		'jquery.ajaxdispatcher',
 	),
 	'messages' => array(
 		'pm-page-does-not-exist',
@@ -483,6 +485,10 @@ $wgResourceModules['ext.translate.workflowselector'] = array(
 ) + $resourcePaths;
 
 // Third party module
+$wgResourceModules['jquery.ajaxdispatcher'] = array(
+	'scripts' => 'resources/js/jquery.ajaxdispatcher.js',
+) + $resourcePaths;
+
 $wgResourceModules['jquery.autosize'] = array(
 	'scripts' => 'resources/js/jquery.autosize.js',
 ) + $resourcePaths;
