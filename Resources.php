859958133a63fec7f2c3e187d87bf9dd98118618
Pratik Lahoti <pr4tiklahoti@gmail.com>
2014-06-12 19:46:46 +0530

    Script for preparing the page for Translation
    A script which currently retrieves the current revision of a page and -
    * Adds the <languages/> bar replacing {{languages}}
    * Adds <translate> tags at the start and end of the page
    * Adds newlines before and after section headers
    * Adds <translate> tags around Files and their translatable content
    * Converts internal links to two-party form and adds the 'Special:MyLanguage/'
      prefix to links in valid namespaces
    * Saves the page with the text resulting out of the above operations
    
    Change-Id: I30dff86c2e941ee7cc3198eaa938745c2b8d80c2

--- Resources.php
+++ Resources.php
@@ -231,6 +231,13 @@ $wgResourceModules['ext.translate.navitoggle'] = array(
 	),
 ) + $resourcePaths;
 
+$wgResourceModules['ext.translate.pagepreparation'] = array(
+	'scripts' => 'resources/js/ext.translate.pagepreparation.js',
+	'dependencies' => array(
+		'mediawiki.api.edit',
+	),
+) + $resourcePaths;
+
 $wgResourceModules['ext.translate.pagetranslation.uls'] = array(
 	'scripts' => 'resources/js/ext.translate.pagetranslation.uls.js',
 	'dependencies' => array(
