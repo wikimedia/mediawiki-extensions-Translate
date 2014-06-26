4e0ef51e74a7aa364d8438a33d8d93999bb60da3
Pratik Lahoti <pr4tiklahoti@gmail.com>
2014-06-26 19:19:40 +0530

    Special page Special:PagePreparation for translate page preparation
    The script ext.translate.pagepreparation.js is now invoked on a button click
    at Special:PagePreparation by taking the page name as input from user. The diff
    between the current revision and the prepared text is shown. The user can then
    click on the 'Save' button to Save the page with the prepared text. A link to edit
    the page is provided after saving is done.
    
    Change-Id: I42a06acc6cc15c3c06bb74964a525eacca910a3b

--- TranslateHooks.php
+++ TranslateHooks.php
@@ -57,6 +57,8 @@ class TranslateHooks {
 			if ( $wgTranslatePageMigration ) {
 				$wgSpecialPages['PageMigration'] = 'SpecialPageMigration';
 				$wgSpecialPageGroups['PageMigration'] = 'wiki';
+				$wgSpecialPages['PagePreparation'] = 'SpecialPagePreparation';
+				$wgSpecialPageGroups['PagePreparation'] = 'wiki';
 			}
 
 			global $wgLogActionsHandlers, $wgLogTypes;
