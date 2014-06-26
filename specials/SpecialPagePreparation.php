7706ab3577de63c5139b639b62a40add52a278ce
Pratik Lahoti <pr4tiklahoti@gmail.com>
2014-06-26 19:19:40 +0530

    Special page Special:PagePreparation for translate page preparation
    The script ext.translate.pagepreparation.js is now invoked on a button click
    at Special:PagePreparation by taking the page name as input from user. The diff
    between the current revision and the prepared text is shown. The user can then
    click on the 'Save' button to Save the page with the prepared text. A link to edit
    the page is provided after saving is done.
    
    Change-Id: I42a06acc6cc15c3c06bb74964a525eacca910a3b

---
+++ specials/SpecialPagePreparation.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Contains code for special page Special:PagePreparation
+ *
+ * @file
+ * @author Pratik Lahoti
+ * @copyright Copyright © 2014-2015 Pratik Lahoti
+ * @license GPL-2.0+
+ */
+
+class SpecialPagePreparation extends SpecialPage {
+	function __construct() {
+		parent::__construct( 'PagePreparation', 'pagetranslation' );
+	}
+
+	function execute( $par ) {
+		$request = $this->getRequest();
+		$output = $this->getOutput();
+		$this->setHeaders();
+		$this->checkPermissions();
+		$output->addModules( 'ext.translate.pagepreparation' );
+		$output->addModuleStyles( 'jquery.uls.grid' );
+		$param = $request->getText( 'param' );
+
+		$out = '';
+		$out = <<<HTML
+<div class="grid">
+	<form class="mw-tpp-sp-form row" id="mw-tpp-sp-primary-form" name="mw-tpp-sp-input-form">
+		<input name="title" id="title" class="mw-searchInput"
+			placeholder= "{$this->msg( 'pp-pagename-placeholder' )->text()}" />
+		<input id="action-prepare" class="mw-ui-button mw-ui-primary" type="button"
+			value="{$this->msg( 'pp-prepare-button-label' )->text()}" />
+		<input id="action-save" class="mw-ui-button mw-ui-constructive hide" type="button"
+			value="{$this->msg( 'pp-save-button-label' )->text()}" />
+	</form>
+	<div class="messageDiv hide"></div>
+	<div class="divDiff hide">
+		<table class="diff diff-contentalign-left">
+			<colgroup>
+				<col class="diff-marker"/>
+				<col class="diff-content"/>
+				<col class="diff-marker"/>
+				<col class="diff-content"/>
+			</colgroup>
+			<tbody id="diff-body"></tbody>
+		</table>
+	</div>
+</div>
+HTML;
+		$output->addHTML( $out );
+	}
+}
