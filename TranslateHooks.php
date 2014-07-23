70667b6031887c11a12cc2119c57422d560a36e4
Max Semenik <maxsem.wiki@gmail.com>
2014-07-23 10:58:15 -0700

    Move LinkBegin hook handler around
    In preparation to Special:MyLanguage integration to core
    (I520077c931431b5919e0208f75c20b5b25f3159d), make sure that
    hook handler stays in place even when the page class is overridden
    by core.
    
    Change-Id: Id28c7659e7273c29d7e57a6d0c2c4e68c8582095

--- TranslateHooks.php
+++ TranslateHooks.php
@@ -539,4 +539,41 @@ class TranslateHooks {
 
 		return true;
 	}
+
+	/**
+	 * Make Special:MyLanguage links red if the target page doesn't exists.
+	 * A bit hacky because the core code is not so flexible.
+	 * @param $dummy
+	 * @param $target Title
+	 * @param $html
+	 * @param $customAttribs
+	 * @param $query
+	 * @param $options
+	 * @param $ret
+	 * @return bool
+	 */
+	public static function linkfix( $dummy, $target, &$html, &$customAttribs,
+									&$query, &$options, &$ret
+	) {
+		if ( $target->getNamespace() == NS_SPECIAL ) {
+			list( $name, $subpage ) = SpecialPageFactory::resolveAlias( $target->getDBkey() );
+			if ( $name === 'MyLanguage' ) {
+				$realTarget = Title::newFromText( $subpage );
+				if ( !$realTarget || !$realTarget->exists() ) {
+					$options[] = 'broken';
+					$index = array_search( 'known', $options, true );
+					if ( $index !== false ) {
+						unset( $options[$index] );
+					}
+
+					$index = array_search( 'noclasses', $options, true );
+					if ( $index !== false ) {
+						unset( $options[$index] );
+					}
+				}
+			}
+		}
+
+		return true;
+	}
 }
