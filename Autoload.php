a6bd8d335e2584e133c4d2a25408822c5e9b4688
Kunal Grover <kunalgrover05@gmail.com>
2014-07-26 08:55:44 +0530

    [WIP] Language bar first design
    Done:
    Initial design and implementation
    Using available and priority languages for selection
    Removed translation page header hooks
    Added Special:Translate links to pages for all languages
    
    To do:
    Better RTL support
    
    Bug: 51533
    Change-Id: I978ce3a9aa78d5ed0531d5881b991c12af72fdd4

--- Autoload.php
+++ Autoload.php
@@ -105,6 +105,7 @@ $al['TranslationStats'] = "$dir/utils/TranslationStats.php";
 $al['TranslationStatsBase'] = "$dir/specials/SpecialTranslationStats.php";
 $al['TranslationStatsInterface'] = "$dir/specials/SpecialTranslationStats.php";
 $al['TuxMessageTable'] = "$dir/utils/TuxMessageTable.php";
+$al['LangPopulate'] = "$dir/utils/LangPopulate.php";
 /**@}*/
 
 /**
