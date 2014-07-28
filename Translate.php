3a20ec655af97de06b63a80a95e76c83f53c14e7
Kartik Mistry <kartik.mistry@gmail.com>
2014-07-28 15:33:26 +0000

    Update version for MLEB 2014.07 release
    Change-Id: Ie8bc694093c7c9b122b23a69c17f992e61d7084e

--- Translate.php
+++ Translate.php
@@ -17,7 +17,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Version number used in extension credits and in other places where needed.
  */
-define( 'TRANSLATE_VERSION', '2014-06-26' );
+define( 'TRANSLATE_VERSION', '2014-07-28' );
 
 /**
  * Extension credits properties.
