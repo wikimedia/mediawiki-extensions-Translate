919542ed19f8503ba01f2c367c12411db3b98733
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-08-02 12:12:26 +0200

    Split StringMatcher to separate files in own directory
    First step of refactoring this part of code
    
    Bug: 57348
    Change-Id: Iac7c7b829c39e64d35fb10bfcd421aba67a61d44

---
+++ stringmangler/StringMangler.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * StringMangler interface.
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ */
+
+/**
+ * Interface that key-mangling classes must implement. Mangling is done to:
+ * - converting characters which would be invalid in titles to something valid
+ * - prefixing a set of messages to avoid conflicts when sharing a namespace
+ *   with multiple message groups.
+ *
+ * The operations have to be reversible so that
+ * x equals unMangle( mangle( x ) ).
+ *
+ */
+interface StringMangler {
+	/// @todo Does this really need to be in the interface???
+	public static function EmptyMatcher();
+
+	/**
+	 * General way to pass configuration to the mangler.
+	 * @param array $configuration
+	 */
+	public function setConf( $configuration );
+
+	/**
+	 * Match strings against a pattern.
+	 * If string matches, mangle() should mangle the key.
+	 * @param string $string Message key.
+	 * @return \bool
+	 */
+	public function match( $string );
+
+	/**
+	 * Mangles a list of message keys.
+	 * @param string|string[] $data Unmangled message keys.
+	 * @return string|string[] Mangled message keys.
+	 */
+	public function mangle( $data );
+
+	/**
+	 * Reverses the operation mangle() did.
+	 * @param string|string[] $data Mangled message keys.
+	 * @return string|string[] Umangled message keys.
+	 */
+	public function unMangle( $data );
+}
