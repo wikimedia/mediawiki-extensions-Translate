bf9e1340fe01e508e940cdc1389c815ecacd3038
Kunal Grover <kunalgrover05@gmail.com>
2014-08-05 20:40:15 +0530

    Update translation pages upon deleting translation unit
    Bug: T41415
    Change-Id: Ibd910a16ba3d7cc217ad6a3fc07ab648a25599bd

--- TranslateHooks.php
+++ TranslateHooks.php
@@ -164,6 +164,9 @@ class TranslateHooks {
 
 			// Update translated page when translation unit is moved
 			$wgHooks['TitleMoveComplete'][] = 'PageTranslationHooks::onMoveTranslationUnits';
+
+			// Update translated page when translation unit is deleted
+			$wgHooks['ArticleDeleteComplete'][] = 'PageTranslationHooks::onDeleteTranslationUnits';
 		}
 	}
 
