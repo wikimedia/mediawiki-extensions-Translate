4866487b07a0f242616bf6a9602d4059fa1cb780
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-08-15 11:59:51 +0200

    Show an alert to the user if session has expired
    Not pretty or user friendly at the moment. This can be
    improved in follow ups.
    
    Bug: 69314
    Change-Id: I077a0bd7e81d049e90b4d888b7438a90742c655d

--- resources/js/ext.translate.editor.js
+++ resources/js/ext.translate.editor.js
@@ -211,6 +211,9 @@
 				translateEditor.onSaveSuccess();
 			} ).fail( function ( errorCode, response ) {
 				translateEditor.onSaveFail( response.error && response.error.info || 'Unknown error' );
+				if ( errorCode === 'assertuserfailed' ) {
+					window.alert( mw.msg( 'tux-session-expired' ) );
+				}
 			} );
 		},
 
