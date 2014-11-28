d133ee3d623d29cf743faee04986a5c4ca394914
Kartik Mistry <kartik.mistry@gmail.com>
2014-11-28 21:35:41 +0530

    Update version before MLEB release
    Change-Id: If16619829401c018a786226bb4a30f00f4dcb23d

--- Translate.php
+++ Translate.php
@@ -17,7 +17,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 /**
  * Version number used in extension credits and in other places where needed.
  */
-define( 'TRANSLATE_VERSION', '2014-10-31' );
+define( 'TRANSLATE_VERSION', '2014-11-28' );
 
 /**
  * Extension credits properties.
