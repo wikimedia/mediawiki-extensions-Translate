1a3b51be1f7ced5dfbca99a2f9a7a53c0d3149c1
Noah Manneschmidt <nmanneschmidt@curse.com>
2015-01-14 18:28:12 -0600

    Prevent patrol footer from appearing on translation pages
    Hooks getUserPermissionsErrors to return false when action is 'patrol'.
    
    Bug: T44162
    Change-Id: Ia1ccd346acf92a129b7d094b3085ea45ff321bbf

--- tag/PageTranslationHooks.php
+++ tag/PageTranslationHooks.php
@@ -595,6 +595,32 @@ class PageTranslationHooks {
 	}
 
 	/**
+	 * Prevent patrol links from appearing on translation pages.
+	 * Hook: getUserPermissionsErrors
+	 *
+	 * @param Title $title
+	 * @param User $user
+	 * @param string $action
+	 * @param $result
+	 *
+	 * @return bool
+	 */
+	public static function preventPatrolling( Title $title, User $user, $action, &$result ) {
+		if ( $page !== 'patrol') {
+			return true;
+		}
+
+		$page = TranslatablePage::isTranslationPage( $title );
+
+		if ( $page !== false && $action === 'patrol' ) {
+			$result[] = 'tpt-patrolling-blocked';
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
 	 * Redirects the delete action to our own for translatable pages.
 	 * Hook: ArticleConfirmDelete
 	 *
