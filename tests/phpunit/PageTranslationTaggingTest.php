628af3e5048f29aad7920a0d94220ed8df35e116
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-04-25 22:39:43 +0200

    Refactor MessageGroups and MessageIndex for test friendliness
    Allow using in-memory caches instead of using database. Introduced
    HashMessageIndex and MessageGroups::setCache.
    
    MessageGroups::recache made non-static. New function so no external
    uses yet. The static methods everywhere are still problematic, but
    out of scope of this patch. Made some protected functions non-static
    in MessageGroups though. In future, all of those should be made
    non-static so that they can easily be replaced during testing.
    
    MessageGroups::loadGroupDefinitions simplified. Moved all loaders
    to separate methods called via the global hook. This way all those
    can easily be skipped in tests. $wgTranlateCC will start throwing
    deprecation notices in a future patch.
    
    Remove @copyright in files I touched. Redundant with @author and
    git history can be used to check years.
    
    Change-Id: I8de682d65373fd26665612c4a51323c41812cab8

--- tests/phpunit/PageTranslationTaggingTest.php
+++ tests/phpunit/PageTranslationTaggingTest.php
@@ -11,17 +11,18 @@ class PageTranslationTaggingText extends MediaWikiTestCase {
 		global $wgHooks;
 		$this->setMwGlobals( array(
 			'wgHooks' => $wgHooks,
-			'wgTranslateCC' => array(),
-			'wgTranslateMessageIndex' => array( 'DatabaseMessageIndex' ),
-			'wgTranslateWorkflowStates' => false,
 			'wgEnablePageTranslation' => true,
-			'wgTranslateGroupFiles' => array(),
 			'wgTranslateTranslationServices' => array(),
 		) );
 		TranslateHooks::setupTranslate();
-		$wgHooks['TranslatePostInitGroups'] = array();
-		MessageGroups::clearCache();
-		MessageIndexRebuildJob::newJob()->run();
+		$wgHooks['TranslatePostInitGroups'] = array( 'MessageGroups::getTranslatablePages' );
+
+		$mg = MessageGroups::singleton();
+		$mg->setCache( wfGetCache( 'hash' ) );
+		$mg->recache();
+
+		MessageIndex::setInstance( new HashMessageIndex() );
+		MessageIndex::singleton()->rebuild();
 	}
 
 	public function testNormalPage() {
@@ -108,7 +109,7 @@ class PageTranslationTaggingText extends MediaWikiTestCase {
 		$revision = $status->value['revision']->getId();
 		$translatablePage = TranslatablePage::newFromRevision( $title, $revision );
 		$translatablePage->addMarkedTag( $revision );
-		MessageGroups::clearCache();
+		MessageGroups::singleton()->recache();
 
 		$translationPage = Title::newFromText( 'Translatable page/fi' );
 		TranslateRenderJob::newJob( $translationPage )->run();
--- tests/phpunit/TranslationFuzzyUpdaterTest.php
+++ tests/phpunit/TranslationFuzzyUpdaterTest.php
@@ -3,7 +3,6 @@
  * Tests for fuzzy flag change on edits.
  * @author Niklas Laxström
  * @file
- * @copyright Copyright © 2012-2013, Niklas Laxström
  * @license GPL-2.0+
  */
 
@@ -19,16 +18,17 @@ class TranslationFuzzyUpdaterTest extends MediaWikiTestCase {
 		global $wgHooks;
 		$this->setMwGlobals( array(
 			'wgHooks' => $wgHooks,
-			'wgTranslateCC' => array(),
-			'wgTranslateMessageIndex' => array( 'DatabaseMessageIndex' ),
-			'wgTranslateWorkflowStates' => false,
-			'wgTranslateGroupFiles' => array(),
 			'wgTranslateTranslationServices' => array(),
 			'wgTranslateMessageNamespaces' => array( NS_MEDIAWIKI ),
 		) );
 		$wgHooks['TranslatePostInitGroups'] = array( array( $this, 'getTestGroups' ) );
-		MessageGroups::clearCache();
-		MessageIndexRebuildJob::newJob()->run();
+
+		$mg = MessageGroups::singleton();
+		$mg->setCache( wfGetCache( 'hash' ) );
+		$mg->recache();
+
+		MessageIndex::setInstance( new HashMessageIndex() );
+		MessageIndex::singleton()->rebuild();
 	}
 
 	public function getTestGroups( &$list ) {
