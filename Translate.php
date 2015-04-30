4166f720145ab579cbc318c7deea940abd5943e2
Kartik Mistry <kartik.mistry@gmail.com>
2015-04-30 21:25:38 +0530

    MediaWiki language extension bundle 2015.04

--- Translate.php
+++ Translate.php
@@ -25,7 +25,7 @@ define( 'TRANSLATE_VERSION', '2015-04-29' );
 $GLOBALS['wgExtensionCredits']['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'Translate',
-	'version' => TRANSLATE_VERSION,
+	'version' => '[https://www.mediawiki.org/wiki/MLEB MLEB 2015.04]',
 	'author' => array(
 		'Niklas Laxström',
 		'Santhosh Thottingal',
