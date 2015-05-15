38d43fec9b514bb23b34910e6c33c73a8e9c72c8
Divya <divyalife526@gmail.com>
2015-05-15 12:06:00 -0400

    Support for search operators.
    Use search operators to get more specific search results.
    
    Examples:
    wiki language:hi
    wiki language:hi group:mediawiki
    
    Bug: T97943
    Change-Id: I861934132cabce80cef6d69fb8f8c61057439662

--- specials/SpecialSearchTranslations.php
+++ specials/SpecialSearchTranslations.php
@@ -64,6 +64,7 @@ class SpecialSearchTranslations extends SpecialPage {
 
 		$this->opts = $opts = new FormOptions();
 		$opts->add( 'query', '' );
+		$opts->add( 'searchstring', '' );
 		$opts->add( 'language', '' );
 		$opts->add( 'group', '' );
 		$opts->add( 'grouppath', '' );
@@ -72,7 +73,9 @@ class SpecialSearchTranslations extends SpecialPage {
 
 		$opts->fetchValuesFromRequest( $this->getRequest() );
 
-		$queryString = $opts->getValue( 'query' );
+		$query = $opts->getValue( 'query' );
+		$searchstring = $opts->getValue( 'searchstring' );
+		$queryString = ( $searchstring !== '' ) ? $searchstring : $query;
 
 		if ( $queryString === '' ) {
 			$this->showEmptySearch();
@@ -213,7 +216,7 @@ class SpecialSearchTranslations extends SpecialPage {
 			"$prev $next"
 		);
 
-		$search = $this->getSearchInput( $queryString );
+		$search = $this->getSearchInput( $query );
 		$count = $this->msg( 'tux-sst-count' )->numParams( $total );
 
 		$this->showSearch( $search, $count, $facetHtml, $resultsHtml );
@@ -344,7 +347,7 @@ HTML
 		$lang = $this->getRequest()->getVal( 'language' );
 		$language = is_null( $lang ) ? '' : Html::hidden( 'language', $lang );
 
-		$form = Html::rawElement( 'form', array( 'action' => wfScript() ),
+		$form = Html::rawElement( 'form', array( 'action' => wfScript(), 'name' => 'searchform' ),
 			$title . $input . $submit . $language
 		);
 
