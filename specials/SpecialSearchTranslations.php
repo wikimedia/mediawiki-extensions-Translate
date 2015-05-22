5a753d0dcad962f7f96f10356322daf2da070596
Divya <divyalife526@gmail.com>
2015-05-22 18:54:23 -0400

    Allow case sensitive search
    Added checkbox to enable case sensitive search
    Used Multi-field mapping to map 'content' field, once when it's analyzed
    using Standard analyzer and once when it's analyzed using
    analyzer similar to Standard analyzer with no lowercase and stop token filter.
    
    Run "php scripts/ttmserver-export.php --reindex" to update index mapping.
    
    Can be tested here: http://translation-search.wmflabs.org/w/index.php?title=Special%3ASearchTranslations&query=wiki
    
    Bug: T100013
    Change-Id: I07ffa2f6eff2b905caaccca781fe438d91da65d7

--- specials/SpecialSearchTranslations.php
+++ specials/SpecialSearchTranslations.php
@@ -72,6 +72,7 @@ class SpecialSearchTranslations extends SpecialPage {
 		$opts->add( 'grouppath', '' );
 		$opts->add( 'filter', '' );
 		$opts->add( 'match', '' );
+		$opts->add( 'case', '' );
 		$opts->add( 'limit', $this->limit );
 		$opts->add( 'offset', 0 );
 
@@ -500,11 +501,20 @@ HTML
 		$title = Html::hidden( 'title', $this->getPageTitle()->getPrefixedText() );
 		$input = Xml::input( 'query', false, $query, $attribs );
 		$submit = Xml::submitButton( $this->msg( 'tux-sst-search' ), array( 'class' => 'button' ) );
+		$chkBox= Xml::check( 'case', $this->opts->getValue( 'case' ) );
+		$chkBox = Html::openElement( 'div', array( 'class' => 'search-operators' ) ) .
+			$chkBox .
+			Html::element(
+				'label',
+				array( 'for' => 'case-sensitive' ),
+				$this->msg( 'tux-sst-case-sensitive' )
+			) .
+			Html::closeElement( 'div' );
 		$lang = $this->getRequest()->getVal( 'language' );
 		$language = is_null( $lang ) ? '' : Html::hidden( 'language', $lang );
 
 		$form = Html::rawElement( 'form', array( 'action' => wfScript() ),
-			$title . $input . $submit . $language
+			$title . $input . $submit . $chkBox . $language
 		);
 
 		return $form;
