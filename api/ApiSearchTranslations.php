6af5ab3aecf3752a0fb24f71efb7f1a7e2405907
Divya <divyalife526@gmail.com>
2015-06-22 12:32:36 +0530

    Search translations using API
    Bug: T100176
    Change-Id: I1f4dc02911d0bf2b63e2f4c0bec141422765c01d

---
+++ api/ApiSearchTranslations.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * API module for search translations
+ * @since 2015.06
+ */
+class ApiSearchTranslations extends ApiBase {
+
+	public function execute() {
+		global $wgTranslateTranslationServices;
+		$params = $this->extractRequestParams();
+
+		$config = $wgTranslateTranslationServices[$params['service']];
+		$server = TTMServer::factory( $config );
+
+		$opts = new FormOptions();
+		foreach ( $params as $param => $value ) {
+			$opts->add( $param, $value );
+		}
+
+		$searchResults = $server->search(
+			$params['query'],
+			$opts,
+			array( '', '' )
+		);
+
+		$result = $this->getResult();
+		$documents = $server->getDocuments( $searchResults );
+
+		$result->addValue( 'search', 'translations', $documents );
+	}
+
+	protected function getAvailableTranslationServices() {
+		global $wgTranslateTranslationServices;
+
+		$good = array();
+		foreach ( $wgTranslateTranslationServices as $id => $config ) {
+			if ( isset( $config['public'] ) && $config['public'] === true ) {
+				$good[] = $id;
+			}
+		}
+
+		return $good;
+	}
+
+	public function getAllowedParams() {
+		$available = $this->getAvailableTranslationServices();
+
+		return array(
+			'service' => array(
+				ApiBase::PARAM_TYPE => $available,
+				ApiBase::PARAM_DFLT => 'TTMServer',
+			),
+			'query' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'language' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_DFLT => '',
+			),
+			'group' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_DFLT => '',
+			),
+			'offset' => array(
+				ApiBase::PARAM_TYPE => 'integer',
+				ApiBase::PARAM_DFLT => 0,
+			),
+			'limit' => array(
+				ApiBase::PARAM_DFLT => 25,
+				ApiBase::PARAM_TYPE => 'integer',
+				ApiBase::PARAM_MIN => 1,
+				ApiBase::PARAM_MAX => ApiBase::LIMIT_SML1,
+				ApiBase::PARAM_MAX2 => ApiBase::LIMIT_SML2
+			),
+		);
+	}
+
+	// Get examples
+	public function getExamples() {
+		return array(
+			'api.php?action=searchtranslations&language=fr&query=Help'
+				=> 'apihelp-searchtranslations-example-1',
+		);
+	}
+}
