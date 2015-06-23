f9f320a26b6b6c1d05e93f03bc3915ac1bc2d6d6
Divya <divyalife526@gmail.com>
2015-06-23 15:18:31 +0530

    Allow wildcard strings search
    Search 'inter*', it returns all the messages containing
    string starts with 'inter' like interwiki, internal, internally.
    Search 'inter*wiki*', there is no support for * in the middle
    of search strings and ignores all the letters after the first * found,
    therefore, it is equivalent to search 'inter*'.
    
    Used Edge-n-grams only at the index time and standard analyzer at
    the search time. For search string like 'Returns thumb*', it returns
    all the messages containing returns or thumb*.
    
    Provides highlighter for prefix matching.
    
    Bug: T100345
    Change-Id: I309d6d7d717c9d2e09c7411b2c098ff3ab59e016

--- ttmserver/ElasticSearchTTMServer.php
+++ ttmserver/ElasticSearchTTMServer.php
@@ -292,12 +292,28 @@ GROOVY;
 	public function createIndex( $rebuild ) {
 		$type = $this->getType();
 		$type->getIndex()->create(
-				array(
-					'number_of_shards' => $this->getShardCount(),
-					'number_of_replicas' => $this->getReplicaCount(),
-				),
-				$rebuild
-			);
+			array(
+				'number_of_shards' => $this->getShardCount(),
+				'number_of_replicas' => $this->getReplicaCount(),
+				'analysis' => array(
+					'filter' => array(
+						'prefix_filter' => array(
+							'type' => 'edge_ngram',
+							'min_gram'=> 2,
+							'max_gram'=> 20
+						)
+					),
+					'analyzer' => array(
+						'prefix' => array(
+							'type' => 'custom',
+							'tokenizer' => 'standard',
+							'filter' => array( 'standard', 'lowercase', 'prefix_filter' )
+						)
+					)
+				)
+			),
+			$rebuild
+		);
 	}
 
 	public function beginBootstrap() {
@@ -325,7 +341,22 @@ GROOVY;
 			'uri'      => array( 'type' => 'string', 'index' => 'not_analyzed' ),
 			'language' => array( 'type' => 'string', 'index' => 'not_analyzed' ),
 			'group'    => array( 'type' => 'string', 'index' => 'not_analyzed' ),
-			'content'  => array( 'type' => 'string', 'index' => 'analyzed', 'term_vector' => 'yes' ),
+			'content'  => array(
+				'type' => 'string',
+				'fields' => array(
+					'content' => array(
+						'type' => 'string',
+						'index' => 'analyzed',
+						'term_vector' => 'yes'
+					),
+					'prefix_complete' => array(
+						'type' => 'string',
+						'index_analyzer' => 'prefix',
+						'search_analyzer' => 'standard',
+						'term_vector' => 'yes'
+					)
+				)
+			),
 		) );
 		$mapping->send();
 
@@ -459,20 +490,53 @@ GROOVY;
 		$this->updateMapping = true;
 	}
 
+	// Parse query string and build the search query
+	protected function parseQueryString( $queryString ) {
+		$fields = $highlights = array();
+		$terms = preg_split( '/\s+/', $queryString );
+
+		// Map each word in the query string with its corresponding field
+		foreach ( $terms as $term ) {
+			$prefix = strstr( $term, '*', true );
+			// For wildcard search
+			if ( $prefix ) {
+				$fields['content.prefix_complete'][] = $prefix;
+			} else {
+				$fields['content'][] = $term;
+			}
+		}
+
+		// Allow searching either by message content or message id (page name
+		// without language subpage) with exact match only.
+		$searchQuery = new \Elastica\Query\Bool();
+		foreach ( $fields as $analyzer => $words ) {
+			foreach ( $words as $word ) {
+				$boolQuery = new \Elastica\Query\Bool();
+				$contentQuery = new \Elastica\Query\Match();
+				$contentQuery->setFieldQuery( $analyzer, $word );
+				$boolQuery->addShould( $contentQuery );
+				$messageQuery = new \Elastica\Query\Term();
+				$messageQuery->setTerm( 'localid', $word );
+				$boolQuery->addShould( $messageQuery );
+				$searchQuery->addShould( $boolQuery );
+
+				// Fields for highlighting
+				$highlights[$analyzer] =  array(
+					'number_of_fragments' => 0
+				);
+			}
+		}
+
+		return array( $searchQuery, $highlights );
+	}
+
 	// Search interface
 	public function search( $queryString, $opts, $highlight ) {
 		$query = new \Elastica\Query();
 
-		// Allow searching either by message content or message id (page name
-		// without language subpage) with exact match only.
-		$serchQuery = new \Elastica\Query\Bool();
-		$contentQuery = new \Elastica\Query\Match();
-		$contentQuery->setFieldQuery( 'content', $queryString );
-		$serchQuery->addShould( $contentQuery );
-		$messageQuery = new \Elastica\Query\Term();
-		$messageQuery->setTerm( 'localid', $queryString );
-		$serchQuery->addShould( $messageQuery );
-		$query->setQuery( $serchQuery );
+		$this->highlight = $highlight;
+		list( $searchQuery, $highlights ) = $this->parseQueryString( $queryString );
+		$query->setQuery( $searchQuery );
 
 		$language = new \Elastica\Facet\Terms( 'language' );
 		$language->setField( 'language' );
@@ -517,13 +581,9 @@ GROOVY;
 		list( $pre, $post ) = $highlight;
 		$query->setHighlight( array(
 			// The value must be an object
-			'fields' => array(
-				'content' => array(
-					'number_of_fragments' => 0,
-				),
-			),
 			'pre_tags' => array( $pre ),
 			'post_tags' => array( $post ),
+			'fields' => $highlights,
 		) );
 
 		try {
@@ -559,7 +619,9 @@ GROOVY;
 		foreach ( $resultset->getResults() as $document ) {
 			$data = $document->getData();
 			$hl = $document->getHighlights();
-			if ( isset( $hl['content'][0] ) ) {
+			if ( isset( $hl['content.prefix_complete'][0] ) ) {
+				$data['content'] = $hl['content.prefix_complete'][0];
+			} elseif ( isset( $hl['content'][0] ) ) {
 				$data['content'] = $hl['content'][0];
 			}
 			$ret[] = $data;
