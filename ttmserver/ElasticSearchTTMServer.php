a3f259aa8b001547c7938ab12ecb2ae91d27e34d
Divya <divyalife526@gmail.com>
2015-06-24 17:53:58 +0530

    Get untranslated messages in the target language using MessageCollection.
    Steps:
    1. Used filtered query to search for a string in a source language.
    Suppose source language is finnish, search for 'Aloita', it returns all
    finnish messages with 'Aloita'.(Default source language is the wiki's content language)
    2. Create message definitions for all message keys found in step 1.
    3. Filter to get all the untranslated messages in the selected target language.
    By default the target language is the interface language.
    
    Use parameter 'filter=untranslated' to get the untranslated messages.
    
    Bug: T101220
    Change-Id: I3b24a6170a1b27f5e7b8ae5828afd32d85bfeb2b
    Followup-id: I246aeea623c1d81f5ce148185be8c9c9ef8f0923

--- ttmserver/ElasticSearchTTMServer.php
+++ ttmserver/ElasticSearchTTMServer.php
@@ -472,7 +472,30 @@ GROOVY;
 		$messageQuery = new \Elastica\Query\Term();
 		$messageQuery->setTerm( 'localid', $queryString );
 		$serchQuery->addShould( $messageQuery );
-		$query->setQuery( $serchQuery );
+
+		if ( $opts->getValue( 'filter' ) === 'untranslated' ) {
+			$filteredQuery = new \Elastica\Query\Filtered();
+			$filterbool = new \Elastica\Filter\Bool();
+
+			if ( $opts->getValue( 'sourcelanguage' ) !== ''
+				&& $opts->getValue( 'sourcelanguage' ) !== null
+			) {
+				$sourcelanguage = $opts->getValue( 'sourcelanguage' );
+			} else {
+				$sourcelanguage = $wgLanguageCode;
+			}
+
+			$languageFilter = new \Elastica\Filter\Term();
+			$languageFilter->setTerm( 'language', $sourcelanguage );
+			$filterbool->addMust( $languageFilter );
+
+			$filteredQuery->setFilter( $filterbool );
+			$filteredQuery->setQuery( $serchQuery );
+
+			$query->setQuery( $filteredQuery );
+		} else {
+			$query->setQuery( $serchQuery );
+		}
 
 		$language = new \Elastica\Facet\Terms( 'language' );
 		$language->setField( 'language' );
@@ -495,7 +518,12 @@ GROOVY;
 		// of the subfilters are reused. May not make a difference in this context.
 		$filters = new \Elastica\Filter\Bool();
 
-		$language = $opts->getValue( 'language' );
+		if ( $opts->getValue( 'filter' ) === 'untranslated' ) {
+			$language = '';
+		} else {
+			$language = $opts->getValue( 'language' );
+		}
+
 		if ( $language !== '' ) {
 			$languageFilter = new \Elastica\Filter\Term();
 			$languageFilter->setTerm( 'language', $language );
@@ -526,11 +554,21 @@ GROOVY;
 			'post_tags' => array( $post ),
 		) );
 
-		try {
-			return $this->getType()->getIndex()->search( $query );
-		} catch ( \Elastica\Exception\ExceptionInterface $e ) {
-			throw new TTMServerException( $e->getMessage() );
-		}
+		do {
+			try {
+				$resultset = $this->getType()->getIndex()->search( $query );
+			} catch ( \Elastica\Exception\ExceptionInterface $e ) {
+				throw new TTMServerException( $e->getMessage() );
+			}
+			$query->setFrom( 0 );
+			$size = $query->getParam( 'size' );
+			$query->setSize( $resultset->getTotalHits() );
+
+		} while ( $opts->getValue('filter') === 'untranslated'
+			&& $resultset->getTotalHits() > $size
+		);
+
+		return $resultset;
 	}
 
 	public function getFacets( $resultset ) {
