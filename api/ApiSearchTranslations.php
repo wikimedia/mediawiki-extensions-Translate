48a8f505a7a4d9f48001b0c03a87f4bc6ba79ecf
Divya <divyalife526@gmail.com>
2015-07-27 13:30:33 +0530

    API support to get translated messages in the target language, using MessageCollection.
    Steps:
    1. Use a filtered query to search for the string in the source language.
    2. Create message definitions for all message keys found in step 1.
    3. Filter to get all the translations of said messages existing
       in the selected target language.
    
    By default:
    * the source language is the wiki's content language,
    * the target language is the interface language.
    
    Use parameter 'filter=translated' to get translated messages.
    
    Bug: T106931
    Change-Id: I39f2e4f630c1055f22564740ea96f00c572814ef

--- api/ApiSearchTranslations.php
+++ api/ApiSearchTranslations.php
@@ -24,13 +24,79 @@ class ApiSearchTranslations extends ApiBase {
 		);
 
 		$result = $this->getResult();
-		$documents = $server->getDocuments( $searchResults );
 
-		$result->addValue( 'search', 'translations', $documents );
+		if ( $params['filter'] === 'translated' ) {
+			$sourceLanguage = $params['sourcelanguage'];
+			$language = $params['language'];
+			if ( $language === '' ) {
+				$language = $this->getLanguage()->getCode();
+			}
+
+			$documents = $this->extractMessages( $searchResults );
+			$result->addValue(
+				'search',
+				'translations from ' . $sourceLanguage . ' to ' . $language,
+				$documents
+			);
+		} else {
+			$documents = $server->getDocuments( $searchResults );
+			$result->addValue( 'search', 'translations', $documents );
+		}
+	}
+
+	protected function extractMessages( $resultset ) {
+		$messages = $documents = $ret = array();
+
+		$server = TTMServer::primary();
+		$params = $this->extractRequestParams();
+		foreach ( $resultset->getResults() as $document ) {
+			$data = $document->getData();
+			if ( !$server->isLocalSuggestion( $data ) ) {
+				continue;
+			}
+
+			$title = Title::newFromText( $data['localid'] );
+			if ( !$title ) {
+				continue;
+			}
+
+			$handle = new MessageHandle( $title );
+			if ( !$handle->isValid() ) {
+				continue;
+			}
+
+			$key = $title->getNamespace() . ':' . $title->getDBKey();
+			$messages[$key] = $data['content'];
+		}
+
+		$definitions = new MessageDefinitions( $messages );
+		$language = $params['language'];
+		if ( $language === '' ) {
+			$language = $this->getLanguage()->getCode();
+		}
+		$collection = MessageCollection::newFromDefinitions( $definitions, $language );
+		$collection->filter( 'translated', false );
+		$offset = $collection->slice(
+			$params['offset'],
+			$params['limit']
+		);
+		$collection->loadTranslations();
+
+		foreach ( $collection->keys() as $mkey => $title ) {
+			$documents[$mkey]['content'] = $collection[$mkey]->translation();
+			$output = explode( '/', $title->getPrefixedText() );
+			$documents[$mkey]['localid'] = $output[0];
+			$handle = new MessageHandle( $title );
+			$documents[$mkey]['uri'] = $handle->getTitle()->getCanonicalUrl();
+			$ret[] = $documents[$mkey];
+		}
+		$documents = $ret;
+
+		return $documents;
 	}
 
 	public function getAllowedParams() {
-		global $wgTranslateTranslationServices;
+		global $wgTranslateTranslationServices, $wgLanguageCode;
 
 		return array(
 			'service' => array(
@@ -46,6 +112,14 @@ class ApiSearchTranslations extends ApiBase {
 				ApiBase::PARAM_DFLT => '',
 			),
 			'group' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_DFLT => '',
+			),
+			'sourcelanguage' => array(
+				ApiBase::PARAM_TYPE => 'string',
+				ApiBase::PARAM_DFLT => $wgLanguageCode,
+			),
+			'filter' => array(
 				ApiBase::PARAM_TYPE => 'string',
 				ApiBase::PARAM_DFLT => '',
 			),
