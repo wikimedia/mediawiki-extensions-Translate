959a6b9be53d4a61125e42212ecc3fc911847cc2
Divya <divyalife526@gmail.com>
2015-07-27 13:30:33 +0530

    API support to get translated messages in the target language, using MessageCollection.
    Steps:
    1. Use a filtered query to search for the string in the source language.
    2. Create message definitions for all message keys found in step 1.
    3. Filter to get all the translations of said messages existing
       in the selected target language.
    
    By default:
    * the source language is the wiki's content language,
    * the target language is the interface language.
    
    Use parameter 'filter=translated' to get translated messages.
    
    Bug: T106931
    Change-Id: I39f2e4f630c1055f22564740ea96f00c572814ef

--- ttmserver/ElasticSearchTTMServer.php
+++ ttmserver/ElasticSearchTTMServer.php
@@ -461,6 +461,7 @@ GROOVY;
 
 	// Search interface
 	public function search( $queryString, $opts, $highlight ) {
+		global $wgLanguageCode;
 		$query = new \Elastica\Query();
 
 		// Allow searching either by message content or message id (page name
@@ -472,7 +473,25 @@ GROOVY;
 		$messageQuery = new \Elastica\Query\Term();
 		$messageQuery->setTerm( 'localid', $queryString );
 		$serchQuery->addShould( $messageQuery );
-		$query->setQuery( $serchQuery );
+
+		$filter = $opts->getValue( 'filter' );
+		if ( $filter === 'translated' ) {
+			$filteredQuery = new \Elastica\Query\Filtered();
+			$filterbool = new \Elastica\Filter\Bool();
+
+			$sourcelanguage = $opts->getValue( 'sourcelanguage' );
+			if ( $sourcelanguage === '' || $sourcelanguage === null	) {
+				$sourcelanguage = $wgLanguageCode;
+			}
+			$languageFilter = new \Elastica\Filter\Term();
+			$languageFilter->setTerm( 'language', $sourcelanguage );
+			$filterbool->addMust( $languageFilter );
+			$filteredQuery->setFilter( $filterbool );
+			$filteredQuery->setQuery( $serchQuery );
+			$query->setQuery( $filteredQuery );
+		} else {
+			$query->setQuery( $serchQuery );
+		}
 
 		$language = new \Elastica\Facet\Terms( 'language' );
 		$language->setField( 'language' );
@@ -495,7 +514,12 @@ GROOVY;
 		// of the subfilters are reused. May not make a difference in this context.
 		$filters = new \Elastica\Filter\Bool();
 
-		$language = $opts->getValue( 'language' );
+		if ( $filter === 'translated' ) {
+			$language = '';
+		} else {
+			$language = $opts->getValue( 'language' );
+		}
+
 		if ( $language !== '' ) {
 			$languageFilter = new \Elastica\Filter\Term();
 			$languageFilter->setTerm( 'language', $language );
@@ -526,11 +550,21 @@ GROOVY;
 			'post_tags' => array( $post ),
 		) );
 
-		try {
-			return $this->getType()->getIndex()->search( $query );
-		} catch ( \Elastica\Exception\ExceptionInterface $e ) {
-			throw new TTMServerException( $e->getMessage() );
-		}
+		do {
+			try {
+				$resultset = $this->getType()->getIndex()->search( $query );
+			} catch ( \Elastica\Exception\ExceptionInterface $e ) {
+				throw new TTMServerException( $e->getMessage() );
+			}
+			$query->setFrom( 0 );
+			$size = $query->getParam( 'size' );
+			$query->setSize( $resultset->getTotalHits() );
+		} while (
+			$filter === 'translated' &&
+			$resultset->getTotalHits() > $size
+		);
+
+		return $resultset;
 	}
 
 	public function getFacets( $resultset ) {
