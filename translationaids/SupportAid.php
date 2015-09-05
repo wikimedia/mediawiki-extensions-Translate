a02186cdfdb1561960008d80b4f0619ac469042d
Federico Leva <federicoleva@tiscali.it>
2015-09-05 13:20:44 +0200

    Refactor $wgTranslateSupportUrl constructions
    Bug: T40635
    Change-Id: I72b4c8d67a408f1627c7aecf9d7b6f57ec978fa1

--- translationaids/SupportAid.php
+++ translationaids/SupportAid.php
@@ -9,31 +9,42 @@
  */
 
 /**
- * Translation aid which gives an url where users can ask for hlep
+ * Translation aid which gives an url where users can ask for help
  *
  * @ingroup TranslationAids
  * @since 2013-01-02
  */
 class SupportAid extends TranslationAid {
 	public function getData() {
+		return array(
+			'url' => SupportAid::getSupportUrl(),
+		);
+	}
+
+	/**
+	* Target URL for a link provided by a support button/aid.
+	*
+	* @param $title Title|null Optional Title object for the translatable message.
+	* @since 2015-09
+	*/
+	static function getSupportUrl( $title = null ) {
 		global $wgTranslateSupportUrl;
 		if ( !$wgTranslateSupportUrl ) {
 			throw new TranslationHelperException( "Support page not configured" );
+		} else {
+			$supportTitle = Title::newFromText( $wgTranslateSupportUrl['page'] );
+			$supportParams = $wgTranslateSupportUrl['params'];
 		}
 
-		$supportTitle = Title::newFromText( $wgTranslateSupportUrl['page'] );
-		if ( !$supportTitle ) {
+		$title = $this->handle->getTitle();
+		if ( $supportTitle ) {
+			foreach ( $supportParams as &$value ) {
+				$value = str_replace( '%MESSAGE%', $title->getPrefixedText(), $value );
+			}
+		return $supportTitle->getFullUrl( $supportParams );
+		} else {
 			throw new TranslationHelperException( "Support page not configured properly" );
 		}
-
-		$supportParams = $wgTranslateSupportUrl['params'];
-		$title = $this->handle->getTitle();
-		foreach ( $supportParams as &$value ) {
-			$value = str_replace( '%MESSAGE%', $title->getPrefixedText(), $value );
-		}
-
-		return array(
-			'url' => $supportTitle->getFullUrl( $supportParams ),
-		);
 	}
+
 }
