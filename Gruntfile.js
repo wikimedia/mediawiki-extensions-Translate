5b26d7f93761302cc6006a283db8be5a474d56fb
Paladox <thomasmulhall410@yahoo.com>
2015-10-01 17:09:39 +0100

    Add support for running npm tests
    Change-Id: I0e119d24ed4f885de2ee58ee9994a7669ab4a7d4

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+
+	grunt.initConfig( {
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		},
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jsonlint', 'jshint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
