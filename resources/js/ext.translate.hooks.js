8b779c4a7ddb221890d1421684b2d0d95b423d51
Siebrand Mazeland <siebrand@kitano.nl>
2015-10-02 14:48:11 +0200

    Add/update tests
    Fix all issues so tests pass.
    
    Requires:
    * Change-Id: If01cb1106e3038c5f65d3fec4d4733c943a68a37
    
    Bug: T90943
    Change-Id: I934a9545c275c6a16fab83c52d077dd5b62a7ada

--- resources/js/ext.translate.hooks.js
+++ resources/js/ext.translate.hooks.js
@@ -3,7 +3,7 @@
  * yet have one. See hooks.txt in Translate directory for how to use hooks.
  *
  * @author Harry Burt
- * @license GPL-2.0+
+ * License GPL-2.0+
  * @since 2012-08-22
  */
 
@@ -14,10 +14,10 @@
 
 	mw.translateHooks = {
 		add: function ( name, func ) {
-			if ( !registry[name] ) {
-				registry[name] = [];
+			if ( !registry[ name ] ) {
+				registry[ name ] = [];
 			}
-			registry[name].push( func );
+			registry[ name ].push( func );
 		},
 
 		run: function ( /* infinite list of parameters */ ) {
@@ -26,11 +26,11 @@
 			args = Array.prototype.slice.call( arguments );
 			name = args.shift();
 
-			if ( registry[name] ) {
-				length = registry[name].length;
+			if ( registry[ name ] ) {
+				length = registry[ name ].length;
 
 				for ( i = 0; i < length; i++ ) {
-					registry[name][i].apply( null, args );
+					registry[ name ][ i ].apply( null, args );
 				}
 			}
 		}
--- resources/js/ext.translate.special.pagemigration.js
+++ resources/js/ext.translate.special.pagemigration.js
@@ -7,6 +7,7 @@
 	 * Create translation pages using content of right hand side blocks
 	 * and identifiers from left hand side blocks. Create pages only if
 	 * content is not empty.
+	 *
 	 * @return {Function} Returns a function which returns a jQuery.Promise
 	 */
 	function createTranslationPage( i, content ) {
@@ -15,7 +16,7 @@
 			var identifier, title, summary,
 				api = new mw.Api();
 
-			identifier = sourceUnits[i].identifier;
+			identifier = sourceUnits[ i ].identifier;
 			title = 'Translations:' + pageName + '/' + identifier + '/' + langCode;
 			summary = $( '#pm-summary' ).val();
 
@@ -25,13 +26,14 @@
 				watchlist: 'nochange',
 				title: title,
 				text: content,
-				summary: summary,
+				summary: summary
 			} );
 		};
 	}
 
 	/**
 	 * Get the old translations of a given page at given time.
+	 *
 	 * @param {string} fuzzyTimestamp Timestamp in MediaWiki format
 	 * @param {string} pageTitle
 	 * @return {jQuery.Promise}
@@ -52,7 +54,7 @@
 			var pageContent, oldTranslationUnits, obj, page,
 				errorBox = $( '.mw-tpm-sp-error__message' );
 			for ( page in data.query.pages ) {
-				obj = data.query.pages[page];
+				obj = data.query.pages[ page ];
 			}
 			if ( typeof obj === undefined ) {
 				// obj was not initialized
@@ -64,14 +66,15 @@
 				errorBox.text( mw.msg( 'pm-old-translations-missing', pageTitle ) ).show( 'fast' );
 				return $.Deferred().reject();
 			}
-			pageContent = obj.revisions[0]['*'];
+			pageContent = obj.revisions[ 0 ][ '*' ];
 			oldTranslationUnits = pageContent.split( '\n\n' );
 			return oldTranslationUnits;
 		} );
 	}
 
 	/**
-	 * Get the timestamp before FuzzyBot's first edit on page
+	 * Get the timestamp before FuzzyBot's first edit on page.
+	 *
 	 * @param {string} pageTitle
 	 * @return {jQuery.Promise}
 	 * @return {Function} return.done
@@ -89,12 +92,12 @@
 			rvuser: 'FuzzyBot',
 			rvdir: 'newer',
 			titles: pageTitle
-		} ).then ( function ( data ) {
+		} ).then( function ( data ) {
 			var timestampFB, dateFB, timestampOld,
 				page, obj,
 				errorBox = $( '.mw-tpm-sp-error__message' );
 			for ( page in data.query.pages ) {
-				obj = data.query.pages[page];
+				obj = data.query.pages[ page ];
 			}
 			// Page does not exist if missing field is present
 			if ( obj.missing === '' ) {
@@ -107,7 +110,7 @@
 				return $.Deferred().reject();
 			} else {
 				// FB over here refers to FuzzyBot
-				timestampFB = obj.revisions[0].timestamp;
+				timestampFB = obj.revisions[ 0 ].timestamp;
 				dateFB = new Date( timestampFB );
 				dateFB.setSeconds( dateFB.getSeconds() - 1 );
 				timestampOld = dateFB.toISOString();
@@ -118,7 +121,8 @@
 	}
 
 	/**
-	 * Get the translation units created by Translate extension
+	 * Get the translation units created by Translate extension.
+	 *
 	 * @param {string} pageName
 	 * return {jQuery.Promise}
 	 * return {Function} return.done
@@ -134,15 +138,15 @@
 			mcgroup: 'page-' + pageName,
 			mclanguage: 'en',
 			mcprop: 'definition'
-		} ).then ( function ( data ) {
+		} ).then( function ( data ) {
 			var result, i, sUnit, key;
 			sourceUnits = [];
 			result = data.query.messagecollection;
 			for ( i = 1; i < result.length; i++ ) {
 				sUnit = {};
-				key = result[i].key;
+				key = result[ i ].key;
 				sUnit.identifier = key.slice( key.lastIndexOf( '/' ) + 1 );
-				sUnit.definition = result[i].definition;
+				sUnit.definition = result[ i ].definition;
 				sourceUnits.push( sUnit );
 			}
 			return sourceUnits;
@@ -151,10 +155,14 @@
 
 	/**
 	 * Shift rows up by one unit. This is called after a unit is deleted.
+	 *
 	 * @param {jQuery} $start The starting node
 	 */
 	function shiftRowsUp( $start ) {
-		var $current = $start, $next = $start.next(), nextVal;
+		var $current = $start,
+			$next = $start.next(),
+			nextVal;
+
 		while ( $next.length ) {
 			nextVal = $next.find( '.mw-tpm-sp-unit__target' ).val();
 			$current.find( '.mw-tpm-sp-unit__target' ).val( nextVal );
@@ -171,6 +179,7 @@
 	/**
 	 * Shift rows down by one unit. This is called after a new empty unit is
 	 * added.
+	 *
 	 * @param {jQuery} $nextRow The next row to start with
 	 * @param {string} text The text of the next row
 	 * @return {string} text The text of the last row
@@ -188,7 +197,8 @@
 	}
 
 	/**
-	 * Create a new row of source text and target text with action icons
+	 * Create a new row of source text and target text with action icons.
+	 *
 	 * @param {string} sourceText
 	 * @param {string} targetText
 	 * @return {jQuery} newUnit The new row unit object
@@ -215,6 +225,7 @@
 
 	/**
 	 * Display the source and target units alongwith the action icons.
+	 *
 	 * @param {Array} sourceUnits
 	 * @param {Array} translations
 	 */
@@ -229,11 +240,11 @@
 		unitListing.html( '' );
 		for ( i = 0; i < totalUnits; i++ ) {
 			sourceText = targetText = '';
-			if ( sourceUnits[i] !== undefined ) {
-				sourceText = sourceUnits[i].definition;
+			if ( sourceUnits[ i ] !== undefined ) {
+				sourceText = sourceUnits[ i ].definition;
 			}
-			if ( translations[i] !== undefined ) {
-				targetText = translations[i];
+			if ( translations[ i ] !== undefined ) {
+				targetText = translations[ i ];
 			}
 			newUnit = createNewUnit( sourceText, targetText );
 			unitListing.append( newUnit );
@@ -242,6 +253,7 @@
 
 	/*
 	 * Split headers from remaining text in each translation unit if present.
+	 *
 	 * @param {Array} translations Array of initial units obtained on splitting
 	 * @return {string[]} translationUnits Array having the headers split into new unit
 	 */
@@ -253,7 +265,8 @@
 	}
 
 	/**
-	 * Get the index of next translation unit containing h2 header
+	 * Get the index of next translation unit containing h2 header.
+	 *
 	 * @param {Integer} startIndex Index to start the scan from
 	 * @return {Integer} i Index of the next unit found, -1 if not
 	 */
@@ -261,7 +274,7 @@
 		var i, regex;
 		regex = new RegExp( /^==[^=]+==$/m );
 		for ( i = startIndex; i < translationUnits.length; i++ ) {
-			if ( regex.test( translationUnits[i] ) ) {
+			if ( regex.test( translationUnits[ i ] ) ) {
 				return i;
 			}
 		}
@@ -279,7 +292,7 @@
 
 		regex = new RegExp( /^==[^=]+==$/m );
 		for ( i = 0; i < sourceUnits.length; i++ ) {
-			if ( regex.test( sourceUnits[i].definition ) ) {
+			if ( regex.test( sourceUnits[ i ].definition ) ) {
 				tIndex = getHeaderUnit( tIndex, translationUnits );
 				mergeText = '';
 				// search is over
@@ -303,7 +316,7 @@
 						emptyCount += 1;
 					}
 					if ( i !== 0 ) {
-						translationUnits[i - 1] += '\n' + mergeText;
+						translationUnits[ i - 1 ] += '\n' + mergeText;
 					} else {
 						matchText = mergeText + matchText;
 					}
@@ -320,7 +333,8 @@
 	 * Handler for 'Save' button click event.
 	 */
 	function saveHandler() {
-		var i, list = [], content;
+		var list = [],
+			content, i;
 
 		$( '.mw-tpm-sp-error__message' ).hide( 'fast' );
 		if ( noOfSourceUnits < noOfTranslationUnits ) {
@@ -399,7 +413,7 @@
 		var rowUnit, tempText, nextVal;
 		rowUnit = $( event.target ).closest( '.mw-tpm-sp-unit' );
 		tempText = rowUnit.find( '.mw-tpm-sp-unit__target' ).val();
-		nextVal = rowUnit.next().find( '.mw-tpm-sp-unit__target').val();
+		nextVal = rowUnit.next().find( '.mw-tpm-sp-unit__target' ).val();
 		rowUnit.find( '.mw-tpm-sp-unit__target' ).val( nextVal );
 		rowUnit.next().find( '.mw-tpm-sp-unit__target' ).val( tempText );
 	}
@@ -430,16 +444,16 @@
 		}
 		$.when( getSourceUnits( pageName ), getFuzzyTimestamp( pageTitle ) )
 			.then( function ( sourceUnits, fuzzyTimestamp ) {
-			noOfSourceUnits = sourceUnits.length;
-			splitTranslationPage( fuzzyTimestamp, pageTitle ).done( function ( translations ) {
-				var translationUnits = splitHeaders( translations );
-				translationUnits = alignHeaders( sourceUnits, translationUnits );
-				noOfTranslationUnits = translationUnits.length;
-				displayUnits( sourceUnits, translationUnits );
-				$( '#action-save, #action-cancel').removeClass( 'hide' );
-				$( '#action-import' ).addClass( 'hide' );
+				noOfSourceUnits = sourceUnits.length;
+				splitTranslationPage( fuzzyTimestamp, pageTitle ).done( function ( translations ) {
+					var translationUnits = splitHeaders( translations );
+					translationUnits = alignHeaders( sourceUnits, translationUnits );
+					noOfTranslationUnits = translationUnits.length;
+					displayUnits( sourceUnits, translationUnits );
+					$( '#action-save, #action-cancel' ).removeClass( 'hide' );
+					$( '#action-import' ).addClass( 'hide' );
+				} );
 			} );
-		} );
 	}
 
 	/**
@@ -465,4 +479,4 @@
 		alignHeaders: alignHeaders
 	} );
 
-} ( jQuery, mediaWiki ) );
+}( jQuery, mediaWiki ) );
