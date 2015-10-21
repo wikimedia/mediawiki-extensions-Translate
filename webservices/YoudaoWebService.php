cdd898cab0dae48394f94fe70c58027222687551
谷溪 <guxi99@gmail.com>
2015-10-21 15:20:55 +0800

    Update YoudaoWebService.php

--- webservices/YoudaoWebService.php
+++ webservices/YoudaoWebService.php
@@ -60,7 +60,7 @@ class YoudaoWebService extends TranslationWebService {
 	}
 	protected function parseResponse( TranslationQueryResponse $reply ) {
 		$body = $reply->getBody();
-		$text = preg_replace( '~<paragraph>(.*)</paragraph>~', '\\1', $body );
+		$text = preg_replace( '~<translation>(.*)</translation>~', '\\1', $body );
 		$text = Sanitizer::decodeCharReferences( $text );
 		$text = $this->unwrapUntranslatable( $text );
 		return $text;
