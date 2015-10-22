18e0f05ca804bde34a51c4175cd7e45a3b0c1bc1
谷溪 <guxi99@gmail.com>
2015-10-22 16:04:18 +0800

    Update YoudaoWebService.php

--- webservices/YoudaoWebService.php
+++ webservices/YoudaoWebService.php
@@ -10,24 +10,52 @@
  * Implements support for youdao dict.
  * @see http://fanyi.youdao.com/openapi?path=data-mode
  * @ingroup TranslationWebService
- * @since 2013-01-01
+ * @since 2015-10-20
  */
 class YoudaoWebService extends TranslationWebService {
-	protected function mapCode( $code ) {
-		$map = array(
-			'zh-hant' => 'zh',
-			'zh-hans' => 'zh',
-			'zh-cn' => 'zh',
-			'zh-hk' => 'zh',
-			'zh-tw' => 'zh',
-		);
-		return isset( $map[$code] ) ? $map[$code] : $code;
-	}
-	protected function doPairs() {
-		if ( !isset( $this->config['key'] ) ) {
-			throw new TranslationWebServiceException( 'API key is not set' );
-		}
-		// add a few more hacks 
+        public function getType() {
+                return 'mt';
+        }
+        /**
+         * Some mangling that tries to keep some parts of the message unmangled
+         * by the translation service. Most of them support either class=notranslate
+         * or translate=no.
+         */
+        protected function wrapUntranslatable( $text ) {
+                $text = str_replace( "\n", "?~TN?~T", $text );
+                $pattern = '~%[^% ]+%|\$\d|{VAR:[^}]+}|{?{(PLURAL|GRAMMAR|GENDER):[^|]+\||%(\d\$)?[sd]~';
+                $wrap = '<span class="notranslate" translate="no">\0</span>';
+                $text = preg_replace( $pattern, $wrap, $text );
+
+                return $text;
+        }
+
+        /**
+         * Undo the hopyfully untouched mangling done by wrapUntranslatable.
+         */
+        protected function unwrapUntranslatable( $text ) {
+                $text = str_replace( '?~TN?~T', "\n", $text );
+                $pattern = '~<span class="notranslate" translate="no">(.*?)</span>~';
+                $text = preg_replace( $pattern, '\1', $text );
+
+                return $text;
+        }
+
+        protected function mapCode( $code ) {
+                $map = array(
+                        'zh-hant' => 'zh',
+                        'zh-hans' => 'zh',
+                        'zh-cn' => 'zh',
+                        'zh-hk' => 'zh',
+                        'zh-tw' => 'zh',
+                );
+                return isset( $map[$code] ) ? $map[$code] : $code;
+        }
+        protected function doPairs() {
+                if ( !isset( $this->config['key'] ) ) {
+                        throw new TranslationWebServiceException( 'API key is not set' );
+                }
+                // add a few more hacks 
                 $pairs['en']['zh-cn'] = true;
                 $pairs['en']['zh-tw'] = true;
                 $pairs['en']['zh-hk'] = true;
@@ -70,31 +98,31 @@ class YoudaoWebService extends TranslationWebService {
                 $pairs['zh-hans']['en'] = true;
                 $pairs['zh-hant']['en'] = true;
                 $pairs['zh']['en'] = true;
-		return $pairs;
-	}
-	protected function getQuery( $text, $from, $to ) {
-		if ( !isset( $this->config['key'] ) ) {
-			throw new TranslationWebServiceException( 'API key is not set' );
-		}
-		$text = trim( $text );
-		$text = $this->wrapUntranslatable( $text );
-		$params = array(
-			'q' => $text,
-			'key' => $this->config['key'],
-			'keyfrom' => $this->config['keyfrom'],
-			'type' => 'data',
-			'doctype' => 'xml',
-			'version' => '1.1',
-		);
-		$url = 'http://fanyi.youdao.com/openapi.do?';
-		return TranslationQuery::factory( $this->config['url'] )
-			->timeout( $this->config['timeout'] )
-			->queryParamaters( $params );
-	}
-	protected function parseResponse( TranslationQueryResponse $reply ) {
-		$xml = simplexml_load_string( $reply->getBody() );
-		$text = Sanitizer::decodeCharReferences( $xml->translation->paragraph );
-		$text = $this->unwrapUntranslatable( $text );
-		return $text;
-	}
+                return $pairs;
+        }
+        protected function getQuery( $text, $from, $to ) {
+                if ( !isset( $this->config['key'] ) ) {
+                        throw new TranslationWebServiceException( 'API key is not set' );
+                }
+                $text = trim( $text );
+                $text = $this->wrapUntranslatable( $text );
+                $params = array(
+                        'q' => $text,
+                        'key' => $this->config['key'],
+                        'keyfrom' => $this->config['keyfrom'],
+                        'type' => 'data',
+                        'doctype' => 'xml',
+                        'version' => '1.1',
+                );
+                $url = 'http://fanyi.youdao.com/openapi.do?';
+                return TranslationQuery::factory( $this->config['url'] )
+                        ->timeout( $this->config['timeout'] )
+                        ->queryParamaters( $params );
+        }
+        protected function parseResponse( TranslationQueryResponse $reply ) {
+                $xml = simplexml_load_string( $reply->getBody() );
+                $text = Sanitizer::decodeCharReferences( $xml->translation->paragraph );
+                $text = $this->unwrapUntranslatable( $text );
+                return $text;
+        }
 }
