00c6fcc7413720c52d1ddfe3c19000c5c0b74712
谷溪 <guxi99@gmail.com>
2015-10-22 14:52:46 +0800

    Update SpecialTranslate.php

--- specials/SpecialTranslate.php
+++ specials/SpecialTranslate.php
@@ -42,6 +42,7 @@ class SpecialTranslate extends SpecialPage {
 		$out = $this->getOutput();
 		$out->addModuleStyles( array(
 			'ext.translate.special.translate.styles',
+			'jquery.uls.grid', 
 		) );
 		$out->addModules( 'ext.translate.special.translate' );
 
