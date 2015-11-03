fda5e8d8dfad6829edc20c8e1fc038655bd429b1
Paladox <thomasmulhall410@yahoo.com>
2015-11-03 16:49:36 +0000

    Add support for npm tests
    Run jshint, jsonlint and banana test through npm.
    
    Change-Id: Idc71cdd65cda36bc3639eec63a08532108a55c96

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'**/*.js',
+				'!node_modules/**'
+			]
+		},
+		banana: {
+			api: 'i18n/api',
+			core: 'i18n/core',
+			pagetranslation: 'i18n/pagetranslation',
+			sandbox: 'i18n/sandbox',
+			search: 'i18n/search'
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
