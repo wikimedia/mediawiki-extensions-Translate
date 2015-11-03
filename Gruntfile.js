83d1a3f93ecebda5aa42713aa255bee8b0d9370d
Paladox <thomasmulhall410@yahoo.com>
2015-11-03 16:49:36 +0000

    Add support for npm tests
    Run jshint and jsonlint tests through npm.
    
    Disable banana checker due to missing messages in documentation.
    
    Will re enable banana in a follow up patch.
    
    Change-Id: Idc71cdd65cda36bc3639eec63a08532108a55c96

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'**/*.js',
+				'!node_modules/**'
+			]
+		},
+		/* banana: {
+			all: [
+				'i18n/api',
+				'i18n/core',
+				'i18n/pagetranslation',
+				'i18n/sandbox',
+				'i18n/search'
+			]
+		},
+		*/
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
