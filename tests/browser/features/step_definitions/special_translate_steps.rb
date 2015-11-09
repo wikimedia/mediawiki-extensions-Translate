5b4b2e567f640d6e9abfe8cfa9f4c4bb52c4a383
Željko Filipin <zeljko.filipin@gmail.com>
2015-11-09 15:31:49 +0100

    Fixed Style/SpaceInsideHashLiteralBraces RuboCop offense
    Bug: T117987
    Change-Id: I4dcc5b6a77d6a82ae79673be077493ac6475de0e

--- tests/browser/features/step_definitions/special_translate_steps.rb
+++ tests/browser/features/step_definitions/special_translate_steps.rb
@@ -1,9 +1,9 @@
 Given(/^I am translating a message group which doesn't have workflow states$/) do
-  visit(TranslatePage, using_params: {extra: "language=fi"})
+  visit(TranslatePage, using_params: { extra: "language=fi" })
 end
 
 Given(/^I am translating a message group which has workflow states$/) do
-  visit(TranslatePage, using_params: {extra: "language=fi&group=page-Language+committee"})
+  visit(TranslatePage, using_params: { extra: "language=fi&group=page-Language+committee" })
 end
 
 When(/^I click the workflow state$/) do
