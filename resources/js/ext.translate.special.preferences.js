211bb3e34ef3d287d12ae5399b9a27e2b064fce3
Geoffrey Mon <geofbot@gmail.com>
2016-01-16 21:42:27 -0500

    In Special:Preferences, update defaultSelected for options in language selector
    This selector is not actually a preference option and is used to help set the
    assistant language preference option.
    
    New module ext.translate.special.preferences sets defaultSelected.
    
    Bug: T122702
    Change-Id: I18e14cfe70635d410c15e83426f9f3093a9439df

---
+++ resources/js/ext.translate.special.preferences.js
@@ -0,0 +1,15 @@
+( function ( mw, $ ) {
+	// Make sure the language selection dropdown doesn't trigger
+	// Special:Preferences unsaved preferences check
+	var $langSelector = $( '#mw-language-selector' );
+
+	function updateDefaultSelected () {
+		var $opt = $langSelector.find( 'option:selected' );
+		$opt.prop( 'defaultSelected', $opt.prop( 'selected' ) );
+	}
+
+	$langSelector.on( 'change keydown mousedown', function () {
+		updateDefaultSelected();
+	} );
+	updateDefaultSelected();
+}( mediaWiki, jQuery ) );
