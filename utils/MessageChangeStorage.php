89ce1583e190aefc1126e578b5470145da954cd3
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-02-19 17:16:09 +0100

    Allow name option on scripts/processMessageChanges.php
    This allows each person to use different name to avoid conflicts.
    
    Change-Id: I3f46285f24b35ef2ba0009a6b753f39fe30e72d6

---
+++ utils/MessageChangeStorage.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Handles storage of message change files.
+ *
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ * @since 2016.02
+ * @file
+ */
+
+class MessageChangeStorage {
+	const DEFAULT_NAME = 'default';
+
+	/**
+	 * Writes change array as a serialized file.
+	 *
+	 * @param array $array Array of changes as returned by processGroup
+	 * indexed by message group id.
+	 * @param string $file Which file to use.
+	 */
+	public static function writeChanges( $array, $file ) {
+		$cache = CdbWriter::open( $file );
+		$keys = array_keys( $array );
+		$cache->set( '#keys', serialize( $keys ) );
+
+		foreach ( $array as $key => $value ) {
+			$value = serialize( $value );
+			$cache->set( $key, $value );
+		}
+		$cache->close();
+	}
+
+	/**
+	 * Validate a name.
+	 *
+	 * @param string $file Which file to use.
+	 * @return bool
+	 */
+	public static function isValidCdbName( $name ) {
+		return preg_match( '/^[a-zA-Z_-]{1,100}$/', $name );
+	}
+
+	/**
+	 * Get a full path to file in a known location.
+	 *
+	 * @param string $file Which file to use.
+	 * @return string
+	 */
+	public static function getCdbPath( $name ) {
+		return TranslateUtils::cacheFile( "messagechanges.$name.cdb" );
+	}
+}
