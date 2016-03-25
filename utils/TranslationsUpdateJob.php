d6633da39b8f792e926e30578b72cc2cb620bdf3
Glaisher <glaisher.wiki@gmail.com>
2016-03-25 22:15:45 +0500

    Make sure MessageUpdateJobs are run before TranslateRenderJobs when marking
    The changes made by MessageUpdateJobs are needed for the TranslateRenderJobs
    so add a new job which runs the MessageUpdateJobs first and then push the
    TranslateRenderJobs to the job queue once the MessageUpdateJobs are complete.
    
    Bug: T53731
    Change-Id: Ia92e726879e44d0b24e39abe8f48584905a841f2

---
+++ utils/TranslationsUpdateJob.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Job for updating translation units and translation pages when
+ * a page is marked for translation.
+ *
+ * @note MessageUpdateJobs should be run before the TranslateRenderJobs
+ * so that the latest changes can take effect on the translatable pages.
+ */
+class TranslationsUpdateJob extends Job {
+	/**
+	 * @param Title $title
+	 * @param array $params
+	 * @param int $id
+	 */
+	public function __construct( Title $title, array $params, $id = 0 ) {
+		parent::__construct( __CLASS__, $title, $params, $id );
+
+		$this->page = $params['page'];
+		$this->sections = $params['sections'];
+	}
+
+	public function run() {
+		// Units should be updated before the render jobs are run
+		$unitJobs = SpecialPageTranslation::getTranslationUnitJobs( $this->page, $this->sections );
+		foreach( $unitJobs as $job ) {
+			$job->run();
+		}
+
+		$renderJobs = SpecialPageTranslation::getRenderJobs( $this->page );
+		JobQueueGroup::singleton()->push( $renderJobs );
+		return true;
+	}
+}
