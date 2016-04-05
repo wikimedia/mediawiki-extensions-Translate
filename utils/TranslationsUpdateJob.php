c3dfc61a316eae753db1463a83ab146b5a4d8165
Glaisher <glaisher.wiki@gmail.com>
2016-04-05 22:33:45 +0500

    Ensure that TranslateRenderJob for the source language page is added when marked for translation
    Currently, the source page is updated through the page save hook triggered when
    the translation units are created which checks whether $handle->isValid(). Since getGroupIds()
    is empty at that point because the message index hasn't been built for newly translatable pages,
    the hook is terminated.
    
    This patch adds a new TranslateRenderJob explicitly for the source language page.
    
    Bug: T56579
    Change-Id: Ic8d2035bb7e1d36a40f0e2fae7ca727aa2932859

--- utils/TranslationsUpdateJob.php
+++ utils/TranslationsUpdateJob.php
@@ -30,6 +30,9 @@ class TranslationsUpdateJob extends Job {
 			$job->run();
 		}
 
+		$wikiPage = WikiPage::factory( $this->page->getTitle() );
+		$wikiPage->doPurge();
+
 		$renderJobs = self::getRenderJobs( $this->page );
 		JobQueueGroup::singleton()->push( $renderJobs );
 		return true;
@@ -68,8 +71,13 @@ class TranslationsUpdateJob extends Job {
 	public static function getRenderJobs( TranslatablePage $page ) {
 		$jobs = array();
 
-		$titles = $page->getTranslationPages();
-		foreach ( $titles as $t ) {
+		$jobTitles = $page->getTranslationPages();
+		// $jobTitles may have the source language title already but duplicate TranslateRenderJobs
+		// are not executed so it's not run twice for the source language page present. This is
+		// added to ensure that we create the source language page from the very beginning.
+		$sourceLangTitle = $page->getTitle()->getSubpage( $page->getSourceLanguageCode() );
+		$jobTitles[] = $sourceLangTitle;
+		foreach ( $jobTitles as $t ) {
 			$jobs[] = TranslateRenderJob::newJob( $t );
 		}
 
