d1b95f8d6623c1d370778216397bf9915fc45eb3
Glaisher <glaisher.wiki@gmail.com>
2016-04-05 22:33:45 +0500

    Move several updates to TranslationsUpdateJob when page is marked for translation
    * Move MessageIndex rebuild to this job and run it before TranslateRenderJobs
    
    * Ensure that TranslateRenderJob for the source language page is added when marked for translation
      Previously, the source page is updated through the page save hook triggered when the
      translation units are created which checks whether $handle->isValid(). Since getGroupIds() is
      empty at that point because the message index hasn't been built for newly translatable pages,
      the hook is terminated.
      This patch adds a new TranslateRenderJob explicitly for the source language page.
    
    * Recalculate statistics when a page is marked for translation
      Translation statistics need to be reset when page is marked for translation
      with units invalidation enabled.
    
    * Don't run onSectionSave() changes for FuzzyBot edits
      This causes FuzzyBot to run unnecessary TranslateRenderJobs when pages are
      marked for translation. These updates are now done within the TranslationsUpdateJob.
    
    Bug: T56579
    Bug: T104172
    Change-Id: Ic8d2035bb7e1d36a40f0e2fae7ca727aa2932859

--- utils/TranslationsUpdateJob.php
+++ utils/TranslationsUpdateJob.php
@@ -30,6 +30,16 @@ class TranslationsUpdateJob extends Job {
 			$job->run();
 		}
 
+		MessageIndex::singleton()->rebuild();
+
+		// Refresh translations statistics
+		$id = $this->page->getMessageGroupId();
+		MessageGroupStats::clearGroup( $id );
+		MessageGroupStats::forGroup( $id );
+
+		$wikiPage = WikiPage::factory( $this->page->getTitle() );
+		$wikiPage->doPurge();
+
 		$renderJobs = self::getRenderJobs( $this->page );
 		JobQueueGroup::singleton()->push( $renderJobs );
 		return true;
@@ -68,8 +78,13 @@ class TranslationsUpdateJob extends Job {
 	public static function getRenderJobs( TranslatablePage $page ) {
 		$jobs = array();
 
-		$titles = $page->getTranslationPages();
-		foreach ( $titles as $t ) {
+		$jobTitles = $page->getTranslationPages();
+		// $jobTitles may have the source language title already but duplicate TranslateRenderJobs
+		// are not executed so it's not run twice for the source language page present. This is
+		// added to ensure that we create the source language page from the very beginning.
+		$sourceLangTitle = $page->getTitle()->getSubpage( $page->getSourceLanguageCode() );
+		$jobTitles[] = $sourceLangTitle;
+		foreach ( $jobTitles as $t ) {
 			$jobs[] = TranslateRenderJob::newJob( $t );
 		}
 
