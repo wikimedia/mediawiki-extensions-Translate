cc21a8ef878ceb4bbd0080f123ca53cbc495cae1
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-04-11 08:34:08 +0200

    Move all TTMServer updates to the JobQueue
    Slight speed-up to translation saves and deletes as updates
    are now done via JobQueue. But more importantly failures
    will now be caught by JobQueue rather than breaking
    saves and deletes.
    
    Remove return true from hooks per conventions.
    Remove my copyright years per conventions (too lazy to update).
    
    Bug: T132254
    Change-Id: Ie6f1bfd5ba8224ef5642ac6c04c22a121d442f5b

--- ttmserver/TTMServerMessageUpdateJob.php
+++ ttmserver/TTMServerMessageUpdateJob.php
@@ -4,7 +4,6 @@
  *
  * @file
  * @author Niklas Laxström
- * @copyright Copyright © 2012-2013, Niklas Laxström
  * @license GPL-2.0+
  */
 
@@ -18,8 +17,8 @@ class TTMServerMessageUpdateJob extends Job {
 	 * @param MessageHandle $handle
 	 * @return TTMServerMessageUpdateJob
 	 */
-	public static function newJob( MessageHandle $handle ) {
-		$job = new self( $handle->getTitle() );
+	public static function newJob( MessageHandle $handle, $command ) {
+		$job = new self( $handle->getTitle(), array( 'command' => $command ) );
 
 		return $job;
 	}
@@ -38,13 +37,47 @@ class TTMServerMessageUpdateJob extends Job {
 	 */
 	public function run() {
 		$handle = new MessageHandle( $this->title );
+
+		// For old jobs before this param was added
+		$command = isset( $this->params['command'] ) ? $this->params['command'] : 'rebuild';
+
+		// JobQueue will catch exceptions and retry the job few times,
+		// after which it will be abandoned.
+		if ( $command === 'delete' ) {
+			$this->updateItem( $handle, null, false );
+		} elseif ( $command === 'rebuild' ) {
+			$this->updateMessage( $handle );
+		} elseif ( $command === 'refresh' ) {
+			$this->updateTranslation( $handle );
+		}
+
+		return true;
+	}
+
+	private function updateMessage( MessageHandle $handle ) {
+		// Base page update, e.g. group change. Update everything.
 		$translations = ApiQueryMessageTranslations::getTranslations( $handle );
 		foreach ( $translations as $page => $data ) {
 			$tTitle = Title::makeTitle( $this->title->getNamespace(), $page );
 			$tHandle = new MessageHandle( $tTitle );
-			TTMServer::onChange( $tHandle, $data[0], $tHandle->isFuzzy() );
+			$this->updateItem( $tHandle, $data[0], $tHandle->isFuzzy() );
 		}
+	}
 
-		return true;
+	private function updateTranslation( MessageHandle $handle ) {
+		// Update only this translation
+		$translation = TranslateUtils::getMessageContent(
+			$handle->getKey(),
+			$handle->getCode(),
+			$handle->getTitle()->getNamespace()
+		);
+		$this->updateItem( $handle, $translation, $handle->isFuzzy() );
+	}
+
+	private function updateItem( MessageHandle $handle, $text, $fuzzy ) {
+		if ( $fuzzy ) {
+			$text = null;
+		}
+		TTMServer::primary()->update( $handle, $text );
 	}
 }
