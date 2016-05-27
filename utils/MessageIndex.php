7be8490549c21fb0de5d2053a233915e87b78be7
Glaisher <glaisher.wiki@gmail.com>
2016-05-27 22:03:01 +0500

    Cache MessageIndex key values
    Currently this does the same query lots of times
    (tens of times in one request). Applying this patch
    decreased the number of DatabaseMessageIndex::get()
    queries in one request from 55 to 3.
    Limit is 30, which is just an arbitrary number.
    
    Bug: T136219
    Change-Id: If9cb5ebc443927b2be4fc549cfcf3c6be489b576

--- utils/MessageIndex.php
+++ utils/MessageIndex.php
@@ -23,6 +23,11 @@ abstract class MessageIndex {
 	protected static $instance;
 
 	/**
+	 * @var MapCacheLRU|null
+	 */
+	private static $keysCache;
+
+	/**
 	 * @return self
 	 */
 	public static function singleton() {
@@ -65,12 +70,27 @@ abstract class MessageIndex {
 		$key = $handle->getKey();
 		$normkey = TranslateUtils::normaliseKey( $namespace, $key );
 
-		$value = self::singleton()->get( $normkey );
-		if ( $value !== null ) {
-			return (array)$value;
-		} else {
-			return array();
+		$cache = self::getCache();
+		$value = $cache->get( $normkey );
+		if ( $value === null ) {
+			$value = self::singleton()->get( $normkey );
+			$value = $value !== null
+				? (array)$value
+				: array();
+			$cache->set( $normkey, $value );
 		}
+
+		return $value;
+	}
+
+	/**
+	 * @return MapCacheLRU
+	 */
+	private static function getCache() {
+		if ( self::$keysCache === null ) {
+			self::$keysCache = new MapCacheLRU( 30 );
+		}
+		return self::$keysCache;
 	}
 
 	/**
@@ -130,6 +150,8 @@ abstract class MessageIndex {
 		if ( !$this->lock() ) {
 			throw new Exception( __CLASS__ . ': unable to acquire lock' );
 		}
+
+		self::getCache()->clear();
 
 		$new = $old = array();
 		$old = $this->retrieve( 'rebuild' );
