5d2bc46a35f86dcf32fd5b6a8403a8a3d82e3b28
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-07-12 18:08:29 +0200

    Rewrite of translatable page moving code
    Get rid of some ugly old code by using MovePage. Instead of splitting
    moves into many many jobs, use one job that does it all. This allows
    to simplify many things.
    
    Old job class to be removed later.
    
    Change-Id: Idbbdd09dcc214fecabc28607cc3207324fcb7fee

--- Autoload.php
+++ Autoload.php
@@ -134,6 +134,7 @@ $al['SpecialPageAliasesCM'] = "$dir/ffs/MediaWikiComplexMessages.php";
  * @ingroup PageTranslation
  * @{
  */
+$al['TranslatablePageMoveJob'] = "$dir/tag/TranslatablePageMoveJob.php";
 $al['TranslateDeleteJob'] = "$dir/tag/TranslateDeleteJob.php";
 $al['TranslateMoveJob'] = "$dir/tag/TranslateMoveJob.php";
 $al['PageTranslationHooks'] = "$dir/tag/PageTranslationHooks.php";
