056d1816310fd30f5035218f3cc32c8bafc19d84
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-07-12 18:08:29 +0200

    Rewrite of translatable page moving code
    Get rid of some ugly old code by using MovePage. Instead of splitting
    moves into many many jobs, use one job that does it all. This allows
    to simplify many things.
    
    Old job class to be removed later.
    
    Change-Id: Idbbdd09dcc214fecabc28607cc3207324fcb7fee

--- tag/PageTranslationHooks.php
+++ tag/PageTranslationHooks.php
@@ -790,8 +790,7 @@ class PageTranslationHooks {
 
 		$cache = wfGetCache( CACHE_ANYTHING );
 		$key = wfMemcKey( 'pt-lock', sha1( $title->getPrefixedText() ) );
-		// At least memcached mangles true to "1"
-		if ( $cache->get( $key ) !== false ) {
+		if ( $cache->get( $key ) === 'locked' ) {
 			$result = array( 'pt-locked-page' );
 
 			return false;
