7a7f28369d8815e727c7b33102fe3dbc910d4aeb
Glaisher <glaisher.wiki@gmail.com>
2016-08-25 10:28:43 +0500

    Use getUserPermissionsErrorsExpensive hook for preventing translations to non-translatable languages
    Modifying EditPage's UI doesn't guarantee preventing edits as the edit API
    won't check for this. So use the proper hook for preventing such edits reliably.
    
    Also cleanup AlternateEdit hook handler used for suppressing intros.
    
    Change-Id: Ie2d88705548cfa7e8194f976496d018f9d413140

--- Translate.php
+++ Translate.php
@@ -99,7 +99,8 @@ $wgAPIModules['searchtranslations'] = 'ApiSearchTranslations';
 
 // Register hooks.
 $wgHooks['EditPage::showEditForm:initial'][] = 'TranslateEditAddons::addTools';
-$wgHooks['AlternateEdit'][] = 'TranslateEditAddons::intro';
+$wgHooks['AlternateEdit'][] = 'TranslateEditAddons::suppressIntro';
+$wgHooks['getUserPermissionsErrorsExpensive'][] = 'TranslateEditAddons::disallowLangTranslations';
 $wgHooks['EditPageBeforeEditButtons'][] = 'TranslateEditAddons::buttonHack';
 $wgHooks['LanguageGetTranslatedLanguageNames'][] =
 	'TranslateHooks::translateMessageDocumentationLanguage';
