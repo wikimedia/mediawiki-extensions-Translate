a23b555ae8d7be2cf4c922f2dba5bc50f275db84
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-09-13 17:44:04 +0200

    Special:TranslationStats: use mediawiki.widgets.datetime if available
    Change-Id: I22f06f264b143fd7bb5c65516d9265170974b893

--- resources/js/ext.translate.special.translationstats.js
+++ resources/js/ext.translate.special.translationstats.js
@@ -8,20 +8,52 @@
  * @license GPL-2.0+
  */
 
-jQuery( document ).ready( function ( $ ) {
+( function ( $, mw ) {
 	'use strict';
 
-	// Based on UploadWizard, TranslationNotifications
-	$( '#start' )
-		.datepicker( {
-			dateFormat: 'yy-mm-ddT00:00:00',
-			constrainInput: false,
-			showOn: 'focus',
-			changeMonth: true,
-			changeYear: true,
-			showAnim: false,
-			showButtonPanel: true,
-			maxDate: new Date(),
-		} )
-		.attr( 'autocomplete', 'off' );
-} );
+	$( function () {
+		var $input = $( '#start' ),
+			datepicker = mw.loader.getState( 'mediawiki.widgets.datetime' ) === null;
+
+		// Remove when MediaWiki 1.27 is no longer supported
+		if ( datepicker ) {
+			mw.loader.using( 'jquery.ui.datepicker' ).done( function () {
+				$input.datepicker( {
+					dateFormat: 'yy-mm-ddT00:00:00',
+					constrainInput: false,
+					showOn: 'focus',
+					changeMonth: true,
+					changeYear: true,
+					showAnim: false,
+					showButtonPanel: true,
+					maxDate: new Date(),
+				} )
+				.attr( 'autocomplete', 'off' );
+			} );
+		} else {
+			mw.loader.using( 'mediawiki.widgets.datetime' ).done( function () {
+				var widget, defaultDate;
+
+				defaultDate = new Date();
+				defaultDate.setDate( 1 );
+				defaultDate.setHours( 0, 0, 0, 0 );
+
+				widget = new mw.widgets.datetime.DateTimeInputWidget( {
+					formatter: {
+						format: '${year|0}-${month|0}-${day|0}T${hour|0}:${minute|0}:${second|0}',
+						value: Date.parse( $input.val() ),
+						defaultDate: defaultDate,
+						local: true
+					},
+					value: Date.parse( $input.val() ), // FIXME: does not work
+					max: new Date()
+				} );
+
+				$input.after( widget.$element ).hide();
+				widget.on( 'change', function ( data ) {
+					$input.val( data.replace( /\.?0+$/, '' ) );
+				} );
+			} );
+		}
+	} );
+}( jQuery, mediaWiki ) );
