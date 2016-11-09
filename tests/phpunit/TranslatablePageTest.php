44ac1254bec807201bef723a3233b18fe4d59053
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-11-09 11:26:16 +0100

    PageTranslation: do not add line breaks to inline usage of <translate> tags
    This causes problems for example with headings. This should also
    make the source a bit more readable in places when line breaks
    are not forced.
    
    Bug: T150188
    Change-Id: Ia54091ba857e8d513ea302a4b80342b5f5b97b22

---
+++ tests/phpunit/TranslatablePageTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Unit tests for class TPSection
+ *
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ * @file
+ */
+
+/**
+ * Unit tests for class TPSection
+ * @ingroup PageTranslation
+ */
+class TPSectionTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @dataProvider provideTestSectionise
+	 */
+	public function testSectionise( $input, $pattern, $comment ) {
+		$result = TranslatablePage::sectionise( $input );
+		$pattern = addcslashes( $pattern, '~' );
+		$this->assertRegExp( "~^$pattern$~", $result['template'], $comment );
+	}
+
+	public static function provideTestSectionise() {
+		// Ugly implicit assumption
+		$ph = "\x7fUNIQ[a-z0-9]{8,16}-\d+";
+
+		$cases = array();
+
+		$cases[] = array(
+			'Hello',
+			"$ph",
+			'No surrounding whitespace',
+		);
+
+		$cases[] = array(
+			"\nHello",
+			"\n$ph",
+			'With surrounding whitespace',
+		);
+
+		$cases[] = array(
+			"\nHello world\n\nBunny\n",
+			"\n$ph\n\n$ph\n",
+			'Splitting at one empty line',
+		);
+
+		$cases[] = array(
+			"First\n\n\n\n\nSecond\n\nThird",
+			"$ph\n\n\n\n\n$ph\n\n$ph",
+			'Splitting with multiple empty lines',
+		);
+
+		return $cases;
+	}
+}
