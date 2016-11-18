319c10bdad42ca37a8c657f6105e950733abe0e3
Federico Leva <federicoleva@tiscali.it>
2016-11-18 12:08:29 +0100

    Also remove recentchanges entry for deleted sandbox users
    Otherwise Special:RecentChanges will have incorrect rows such as
    newusers log entries stating a new account was created with user
    name identical to the current user's IP address.
    
    Bug: T151037
    Change-Id: I1f64ca64e2839c0036d7e2d0e4ae16ed4b61d347

--- utils/TranslateSandbox.php
+++ utils/TranslateSandbox.php
@@ -98,6 +98,7 @@ class TranslateSandbox {
 		$dbw->delete( 'user_groups', array( 'ug_user' => $uid ), __METHOD__ );
 		$dbw->delete( 'user_properties', array( 'up_user' => $uid ), __METHOD__ );
 		$dbw->delete( 'logging', array( 'log_user' => $uid ), __METHOD__ );
+		$dbw->delete( 'recentchanges', array( 'rc_user' => $uid ), __METHOD__ );
 
 		// If someone tries to access still object still, they will get anon user
 		// data.
