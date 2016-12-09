80622fe0af72b27c53ae8f70084e6782c4a75091
Aaron Schulz <aschulz@wikimedia.org>
2016-12-09 15:07:49 -0800

    Convert isSourcePage() to using WAN cache
    This deals with replication/snapshot lag and cache slams
    (via preemptive random invalidation).
    
    Change-Id: I07e0f533c2e67a821373ec87f295b7f90564850f

--- TranslateUtils.php
+++ TranslateUtils.php
@@ -385,7 +385,7 @@ class TranslateUtils {
 	/**
 	 * Get a DB handle suitable for read and read-for-write cases
 	 *
-	 * @return DatabaseBase Master for HTTP POST, CLI, DB already changed; slave otherwise
+	 * @return IDatabase Master for HTTP POST, CLI, DB already changed; slave otherwise
 	 */
 	public static function getSafeReadDB() {
 		// Parsing APIs need POST for payloads but are read-only, so avoid spamming
