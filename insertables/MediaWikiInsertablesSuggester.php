a37d6c9d73a950e2111c83d5fe2e098c1e0a3502
Phantom42 <nikitav30@gmail.com>
2016-12-25 20:11:07 +0200

    Use HTML tags as insertables for MediaWiki messages
    Bug: T106632
    Change-Id: I1abb3b1128ae8768cab48042dcb1657326776cde

--- insertables/MediaWikiInsertablesSuggester.php
+++ insertables/MediaWikiInsertablesSuggester.php
@@ -33,6 +33,13 @@ class MediaWikiInsertablesSuggester {
 		}, $matches );
 		$insertables = array_merge( $insertables, $new );
 
+		$matches = array();
+		preg_match_all( '/<\/?[a-z]+>/', $text, $matches, PREG_SET_ORDER );
+		$new = array_map( function( $match ) {
+			return new Insertable( $match[0], $match[0] );
+		}, $matches );
+		$insertables = array_merge( $insertables, $new );
+
 		return $insertables;
 	}
 }
