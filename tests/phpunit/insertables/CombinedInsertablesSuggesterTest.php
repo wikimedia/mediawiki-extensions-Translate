e2197ebda932644686abe9dfaaa41be8ce455ce3
Geoffrey Mon <geofbot@gmail.com>
2017-01-05 11:56:20 -0500

    Insertables suggester class for $1, $2 and allow multiple suggesters
    * New class NumericalParameterInsertablesSuggester for suggesting $1, $2,
      etc.
    * New class CombinedInsertablesSuggester for handling multiple
      suggesters
    * Modify YAML schema to take multiple suggester class names with
      'classes' option (single classes still allowed with 'class')
    * Unit tests
    
    Bug: T152837
    Change-Id: Iac7ff7bdc31e7a648b140349193df9077fcd7031

---
+++ tests/phpunit/insertables/CombinedInsertablesSuggesterTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Tests for class CombinedInsertablesSuggester
+ *
+ * @file
+ * @author Geoffrey Mon
+ * @license GPL-2.0+
+ */
+class CombinedInsertablesSuggesterTest extends MediaWikiTestCase {
+
+	/**
+	 * @dataProvider getInsertablesProvider
+	 */
+	public function testGetInsertables( $suggesters, $input, $expected ) {
+		$suggester = new CombinedInsertablesSuggester( $suggesters );
+		$this->assertArrayEquals( $expected, $suggester->getInsertables( $input ) );
+	}
+
+	public function getInsertablesProvider() {
+		return [
+			// Test basic combination of multiple InsertablesSuggesters
+			[
+				[
+					new TestingInsertablesSuggester(),
+					new NumericalParameterInsertablesSuggester(),
+				],
+				'test $1 foo $2 bar $3spam eggs',
+				[
+					new Insertable( 'Test', 'Test', '' ),
+					new Insertable( '$1', '$1', '' ),
+					new Insertable( '$2', '$2', '' ),
+					new Insertable( '$3', '$3', '' ),
+				]
+			],
+			// Test removal of duplicate suggestions
+			[
+				[
+					new NumericalParameterInsertablesSuggester(),
+					new NumericalParameterInsertablesSuggester(),
+				],
+				'test $1 duplicates $2 $3',
+				[
+					new Insertable( '$1', '$1', '' ),
+					new Insertable( '$2', '$2', '' ),
+					new Insertable( '$3', '$3', '' ),
+				]
+			],
+			// Test removal of duplicate suggestions
+			[
+				[
+					new TestingDuplicateInsertablesSuggester(),
+					new NumericalParameterInsertablesSuggester(),
+				],
+				'test $1 duplicates $2 $3',
+				[
+					new Insertable( '$1', '$1', '' ),
+					new Insertable( '$2', '$2', '' ),
+					new Insertable( '$3', '$3', '' ),
+					new Insertable( 'Test', 'Test', '' ),
+					new Insertable( '', 'Test', 'Test' ),
+				]
+			],
+			// Test no InsertablesSuggesters
+			[
+				[],
+				'test $1 duplicates $2 $3',
+				[]
+			],
+		];
+	}
+}
+
+class TestingInsertablesSuggester implements InsertablesSuggester {
+	public function getInsertables( $text ) {
+		return [ new Insertable( 'Test', 'Test', '' ) ];
+	}
+}
+
+class TestingDuplicateInsertablesSuggester implements InsertablesSuggester {
+	public function getInsertables( $text ) {
+		return [
+			new Insertable( '$1', '$1', '' ),
+			new Insertable( '$1', '$1', '' ),
+			new Insertable( 'Test', 'Test', '' ),
+			new Insertable( 'Test', 'Test', '' ),
+			new Insertable( '', 'Test', 'Test' ),
+		];
+	}
+}
