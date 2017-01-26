2a3ba3af47f0af4271af66efb6bb4629f89fe329
datguy <datguysteam@gmail.com>
2017-01-26 19:17:09 +0100

    Add confirmation box
    Adding a confirmation box if a user accepts/rejects a translator.
    
    Bug: T60706
    Change-Id: I13f132ed08e6be55363417db5930769bd1bc16b3

--- resources/js/ext.translate.special.managetranslatorsandbox.js
+++ resources/js/ext.translate.special.managetranslatorsandbox.js
@@ -122,6 +122,13 @@
 						.addClass( 'accept mw-ui-button mw-ui-progressive' )
 						.text( mw.msg( 'tsb-accept-button-label' ) )
 						.on( 'click', function () {
+
+							if ( window.confirm("Are you sure?") == true {
+								continue;
+							} else {
+								break;
+							}
+
 							window.tsbUpdatingUsers = true;
 
 							doApiAction( {
@@ -137,6 +144,12 @@
 						.addClass( 'reject mw-ui-button mw-ui-destructive' )
 						.text( mw.msg( 'tsb-reject-button-label' ) )
 						.on( 'click', function () {
+							if ( window.confirm("Are you sure?") == true {
+								continue;
+							} else {
+								break;
+							}
+
 							window.tsbUpdatingUsers = true;
 
 							doApiAction( {
@@ -280,6 +293,12 @@
 						.addClass( 'accept-all mw-ui-button mw-ui-progressive' )
 						.text( mw.msg( 'tsb-accept-all-button-label' ) )
 						.on( 'click', function () {
+							if ( window.confirm("Are you sure?") == true {
+								continue;
+							} else {
+								break;
+							}
+							
 							window.tsbUpdatingUsers = true;
 
 							doApiAction( {
@@ -295,6 +314,12 @@
 						.addClass( 'reject-all mw-ui-button mw-ui-destructive' )
 						.text( mw.msg( 'tsb-reject-all-button-label' ) )
 						.on( 'click', function () {
+							if ( window.confirm("Are you sure?") == true {
+								continue;
+							} else {
+								break;
+							}
+
 							window.tsbUpdatingUsers = true;
 
 							doApiAction( {
