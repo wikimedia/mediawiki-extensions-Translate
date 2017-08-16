4f0e3f1b488c55ec96f838d53aa74ed1bb6bbe95
Niklas Laxström <niklas.laxstrom@gmail.com>
2017-08-16 14:54:00 +0200

    Add RESTBaseWebService
    This allows to use Apertium using CXServer MT API through RESTBase.
    
    Example configuration:
    $GLOBALS['wgTranslateTranslationServices']['Restbase (WMF)'] = array(
    	'type' => 'restbase',
    	'host' => 'https://wikimedia.org/api',
    );
    
    Change-Id: I3e8130df2e302be629d9cc9c3c591eb194eb7d77

---
+++ webservices/RESTBaseWebService.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Contains a class for querying external translation service.
+ *
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ */
+
+/**
+ * Implements support for cxserver proxied through RESTBase
+ * @ingroup TranslationWebService
+ * @since 2017.10
+ */
+class RESTBaseWebService extends TranslationWebService {
+	public function getType() {
+		return 'mt';
+	}
+
+	protected function mapCode( $code ) {
+		return $code;
+	}
+
+	protected function doPairs() {
+		if ( !isset( $this->config['host'] ) ) {
+			throw new TranslationWebServiceConfigurationException( 'RESTBase host not set' );
+		}
+
+		$pairs = [];
+
+		$url = $this->config['host'] . '/rest_v1/transform/list/tool/mt/';
+		$json = Http::get(
+			$url,
+			[ $this->config['timeout'] ],
+			__METHOD__
+		);
+		$response = FormatJson::decode( $json, true );
+
+		if ( !is_array( $response ) ) {
+			$exception = 'Malformed reply from remote server: ' . $url . ' ' . (string)$json;
+			throw new TranslationWebServiceException( $exception );
+		}
+
+		foreach ( $response['Apertium'] as $source => $targets ) {
+			foreach ( $targets as $target ) {
+				$pairs[$source][$target] = true;
+			}
+		}
+
+		return $pairs;
+	}
+
+	protected function getQuery( $text, $from, $to ) {
+		if ( !isset( $this->config['host'] ) ) {
+			throw new TranslationWebServiceConfigurationException( 'RESTBase host not set' );
+		}
+
+		$text = trim( $text );
+		$text = $this->wrapUntranslatable( $text );
+		$url = $this->config['host'] . "/rest_v1/transform/html/from/$from/to/$to/Apertium";
+
+		return TranslationQuery::factory( $url )
+			->timeout( $this->config['timeout'] )
+			->postWithData( wfArrayToCgi( [ 'html' => $text ] ) );
+	}
+
+	protected function parseResponse( TranslationQueryResponse $reply ) {
+		$body = $reply->getBody();
+
+		throw new TranslationWebServiceException( $body );
+		$response = FormatJson::decode( $body );
+		if ( !is_object( $response ) ) {
+			throw new TranslationWebServiceException( 'Invalid json: ' . serialize( $body ) );
+		}
+
+		$text = preg_replace( '~^<div>(.*)</div>$~', '\1', $response->contents );
+		$text = $this->unwrapUntranslatable( $text );
+
+		return trim( $text );
+	}
+}
