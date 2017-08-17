11951230cfcb90599d6d3925678626f6b594b869
Niklas Laxström <niklas.laxstrom@gmail.com>
2017-08-17 21:12:38 +0200

    Remove almost all legacy non-TUX code
    Some of boxes shown on action=edit can still remain.
    
    Change-Id: I0b09da0d576a35bc79f751251d5952e4993c424c

--- Resources.php
+++ Resources.php
@@ -249,18 +249,7 @@ $wgResourceModules['ext.translate.parsers'] = [
 ] + $resourcePaths;
 
 $wgResourceModules['ext.translate.quickedit'] = [
-	'scripts' => 'resources/js/ext.translate.quickedit.js',
 	'styles' => 'resources/css/ext.translate.quickedit.css',
-	'messages' => [ 'translate-js-nonext', 'translate-js-save-failed' ],
-	'dependencies' => [
-		'ext.translate.hooks',
-		'jquery.accessKeyLabel',
-		'jquery.autosize',
-		'jquery.form',
-		'jquery.ui.dialog',
-		'mediawiki.util',
-	],
-	'targets' => [ 'desktop' ],
 ] + $resourcePaths;
 
 $wgResourceModules['ext.translate.selecttoinput'] = [
@@ -478,18 +467,6 @@ $wgResourceModules['ext.translate.special.translate'] = [
 		'translate-documentation-language',
 		'tux-editor-proofreading-hide-own-translations',
 		'tux-editor-proofreading-show-own-translations',
-	],
-] + $resourcePaths;
-
-$wgResourceModules['ext.translate.special.translate.legacy'] = [
-	'scripts' => 'resources/js/ext.translate.special.translate.legacy.js',
-	'dependencies' => [
-		'mediawiki.api',
-	],
-	'messages' => [
-		'translate-messagereview-done',
-		'translate-messagereview-failure',
-		'translate-messagereview-progress',
 	],
 ] + $resourcePaths;
 
