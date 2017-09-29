03ade0241e15a46044998f4c49b4402c3b0b6970
Željko Filipin <zeljko.filipin@gmail.com>
2017-09-29 17:54:14 +0200

    Deleted all Ruby code
    The only Ruby code were broken Selenium tests.
    
    Bug: T164479
    Change-Id: I7506dfb6250237f45dcea5c4dc64a864fa321732
    Depends-On: I3d830320891d400fb78ff5f4c97859443983d257

--- tests/browser/features/support/pages/translate_page.rb
+++
@@ -1,8 +0,0 @@
-class TranslatePage
-  include PageObject
-
-  page_url 'Special:Translate?<%=params[:extra]%>'
-
-  div(:workflow_state, class: 'tux-workflow-status')
-  ul(:workflow_state_selector, class: 'tux-workflow-status-selector')
-end
