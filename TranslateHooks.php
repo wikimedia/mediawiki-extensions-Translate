e97447f49df10a7d7a484524150042e5c137bb92
James D. Forrester <jforrester@wikimedia.org>
2017-11-13 12:57:42 -0800

    [WIP] Convert to static extension loading
    What a mess. Not tested. Almost certainly doesn't work, and some bits
    definitely won't.
    
    Bug: T87985
    Change-Id: I410a4d499c44a18fb686626775f17187547e74f1

--- TranslateHooks.php
+++ TranslateHooks.php
@@ -828,3 +828,43 @@ class TranslateHooks {
 		$resourceLoader->register( $modules );
 	}
 }
+
+/**
+ * Version number used in extension credits and in other places where needed.
+ */
+define( 'TRANSLATE_VERSION', '2017-10-30' );
+
+/**
+ * Helper function for adding namespace for message groups.
+ *
+ * It defines constants for the namespace (and talk namespace) and sets up
+ * restrictions and some other configuration.
+ * @param \int $id Namespace number
+ * @param \string $name Name of the namespace
+ * @param \string $constant (optional) name of namespace constant, defaults to
+ *   NS_ followed by upper case version of $name, e.g., NS_MEDIAWIKI
+ */
+function wfAddNamespace( $id, $name, $constant = null ) {
+	global $wgExtraNamespaces, $wgContentNamespaces, $wgTranslateMessageNamespaces,
+		$wgNamespaceProtection, $wgNamespacesWithSubpages, $wgNamespacesToBeSearchedDefault;
+
+	if ( is_null( $constant ) ) {
+		$constant = strtoupper( "NS_$name" );
+	}
+
+	define( $constant, $id );
+	define( $constant . '_TALK', $id + 1 );
+
+	$wgExtraNamespaces[$id] = $name;
+	$wgExtraNamespaces[$id + 1] = $name . '_talk';
+
+	$wgContentNamespaces[] = $id;
+	$wgTranslateMessageNamespaces[] = $id;
+
+	$wgNamespacesWithSubpages[$id] = true;
+	$wgNamespacesWithSubpages[$id + 1] = true;
+
+	$wgNamespaceProtection[$id] = [ 'translate' ];
+
+	$wgNamespacesToBeSearchedDefault[$id] = true;
+}
