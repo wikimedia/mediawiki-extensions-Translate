8b496e68d7cca26fe7f4c958c92fe56aa908b753
Niklas Laxström <niklas.laxstrom@gmail.com>
2017-12-12 19:47:22 +0100

    Reduce queries on typical ApiQueryTranslationsAids api call
    This replaces hundreds of queries with just single database query.
    The actual amount of queries depends on how many translations for
    a message exists in the database.
    
    There were multiple issues:
    * The static cache in TranslationAid::getTranslations did not work.
      It seemed like it should have worked, maybe HHVM bug but not sure.
      This caused all queries to be executed twice.
    * There was one query to find all the page titles for translations,
      and a second query to load the text contents.
    * After that we would query the revtag table once for each language
    * We would also query the message index once per each language.
    
    The fix is to create a new class that caches the query results.
    Instances of the new class is injected into the translation aids,
    so the cache lifetime is nicely bound. The old static cache, if it
    worked, could grow indefinitely and leak memory.
    
    The new class does everything in one query and uses simple string
    manipulation to avoid querying the message index.
    
    The new query has the following query plan:
    *************************** 1. row ***************************
               id: 1
      select_type: SIMPLE
            table: bw_page
             type: range
    possible_keys: PRIMARY,name_title
              key: name_title
          key_len: 261
              ref: NULL
             rows: 109
            Extra: Using index condition
    *************************** 2. row ***************************
               id: 1
      select_type: SIMPLE
            table: bw_revtag
             type: eq_ref
    possible_keys: rt_type_page_revision,rt_revision_type
              key: rt_type_page_revision
          key_len: 70
              ref: const,translatewiki_net.bw_page.page_id,translatewiki_net.bw_page.page_latest
             rows: 1
            Extra: Using where; Using index; Not exists
    *************************** 3. row ***************************
               id: 1
      select_type: SIMPLE
            table: bw_revision
             type: eq_ref
    possible_keys: PRIMARY,rev_id,page_timestamp,page_user_timestamp
              key: PRIMARY
          key_len: 8
              ref: translatewiki_net.bw_page.page_id,translatewiki_net.bw_page.page_latest
             rows: 1
            Extra:
    *************************** 4. row ***************************
               id: 1
      select_type: SIMPLE
            table: bw_text
             type: eq_ref
    possible_keys: PRIMARY
              key: PRIMARY
          key_len: 4
              ref: translatewiki_net.bw_revision.rev_text_id
             rows: 1
            Extra:
    
    Bug: T187165
    Change-Id: I22f0bfdd948f3777bdecad85bf92719307da3a02

---
+++ translationaids/TranslationAidDataProvider.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Translation aid code.
+ *
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ */
+
+/**
+ * @since 2018.01
+ */
+class TranslationAidDataProvider {
+	private $handle;
+	private $group;
+
+	private $definition;
+	private $translations;
+
+	public function __construct( MessageHandle $handle ) {
+		$this->handle = $handle;
+		$this->group = $handle->getGroup();
+	}
+
+	/**
+	 * Get the message definition. Cached for performance.
+	 *
+	 * @return string
+	 */
+	public function getDefinition() {
+		if ( $this->definition !== null ) {
+			return $this->definition;
+		}
+
+		// Optional performance optimization
+		if ( method_exists( $this->group, 'getMessageContent' ) ) {
+			$this->definition = $this->group->getMessageContent( $this->handle );
+		} else {
+			$this->definition = $this->group->getMessage(
+				$this->handle->getKey(),
+				$this->group->getSourceLanguage()
+			);
+		}
+
+		return $this->definition;
+	}
+
+	/**
+	 * @return Content
+	 */
+	public function getDefinitionContent() {
+		return ContentHandler::makeContent( $this->getDefinition(), $this->handle->getTitle() );
+	}
+
+	/**
+	 * Get the translations in all languages. Cached for performance.
+	 * Fuzzy translation are not included.
+	 *
+	 * @return array Language code => Translation
+	 */
+	public function getGoodTranslations() {
+		if ( $this->translations !== null ) {
+			return $this->translations;
+		}
+
+		$data = self::loadTranslationData( wfGetDB( DB_REPLICA ), $this->handle );
+		$translations = [];
+		$prefixLength = strlen( $this->handle->getTitleForBase()->getDBKey() . '/' );
+
+		foreach ( $data as $page => $translation ) {
+			// Could use MessageHandle here, but that queries the message index.
+			// Instead we can get away with simple string manipulation.
+			$code = substr( $page, $prefixLength );
+			if ( !Language::isKnownLanguageTag( $code ) ) {
+				continue;
+			}
+
+			$translations[ $code ] = $translation;
+		}
+
+		$this->translations = $translations;
+
+		return $translations;
+	}
+
+	private static function loadTranslationData( IDatabase $db, MessageHandle $handle ) {
+		if ( method_exists( 'Revision', 'getQueryInfo' ) ) {
+			$queryInfo = Revision::getQueryInfo( [ 'page', 'text' ] );
+			$tables = $queryInfo[ 'tables' ];
+			$fields = $queryInfo[ 'fields' ];
+			$conds = [];
+			$options = [];
+			$joins = $queryInfo[ 'joins' ];
+		} else {
+			// BC for <= MW 1.31
+			$tables = [ 'page', 'text', 'revision' ];
+			$fields = array_merge(
+				Revision::selectFields(),
+				Revision::selectPageFields(),
+				Revision::selectTextFields()
+			);
+			$conds = [];
+			$options = [];
+			$joins = [
+				'page' => Revision::pageJoinCond(),
+				'text' => [ 'INNER JOIN', [ 'rev_text_id=old_id' ] ]
+			];
+		}
+
+		// The list of pages we want to select, and their latest versions
+		$conds['page_namespace'] = $handle->getTitle()->getNamespace();
+		$base = $handle->getKey();
+		$conds[] = 'page_title ' . $db->buildLike( "$base/", $db->anyString() );
+		$conds[] = 'rev_id=page_latest';
+
+		// For fuzzy tags we also need:
+		$tables[] = 'revtag';
+		$conds[ 'rt_type' ] = null;
+		$joins[ 'revtag' ] = [
+			'LEFT JOIN',
+			[ 'page_id=rt_page', 'page_latest=rt_revision', 'rt_type' => 'fuzzy' ]
+		];
+
+		$rows = $db->select( $tables, $fields, $conds, __METHOD__, $options, $joins );
+
+		$pages = [];
+		foreach ( $rows as $row ) {
+			$pages[$row->page_title] = Revision::getRevisionText( $row );
+		}
+
+		return $pages;
+	}
+}
