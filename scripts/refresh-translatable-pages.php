e4f89b99aa3af89482c8f641b7a580feca6029ef
Niklas Laxström <niklas.laxstrom@gmail.com>
2018-06-01 08:43:07 +0200

    refresh-translatable-pages.php: add JobQueue option
    For wikis with lots of translatable pages (thousands) this script
    becomes unwieldy in both running time and memory usage (due to
    in process caches leaking memory). JobQueue can be used to solve
    both issues.
    
    Bug: T195347
    Change-Id: I0580d096f3fe036951feaf5d404fd24bc1834d9b

--- scripts/refresh-translatable-pages.php
+++ scripts/refresh-translatable-pages.php
@@ -25,11 +25,13 @@ class RefreshTranslatablePages extends Maintenance {
 		parent::__construct();
 		$this->mDescription = 'Ensure all translation pages are up to date.';
 		$this->setBatchSize( 300 );
+		$this->addOption( 'jobqueue', 'Use JobQueue (asynchronous)' );
 	}
 
 	public function execute() {
 		$groups = MessageGroups::singleton()->getGroups();
 		$counter = 0;
+		$useJobQueue = $this->hasOption( 'jobqueue' );
 
 		/** @var MessageGroup $group */
 		foreach ( $groups as $group ) {
@@ -44,12 +46,20 @@ class RefreshTranslatablePages extends Maintenance {
 
 			$page = TranslatablePage::newFromTitle( $group->getTitle() );
 			$jobs = TranslationsUpdateJob::getRenderJobs( $page );
-			foreach ( $jobs as $job ) {
-				$job->run();
+			if ( $useJobQueue ) {
+				JobQueueGroup::singleton()->push( $jobs );
+			} else {
+				foreach ( $jobs as $job ) {
+					$job->run();
+				}
 			}
 		}
 
-		$this->output( "Refreshed $counter translatable pages.\n" );
+		if ( $useJobQueue ) {
+			$this->output( "Queued refresh for $counter translatable pages.\n" );
+		} else {
+			$this->output( "Refreshed $counter translatable pages.\n" );
+		}
 	}
 }
 
