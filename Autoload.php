wikimedia/mediawiki-extensions-Translate

commit 2df54a21e3d33fc2a6f6d81d241edd661b60f73c

Reduce contention on translate_messagegroupstats
Main changes in this patch:
* Instead of doing cache clear (and sometimes rebuild), I have updated
  the code to rebuild (without using cache to avoid stale values) the
  values instead. This reduces the situations where cache is missing
  values.
* I removed the time limit option that was doing write queries during
  GET requests. This has been replaced with DeferredUpdate that runs
  after user request is served. Hopefully this also avoids warnings
  about write queries during GET requests - if not, then it would need
  to defer to the JobQueue.

The action=purge on the special pages is retained, but it now requires
a POST action that can be done with the displayed form. This allows
users to purge part of the cache if it gets out of sync.

The timelimit parameter for the api modules is deprecated and no
longer does anything.

Part of the code of MessageGroupStats::forItemInternal was split to
a separate function ::calculateAggregageGroup to allow fixing a bug
where AggregateMessageGroup with no members did not get its stats
saved to the database. With the new FLAG_NO_CACHE it would then be
permanently shown as missing stats.

Most of the functions in MessageGroupStats got a new $flags param:
in this case it feels a cleaner solution that introducing new
variants for the many for* functions.

This patch does not fully prevent stampedes. In case of an empty
cache, many simultaneous requests to the api or special pages will
cause many simultaneous cache rebuilds in the deferred updates.
Bug due to above changes, the cache should always be nearly 100%
up to date. According to codesearch, there is one call outside
Translate extension to MessageGroupStats::clear*, and that is in
TranslateSVG which is currently not in use anywhere.

Bug: T134252
Change-Id: I3892802fae3c944218d7122cf3f9e1ff9f453678

TranslateEditAddons.php
@@ -225,10 +225,13 @@ class TranslateEditAddons {
 		self::updateFuzzyTag( $title, $rev, $fuzzy );
 
 		$group = $handle->getGroup();
-		// Update translation stats - source language should always be update
+		// Update translation stats - source language should always be up to date
 		if ( $handle->getCode() !== $group->getSourceLanguage() ) {
-			MessageGroupStats::clear( $handle );
-			MessageGroupStats::forItem( $group->getId(), $handle->getCode() );
+			MessageGroupStats::forItem(
+				$group->getId(),
+				$handle->getCode(),
+				MessageGroupStats::FLAG_NO_CACHE
+			);
 		}
 
 		MessageGroupStatesUpdaterJob::onChange( $handle );

specials/SpecialLanguageStats.php
@@ -36,13 +36,6 @@ class SpecialLanguageStats extends SpecialPage {
 	protected $totals;
 
 	/**
-	 * How long spend time calculating missing numbers, before
-	 * bailing out.
-	 * @var int
-	 */
-	protected $timelimit = 2;
-
-	/**
 	 * Flag to set if nothing to show.
 	 * @var bool
 	 */
@@ -110,6 +103,11 @@ class SpecialLanguageStats extends SpecialPage {
 		$request = $this->getRequest();
 
 		$this->purge = $request->getVal( 'action' ) === 'purge';
+		if ( $this->purge && !$request->wasPosted() ) {
+			$this->showPurgeForm();
+			return;
+		}
+
 		$this->table = new StatsTable();
 
 		$this->setHeaders();
@@ -153,12 +151,20 @@ class SpecialLanguageStats extends SpecialPage {
 
 		if ( $this->isValidValue( $this->target ) ) {
 			$this->outputIntroduction();
-			$output = $this->getTable();
+
+			$stats = $this->loadStatistics( $this->target, MessageGroupStats::FLAG_CACHE_ONLY );
+			$output = $this->getTable( $stats );
 			if ( $this->incomplete ) {
 				$out->wrapWikiMsg(
 					"<div class='error'>$1</div>",
 					'translate-langstats-incomplete'
 				);
+
+				// $this->purge is only true if request was posted
+				DeferredUpdates::addCallableUpdate( function () {
+					$flags = $this->purge ? MessageGroupStats::FLAG_NO_CACHE : 0;
+					$this->loadStatistics( $this->target, $flags );
+				} );
 			}
 			if ( $this->nothing ) {
 				$out->wrapWikiMsg( "<div class='error'>$1</div>", 'translate-mgs-nothing' );
@@ -167,6 +173,16 @@ class SpecialLanguageStats extends SpecialPage {
 		} elseif ( $submitted ) {
 			$this->invalidTarget();
 		}
+	}
+
+	/**
+	 * Get stats
+	 * @param string $target For which target to get stats
+	 * @param int $flags See MessageGroupStats for possible flags
+	 * @return array[]
+	 */
+	protected function loadStatistics( $target, $flags ) {
+		return MessageGroupStats::forLanguage( $target, $flags );
 	}
 
 	/**
@@ -188,6 +204,24 @@ class SpecialLanguageStats extends SpecialPage {
 			"<div class='error'>$1</div>",
 			'translate-page-no-such-language'
 		);
+	}
+
+	protected function showPurgeForm() {
+		$formDescriptor[ 'intro' ] = [
+			'type' => 'info',
+			'vertical-label' => true,
+			'raw' => true,
+			'default' => $this->msg( 'confirm-purge-top' )->parse()
+		];
+
+		$context = new DerivativeContext( $this->getContext() );
+		$requestValues = $this->getRequest()->getQueryValues();
+
+		HTMLForm::factory( 'ooui', $formDescriptor, $context )
+			->setWrapperLegendMsg( 'confirm-purge-title' )
+			->setSubmitTextMsg( 'confirm_purge_button' )
+			->addHiddenFields( $requestValues )
+			->show();
 	}
 
 	/**
@@ -333,24 +367,18 @@ class SpecialLanguageStats extends SpecialPage {
 
 	/**
 	 * Returns the table itself.
+	 * @param array $stats
 	 * @return string HTML
 	 */
-	protected function getTable() {
+	protected function getTable( $stats ) {
 		$table = $this->table;
 
 		$this->addWorkflowStatesColumn();
 		$out = '';
 
-		MessageGroupStats::setTimeLimit( $this->timelimit );
-		$cache = MessageGroupStats::forLanguage( $this->target );
-
-		if ( $this->purge ) {
-			MessageGroupStats::clearLanguage( $this->target );
-		}
-
 		$structure = MessageGroups::getGroupStructure();
 		foreach ( $structure as $item ) {
-			$out .= $this->makeGroupGroup( $item, $cache );
+			$out .= $this->makeGroupGroup( $item, $stats );
 		}
 
 		if ( $out ) {
@@ -373,12 +401,6 @@ class SpecialLanguageStats extends SpecialPage {
 
 			return '';
 		}
-
-		/// @todo Allow extra message here, once total translated volume goes
-		///       over a certain percentage? (former live hack at translatewiki)
-		/// if ( $this->totals['2'] && ( $this->totals['1'] / $this->totals['2'] ) > 0.95 ) {
-		/// 	$out .= $this->msg( 'translate-somekey' );
-		/// }
 	}
 
 	/**
@@ -386,7 +408,7 @@ class SpecialLanguageStats extends SpecialPage {
 	 * If $item is an array, meaning that the first group is an
 	 * AggregateMessageGroup and the latter are its children, it will recurse
 	 * and create rows for them too.
-	 * @param array|MessageGroup $item
+	 * @param MessageGroup|MessageGroup[] $item
 	 * @param array $cache Cache as returned by MessageGroupStats::forLanguage
 	 * @param MessageGroup|null $parent MessageGroup (do not use, used internally only)
 	 * @return string
@@ -439,6 +461,10 @@ class SpecialLanguageStats extends SpecialPage {
 			return '';
 		}
 
+		if ( $total === null ) {
+			$this->incomplete = true;
+		}
+
 		// Calculation of summary row values
 		if ( !$group instanceof AggregateMessageGroup &&
 			!isset( $this->statsCounted[$groupId] )
@@ -449,6 +475,7 @@ class SpecialLanguageStats extends SpecialPage {
 
 		$state = $this->getWorkflowStateValue( $groupId );
 
+		// Place any state checks like $this->incomplete above this
 		$params = $stats;
 		$params[] = $state;
 		$params[] = md5( $groupId );
@@ -456,14 +483,12 @@ class SpecialLanguageStats extends SpecialPage {
 		$params[] = md5( $this->target );
 		$cachekey = wfMemcKey( __METHOD__, implode( '-', $params ) );
 		$cacheval = wfGetCache( CACHE_ANYTHING )->get( $cachekey );
-		if ( !$this->purge && is_string( $cacheval ) ) {
+		if ( is_string( $cacheval ) ) {
 			return $cacheval;
 		}
 
 		$extra = [];
-		if ( $total === null ) {
-			$this->incomplete = true;
-		} elseif ( $translated === $total ) {
+		if ( $translated === $total ) {
 			$extra = [ 'action' => 'proofread' ];
 		}
 

specials/SpecialMessageGroupStats.php
@@ -36,8 +36,9 @@ class SpecialMessageGroupStats extends SpecialLanguageStats {
 		return $this->msg( 'translate-mgs-pagename' )->text();
 	}
 
-	protected function getGroupName() {
-		return 'wiki';
+	/// Overwritten from SpecialLanguageStats
+	protected function loadStatistics( $target, $flags = 0 ) {
+		return MessageGroupStats::forGroup( $target, $flags );
 	}
 
 	/// Overwritten from SpecialLanguageStats
@@ -121,35 +122,24 @@ class SpecialMessageGroupStats extends SpecialLanguageStats {
 			->displayForm( false );
 	}
 
-	/**
-	 * Overwritten from SpecialLanguageStats
-	 *
-	 * @return string
-	 */
-	protected function getTable() {
+	/// Overwritten from SpecialLanguageStats
+	protected function getTable( $stats ) {
 		$table = $this->table;
 
 		$this->addWorkflowStatesColumn();
 		$out = '';
-
-		if ( $this->purge ) {
-			MessageGroupStats::clearGroup( $this->target );
-		}
-
-		MessageGroupStats::setTimeLimit( $this->timelimit );
-		$cache = MessageGroupStats::forGroup( $this->target );
 
 		$this->numberOfShownLanguages = 0;
 		$languages = array_keys(
 			TranslateUtils::getLanguageNames( $this->getLanguage()->getCode() )
 		);
 		sort( $languages );
-		$this->filterPriorityLangs( $languages, $this->target, $cache );
+		$this->filterPriorityLangs( $languages, $this->target, $stats );
 		foreach ( $languages as $code ) {
 			if ( $table->isBlacklisted( $this->target, $code ) !== null ) {
 				continue;
 			}
-			$out .= $this->makeRow( $code, $cache );
+			$out .= $this->makeRow( $code, $stats );
 		}
 
 		if ( $out ) {

utils/MessageGroupStats.php
@@ -28,31 +28,13 @@ class MessageGroupStats {
 	const FUZZY = 2; ///< Array index
 	const PROOFREAD = 3; ///< Array index
 
-	/**
-	 * @var float|null
-	 */
-	protected static $timeStart = null;
-
-	/**
-	 * @var float|null
-	 */
-	protected static $limit = null;
+	const FLAG_CACHE_ONLY = 1;
+	const FLAG_NO_CACHE = 2;
 
 	/**
 	 * @var array[]
 	 */
 	protected static $updates = [];
-
-	/**
-	 * Set the maximum time statistics are calculated.
-	 * If the time limit is exceeded, the missing
-	 * entries will be null.
-	 * @param float $limit time in seconds
-	 */
-	public static function setTimeLimit( $limit ) {
-		self::$timeStart = microtime( true );
-		self::$limit = $limit;
-	}
 
 	/**
 	 * Returns empty stats array. Useful because the number of elements
@@ -78,10 +60,11 @@ class MessageGroupStats {
 	 * Returns stats for given group in given language.
 	 * @param string $id Group id
 	 * @param string $code Language code
+	 * @param int $flags Combination of FLAG_* constants.
 	 * @return null[]|int[]
 	 */
-	public static function forItem( $id, $code ) {
-		$res = self::selectRowsIdLang( [ $id ], $code );
+	public static function forItem( $id, $code, $flags = 0 ) {
+		$res = self::selectRowsIdLang( [ $id ], $code, $flags );
 		$stats = self::extractResults( $res, [ $id ] );
 
 		/* In case some code calls this for dynamic groups, return the default
@@ -93,7 +76,7 @@ class MessageGroupStats {
 		}
 
 		if ( !isset( $stats[$id][$code] ) ) {
-			$stats[$id][$code] = self::forItemInternal( $stats, $group, $code );
+			$stats[$id][$code] = self::forItemInternal( $stats, $group, $code, $flags );
 		}
 
 		self::queueUpdates();
@@ -104,10 +87,11 @@ class MessageGroupStats {
 	/**
 	 * Returns stats for all groups in given language.
 	 * @param string $code Language code
-	 * @return array
+	 * @param int $flags Combination of FLAG_* constants.
+	 * @return array[]
 	 */
-	public static function forLanguage( $code ) {
-		$stats = self::forLanguageInternal( $code );
+	public static function forLanguage( $code, $flags = 0 ) {
+		$stats = self::forLanguageInternal( $code, [], $flags );
 		$flattened = [];
 		foreach ( $stats as $group => $languages ) {
 			$flattened[$group] = $languages[$code];
@@ -121,14 +105,15 @@ class MessageGroupStats {
 	/**
 	 * Returns stats for all languages in given group.
 	 * @param string $id Group id
-	 * @return array
+	 * @param int $flags Combination of FLAG_* constants.
+	 * @return array[]
 	 */
-	public static function forGroup( $id ) {
+	public static function forGroup( $id, $flags = 0 ) {
 		$group = MessageGroups::getGroup( $id );
 		if ( $group === null ) {
 			return [];
 		}
-		$stats = self::forGroupInternal( $group );
+		$stats = self::forGroupInternal( $group, [], $flags );
 
 		self::queueUpdates();
 
@@ -139,13 +124,14 @@ class MessageGroupStats {
 	 * Returns stats for all group in all languages.
 	 * Might be slow, might use lots of memory.
 	 * Returns two dimensional array indexed by group and language.
-	 * @return array
+	 * @param int $flags Combination of FLAG_* constants.
+	 * @return array[]
 	 */
-	public static function forEverything() {
+	public static function forEverything( $flags = 0 ) {
 		$groups = MessageGroups::singleton()->getGroups();
 		$stats = [];
 		foreach ( $groups as $g ) {
-			$stats = self::forGroupInternal( $g, $stats );
+			$stats = self::forGroupInternal( $g, $stats, $flags );
 		}
 
 		self::queueUpdates();
@@ -267,20 +253,21 @@ class MessageGroupStats {
 	/**
 	 * @param string $code Language code
 	 * @param array[] $stats
+	 * @param int $flags Combination of FLAG_* constants.
 	 * @return array[]
 	 */
-	protected static function forLanguageInternal( $code, array $stats = [] ) {
+	protected static function forLanguageInternal( $code, array $stats = [], $flags ) {
 		$groups = MessageGroups::singleton()->getGroups();
 
 		$ids = array_keys( $groups );
-		$res = self::selectRowsIdLang( null, $code );
+		$res = self::selectRowsIdLang( null, $code, $flags );
 		$stats = self::extractResults( $res, $ids, $stats );
 
 		foreach ( $groups as $id => $group ) {
 			if ( isset( $stats[$id][$code] ) ) {
 				continue;
 			}
-			$stats[$id][$code] = self::forItemInternal( $stats, $group, $code );
+			$stats[$id][$code] = self::forItemInternal( $stats, $group, $code, $flags );
 		}
 
 		return $stats;
@@ -308,11 +295,12 @@ class MessageGroupStats {
 	/**
 	 * @param MessageGroup $group
 	 * @param array[] $stats
+	 * @param int $flags Combination of FLAG_* constants.
 	 * @return array[]
 	 */
-	protected static function forGroupInternal( $group, array $stats = [] ) {
+	protected static function forGroupInternal( $group, array $stats = [], $flags ) {
 		$id = $group->getId();
-		$res = self::selectRowsIdLang( [ $id ], null );
+		$res = self::selectRowsIdLang( [ $id ], null, $flags );
 		$stats = self::extractResults( $res, [ $id ], $stats );
 
 		# Go over each language filling missing entries
@@ -323,7 +311,7 @@ class MessageGroupStats {
 			if ( isset( $stats[$id][$code] ) ) {
 				continue;
 			}
-			$stats[$id][$code] = self::forItemInternal( $stats, $group, $code );
+			$stats[$id][$code] = self::forItemInternal( $stats, $group, $code, $flags );
 		}
 
 		// This is for sorting the values added later in correct order
@@ -339,9 +327,14 @@ class MessageGroupStats {
 	 *
 	 * @param null|string[] $ids List of message group ids
 	 * @param null|string[] $codes List of language codes
+	 * @param int $flags Combination of FLAG_* constants.
 	 * @return Traversable Database result object
 	 */
-	protected static function selectRowsIdLang( $ids = null, $codes = null ) {
+	protected static function selectRowsIdLang( $ids = null, $codes = null, $flags ) {
+		if ( $flags & self::FLAG_NO_CACHE ) {
+			return [];
+		}
+
 		$conds = [];
 		if ( $ids !== null ) {
 			$dbids = array_map( 'self::getDatabaseIdForGroupId', $ids );
@@ -362,49 +355,24 @@ class MessageGroupStats {
 	 * @param array[] &$stats
 	 * @param MessageGroup $group
 	 * @param string $code Language code
-	 *
+	 * @param int $flags Combination of FLAG_* constants.
 	 * @return null[]|int[]
 	 */
-	protected static function forItemInternal( &$stats, $group, $code ) {
+	protected static function forItemInternal( &$stats, $group, $code, $flags ) {
 		$id = $group->getId();
 
-		if ( self::$timeStart !== null && ( microtime( true ) - self::$timeStart ) > self::$limit ) {
+		if ( $flags & self::FLAG_CACHE_ONLY ) {
 			$stats[$id][$code] = self::getUnknownStats();
 			return $stats[$id][$code];
 		}
 
 		if ( $group instanceof AggregateMessageGroup ) {
-			$aggregates = self::getEmptyStats();
-
-			$expanded = self::expandAggregates( $group );
-			if ( $expanded === [] ) {
-				return $aggregates;
-			}
-
-			$subGroupIds = array_keys( $expanded );
-			$res = self::selectRowsIdLang( $subGroupIds, $code );
-			$stats = self::extractResults( $res, $subGroupIds, $stats );
-
-			foreach ( $expanded as $sid => $subgroup ) {
-				# Discouraged groups may belong to another group, usually if there
-				# is an aggregate group for all translatable pages. In that case
-				# calculate and store the statistics, but don't count them as part of
-				# the aggregate group, so that the numbers in Special:LanguageStats
-				# add up. The statistics for discouraged groups can still be viewed
-				# through Special:MessageGroupStats.
-				if ( !isset( $stats[$sid][$code] ) ) {
-					$stats[$sid][$code] = self::forItemInternal( $stats, $subgroup, $code );
-				}
-
-				$include = Hooks::run( 'Translate:MessageGroupStats:isIncluded', [ $sid, $code ] );
-				if ( $include ) {
-					$aggregates = self::multiAdd( $aggregates, $stats[$sid][$code] );
-				}
-			}
-			$stats[$id][$code] = $aggregates;
+			$aggregates = self::calculateAggregageGroup( $stats, $group, $code, $flags );
 		} else {
 			$aggregates = self::calculateGroup( $group, $code );
 		}
+		// Cache for use in subsequent forItemInternal calls
+		$stats[$id][$code] = $aggregates;
 
 		// Don't add nulls to the database, causes annoying warnings
 		if ( $aggregates[self::TOTAL] === null ) {
@@ -419,6 +387,38 @@ class MessageGroupStats {
 			'tgs_fuzzy' => $aggregates[self::FUZZY],
 			'tgs_proofread' => $aggregates[self::PROOFREAD],
 		];
+
+		return $aggregates;
+	}
+
+	private static function calculateAggregageGroup( &$stats, $group, $code, $flags ) {
+		$aggregates = self::getEmptyStats();
+
+		$expanded = self::expandAggregates( $group );
+		if ( $expanded === [] ) {
+			return $aggregates;
+		}
+
+		$subGroupIds = array_keys( $expanded );
+		$res = self::selectRowsIdLang( $subGroupIds, $code, $flags );
+		$stats = self::extractResults( $res, $subGroupIds, $stats );
+
+		foreach ( $expanded as $sid => $subgroup ) {
+			# Discouraged groups may belong to another group, usually if there
+			# is an aggregate group for all translatable pages. In that case
+			# calculate and store the statistics, but don't count them as part of
+			# the aggregate group, so that the numbers in Special:LanguageStats
+			# add up. The statistics for discouraged groups can still be viewed
+			# through Special:MessageGroupStats.
+			if ( !isset( $stats[$sid][$code] ) ) {
+				$stats[$sid][$code] = self::forItemInternal( $stats, $subgroup, $code, $flags );
+			}
+
+			$include = Hooks::run( 'Translate:MessageGroupStats:isIncluded', [ $sid, $code ] );
+			if ( $include ) {
+				$aggregates = self::multiAdd( $aggregates, $stats[$sid][$code] );
+			}
+		}
 
 		return $aggregates;
 	}

api/ApiQueryLanguageStats.php
@@ -18,10 +18,21 @@ class ApiQueryLanguageStats extends ApiStatsQuery {
 		parent::__construct( $query, $moduleName, 'ls' );
 	}
 
-	protected function getData() {
-		$params = $this->extractRequestParams();
+	/// Overwritten from ApiStatsQuery
+	protected function validateTargetParamater( array $params ) {
+		$all = TranslateUtils::getLanguageNames( null );
+		$requested = $params[ 'language' ];
 
-		return MessageGroupStats::forLanguage( $params['language'] );
+		if ( !isset( $all[ $requested ] ) ) {
+			$this->dieWithError( [ 'apierror-translate-invalidlanguage' ] );
+		}
+
+		return $requested;
+	}
+
+	/// Overwritten from ApiStatsQuery
+	protected function loadStatistics( $target, $flags = 0 ) {
+		return MessageGroupStats::forLanguage( $target, $flags );
 	}
 
 	protected function makeItem( $item, $stats ) {

api/ApiQueryMessageGroupStats.php
@@ -19,8 +19,8 @@ class ApiQueryMessageGroupStats extends ApiStatsQuery {
 		parent::__construct( $query, $moduleName, 'mgs' );
 	}
 
-	protected function getData() {
-		$params = $this->extractRequestParams();
+	/// Overwritten from ApiStatsQuery
+	protected function validateTargetParamater( array $params ) {
 		$group = MessageGroups::getGroup( $params['group'] );
 		if ( !$group ) {
 			$this->dieWithError( [ 'apierror-missingparam', 'mgsgroup' ] );
@@ -28,7 +28,12 @@ class ApiQueryMessageGroupStats extends ApiStatsQuery {
 			$this->dieWithError( 'apierror-translate-nodynamicgroups', 'invalidparam' );
 		}
 
-		return MessageGroupStats::forGroup( $group->getId() );
+		return $group->getId();
+	}
+
+	/// Overwritten from ApiStatsQuery
+	protected function loadStatistics( $target, $flags = 0 ) {
+		return MessageGroupStats::forGroup( $target, $flags );
 	}
 
 	protected function makeItem( $item, $stats ) {

api/ApiStatsQuery.php
@@ -18,12 +18,30 @@ abstract class ApiStatsQuery extends ApiQueryBase {
 		return 'public';
 	}
 
+	/**
+	 * Implement this to implement input validation and return the name of the target that
+	 * is then given to loadStats.
+	 * @param array $params
+	 * @return string
+	 */
+	abstract protected function validateTargetParamater( array $params );
+
+	/**
+	 * Implement this to load stats.
+	 * @param string $target
+	 * @param int $flags See MessageGroupStats for possible flags
+	 * @return array[]
+	 */
+	abstract protected function loadStatistics( $target, $flags );
+
 	public function execute() {
 		$params = $this->extractRequestParams();
-		MessageGroupStats::setTimeLimit( $params['timelimit'] );
 
-		$cache = $this->getData();
+		$target = $this->validateTargetParamater( $params );
+		$cache = $this->loadStatistics( $target, MessageGroupStats::FLAG_CACHE_ONLY );
+
 		$result = $this->getResult();
+		$incomplete = false;
 
 		foreach ( $cache as $item => $stats ) {
 			if ( $item < $params['offset'] ) {
@@ -31,6 +49,7 @@ abstract class ApiStatsQuery extends ApiQueryBase {
 			}
 
 			if ( $stats[MessageGroupStats::TOTAL] === null ) {
+				$incomplete = true;
 				$this->setContinueEnumParameter( 'offset', $item );
 				break;
 			}
@@ -40,6 +59,12 @@ abstract class ApiStatsQuery extends ApiQueryBase {
 		}
 
 		$result->addIndexedTagName( [ 'query', $this->getModuleName() ], 'stats' );
+
+		if ( $incomplete ) {
+			DeferredUpdates::addCallableUpdate( function () use ( $target ) {
+				$this->loadStatistics( $target );
+			} );
+		}
 	}
 
 	protected function makeItem( $item, $stats ) {
@@ -63,6 +88,7 @@ abstract class ApiStatsQuery extends ApiQueryBase {
 				ApiBase::PARAM_TYPE => 'integer',
 				ApiBase::PARAM_MAX => 10,
 				ApiBase::PARAM_MIN => 0,
+				ApiBase::PARAM_DEPRECATED => true, // Since 2018.10
 			],
 		];
 	}

tag/PageTranslationHooks.php
@@ -1304,8 +1304,11 @@ class PageTranslationHooks {
 
 				$page = TranslatablePage::newFromTitle( $target );
 
-				MessageGroupStats::clear( $handle );
-				MessageGroupStats::forItem( $page->getMessageGroupId(), $langCode );
+				MessageGroupStats::forItem(
+					$page->getMessageGroupId(),
+					$langCode,
+					MessageGroupStats::FLAG_NO_CACHE
+				);
 
 				if ( !$handle->isDoc() ) {
 					// Assume that $user and $reason for the first deletion is the same for all

commit 38b6bbd3485643b4e68add755d9f6a5bec0ce031

Fix message group stats not updating
Updating was broken in 2df54a21e3 because updates were using insert+
ignore, but with the new flag the rows usually already exist. Fixed
by replacing insert with replace.

Also introduced a MessageGroupStatsRebuildJob and use it on
Special(Language|MessageGroup)Stats, because the deferred updates
might time out. With JobQueue they are reliably updated.

Added some more inline comments.

Bug: T134252
Change-Id: I26f63ed24c3ca34c460596341a1eaa0069a38358

Autoload.php
@@ -70,6 +70,7 @@ $al['MessageGroupCache'] = "$dir/utils/MessageGroupCache.php";
 $al['MessageGroupStates'] = "$dir/utils/MessageGroupStates.php";
 $al['MessageGroupStatesUpdaterJob'] = "$dir/utils/MessageGroupStatesUpdaterJob.php";
 $al['MessageGroupStats'] = "$dir/utils/MessageGroupStats.php";
+$al['MessageGroupStatsRebuildJob'] = "$dir/utils/MessageGroupStatsRebuildJob.php";
 $al['MessageHandle'] = "$dir/utils/MessageHandle.php";
 $al['MessageIndex'] = "$dir/utils/MessageIndex.php";
 $al['MessageIndexRebuildJob'] = "$dir/utils/MessageIndexRebuildJob.php";

Translate.php
@@ -198,9 +198,10 @@ $wgLogActionsHandlers['translationreview/message'] = 'TranslateLogFormatter';
 $wgLogActionsHandlers['translationreview/group'] = 'TranslateLogFormatter';
 
 // New jobs
+$wgJobClasses['MessageGroupStatesUpdaterJob'] = 'MessageGroupStatesUpdaterJob';
+$wgJobClasses['MessageGroupStatsRebuildJob'] = 'MessageGroupStatsRebuildJob';
 $wgJobClasses['MessageIndexRebuildJob'] = 'MessageIndexRebuildJob';
 $wgJobClasses['MessageUpdateJob'] = 'MessageUpdateJob';
-$wgJobClasses['MessageGroupStatesUpdaterJob'] = 'MessageGroupStatesUpdaterJob';
 $wgJobClasses['TTMServerMessageUpdateJob'] = 'TTMServerMessageUpdateJob';
 
 $wgParserTestFiles[] = "$dir/tests/parser/translateParserTests.txt";

TranslateEditAddons.php
@@ -227,6 +227,10 @@ class TranslateEditAddons {
 		$group = $handle->getGroup();
 		// Update translation stats - source language should always be up to date
 		if ( $handle->getCode() !== $group->getSourceLanguage() ) {
+			// This will update in-process cache immediately, but the value is saved
+			// to the database in a deferred update. See MessageGroupStats::queueUpdates.
+			// In case an error happens before that, the stats may be stale, but that
+			// would be fixed by the next update or purge.
 			MessageGroupStats::forItem(
 				$group->getId(),
 				$handle->getCode(),

specials/SpecialLanguageStats.php
@@ -159,11 +159,29 @@ class SpecialLanguageStats extends SpecialPage {
 					"<div class='error'>$1</div>",
 					'translate-langstats-incomplete'
 				);
+			}
 
-				// $this->purge is only true if request was posted
+			if ( $this->incomplete || $this->purge ) {
 				DeferredUpdates::addCallableUpdate( function () {
-					$flags = $this->purge ? MessageGroupStats::FLAG_NO_CACHE : 0;
-					$this->loadStatistics( $this->target, $flags );
+					// Attempt to recache on the fly the missing stats, unless a
+					// purge was requested, because that is likely to time out.
+					// Even though this is executed inside a deferred update, it
+					// counts towards the maximum execution time limit. If that is
+					// reached, or any other failure happens, no updates at all
+					// will be written into the database, as it does only single
+					// update at the end. Hence we always add a job too, so that
+					// even the slower updates will get done at some point. In
+					// regular case (no purge), the job sees that the stats are
+					// already updated, so it is not much of an overhead.
+					$jobParams = $this->getCacheRebuildJobParameters( $this->target );
+					$jobParams[ 'purge' ] = $this->purge;
+					$job = MessageGroupStatsRebuildJob::newJob( $jobParams );
+					JobQueueGroup::singleton()->push( $job );
+
+					// $this->purge is only true if request was posted
+					if ( !$this->purge ) {
+						$this->loadStatistics( $this->target );
+					}
 				} );
 			}
 			if ( $this->nothing ) {
@@ -176,13 +194,17 @@ class SpecialLanguageStats extends SpecialPage {
 	}
 
 	/**
-	 * Get stats
+	 * Get stats.
 	 * @param string $target For which target to get stats
 	 * @param int $flags See MessageGroupStats for possible flags
 	 * @return array[]
 	 */
 	protected function loadStatistics( $target, $flags ) {
 		return MessageGroupStats::forLanguage( $target, $flags );
+	}
+
+	protected function getCacheRebuildJobParameters( $target ) {
+		return [ 'languagecode' => $target ];
 	}
 
 	/**

specials/SpecialMessageGroupStats.php
@@ -42,6 +42,11 @@ class SpecialMessageGroupStats extends SpecialLanguageStats {
 	}
 
 	/// Overwritten from SpecialLanguageStats
+	protected function getCacheRebuildJobParameters( $target ) {
+		return [ 'groupid' => $target ];
+	}
+
+	/// Overwritten from SpecialLanguageStats
 	protected function isValidValue( $value ) {
 		$group = MessageGroups::getGroup( $value );
 		if ( $group ) {

utils/MessageGroupStats.php
@@ -514,13 +514,8 @@ class MessageGroupStats {
 			'updates',
 			__METHOD__,
 			function ( IDatabase $dbw, $method ) use( $table, &$updates ) {
-				$dbw->insert(
-					$table,
-					$updates,
-					$method,
-					[ 'IGNORE' ]
-				);
-
+				$primaryKey = [ 'tgs_group', 'tgs_lang' ];
+				$dbw->replace( $table, $primaryKey, $updates, $method );
 				$updates = [];
 			}
 		);

utils/MessageGroupStatsRebuildJob.php
@@ -1,0 +1,48 @@
+<?php
+/**
+ * Contains class with job for rebuilding message group stats.
+ *
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0-or-later
+ */
+
+/**
+ * Job for rebuilding message index.
+ *
+ * @ingroup JobQueue
+ */
+class MessageGroupStatsRebuildJob extends Job {
+	/**
+	 * @param array $params
+	 * @return self
+	 */
+	public static function newJob( $params ) {
+		$job = new self( Title::newMainPage(), $params );
+		return $job;
+	}
+
+	/**
+	 * @param Title $title
+	 * @param array $params
+	 */
+	public function __construct( $title, $params = [] ) {
+		parent::__construct( __CLASS__, $title, $params );
+	}
+
+	public function run() {
+		$params = $this->params;
+		$flags = 0;
+
+		if ( isset( $params[ 'purge' ] ) && $params[ 'purge' ] ) {
+			$flags = MessageGroupStats::FLAG_NO_CACHE;
+		}
+
+		if ( isset( $params[ 'groupid' ] ) ) {
+			MessageGroupStats::forGroup( $params[ 'groupid' ], $flags );
+		}
+		if ( isset( $params[ 'languagecode' ] ) ) {
+			MessageGroupStats::forGroup( $params[ 'languagecode' ], $flags );
+		}
+	}
+}