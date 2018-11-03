a45f5e42045052fd73b17b890d8792916678e1d5
Ed Sanders <esanders@wikimedia.org>
2018-11-03 18:57:48 +0000

    eslint: Introduce wikimedia/qunit ruleset
    Change-Id: I7470fa4c5f97a2c67731325ac3125b74837abf85

--- tests/qunit/ext.translate.parsers.test.js
+++ tests/qunit/ext.translate.parsers.test.js
@@ -13,20 +13,20 @@
 		mw.config.set( 'wgArticlePath', '/wiki/$1' );
 
 		assert.strictEqual(
-			'This page is [in English]',
 			mw.translate.formatMessageGently( 'This page is [in English]' ),
+			'This page is [in English]',
 			'Brackets without protocol doesn\'t make a link'
 		);
 
 		assert.strictEqual(
-			'This page has <a href="https://www.mediawiki.org">a link</a>',
 			mw.translate.formatMessageGently( 'This page has [https://www.mediawiki.org a link]' ),
+			'This page has <a href="https://www.mediawiki.org">a link</a>',
 			'Brackets with https:// protocol creates a link'
 		);
 
 		assert.strictEqual(
-			'No kun <a href="/wiki/m:MassMessage">m:MassMessage</a> ja plum <a href="/wiki/m:">Meta-Wiki</a>.',
 			mw.translate.formatMessageGently( 'No kun [[m:MassMessage]] ja plum [[m:|Meta-Wiki]].' ),
+			'No kun <a href="/wiki/m:MassMessage">m:MassMessage</a> ja plum <a href="/wiki/m:">Meta-Wiki</a>.',
 			'Link parsing is non-greedy'
 		);
 	} );
