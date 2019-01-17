7a28e66a684d6254b3aae784c529ab8e40a3dfb7
Adam Wight <awight@wikimedia.org>
2019-01-16 18:28:48 -0800

    Move wfAddNamespace into a dedicated source file
    Change-Id: I8b0bc47f7ece50ed6c37ec2573a84c96e93b29f2

--- Translate.php
+++ Translate.php
@@ -37,39 +37,7 @@ $wgExtensionMessagesFiles['TranslateMagic'] = __DIR__ . '/Translate.i18n.magic.p
 
 /** @endcond */
 
-/**
- * Helper function for adding namespace for message groups.
- *
- * It defines constants for the namespace (and talk namespace) and sets up
- * restrictions and some other configuration.
- * @param int $id Namespace number
- * @param string $name Name of the namespace
- * @param string|null $constant (optional) name of namespace constant, defaults to
- *   NS_ followed by upper case version of $name, e.g., NS_MEDIAWIKI
- */
-function wfAddNamespace( $id, $name, $constant = null ) {
-	global $wgExtraNamespaces, $wgContentNamespaces, $wgTranslateMessageNamespaces,
-		$wgNamespaceProtection, $wgNamespacesWithSubpages, $wgNamespacesToBeSearchedDefault;
-
-	if ( is_null( $constant ) ) {
-		$constant = strtoupper( "NS_$name" );
-	}
-
-	define( $constant, $id );
-	define( $constant . '_TALK', $id + 1 );
-
-	$wgExtraNamespaces[$id] = $name;
-	$wgExtraNamespaces[$id + 1] = $name . '_talk';
-
-	$wgContentNamespaces[] = $id;
-	$wgTranslateMessageNamespaces[] = $id;
-
-	$wgNamespacesWithSubpages[$id] = true;
-	$wgNamespacesWithSubpages[$id + 1] = true;
-
-	$wgNamespaceProtection[$id] = [ 'translate' ];
-
-	$wgNamespacesToBeSearchedDefault[$id] = true;
-}
+// Backwards compatibility to provide wfAddNamespace.
+require_once __DIR__ . '/utils/lc.php';
 
 /** @defgroup TranslateSpecialPage Special pages of Translate extension */
