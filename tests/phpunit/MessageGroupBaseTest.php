77acd1e03b4c515506a05c4b2a493c567ef727b8
Adam Wight <awight@wikimedia.org>
2019-01-22 12:18:50 -0800

    Allow hooks to set custom workflow configuration
    Bug: T213943
    Change-Id: I76103d19b669aeb2dc880d4c6c6abbd646051a0e

--- tests/phpunit/MessageGroupBaseTest.php
+++ tests/phpunit/MessageGroupBaseTest.php
@@ -152,6 +152,33 @@ class MessageGroupBaseTest extends MediaWikiTestCase {
 		$conf['BASIC']['namespace'] = 'ergweofijwef';
 		MessageGroupBase::factory( $conf );
 	}
+
+	public function testModifyMessageGroupStates() {
+		// Create a basic workflow.
+		$this->setMwGlobals( [
+			'wgTranslateWorkflowStates' => [
+				'progress' => [ 'color' => 'd33' ],
+				'proofreading' => [ 'color' => 'fc3' ],
+			],
+		] );
+		// Install a special permission when the group ID is matched.
+		$this->setTemporaryHook(
+			'Translate:modifyMessageGroupStates',
+			function ( $groupId, &$conf ) {
+				if ( $groupId === 'test-id' ) {
+					// No users have this.
+					$conf['proofreading']['right'] = 'inobtanium';
+				}
+			}
+		);
+
+		$expectedStates = [
+			'progress' => [ 'color' => 'd33' ],
+			'proofreading' => [ 'color' => 'fc3', 'right' => 'inobtanium' ],
+		];
+		$states = $this->group->getMessageGroupStates()->getStates();
+		$this->assertEquals( $expectedStates, $states );
+	}
 }
 
 class FakeInsertablesSuggester implements InsertablesSuggester {
