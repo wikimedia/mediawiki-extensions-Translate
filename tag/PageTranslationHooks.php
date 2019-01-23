c6d2bba66bb7b1ea1651da9e8e3f91907c517011
Niklas Laxström <niklas.laxstrom@gmail.com>
2019-01-23 15:59:50 +0100

    Fix fatal error on PageContentLanguage hook subscriber
    Partially reverts f6e5fced.
    
    Bug: T214358
    Bug: T214517
    Change-Id: Ic9d85fec12b0a5826e8fd46df833c776164074b8
    (cherry picked from commit 8269ba6fa0cfcae49dc841f0d30a9a99b229309a)

--- tag/PageTranslationHooks.php
+++ tag/PageTranslationHooks.php
@@ -105,7 +105,7 @@ class PageTranslationHooks {
 	 * Hook: PageContentLanguage
 	 *
 	 * @param Title $title
-	 * @param Language &$pageLang
+	 * @param Language|string &$pageLang
 	 * @return true
 	 */
 	public static function onPageContentLanguage( Title $title, &$pageLang ) {
