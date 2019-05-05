4086102bbcb97d8f42bf3ab67b37143b14ead78c
Abijeet <abijeetpatro@gmail.com>
2019-05-05 22:09:21 +0530

    Fix notice appearing on TwnMainPage due to invalid return format
    Updated MessageGroupStats::forLanguage and MessageGroupStats::forGroup
    to return data in the same format for invalid languages also.
    
    Also removes an unused variable and converts comments starting
    with # to //
    
    Bug: T220583
    Change-Id: I016e8d50d3c5967c92346e97a120794bd60d6d4a

--- tests/phpunit/utils/MessageGroupStatsTest.php
+++ tests/phpunit/utils/MessageGroupStatsTest.php
@@ -5,7 +5,26 @@
  * @license GPL-2.0-or-later
  */
 
-class MessageGroupStatsTest extends PHPUnit\Framework\TestCase {
+class MessageGroupStatsTest extends MediaWikiTestCase {
+	protected function setUp() {
+		parent::setUp();
+
+		global $wgHooks;
+		$this->setTemporaryHook(
+			'TranslatePostInitGroups',
+			function ( &$list ) {
+				$exampleMessageGroup = new WikiMessageGroup( 'theid', 'thesource' );
+				$exampleMessageGroup->setLabel( 'thelabel' ); // Example
+				$exampleMessageGroup->setNamespace( 5 ); // Example
+				$list['theid'] = $exampleMessageGroup;
+			}
+		);
+
+		$mg = MessageGroups::singleton();
+		$mg->setCache( new WANObjectCache( [ 'cache' => wfGetCache( 'hash' ) ] ) );
+		$mg->recache();
+	}
+
 	public function testGetDatabaseIdForGroupId() {
 		$shortId = 'abab';
 		$longId = str_repeat( 'ab', 100 );
@@ -30,5 +49,24 @@ class MessageGroupStatsTest extends PHPUnit\Framework\TestCase {
 			MessageGroupStats::getDatabaseIdForGroupId( $longId2 ),
 			'Two long ids with the same prefix do not collide'
 		);
+	}
+
+	public function testFunctionReturnFormat() {
+		$validLang = MessageGroupStats::forLanguage( 'en', MessageGroupStats::FLAG_CACHE_ONLY );
+		$invalidLang = MessageGroupStats::forLanguage( 'ffff', MessageGroupStats::FLAG_CACHE_ONLY );
+
+		$validGroup = MessageGroupStats::forGroup( 'theid',  MessageGroupStats::FLAG_CACHE_ONLY );
+		$invalidGroup = MessageGroupStats::forGroup( 'invalid-mg-group',
+			MessageGroupStats::FLAG_CACHE_ONLY );
+
+		$this->assertInternalType( 'array', current( $validLang ),
+			'forLanguage returns data in valid format for valid language' );
+		$this->assertInternalType( 'array', current( $invalidLang ),
+			'forLanguage returns data in valid format for invalid language' );
+
+		$this->assertInternalType( 'array', current( $validGroup ),
+			'forGroup returns data in valid format for valid group' );
+		$this->assertInternalType( 'array', current( $invalidGroup ),
+			'forGroup returns data in valid format for invalid group' );
 	}
 }
