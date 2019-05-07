3946fb5e98787bbde07fb504636e44c07c3399c7
James D. Forrester <jforrester@wikimedia.org>
2019-05-07 11:11:16 -0700

    build: Upgrade eslint-config-wikimedia 0.12.0, drop grunt-jsonlint
    Change-Id: I70c823bed8c70bf539719100bd501bad96613241

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,26 +3,20 @@ module.exports = function ( grunt ) {
 	'use strict';
 
 	grunt.loadNpmTasks( 'grunt-eslint' );
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		eslint: {
+			options: {
+				extensions: [ '.js', '.json' ],
+				cache: true
+			},
 			all: [
-				'**/*.js',
-				'!node_modules/**',
+				'**/*.js{,on}',
 				'!extensions/**',
 				'!resources/js/jquery.autosize.js',
-				'!vendor/**'
-			]
-		},
-		jsonlint: {
-			all: [
-				'**/*.json',
-				'!node_modules/**',
-				'!extensions/**',
-				'!vendor/**'
+				'!{vendor,node_modules}/**'
 			]
 		},
 		stylelint: {
@@ -45,6 +39,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'jsonlint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
