0a433ed8a22856bf31f1f29b68a7339fc5467611
Abijeet <abijeetpatro@gmail.com>
2019-05-11 20:15:39 +0530

    Add loaders to load and cache MessageGroups separately
    Added loaders / stores to load MessageGroups and handle the associated
    cache as needed. Added MessageGroupWANCache, an interface on top of
    WANObjectCache, to make it easier for loaders to interact with the
    cache.
    
    All MessageGroupLoaders must implement the MessageGroupLoader
    interface. Additionally loaders using the cache must implement
    CachedMessageGroupLoader interface.
    
    Bug: T213802
    Change-Id: I9dd37c92a7ab6d9c48334ac5a729824e9b2f77ab

---
+++ messagegroups/loaders/FileBasedMessageGroupLoader.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * This file contains a class to load file based message groups and handle
+ * the related cache.
+ *
+ * @file
+ * @author Abijeet Patro
+ * @license GPL-2.0-or-later
+ */
+
+use \MediaWiki\MediaWikiServices;
+
+/**
+ * Loads FileBasedMessageGroup, and handles the related cache.
+ * @since 2019.05
+ */
+class FileBasedMessageGroupLoader extends MessageGroupLoader
+	implements CachedMessageGroupLoader {
+
+	const CACHE_KEY = 'filebased';
+	const CACHE_VERSION = 1;
+
+	/**
+	 * List of groups
+	 * @var array|null
+	 */
+	protected $groups;
+
+	/**
+	 * @var MessageGroupWANCache
+	 */
+	protected $cache;
+
+	public function __construct( MessageGroupWANCache $cache ) {
+		$this->cache = $cache;
+		$this->cache->configure(
+			[
+				'key' => self::CACHE_KEY,
+				'version' => self::CACHE_VERSION,
+				'regenerator' => [ $this, 'getCacheData' ],
+				'touchedCallback' => [ $this, 'isExpired' ]
+			]
+		);
+	}
+
+	/**
+	 * Fetches FileBasedMessageGroups
+	 *
+	 * @return MessageGroup[] Key is the MessageGroup Id and value is the corresponding
+	 * FileBasedMessageGroup
+	 */
+	public function getGroups() {
+		if ( $this->groups === null ) {
+			global $wgAutoloadClasses;
+			$this->groups = [];
+
+			/** @var DependencyWrapper $cacheData */
+			$wrapper = $this->cache->getValue();
+			$cacheData = $wrapper->getValue();
+			$autoload = $cacheData['autoload'];
+			$groupConfs = $cacheData['groups'];
+
+			foreach ( $groupConfs as $id => $conf ) {
+				$this->groups[$id] = MessageGroupBase::factory( $conf );
+			}
+
+			self::appendAutoloader( $autoload, $wgAutoloadClasses );
+		}
+
+		return $this->groups;
+	}
+
+	/**
+	 * Hook: TranslateInitGroupLoaders
+	 *
+	 * @param array &$groupLoader
+	 * @param array $deps Dependencies
+	 */
+	public static function registerLoader( array &$groupLoader, array $deps ) {
+		$groupLoader[] = self::getInstance(
+			$deps['cache']
+		);
+	}
+
+	/**
+	 * Return an instance of this class using the parameters, if passed,
+	 * else initialize the necessary dependencies and return an instance.
+	 *
+	 * @param WANObjectCache|null $cache
+	 * @return FileBasedMessageGroupLoader
+	 */
+	public static function getInstance( WANObjectCache $cache = null ) {
+		return new self(
+			new MessageGroupWANCache(
+				$cache ?? MediaWikiServices::getInstance()->getMainWANObjectCache()
+			)
+		);
+	}
+
+	/**
+	 * Generates data to be stored in the cache.
+	 *
+	 * @return DependencyWrapper
+	 */
+	public function getCacheData() {
+		global $wgTranslateGroupFiles;
+
+		$autoload = $groups = $deps = $value = [];
+		$deps[] = new GlobalDependency( 'wgTranslateGroupFiles' );
+
+		// TODO: See if DI can be used here
+		$parser = new MessageGroupConfigurationParser();
+		foreach ( $wgTranslateGroupFiles as $configFile ) {
+			$deps[] = new FileDependency( realpath( $configFile ) );
+
+			$yaml = file_get_contents( $configFile );
+			$fgroups = $parser->getHopefullyValidConfigurations(
+				$yaml,
+				function ( $index, $config, $errmsg ) use ( $configFile ) {
+					trigger_error( "Document $index in $configFile is invalid: $errmsg", E_USER_WARNING );
+				}
+			);
+
+			foreach ( $fgroups as $id => $conf ) {
+				if ( !empty( $conf['AUTOLOAD'] ) && is_array( $conf['AUTOLOAD'] ) ) {
+					$dir = dirname( $configFile );
+					$additions = array_map( function ( $file ) use ( $dir ) {
+						return "$dir/$file";
+					}, $conf['AUTOLOAD'] );
+					self::appendAutoloader( $additions, $autoload );
+				}
+
+				$groups[$id] = $conf;
+			}
+		}
+		$value['groups'] = $groups;
+		$value['autoload'] = $autoload;
+
+		$wrapper = new DependencyWrapper( $value, $deps );
+		$wrapper->initialiseDeps();
+		return $wrapper;
+	}
+
+	/**
+	 * Clear and refill the cache with the latest values
+	 */
+	public function recache() {
+		$this->clearProcessCache();
+		$this->cache->touchKey();
+
+		$this->cache->getValue( 'recache' );
+		$this->getGroups();
+	}
+
+	/**
+	 * Clear values from the cache
+	 */
+	public function clearCache() {
+		$this->clearProcessCache();
+		$this->cache->delete();
+	}
+
+	/**
+	 * Clears the process cache, mainly the cached groups property.
+	 */
+	protected function clearProcessCache() {
+		$this->groups = null;
+	}
+}
---
+++ tests/phpunit/MockCacheMessageGroupLoader.php
@@ -0,0 +1,20 @@
+<?php
+class MockCacheMessageGroupLoader extends MessageGroupLoader
+	implements CachedMessageGroupLoader {
+
+	public function getGroups() {
+		return [];
+	}
+
+	public function recache() {
+	}
+
+	public function clearCache() {
+	}
+
+	public static function registerLoader( array &$groupLoader, array $deps ) {
+		$groupLoader[] = new self( new MessageGroupWANCache(
+			$deps['cache']
+		) );
+	}
+}
