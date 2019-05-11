d33135c3eeca77bbc0b6273e869d2d1eb5978ac9
Abijeet <abijeetpatro@gmail.com>
2019-05-11 20:15:39 +0530

    Add loaders to load and cache MessageGroups separately
    Added loaders / stores to load MessageGroups and handle the associated
    cache as needed. Added MessageGroupWANCache, an interface on top of
    WANObjectCache, to make it easier for loaders to interact with the cache.
    
    All MessageGroupLoaders must implement the MessageGroupLoader interface.
    Additionally loaders using the cache must implement
    CachedMessageGroupLoader interface and ones using the database must
    implement DatabaseMessageGroupLoader interface.
    
    Bug: T213802
    Change-Id: I9dd37c92a7ab6d9c48334ac5a729824e9b2f77ab

---
+++ messagegroups/loaders/MessageGroupLoader.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file contains an abstract class to be extended by group stores / loaders.
+ *
+ * @file
+ * @author Abijeet Patro
+ * @license GPL-2.0-or-later
+ */
+
+abstract class MessageGroupLoader {
+	abstract public function getGroups();
+	abstract public static function registerLoader( array &$groupLoader );
+
+	/**
+	 * Safely merges first array to second array, throwing warning on duplicates and removing
+	 * duplicates from the first array.
+	 * @param array $additions Things to append
+	 * @param array &$to Where to append
+	 */
+	protected static function appendAutoloader( array $additions, array &$to ) {
+		foreach ( $additions as $class => $file ) {
+			if ( isset( $to[$class] ) && $to[$class] !== $file ) {
+				$msg = "Autoload conflict for $class: {$to[$class]} !== $file";
+				trigger_error( $msg, E_USER_WARNING );
+				continue;
+			}
+
+			$to[$class] = $file;
+		}
+	}
+
+	/**
+	 * Determines if dependencies have expired. Called if data in cache is stored
+	 * as a dependency wrapper.
+	 * @param DependencyWrapper $cacheData
+	 * @return bool true if expired, false otherwise.
+	 */
+	public function isExpired( DependencyWrapper $cacheData ) {
+		return $cacheData->isExpired();
+	}
+}
---
+++ tests/phpunit/messagegroups/MessageGroupLoaderTest.php
@@ -0,0 +1,68 @@
+<?php
+class MessageGroupLoaderTest extends MediaWikiTestCase {
+
+	protected $cacheDbLoaderMock;
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->cacheDbLoaderMock = $this->createMock( MockCacheDatabaseMessageGroupLoader::class );
+		$this->setTemporaryHook( 'TranslateInitGroupLoaders', [ $this, 'registerLoader' ] );
+
+		$mg = MessageGroups::singleton();
+		$mg->setCache( new WANObjectCache( [ 'cache' => wfGetCache( 'hash' ) ] ) );
+		$mg->clearCache();
+	}
+
+	public function registerLoader( array &$groupLoader ) {
+		$groupLoader[] = $this->cacheDbLoaderMock;
+	}
+
+	public function testGroupLoaderConfiguration() {
+		$this->cacheDbLoaderMock->expects( $this->once() )
+			->method( 'setCache' );
+
+		$this->cacheDbLoaderMock->expects( $this->once() )
+			->method( 'setDatabase' );
+
+		$this->cacheDbLoaderMock->expects( $this->once() )
+			->method( 'getGroups' )
+			->willReturn( [] );
+
+		MessageGroups::singleton()->getGroups();
+	}
+
+	public function testGroupLoaderRecache() {
+		$this->cacheDbLoaderMock->expects( $this->once() )
+			->method( 'getGroups' )
+			->willReturn( [] );
+
+		$this->cacheDbLoaderMock->expects( $this->once() )
+			->method( 'recache' );
+
+		MessageGroups::singleton()->recache();
+	}
+
+	public function testGroupLoaderClearCache() {
+		$this->cacheDbLoaderMock->expects( $this->once() )
+			->method( 'clearCache' );
+
+		MessageGroups::singleton()->clearCache();
+	}
+
+	public function testGroupLoaderGetGroups() {
+		$testGroup = new WikiMessageGroup( 'testgroup', 'hello' );
+
+		$this->cacheDbLoaderMock->expects( $this->once() )
+			->method( 'getGroups' )
+			->willReturn( [
+				$testGroup
+			] );
+
+		$groups = MessageGroups::singleton()->getGroups();
+
+		$this->assertCount( 1, $groups, 'the message group returned bythe loader is present' );
+		$this->assertEquals( 'testgroup', $groups[0]->getId(), 'the id of the message group ' .
+			'returned by the loader is present in the groups' );
+	}
+}
