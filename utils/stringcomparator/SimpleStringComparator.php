6211d345d450e68e24af17bea22655729a7f1d75
Abijeet <abijeetpatro@gmail.com>
2019-06-18 21:31:19 +0530

    Add rename and auto-match functionality for imports
    When message keys are modified on remote repositories, the
    system will not try to match them with their old keys based
    on their content. These renames will be displayed on the
    Special:ManageGroups page. A manual rename functionality
    has been added to the Special:ManageGroups page that can be
    used to mark messages as renames incase the system could
    not match them due to their content being different as well
    
    Following major parts of the codebase have been changed,
    
      * ExternalMessageSourceStateComparator - Checks for changes
        during the imports
      * Special:ManageGroups - To display the renames and handles
        the submission of user input
      * MessageUpdateJob.php  - Background job that processes and
        performs the necessary renames
      * ApiManageMessageGroups & ApiQueryManageMessageGroups -
        Provides functionality for the purpose of performing manual
        renames and breaking renames.
      * MessageSourceChange - To track all the changes for a
        MessageGroup, and provide an interface for easy querying
    
    Bug: T196601
    Change-Id: I56cb0767c3ee4e2a50de2d96644aac84144175c8

---
+++ utils/stringcomparator/SimpleStringComparator.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Contains a simple string compare class.
+ * @license GPL-2.0-or-later
+ */
+
+/**
+ * A simple string comparator, that compares two strings and determines if they are an exact match.
+ * @since 2019.06
+ */
+class SimpleStringComparator implements StringComparator {
+	/**
+	 * @inheritDoc
+	 */
+	public function getSimilarity( $addedMessage, $deletedMessage ) {
+		if ( $addedMessage === $deletedMessage ) {
+			return 100;
+		}
+
+		if ( trim( mb_strtolower( $addedMessage ) ) === trim( mb_strtolower( $deletedMessage ) ) ) {
+			// This is an arbitrarily chosen number to differentiate it from an exact match.
+			return 95;
+		}
+
+		return 0;
+	}
+}
