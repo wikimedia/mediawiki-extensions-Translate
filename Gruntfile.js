89025282eee3f1c403cca205a5b467e1599b7578
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2020-01-08 09:18:20 +0000

    build: Updating npm dependencies
    * grunt-eslint: 21.0.0 → 22.0.0
    * eslint-config-wikimedia: 0.13.1 → 0.15.0
    
    Additional changes:
    * Removing manual reportUnusedDisableDirectives for eslint.
    
    Change-Id: I9ddeb560dbffad4d46b4be84966687761a231874

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,7 @@ module.exports = function ( grunt ) {
 		eslint: {
 			options: {
 				extensions: [ '.js', '.json' ],
-				cache: true,
-				reportUnusedDisableDirectives: true
+				cache: true
 			},
 			all: [
 				'**/*.js{,on}',
