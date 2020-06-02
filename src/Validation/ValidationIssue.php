9e7d1473546cc0ceb1ac4779f491800d44275508
Niklas Laxström <niklas.laxstrom@gmail.com>
2020-06-02 11:04:21 +0200

    Add ValidationIssue and ValidationIssues
    To avoid, imho useless, duplication in code comments, I disabled
    MediaWiki.Commenting.FunctionComment.MissingParamTag for all new
    code.
    
    Will use these classes in a follow-up commit.
    
    Change-Id: I1ce8a687753b6cdb01b92b3eb1765b91971915ee

---
+++ src/Validation/ValidationIssue.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0-or-later
+ */
+
+namespace MediaWiki\Extensions\Translate\Validation;
+
+/**
+ * Value object.
+ *
+ * @newable
+ * @since 2020.06
+ */
+class ValidationIssue {
+	/** @var string */
+	private $type;
+	/** @var string */
+	private $subType;
+	/** @var string */
+	private $messageKey;
+	/** @var array */
+	private $messageParams;
+
+	/** @stable for calling */
+	public function __construct(
+		string $type,
+		string $subType,
+		string $messageKey,
+		array $messageParams = []
+	) {
+		$this->type = $type;
+		$this->subType = $subType;
+		$this->messageKey = $messageKey;
+		$this->messageParams = $messageParams;
+	}
+
+	public function type(): string {
+		return $this->type;
+	}
+
+	public function subType(): string {
+		return $this->subType;
+	}
+
+	public function messageKey(): string {
+		return $this->messageKey;
+	}
+
+	public function messageParams(): array {
+		return $this->messageParams;
+	}
+}
---
+++ src/Validation/ValidationIssues.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0-or-later
+ */
+
+namespace MediaWiki\Extensions\Translate\Validation;
+
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+use Traversable;
+
+/**
+ * Mutable collection for validation issues.
+ *
+ * @newable
+ * @since 2020.06
+ */
+class ValidationIssues implements Countable, IteratorAggregate {
+	/** @var ValidationIssue[] */
+	private $issues = [];
+
+	/** Add a new validation issue to the collection. */
+	public function add( ValidationIssue $issue ) {
+		$this->issues[] = $issue;
+	}
+
+	/** Merge another collection to this collection. */
+	public function merge( ValidationIssues $issues ) {
+		$this->issues = array_merge( $this->issues, $issues->issues );
+	}
+
+	/**
+	 * Check whether this collection is not empty.
+	 *
+	 * @return bool False if empty, true otherwise
+	 */
+	public function hasIssues(): bool {
+		return $this->issues !== [];
+	}
+
+	/** @return Traversable<ValidationIssue> */
+	public function getIterator(): Traversable {
+		return new ArrayIterator( $this->issues );
+	}
+
+	/** @inheritDoc */
+	public function count(): int {
+		return count( $this->issues );
+	}
+}
