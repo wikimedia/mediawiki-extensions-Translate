dff90f6a11ccd76d61eddea565f8849f4792946a
Niklas Laxström <niklas.laxstrom@gmail.com>
2020-07-01 13:16:46 +0200

    Convert WikiLinkValidator to the new interface
    Refactor code to reduce code duplication
    
    Change-Id: I7316de3f72afbde0749689e87b63b0bc65c1c715

--- src/MessageValidator/Validators/WikiLinkValidator.php
+++ src/MessageValidator/Validators/WikiLinkValidator.php
@@ -1,12 +1,11 @@
 <?php
-/**
- * @file
- * @license GPL-2.0-or-later
- */
+declare( strict_types = 1 );
 
 namespace MediaWiki\Extensions\Translate\MessageValidator\Validators;
 
-use MediaWiki\Extensions\Translate\MessageValidator\Validator;
+use MediaWiki\Extensions\Translate\Validation\MessageValidator;
+use MediaWiki\Extensions\Translate\Validation\ValidationIssue;
+use MediaWiki\Extensions\Translate\Validation\ValidationIssues;
 use Title;
 use TMessage;
 
@@ -14,60 +13,61 @@ use TMessage;
  * Checks if the translation uses links that are discouraged. Valid links are those that link
  * to Special: or {{ns:special}}: or project pages trough MediaWiki messages like
  * {{MediaWiki:helppage-url}}:. Also links in the definition are allowed.
+ *
+ * @license GPL-2.0-or-later
  * @since 2020.02
  */
-class WikiLinkValidator implements Validator {
-	public function validate( TMessage $message, $code, array &$notices ) {
-		$tc = Title::legalChars() . '#%{}';
+class WikiLinkValidator implements MessageValidator {
+	public function getIssues( TMessage $message, string $targetLanguage ): ValidationIssues {
+		$issues = new ValidationIssues();
 
-		$key = $message->key();
 		$definition = $message->definition();
 		$translation = $message->translation();
 
-		$subcheck = 'extra';
-		$matches = $links = [];
-		preg_match_all( "/\[\[([{$tc}]+)(\\|(.+?))?]]/sDu", $translation, $matches );
-		$count = count( $matches[0] );
-		for ( $i = 0; $i < $count; $i++ ) {
-			$backMatch = preg_quote( $matches[1][$i], '/' );
-
-			if ( preg_match( "/\[\[$backMatch/", $definition ) ) {
-				continue;
-			}
-
-			$links[] = "[[{$matches[1][$i]}{$matches[2][$i]}]]";
-		}
-
-		if ( count( $links ) ) {
-			$notices[$key][] = [
-				[ 'links', $subcheck, $key, $code ],
-				'translate-checks-links',
-				[ 'PARAMS', $links ],
-				[ 'COUNT', count( $links ) ],
-			];
-		}
-
-		$subcheck = 'missing';
-		$matches = $links = [];
-		preg_match_all( "/\[\[([{$tc}]+)(\\|(.+?))?]]/sDu", $definition, $matches );
-		$count = count( $matches[0] );
-		for ( $i = 0; $i < $count; $i++ ) {
-			$backMatch = preg_quote( $matches[1][$i], '/' );
-
-			if ( preg_match( "/\[\[$backMatch/", $translation ) ) {
-				continue;
-			}
-
-			$links[] = "[[{$matches[1][$i]}{$matches[2][$i]}]]";
-		}
-
-		if ( count( $links ) ) {
-			$notices[$key][] = [
-				[ 'links', $subcheck, $key, $code ],
+		$links = $this->getLinksMissingInTarget( $definition, $translation );
+		if ( $links !== [] ) {
+			$issue = new ValidationIssue(
+				'links',
+				'missing',
 				'translate-checks-links-missing',
-				[ 'PARAMS', $links ],
-				[ 'COUNT', count( $links ) ],
-			];
+				[
+					[ 'PARAMS', $links ],
+					[ 'COUNT', count( $links ) ],
+				]
+			);
+			$issues->add( $issue );
 		}
+
+		$links = $this->getLinksMissingInTarget( $translation, $definition );
+		if ( $links !== [] ) {
+			$issue = new ValidationIssue(
+				'links',
+				'extra',
+				'translate-checks-links',
+				[
+					[ 'PARAMS', $links ],
+					[ 'COUNT', count( $links ) ],
+				]
+			);
+			$issues->add( $issue );
+		}
+
+		return $issues;
+	}
+
+	private function getLinksMissingInTarget( string $source, string $target ): array {
+		$tc = Title::legalChars() . '#%{}';
+		$matches = $links = [];
+
+		preg_match_all( "/\[\[([{$tc}]+)(\\|(.+?))?]]/sDu", $source, $matches );
+		$count = count( $matches[0] );
+		for ( $i = 0; $i < $count; $i++ ) {
+			$backMatch = preg_quote( $matches[1][$i], '/' );
+			if ( preg_match( "/\[\[$backMatch/", $target ) !== 1 ) {
+				$links[] = "[[{$matches[1][$i]}{$matches[2][$i]}]]";
+			}
+		}
+
+		return $links;
 	}
 }
