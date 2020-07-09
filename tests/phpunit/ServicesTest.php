527219a17a6a80ee79e10b8ef25c931d6f5fd4e6
Niklas Laxström <niklas.laxstrom@gmail.com>
2020-07-09 13:03:12 +0200

    Add an integration test that checks services can be created
    Change-Id: I18c47225684a5ed333212501165b12e0317eae85

---
+++ tests/phpunit/ServicesTest.php
@@ -0,0 +1,30 @@
+<?php
+declare( strict_types = 1 );
+
+use MediaWiki\Extensions\Translate\Services;
+
+/**
+ * @author Niklas Laxström
+ * @license GPL-2.0-or-later
+ * @covers \MediaWiki\Extensions\Translate\Services
+ */
+class ServicesTest extends MediaWikiIntegrationTestCase {
+	public function testNoExceptions() {
+		$services = Services::getInstance();
+		$class = new ReflectionClass( Services::class );
+		$reflectionMethods = $class->getMethods( ReflectionMethod::IS_PUBLIC );
+		$methods = [];
+		foreach ( $reflectionMethods as $methodObj ) {
+			$method = $methodObj->getName();
+			if ( !$methodObj->isStatic() && preg_match( '/^get[A-Z]/', $method ) ) {
+				$methods[] = $method;
+			}
+		}
+
+		foreach ( $methods as $method ) {
+			$services->$method();
+		}
+
+		$this->assertTrue( true, 'All services can be constructed' );
+	}
+}
