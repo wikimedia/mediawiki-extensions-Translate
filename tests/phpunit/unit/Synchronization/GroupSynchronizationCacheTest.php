4fcc8e65a8751484eab8c46432757626d75981b7
DannyS712 <DannyS712.enwiki@gmail.com>
2020-09-20 17:41:21 +0000

    GroupSynchronizationCacheTest: Add missing parent::setUp call
    Change-Id: I50403d4b6e959af7998d7a2ee4a09fbcd8f2fe13

--- tests/phpunit/unit/Synchronization/GroupSynchronizationCacheTest.php
+++ tests/phpunit/unit/Synchronization/GroupSynchronizationCacheTest.php
@@ -14,6 +14,7 @@ class GroupSynchronizationCacheTest extends MediaWikiUnitTestCase {
 	private $groupSyncCache;
 
 	protected function setUp(): void {
+		parent::setUp();
 		$this->groupSyncCache = $this->getGroupSynchronizationCache();
 	}
 
