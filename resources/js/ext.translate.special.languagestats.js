38e11016c43274f6bf9f49a9df888d7be3456075
Niklas Laxström <niklas.laxstrom@gmail.com>
2020-10-04 17:10:13 +0200

    Make statstable work better in small screens
    If there isn't enough space for all columns, it collapses to a two
    column mode where you can select which value column to show.
    
    Change-Id: Id51990cd9b10f8ff9485685bd9cfee00035b8e9f

--- resources/js/ext.translate.special.languagestats.js
+++ resources/js/ext.translate.special.languagestats.js
@@ -8,19 +8,20 @@
 ( function () {
 	'use strict';
 
+	var $columns;
+
 	/**
 	 * Add css class to every other visible row.
 	 * It's not possible to do zebra colors with CSS only if there are hidden rows.
 	 */
-	function doZebra() {
-		$( '.statstable tr:visible:odd' ).toggleClass( 'tux-statstable-even', false );
-		$( '.statstable tr:visible:even' ).toggleClass( 'tux-statstable-even', true );
+	function doZebra( $table ) {
+		$table.find( 'tr:visible:odd' ).toggleClass( 'tux-statstable-even', false );
+		$table.find( 'tr:visible:even' ).toggleClass( 'tux-statstable-even', true );
 	}
 
-	$( function () {
+	function addExpanders( $table ) {
 		var $allChildRows, $allTogglesCache, $toggleAllButton,
-			$translateTable = $( '.statstable' ),
-			$metaRows = $( 'tr.AggregateMessageGroup', $translateTable );
+			$metaRows = $( 'tr.AggregateMessageGroup', $table );
 
 		// Quick return
 		if ( !$metaRows.length ) {
@@ -31,7 +32,7 @@
 			var $toggler,
 				$parent = $( this ),
 				thisGroupId = $parent.attr( 'data-groupid' ),
-				$children = $( 'tr[data-parentgroup="' + thisGroupId + '"]', $translateTable );
+				$children = $( 'tr[data-parentgroup="' + thisGroupId + '"]', $table );
 
 			// Only do the collapse stuff if this Meta-group actually has children on this page
 			if ( !$children.length ) {
@@ -77,7 +78,7 @@
 		} );
 
 		// Create, bind and append the toggle-all button
-		$allChildRows = $( 'tr[data-parentgroup]', $translateTable );
+		$allChildRows = $( 'tr[data-parentgroup]', $table );
 		$allTogglesCache = null;
 		$toggleAllButton = $( '<span>' ).addClass( 'collapsed' )
 			.append(
@@ -89,7 +90,7 @@
 			)
 			.on( 'click', function ( e ) {
 				var $el = $( this ),
-					$allToggles = $allTogglesCache || $( '.groupexpander', $translateTable );
+					$allToggles = $allTogglesCache || $( '.groupexpander', $table );
 
 				// Switch the state and toggle the rows
 				// and update the local toggles too
@@ -111,34 +112,135 @@
 
 		// Initially hide them
 		$allChildRows.hide();
-		doZebra();
+		doZebra( $table );
 
 		// Add the toggle-all button above the table
-		$( '<p>' ).addClass( 'groupexpander-all' ).append( $toggleAllButton ).insertBefore( $translateTable );
-	} );
+		$( '<p>' ).addClass( 'groupexpander-all' ).append( $toggleAllButton ).insertBefore( $table );
+	}
 
-	$( function () {
+	function applySorting( $table ) {
 		var index,
 			sort = {},
 			re = /#sortable:(\d+)=(asc|desc)/,
-			match = re.exec( window.location.hash ),
-			$tables = $( '.statstable' );
+			match = re.exec( location.hash );
 
 		if ( match ) {
 			index = parseInt( match[ 1 ], 10 );
 			sort[ index ] = match[ 2 ];
 		}
-		$tables.tablesorter( { sortList: [ sort ] } );
+		$table.tablesorter( { sortList: [ sort ] } );
 
-		$tables.on( 'sortEnd.tablesorter', function () {
-			var $table = $( this );
+		$table.on( 'sortEnd.tablesorter', function () {
 			$table.find( '.headerSortDown, .headerSortUp' ).each( function () {
 				var headerIndex = $table.find( 'th' ).index( $( this ) ),
 					dir = $( this ).hasClass( 'headerSortUp' ) ? 'asc' : 'desc';
-				window.location.hash = 'sortable:' + headerIndex + '=' + dir;
+				location.hash = 'sortable:' + headerIndex + '=' + dir;
 
-				doZebra();
+				doZebra( $table );
 			} );
 		} );
+	}
+
+	function narrowTable( $table, enable ) {
+		var $select,
+			labelColumnCount = 1,
+			// 0-indexed
+			defaultValueColumn = 2;
+
+		if ( $columns === undefined ) {
+			$columns = $table.find( 'thead > tr > th ' ).map( function ( index, value ) {
+				return value.textContent;
+			} );
+		}
+
+		$select = makeValueColumnSelector( $columns, labelColumnCount, defaultValueColumn );
+		// Prevent table sorter from making the select inaccessible
+		$select.on( 'mousedown click', function ( e ) {
+			e.stopPropagation();
+		} ).on( 'change', function () {
+			showValueColumn( $table, $select, labelColumnCount );
+		} );
+
+		if ( enable ) {
+			showValueColumn( $table, $select, labelColumnCount );
+		} else {
+			// Restore original headings
+			$table.find( 'thead > tr > th' ).map( function ( index ) {
+				return $( this ).text( $columns[ index ] );
+			} );
+			$table.find( 'tr > *' ).removeClass( 'statstable-hide' );
+		}
+
+	}
+
+	function makeValueColumnSelector( headings, skip, def ) {
+		var i, $select = $( '<select>' );
+
+		for ( i = skip; i < headings.length; i++ ) {
+			$( '<option>' )
+				.text( headings[ i ] )
+				.val( i )
+				.prop( 'selected', i === def )
+				.appendTo( $select );
+		}
+
+		return $select;
+	}
+
+	function showValueColumn( $table, $select, skip ) {
+		var i, index, cssQuery;
+
+		index = parseInt( $select.val(), 10 );
+		cssQuery = 'th:nth-child(_)'.replace( '_', index + 1 );
+		$table.find( cssQuery ).html( $select );
+
+		for ( i = 0; i < $select.children().length; i++ ) {
+			cssQuery = 'tr > *:nth-child(_)'.replace( '_', i + skip + 1 );
+			$table.find( cssQuery ).toggleClass( 'statstable-hide', i + skip !== index );
+		}
+	}
+
+	$( function () {
+		var isNarrowMode, minimumTableWidth,
+			$table = $( '.statstable' );
+
+		// Calculate absolute minimum table width
+		if ( window.ResizeObserver ) {
+			$table.css( 'max-width', '1px' );
+		}
+
+		applySorting( $table );
+		addExpanders( $table );
+
+		if ( !window.ResizeObserver ) {
+			return;
+		}
+
+		// Hopefully previous stuff have time to render by now to have accurate picture of the width
+		( window.requestAnimationFrame || setTimeout )( function () {
+			minimumTableWidth = $table.outerWidth();
+			$table.css( 'max-width', '' );
+		} );
+
+		new ResizeObserver( function ( entries ) {
+			var newMode, shouldCollapse, shouldExpand;
+
+			shouldCollapse = entries[ 0 ].contentRect.width < minimumTableWidth;
+			// Some fudge to avoid flapping
+			shouldExpand = entries[ 0 ].contentRect.width - 20 > minimumTableWidth;
+
+			if ( isNarrowMode && shouldExpand ) {
+				newMode = false;
+			} else if ( !isNarrowMode && shouldCollapse ) {
+				newMode = true;
+			} else {
+				newMode = isNarrowMode;
+			}
+
+			if ( newMode !== isNarrowMode ) {
+				isNarrowMode = newMode;
+				narrowTable( $table, isNarrowMode );
+			}
+		} ).observe( $table.parent().get( 0 ) );
 	} );
 }() );
