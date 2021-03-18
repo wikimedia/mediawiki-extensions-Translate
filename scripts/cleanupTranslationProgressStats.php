9ff60725d57f9f4af9d3fd8ff629a95fba27f776
Niklas Laxström <niklas.laxstrom@gmail.com>
2021-03-18 13:51:57 +0100

    Add a script to cleanup obsolete rows from translate_groupstats
    Motivation: I saw on Logstash that language stats on meta loads over
    10k rows. This is because there are over 10k distinct message group
    ids on this table there. The actual number of groups is still less than
    10k, so with this script we can clean up those rows and buy a little
    more time until we need to reconsider how the statistics page works
    
    Change-Id: Ib27f99486ace5b95814f358913204fe2e4cd8619

---
+++ scripts/cleanupTranslationProgressStats.php
@@ -0,0 +1,6 @@
+<?php
+
+use MediaWiki\Extension\Translate\Statistics\CleanupTranslationProgressStatsMaintenanceScript;
+
+$class = CleanupTranslationProgressStatsMaintenanceScript::class;
+require_once '__bootstrap.php';
---
+++ src/Statistics/CleanupTranslationProgressStatsMaintenanceScript.php
@@ -0,0 +1,81 @@
+<?php
+declare( strict_types = 1 );
+
+namespace MediaWiki\Extension\Translate\Statistics;
+
+use Maintenance;
+use MediaWiki\MediaWikiServices;
+use MessageGroups;
+use RawMessage;
+use TranslateUtils;
+use const DB_MASTER;
+
+/**
+ * @since 2021.03
+ * @license GPL-2.0-or-later
+ * @author Niklas Laxström
+ */
+class CleanupTranslationProgressStatsMaintenanceScript extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Remove obsolete entries from translate_groupstats table' );
+		$this->requireExtension( 'Translate' );
+	}
+
+	public function execute() {
+		$services = MediaWikiServices::getInstance();
+		$db = $services->getDBLoadBalancer()->getConnectionRef( DB_MASTER );
+
+		$dbGroupIds = $db->selectFieldValues(
+			'translate_groupstats',
+			'DISTINCT(tgs_group)',
+			'*',
+			__METHOD__
+		);
+		$knownGroupIds = array_keys( MessageGroups::singleton()->getGroups() );
+		$unknownGroupIds = array_diff( $dbGroupIds, $knownGroupIds );
+
+		if ( $unknownGroupIds !== [] ) {
+			$msg = ( new RawMessage( "Removing rows for $1 unknown group{{PLURAL:$1||s}}:\n" ) )
+				->numParams( count( $unknownGroupIds ) )
+				->inLanguage( 'en' )
+				->text();
+			$this->output( $msg );
+			foreach ( $unknownGroupIds as $id ) {
+				$this->output( "* $id\n" );
+			}
+			$db->delete(
+				'translate_groupstats',
+				[ 'tgs_group' => $unknownGroupIds ],
+				__METHOD__
+			);
+		}
+
+		$dbLanguages = $db->selectFieldValues(
+			'translate_groupstats',
+			'DISTINCT(tgs_lang)',
+			'*',
+			__METHOD__
+		);
+		$knownLanguages = array_keys( TranslateUtils::getLanguageNames( 'en' ) );
+		$unknownLanguages = array_diff( $dbLanguages, $knownLanguages );
+
+		if ( $unknownLanguages !== [] ) {
+			$msg = ( new RawMessage( "Removing rows for $1 unknown language{{PLURAL:$1||s}}:\n" ) )
+				->numParams( count( $unknownLanguages ) )
+				->inLanguage( 'en' )
+				->text();
+			$this->output( $msg );
+			foreach ( $unknownLanguages as $languageCode ) {
+				$this->output( "* $languageCode\n" );
+			}
+			$db->delete(
+				'translate_groupstats',
+				[ 'tgs_lang' => $unknownLanguages ],
+				__METHOD__
+			);
+		}
+
+		$this->output( "Done.\n" );
+	}
+}
