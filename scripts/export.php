e4d41adcc437a6f6df029b0684f54f8ba27a948d
Niklas Laxström <niklas.laxstrom@gmail.com>
2021-03-18 09:27:31 +0100

    Fix export.php
    Regression from 304d9c5fed76ac8fea6b9a36eddb3223b2b213b8
    
    Bug: T277730
    Change-Id: Ic41fd84c8265762cc543f4177f640a2e9f9a0153

--- scripts/export.php
+++ scripts/export.php
@@ -2,9 +2,5 @@
 
 use MediaWiki\Extension\Translate\Synchronization\ExportTranslationsMaintenanceScript;
 
-$env = getenv( 'MW_INSTALL_PATH' );
-$IP = $env !== false ? $env : __DIR__ . '/../../..';
-require_once "$IP/maintenance/Maintenance.php";
-require_once __DIR__ . '/../src/Synchronization/ExportTranslationsMaintenanceScript.php';
-$maintClass = ExportTranslationsMaintenanceScript::class;
-require_once RUN_MAINTENANCE_IF_MAIN;
+$class = ExportTranslationsMaintenanceScript::class;
+require_once '__bootstrap.php';
