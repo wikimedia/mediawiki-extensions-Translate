f686c4e087053e2d15e2cd1dc6bbdf8b421f654b
Abijeet <abijeetpatro@gmail.com>
2021-06-09 12:43:45 +0530

    InsertablesAid: Code improvements
    Change-Id: Icc3caeed65010617c18b0c457ac0d3737125ab3b

--- translationaids/InsertablesAid.php
+++ translationaids/InsertablesAid.php
@@ -1,17 +1,12 @@
 <?php
-/**
- * Translation aid provider.
- *
- * @file
- * @author Niklas Laxström
- * @license GPL-2.0-or-later
- */
+declare( strict_types = 1 );
 
 /**
- * Translation aid which suggests insertables. Insertable is a string that
+ * Translation aid that suggests insertables. Insertable is a string that
  * usually does not need translation and is difficult to type manually.
- *
  * @ingroup TranslationAids
+ * @author Niklas Laxström
+ * @license GPL-2.0-or-later
  * @since 2013.09
  */
 class InsertablesAid extends TranslationAid {
@@ -21,10 +16,10 @@ class InsertablesAid extends TranslationAid {
 		$group = $this->handle->getGroup();
 
 		// This was added later, so not all classes have it. In addition
-		// the message group class hierarche doesn't lend itself easily
+		// the message group class hierarchy doesn't lend itself easily
 		// to the user of interfaces for this purpose.
 		if ( !is_callable( [ $group, 'getInsertablesSuggester' ] ) ) {
-			throw new TranslationHelperException( 'Group does not have a suggester' );
+			throw new TranslationHelperException( 'Group does not have insertable suggesters' );
 		}
 
 		// @phan-suppress-next-line PhanUndeclaredMethod
@@ -32,7 +27,7 @@ class InsertablesAid extends TranslationAid {
 
 		// It is okay to return null suggester
 		if ( !$suggester ) {
-			throw new TranslationHelperException( 'Group does not have a suggester' );
+			throw new TranslationHelperException( 'Group does not have insertable suggesters' );
 		}
 
 		$insertables = $suggester->getInsertables( $this->dataProvider->getDefinition() );
