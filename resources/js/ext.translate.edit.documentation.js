60217b2ef47338dc9a9478a2d2dceaeb303963cd
Isabelle Hurbain-Palatin <ihurbainpalatin@wikimedia.org>
2021-11-01 12:11:34 +0100

    Introduced version check on VisualEditor features
    Bug: T294605
    Change-Id: I61822bd02cbc8e29243212d1aeeb8203bf66394e

--- resources/js/ext.translate.edit.documentation.js
+++ resources/js/ext.translate.edit.documentation.js
@@ -17,6 +17,15 @@
 	$( 'body' ).append( windowManager.$element );
 	windowManager.addWindows( [ warningDialog ] );
 
+	// TODO load ext.translate.ve unconditionally after 1.39 release: T295203
+	// (see gerrit I7a55a09514110fa7d290d6f03ce9c0b7962c9140; this should be
+	// loaded from extension.json, not from here)
+	if ( mw.config.get( 'wgVersion' ) >= '1.38' ) {
+		mw.loader.using( 'ext.visualEditor.targetLoader' ).then( function () {
+			mw.libs.ve.targetLoader.addPlugin( 'ext.translate.ve' );
+		} );
+	}
+
 	$( function () {
 		mw.hook( 've.activationComplete' ).add( function () {
 			// eslint-disable-next-line no-undef
