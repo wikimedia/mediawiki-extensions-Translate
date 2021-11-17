fe090d3bf54d82a6408a59903ba889aa9a1a670b
Reedy <reedy@wikimedia.org>
2021-11-17 21:22:25 +0000

    translate_cache-alter-varbinary.sql: Respect table prefixes
    Bug: T268576
    Change-Id: Ief5ea51c534ce137342d71bab73a763160399c0e
    Follows-Up: I2290d582d6d315c353a0d9a03ec863dcaff34339

--- sql/mysql/translate_cache-alter-varbinary.sql
+++ sql/mysql/translate_cache-alter-varbinary.sql
@@ -1,30 +1,30 @@
-ALTER TABLE translate_cache
+ALTER TABLE /*_*/translate_cache
   MODIFY tc_key VARBINARY(255) NOT NULL;
 
-ALTER TABLE translate_groupreviews
+ALTER TABLE /*_*/translate_groupreviews
   MODIFY tgr_group VARBINARY(200) NOT NULL,
   MODIFY tgr_lang VARBINARY(20) NOT NULL;
 
-ALTER TABLE translate_groupstats
+ALTER TABLE /*_*/translate_groupstats
   MODIFY tgs_group VARBINARY(100) NOT NULL,
   MODIFY tgs_lang VARBINARY(20) NOT NULL;
 
-ALTER TABLE translate_messageindex
+ALTER TABLE /*_*/translate_messageindex
   MODIFY tmi_key VARBINARY(255) NOT NULL,
   MODIFY tmi_value VARBINARY(255) NOT NULL;
 
-ALTER TABLE translate_metadata
+ALTER TABLE /*_*/translate_metadata
   MODIFY tmd_group VARBINARY(200) NOT NULL,
   MODIFY tmd_key VARBINARY(20) NOT NULL;
 
-ALTER TABLE translate_sections
+ALTER TABLE /*_*/translate_sections
   MODIFY trs_key VARBINARY(255) NOT NULL;
 
-ALTER TABLE translate_stash
+ALTER TABLE /*_*/translate_stash
   MODIFY ts_title VARBINARY(255) NOT NULL;
 
-ALTER TABLE translate_tms
+ALTER TABLE /*_*/translate_tms
   MODIFY tms_lang VARBINARY(20) NOT NULL;
 
-ALTER TABLE translate_tmt
+ALTER TABLE /*_*/translate_tmt
   MODIFY tmt_lang VARBINARY(20) NOT NULL;
