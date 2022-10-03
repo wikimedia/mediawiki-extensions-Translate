01769ad873f0238217c262d9d0a8b712b06a161f
Niklas Laxström <niklas.laxstrom@gmail.com>
2022-10-03 14:22:02 +0300

    GenericTranslateJob: parametrize job title
    To allow grouping similar log entries in Logstash.
    
    Change-Id: Ia8a234560ce90b720826e35566e40a10fd19601e

--- src/Jobs/GenericTranslateJob.php
+++ src/Jobs/GenericTranslateJob.php
@@ -18,24 +18,17 @@ use Psr\Log\LoggerInterface;
  * @since 2019.08
  */
 abstract class GenericTranslateJob extends Job {
-	/**
-	 * A logger instance
-	 * @var LoggerInterface
-	 */
-	protected $logger;
+	/** @var LoggerInterface */
+	private $logger;
 
-	/**
-	 * Channel name to be used during logging
-	 * @var string
-	 */
+	/** @var string Channel name to be used during logging */
 	private const CHANNEL_NAME = 'Translate.Jobs';
 
 	/**
 	 * Returns a logger instance with the channel name. Can have only a single
 	 * channel per job, so once instantiated, the same instance is returned.
-	 * @return LoggerInterface
 	 */
-	protected function getLogger() {
+	private function getLogger(): LoggerInterface {
 		if ( $this->logger ) {
 			return $this->logger;
 		}
@@ -44,30 +37,34 @@ abstract class GenericTranslateJob extends Job {
 		return $this->logger;
 	}
 
-	protected function getLogPrefix(): string {
+	/** @phan-return array{0:string,1:array} */
+	private function formatLogEntry( string $msg, array $context = [] ): array {
 		$prefix = $this->getType();
 		if ( isset( $this->title ) ) {
-			$prefix .= ' [' . $this->title->getPrefixedText() . ']';
+			$prefix .= ' [{job_title}]';
+			$context['job_title'] = $this->title->getPrefixedText();
 		}
 
-		// Add a separator at the end.
-		$prefix .= ': ';
-		return $prefix;
+		return [ "$prefix: $msg", $context ];
 	}
 
 	protected function logInfo( $msg, $context = [] ) {
-		$this->getLogger()->info( $this->getLogPrefix() . $msg, $context );
+		[ $msg, $context ] = $this->formatLogEntry( $msg, $context );
+		$this->getLogger()->info( $msg, $context );
 	}
 
 	protected function logDebug( $msg, $context = [] ) {
-		$this->getLogger()->debug( $this->getLogPrefix() . $msg, $context );
+		[ $msg, $context ] = $this->formatLogEntry( $msg, $context );
+		$this->getLogger()->debug( $msg, $context );
 	}
 
 	protected function logError( $msg, $context = [] ) {
-		$this->getLogger()->error( $this->getLogPrefix() . $msg, $context );
+		[ $msg, $context ] = $this->formatLogEntry( $msg, $context );
+		$this->getLogger()->error( $msg, $context );
 	}
 
 	protected function logWarning( $msg, $context = [] ) {
-		$this->getLogger()->warning( $this->getLogPrefix() . $msg, $context );
+		[ $msg, $context ] = $this->formatLogEntry( $msg, $context );
+		$this->getLogger()->warning( $msg, $context );
 	}
 }
