0f71d496d00fdd14641a6c03de6d93a9121a0352
Abijeet <abijeetpatro@gmail.com>
2022-12-20 17:47:41 +0530

    Remove processMessageChanges
    Bug: T193166
    Depends-On: I067510c087f99e19f4f7ee3ebb23dd00aef842d6
    Change-Id: Ie1f910a6c4a53200250f8c3466a7c6f3118b54b5

--- scripts/processMessageChanges.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use MediaWiki\Extension\Translate\Synchronization\ImportExternalTranslationsMaintenanceScript;
-
-/** TODO: Remove after code in the translatewiki repo has been updated to use importExternalTranslations.php */
-$class = ImportExternalTranslationsMaintenanceScript::class;
-require_once '__bootstrap.php';
