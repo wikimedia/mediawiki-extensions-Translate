b3bd7685c5df489ba41a7e20c90f035b308cb116
Abijeet <abijeetpatro@gmail.com>
2023-09-13 13:14:17 +0530

    MessageGroupReview: Code improvements
    * Use property type declaration
    * Use select query builder
    
    Change-Id: I13145e0cfe9de8c44294f87b710283f67df44a78

--- src/MessageGroupProcessing/MessageGroupReview.php
+++ src/MessageGroupProcessing/MessageGroupReview.php
@@ -17,11 +17,9 @@ use Wikimedia\Rdbms\ILoadBalancer;
  * @license GPL-2.0-or-later
  */
 class MessageGroupReview {
-
-	/** @var HookRunner */
-	protected $hookRunner;
-	/** @var ILoadBalancer */
-	protected $loadBalancer;
+	private HookRunner $hookRunner;
+	private ILoadBalancer $loadBalancer;
+	private const TABLE_NAME = 'translate_groupreviews';
 
 	public function __construct( ILoadBalancer $loadBalancer, HookRunner $hookRunner ) {
 		$this->loadBalancer = $loadBalancer;
@@ -30,51 +28,48 @@ class MessageGroupReview {
 
 	/** @return mixed|false — The value from the field, or false if nothing was found */
 	public function getState( MessageGroup $group, string $code ) {
-		$dbw = $this->loadBalancer->getMaintenanceConnectionRef( DB_PRIMARY );
-		$table = 'translate_groupreviews';
-
-		$field = 'tgr_state';
-		$conds = [
-			'tgr_group' => $group->getId(),
-			'tgr_lang' => $code
-		];
-
-		return $dbw->selectField( $table, $field, $conds, __METHOD__ );
+		$dbw = $this->loadBalancer->getConnection( DB_PRIMARY );
+		return $dbw->newSelectQueryBuilder()
+			->select( 'tgr_state' )
+			->from( self::TABLE_NAME )
+			->where( [
+				'tgr_group' => $group->getId(),
+				'tgr_lang' => $code
+			] )->fetchField();
 	}
 
+	/** @return bool true if the message group state changed, otherwise false */
 	public function changeState( MessageGroup $group, string $code, string $newState, User $user ): bool {
-		$currentState = self::getState( $group, $code );
+		$currentState = $this->getState( $group, $code );
 		if ( $currentState === $newState ) {
 			return false;
 		}
 
-		$table = 'translate_groupreviews';
 		$index = [ 'tgr_group', 'tgr_lang' ];
 		$row = [
 			'tgr_group' => $group->getId(),
 			'tgr_lang' => $code,
 			'tgr_state' => $newState,
 		];
-		$dbw = $this->loadBalancer->getMaintenanceConnectionRef( DB_PRIMARY );
-		$dbw->replace( $table, [ $index ], $row, __METHOD__ );
+		$dbw = $this->loadBalancer->getConnection( DB_PRIMARY );
+		$dbw->replace( self::TABLE_NAME, [ $index ], $row, __METHOD__ );
 
 		$entry = new ManualLogEntry( 'translationreview', 'group' );
 		$entry->setPerformer( $user );
 		$entry->setTarget( SpecialPage::getTitleFor( 'Translate', $group->getId() ) );
-		// @todo
-		// $entry->setComment( $comment );
 		$entry->setParameters( [
 			'4::language' => $code,
 			'5::group-label' => $group->getLabel(),
 			'6::old-state' => $currentState,
 			'7::new-state' => $newState,
 		] );
+		// @todo
+		// $entry->setComment( $comment );
 
-		$logid = $entry->insert();
-		$entry->publish( $logid );
+		$logId = $entry->insert();
+		$entry->publish( $logId );
 
-		$this->hookRunner->onTranslateEventMessageGroupStateChange(
-			$group, $code, $currentState, $newState );
+		$this->hookRunner->onTranslateEventMessageGroupStateChange( $group, $code, $currentState, $newState );
 
 		return true;
 	}
