f6715ffb6be568f8b5a156119c8b068bc4622b5b
Abijeet <abijeetpatro@gmail.com>
2024-01-24 16:31:17 +0530

    ImportTranslatableBundle: Mark page for translation automatically
    Use TranslatablePageMarker to mark the page for translation
    automatically.
    
    Introduce new parameters to allow importer to specify metadata such
    as priority languages.
    
    Eventually we want to be able to read this metadata directly from
    the exported file, and these options can still be used to override
    the values from the exported file.
    
    Bug: T347155
    Change-Id: I82a5be64277c630a10d33a38ca78da574efd42c5

--- src/MessageGroupProcessing/ImportTranslatableBundleMaintenanceScript.php
+++ src/MessageGroupProcessing/ImportTranslatableBundleMaintenanceScript.php
@@ -3,8 +3,12 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\Extension\Translate\MessageGroupProcessing;
 
+use IDBAccessObject;
+use MediaWiki\Extension\Translate\PageTranslation\TranslatablePageMarkException;
+use MediaWiki\Extension\Translate\PageTranslation\TranslatablePageSettings;
 use MediaWiki\Extension\Translate\Services;
 use MediaWiki\Extension\Translate\Utilities\BaseMaintenanceScript;
+use MediaWiki\Extension\Translate\Utilities\Utilities;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
 use MediaWiki\User\UserIdentity;
@@ -47,6 +51,40 @@ class ImportTranslatableBundleMaintenanceScript extends BaseMaintenanceScript {
 			self::OPTIONAL
 		);
 
+		// Options related to marking a page for translation
+		$this->addOption(
+			'skip-translating-title',
+			'Should translation of title be skipped',
+			self::OPTIONAL
+		);
+		$this->addOption(
+			'priority-languages',
+			'Comma separated list of priority language codes',
+			self::OPTIONAL,
+			self::HAS_ARG
+		);
+		$this->addOption(
+			'priority-languages-reason',
+			'Reason for setting the priority languages',
+			self::OPTIONAL,
+			self::HAS_ARG
+		);
+		$this->addOption(
+			'force-priority-languages',
+			'Only allow translations to the priority languages',
+			self::OPTIONAL
+		);
+		$this->addOption(
+			'disallow-transclusion',
+			'Disable translation aware transclusion for this page',
+			self::OPTIONAL
+		);
+		$this->addOption(
+			'use-old-syntax-version',
+			'Use the old syntax version for translatable pages',
+			self::OPTIONAL
+		);
+
 		$this->requireExtension( 'Translate' );
 	}
 
@@ -57,7 +95,9 @@ class ImportTranslatableBundleMaintenanceScript extends BaseMaintenanceScript {
 		$comment = $this->getOption( 'comment' );
 		$interwikiPrefix = $this->getInterwikiPrefix();
 		$assignKnownUsers = $this->hasOption( 'assign-known-users' );
+		$translatablePageSettings = $this->getTranslatablePageSettings();
 
+		// First import the page
 		try {
 			$importer = Services::getInstance()->getTranslatableBundleImporter();
 			$importer->setImportCompleteCallback( [ $this, 'logImportComplete' ] );
@@ -72,9 +112,10 @@ class ImportTranslatableBundleMaintenanceScript extends BaseMaintenanceScript {
 			$this->fatalError( "An error occurred during import: {$e->getMessage()}\n" );
 		}
 
-		$this->output(
-			"You should now mark the page '{$bundleTitle->getPrefixedText()}' for translation.\n"
-		);
+		$this->output( "Page {$bundleTitle->getPrefixedText()} was imported, now marking it for translation.\n" );
+
+		// Try to mark the page for translation
+		$this->markPageForTranslation( $bundleTitle, $translatablePageSettings, $importUser );
 	}
 
 	public function logImportComplete( Title $title ): void {
@@ -114,5 +155,75 @@ class ImportTranslatableBundleMaintenanceScript extends BaseMaintenanceScript {
 		}
 
 		return $interwikiPrefix;
+	}
+
+	private function getPriorityLanguages(): array {
+		$priorityLanguageCodes = array_unique(
+			array_filter(
+				array_map(
+					'trim',
+					explode( ',', $this->getOption( 'priority-languages' ) ?? '' )
+				),
+				'strlen'
+			)
+		);
+
+		$knownLanguageCodes = array_keys( Utilities::getLanguageNames( 'en' ) );
+		$invalidLanguageCodes = array_diff( $priorityLanguageCodes, $knownLanguageCodes );
+
+		if ( $invalidLanguageCodes ) {
+			$this->fatalError(
+				'Unknown priority language code(s): ' . implode( ', ', $invalidLanguageCodes )
+			);
+		}
+
+		return $priorityLanguageCodes;
+	}
+
+	private function markPageForTranslation(
+		Title $bundleTitle,
+		TranslatablePageSettings $translatablePageSettings,
+		UserIdentity $importUser
+	): void {
+		$translatablePageMarker = Services::getInstance()->getTranslatablePageMarker();
+		$user = MediaWikiServices::getInstance()->getUserFactory()->newFromUserIdentity( $importUser );
+		try {
+			$operation = $translatablePageMarker->getMarkOperation(
+				$bundleTitle->toPageRecord( IDBAccessObject::READ_LATEST ),
+				null,
+				$translatablePageSettings->shouldTranslateTitle()
+			);
+		} catch ( TranslatablePageMarkException $e ) {
+			$this->error( "Error while marking page {$bundleTitle->getPrefixedText()} for translation.\n" );
+			$this->error( "Fix the issues and mark the page for translation using Special:PageTranslation.\n\n" );
+			$this->fatalError( wfMessage( $e->getMessageObject() )->text() );
+		}
+
+		$unitNameValidationResult = $operation->getUnitValidationStatus();
+		if ( !$unitNameValidationResult->isOK() ) {
+			$this->output( "Unit validation failed for {$bundleTitle->getPrefixedText()}.\n" );
+			$this->fatalError( $unitNameValidationResult->getMessage()->text() );
+		}
+
+		try {
+			$translatablePageMarker->markForTranslation( $operation, $translatablePageSettings, $user );
+			$this->output( "The page {$bundleTitle->getPrefixedText()} has been marked for translation.\n" );
+		} catch ( TranslatablePageMarkException $e ) {
+			$this->error( "Error while marking page {$bundleTitle->getPrefixedText()} for translation.\n" );
+			$this->error( "Fix the issues and mark the page for translation using Special:PageTranslation.\n\n" );
+			$this->fatalError( $e->getMessageObject()->text() );
+		}
+	}
+
+	private function getTranslatablePageSettings(): TranslatablePageSettings {
+		return new TranslatablePageSettings(
+			$this->getPriorityLanguages(),
+			$this->hasOption( 'force-priority-languages' ),
+			$this->getOption( 'priority-languages-reason' ) ?? '',
+			[],
+			!$this->hasOption( 'skip-translating-title' ),
+			!$this->hasOption( 'use-old-syntax-version' ),
+			!$this->hasOption( 'disallow-transclusion' ),
+		);
 	}
 }
