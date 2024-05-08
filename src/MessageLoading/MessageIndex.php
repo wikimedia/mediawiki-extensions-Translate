wikimedia/mediawiki-extensions-Translate

commit 0315cbdae1d742048e76a823f1245d2cc6753915

Fix performance regression in TranslationStats
Fixes: 91724cd0e7a3
Bug: T354753
Change-Id: I27b011b66ff014c1c1e145effb9a1297bd1717c9

src/MessageLoading/MessageIndex.php
@@ -90,6 +90,22 @@ abstract class MessageIndex {
 		return $value;
 	}
 
+	/**
+	 * Fast-path to retrieve groups for database titles.
+	 *
+	 * Performance is critical for stats that need to check groups for many rows.
+	 * Do not include the language code subpage!
+	 * @return string[]
+	 */
+	public function getGroupIdsForDatabaseTitle( int $namespace, string $title ): array {
+		$normalisedKey = $this->normaliseKey( $namespace, $title );
+
+		// Optimization 1: skip LRU cache assuming that hit rate is very low for this use case
+		// Optimization 2: skip interim cache as not essential
+
+		return $this->get( $normalisedKey ) ?? [];
+	}
+
 	private function getCache(): MapCacheLRU {
 		if ( self::$keysCache === null ) {
 			self::$keysCache = new MapCacheLRU( 30 );

src/Statistics/ReviewPerLanguageStats.php
@@ -98,7 +98,7 @@ class ReviewPerLanguageStats extends TranslatePerLanguageStats {
 		if ( $this->groups ) {
 			/* Get list of keys that the message belongs to, and filter
 			 * out those which are not requested. */
-			$groups = Utilities::messageKeyToGroups( (int)$row->log_namespace, $key );
+			$groups = $this->messageIndex->getGroupIdsForDatabaseTitle( (int)$row->log_namespace, $key );
 			$groups = array_intersect( $this->groups, $groups );
 		}
 

src/Statistics/TranslatePerLanguageStats.php
@@ -5,6 +5,8 @@ namespace MediaWiki\Extension\Translate\Statistics;
 
 use Language;
 use MediaWiki\Extension\Translate\MessageGroupProcessing\MessageGroups;
+use MediaWiki\Extension\Translate\MessageLoading\MessageIndex;
+use MediaWiki\Extension\Translate\Services;
 use MediaWiki\Extension\Translate\Utilities\Utilities;
 use MediaWiki\MediaWikiServices;
 use Wikimedia\Rdbms\IDatabase;
@@ -21,6 +23,7 @@ class TranslatePerLanguageStats extends TranslationStatsBase {
 	protected array $groups = [];
 	private Language $dateFormatter;
 	private array $formatCache = [];
+	protected MessageIndex $messageIndex;
 
 	public function __construct( TranslationStatsGraphOptions $opts ) {
 		parent::__construct( $opts );
@@ -28,6 +31,7 @@ class TranslatePerLanguageStats extends TranslationStatsBase {
 		$opts->boundValue( 'days', 1, 400 );
 		// TODO: inject
 		$this->dateFormatter = MediaWikiServices::getInstance()->getContentLanguage();
+		$this->messageIndex = Services::getInstance()->getMessageIndex();
 	}
 
 	public function preQuery(
@@ -116,7 +120,7 @@ class TranslatePerLanguageStats extends TranslationStatsBase {
 		if ( $this->groups ) {
 			// Get list of keys that the message belongs to, and filter
 			// out those which are not requested.
-			$groups = Utilities::messageKeyToGroups( (int)$row->rc_namespace, $key );
+			$groups = $this->messageIndex->getGroupIdsForDatabaseTitle( (int)$row->rc_namespace, $key );
 			$groups = array_intersect( $this->groups, $groups );
 		}
 

src/Utilities/Utilities.php
@@ -15,7 +15,6 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Revision\SlotRecord;
 use MediaWiki\Title\Title;
-use MediaWiki\Title\TitleValue;
 use MessageGroup;
 use RequestContext;
 use TextContent;
@@ -232,16 +231,6 @@ class Utilities {
 		Services::getInstance()->getHookRunner()->onTranslateSupportedLanguages( $languageNames, $code );
 
 		return $languageNames;
-	}
-
-	/**
-	 * Returns all the groups the message belongs to.
-	 * @return string[] Possibly empty list of group ids.
-	 */
-	public static function messageKeyToGroups( int $namespace, string $key ): array {
-		$title = new TitleValue( $namespace, $key );
-		$handle = new MessageHandle( $title );
-		return $handle->getGroupIds();
 	}
 
 	/**

commit 0a9b483bd20fc5f752d8bb71dc37b94f710bf9d7

MessageIndex::getGroupIdsForDatabaseTitle: Always return an array
Fixes: 0315cbdae1d742048e76a823f1245d2cc6753915
Change-Id: I8064f9a186f4a2282a7200dabe09baa8ea147b9d

src/MessageLoading/MessageIndex.php
@@ -106,7 +106,7 @@ abstract class MessageIndex {
 		// Optimization 1: skip LRU cache assuming that hit rate is very low for this use case
 		// Optimization 2: skip interim cache as not essential
 
-		return $this->get( $normalisedKey ) ?? [];
+		return (array)$this->get( $normalisedKey ) ?? [];
 	}
 
 	private function getCache(): MapCacheLRU {