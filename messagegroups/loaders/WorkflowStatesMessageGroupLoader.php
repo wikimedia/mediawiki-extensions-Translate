bef09dd685db85128add6eb6d374cdb17b195603
Abijeet <abijeetpatro@gmail.com>
2024-06-11 11:21:56 +0530

    WorkflowStatesMessageGroupLoader: Code improvements
    Change-Id: I4adc4b014dc9892df004c65ec3a9234b6683b8d5

--- messagegroups/loaders/WorkflowStatesMessageGroupLoader.php
+++ messagegroups/loaders/WorkflowStatesMessageGroupLoader.php
@@ -1,14 +1,10 @@
 <?php
-/**
- * This file contains a class to load the WorkflowStatesMessageGroup.
- *
- * @file
- * @author Abijeet Patro
- * @license GPL-2.0-or-later
- */
+declare( strict_types = 1 );
 
 /**
  * Loads WorkflowStatesMessageGroup, and handles the related cache.
+ * @author Abijeet Patro
+ * @license GPL-2.0-or-later
  * @since 2019.05
  */
 class WorkflowStatesMessageGroupLoader implements MessageGroupLoader {
@@ -22,13 +18,8 @@ class WorkflowStatesMessageGroupLoader implements MessageGroupLoader {
 		return $groups;
 	}
 
-	/**
-	 * Hook: TranslateInitGroupLoaders
-	 *
-	 * @param array &$groupLoader
-	 * @param array $deps
-	 */
-	public static function registerLoader( array &$groupLoader, array $deps ) {
+	/** Hook: TranslateInitGroupLoaders */
+	public static function registerLoader( array &$groupLoader, array $deps ): void {
 		$groupLoader[] = new self();
 	}
 }
