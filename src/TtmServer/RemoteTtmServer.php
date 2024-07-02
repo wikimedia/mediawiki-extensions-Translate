e1b44b8f2015c8ff9edfa9711c0603775f0bd00e
wangombe <ewangombe-ctr@wikimedia.org>
2024-07-02 21:26:13 +0300

    RemoteTTMServer: Move to TtmServer namespace
    * Rename RemoteTTMServer to RemoteTtmServer
    
    Bug: T335342
    Change-Id: I693a1924d01b0a7becd6b9cec847738332fc59d3

--- src/TtmServer/RemoteTtmServer.php
+++ src/TtmServer/RemoteTtmServer.php
@@ -1,24 +1,21 @@
 <?php
+declare( strict_types = 1 );
+
+namespace MediaWiki\Extension\Translate\TtmServer;
+
+use TTMServer;
+
 /**
- * TTMServer - The Translate extension translation memory interface
+ * Class for handling remote TTMServers over MediaWiki API.
+ * Currently, querying is done in TranslationHelpers, and
+ * this class only handles location retrieval.
  *
- * @file
  * @author Niklas Laxström
  * @copyright Copyright © 2012-2013, Niklas Laxström
  * @license GPL-2.0-or-later
  * @ingroup TTMServer
  */
-
-use MediaWiki\Extension\Translate\TtmServer\ReadableTtmServer;
-
-/**
- * Class for handling remote TTMServers over MediaWiki API.
- * Currently querying is done in TranslationHelpers, and
- * this class only handles location retrieval.
- * @since 2012-06-27
- * @ingroup TTMServer
- */
-class RemoteTTMServer extends TTMServer implements ReadableTtmServer {
+class RemoteTtmServer extends TTMServer implements ReadableTtmServer {
 	public function query( string $sourceLanguage, string $targetLanguage, string $text ): array {
 		// @todo Implement some day perhaps?
 		return [];
--- src/TtmServer/TtmServerFactory.php
+++ src/TtmServer/TtmServerFactory.php
@@ -4,7 +4,6 @@ declare( strict_types = 1 );
 namespace MediaWiki\Extension\Translate\TtmServer;
 
 use InvalidArgumentException;
-use RemoteTTMServer;
 use TTMServer;
 
 /**
