f695859dbfd27c3b8cad1048591ecc6f01cac3be
Tacsipacsi <tacsipacsi@jnet.hu>
2024-08-19 16:08:00 +0200

    SubsetMessageGroupTest: Factor out subset mock creation
    We do almost the same ceremony in all tests, so create a helper method,
    which takes care of the repeated bits, so that we can focus on what
    matters in the test cases.
    
    Change-Id: I721df457dad4d5020248ac5b8ea83a1d7c5634a3

--- tests/phpunit/unit/messagegroups/SubsetMessageGroupTest.php
+++ tests/phpunit/unit/messagegroups/SubsetMessageGroupTest.php
@@ -13,11 +13,7 @@ class SubsetMessageGroupTest extends MediaWikiUnitTestCase {
 		$parentGroup = $this->createMock( MessageGroup::class );
 		$parentGroup->method( 'getKeys' )->willReturn( $parentKeys );
 
-		$subsetGroup = $this->getMockBuilder( SubsetMessageGroup::class )
-			->onlyMethods( [ 'getParentGroup' ] )
-			->setConstructorArgs( [ 'testGroupId', 'Test group', 'testParentGroupId', $subsetKeys ] )
-			->getMock();
-		$subsetGroup->method( 'getParentGroup' )->willReturn( $parentGroup );
+		$subsetGroup = $this->getSubsetGroup( $parentGroup, $subsetKeys );
 
 		$this->assertEquals( $subsetKeys, $subsetGroup->getKeys() );
 	}
@@ -30,11 +26,7 @@ class SubsetMessageGroupTest extends MediaWikiUnitTestCase {
 		$parentGroup = $this->createMock( MessageGroup::class );
 		$parentGroup->method( 'getKeys' )->willReturn( $parentKeys );
 
-		$subsetGroup = $this->getMockBuilder( SubsetMessageGroup::class )
-			->onlyMethods( [ 'getParentGroup' ] )
-			->setConstructorArgs( [ 'testGroupId', 'Test group', 'testParentGroupId', $subsetKeys ] )
-			->getMock();
-		$subsetGroup->method( 'getParentGroup' )->willReturn( $parentGroup );
+		$subsetGroup = $this->getSubsetGroup( $parentGroup, $subsetKeys );
 
 		// Side effect of printing to error log, not possible to test?
 		$this->assertEquals( $commonKeys, $subsetGroup->getKeys() );
@@ -49,32 +41,40 @@ class SubsetMessageGroupTest extends MediaWikiUnitTestCase {
 			return $type ? $tags[$type] ?? [] : $tags;
 		} );
 
-		$subsetGroup = $this->getMockBuilder( SubsetMessageGroup::class )
-			->onlyMethods( [ 'getParentGroup' ] )
-			->setConstructorArgs( [ 'testGroupId', 'Test group', 'testParentGroupId', [] ] )
-			->getMock();
-		$subsetGroup->method( 'getParentGroup' )->willReturn( $parentGroup );
+		$subsetGroup = $this->getSubsetGroup( $parentGroup );
 
 		$this->assertEquals( $parentGroup->getTags(), $subsetGroup->getTags() );
 		$this->assertEquals( $parentGroup->getTags( 'optional' ), $subsetGroup->getTags( 'optional' ) );
 	}
 
 	public function testAggregateParentGroup() {
-		$subsetGroup = $this->getMockBuilder( SubsetMessageGroup::class )
-			->onlyMethods( [ 'getParentGroup' ] )
-			->setConstructorArgs( [ 'testGroupId', 'Test group', 'testParentGroupId', [] ] )
-			->getMock();
-
 		$parentGroup = $this->getMockBuilder( AggregateMessageGroup::class )
 			->onlyMethods( [ 'getGroups' ] )
 			->disableOriginalConstructor()
 			->getMock();
 
-		$subsetGroup->method( 'getParentGroup' )->willReturn( $parentGroup );
+		$subsetGroup = $this->getSubsetGroup( $parentGroup );
+
 		$parentGroup->method( 'getGroups' )->willReturn( [ $subsetGroup ] );
 
 		// Just check we don't get into infinite loop
 		$this->assertEquals( [], $subsetGroup->getKeys() );
 		$this->assertEquals( [], $subsetGroup->getTags() );
+	}
+
+	/**
+	 * @param MessageGroup $parentGroup A mock parent group that should be returned
+	 *  by SubsetMessageGroup::getParentGroup
+	 * @param string[] $subsetKeys Subset keys to be passed to the constructor
+	 * @return SubsetMessageGroup
+	 */
+	private function getSubsetGroup( MessageGroup $parentGroup, array $subsetKeys = [] ): SubsetMessageGroup {
+		$subsetGroup = $this->getMockBuilder( SubsetMessageGroup::class )
+			->onlyMethods( [ 'getParentGroup' ] )
+			->setConstructorArgs( [ 'testGroupId', 'Test group', 'testParentGroupId', $subsetKeys ] )
+			->getMock();
+
+		$subsetGroup->method( 'getParentGroup' )->willReturn( $parentGroup );
+		return $subsetGroup;
 	}
 }
