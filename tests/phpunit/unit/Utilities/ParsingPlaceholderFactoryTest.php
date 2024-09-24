4f97c15caad51d4d48e0702f7e0d3178b3e669dd
Abijeet <abijeetpatro@gmail.com>
2024-09-24 18:01:06 +0530

    ParsingPlaceholderFactoryTest: Remove code for PHPUnit 8
    Using PHPUnit 9 since atleast MW 1.40
    
    Bug: T369056
    Change-Id: Ic130c4af58e10c16fdb43393e3b2b73af4c9b091

--- tests/phpunit/unit/Utilities/ParsingPlaceholderFactoryTest.php
+++ tests/phpunit/unit/Utilities/ParsingPlaceholderFactoryTest.php
@@ -11,14 +11,6 @@ use MediaWiki\Extension\Translate\Utilities\ParsingPlaceholderFactory;
 class ParsingPlaceholderFactoryTest extends MediaWikiUnitTestCase {
 	public function testMake() {
 		$obj = new ParsingPlaceholderFactory();
-
-		if ( method_exists( $this, 'assertMatchesRegularExpression' ) ) {
-			// PHPUnit 9.x+
-			$this->assertMatchesRegularExpression( '/[a-zA-Z0-9\x7f]/', $obj->make() );
-
-		} else {
-			// PHPUnit 8.x, to be dropped when dropping master compatibility with REL1_39
-			$this->assertRegExp( '/[a-zA-Z0-9\x7f]/', $obj->make() );
-		}
+		$this->assertMatchesRegularExpression( '/[a-zA-Z0-9\x7f]/', $obj->make() );
 	}
 }
