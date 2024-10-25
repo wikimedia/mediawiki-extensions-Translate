0070c3cd2a0c521eb43924cf163d9f59d11ce822
Abijeet <abijeetpatro@gmail.com>
2024-10-25 13:40:12 +0530

    TranslatablePageMarker: Skip table insert if there are no sections
    If there are no rows to insert, the don't call execute the insert
    query.
    
    Bug: T378160
    Change-Id: I617670a77b2fb292e80b47a7822b879a0198ba04
    (cherry picked from commit 698fb6d244929a3bc25c6c8cef17ccf9023cd7b6)

--- src/PageTranslation/TranslatablePageMarker.php
+++ src/PageTranslation/TranslatablePageMarker.php
@@ -325,11 +325,14 @@ class TranslatablePageMarker {
 			->where( [ 'trs_page' => $title->getArticleID() ] )
 			->caller( __METHOD__ )
 			->execute();
-		$dbw->newInsertQueryBuilder()
-			->insertInto( 'translate_sections' )
-			->rows( $inserts )
-			->caller( __METHOD__ )
-			->execute();
+
+		if ( $inserts ) {
+			$dbw->newInsertQueryBuilder()
+				->insertInto( 'translate_sections' )
+				->rows( $inserts )
+				->caller( __METHOD__ )
+				->execute();
+		}
 
 		$this->saveMetadata( $operation, $pageSettings, $maxId, $user );
 
