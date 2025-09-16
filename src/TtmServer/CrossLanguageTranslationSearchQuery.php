395539402fd880b98cef871b327505a8fb8efa43
Wangombe <ewangombe-ctr@wikimedia.org>
2025-09-16 10:37:33 +0300

    CrossLanguageTranslationSearchQuery:extractMessages() - Fix typo in @return comment
    Change-Id: Ia509cea54a375821427e8d30d511087cc41982d5

--- src/TtmServer/CrossLanguageTranslationSearchQuery.php
+++ src/TtmServer/CrossLanguageTranslationSearchQuery.php
@@ -84,7 +84,7 @@ class CrossLanguageTranslationSearchQuery {
 	 * Filter the message collection to get filtered messages.
 	 * Slice messages according to limit and offset given.
 	 * @param Document[] $documents
-	 * return array[]
+	 * @return array[]
 	 */
 	private function extractMessages( array $documents ): array {
 		$messages = $ret = [];
